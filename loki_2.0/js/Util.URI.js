e63d2bff42f164fc34d357e5e7a6bdb143734d66
Matt Ryan <mryan@carleton.edu>
2007-04-17 04:40:55 +0000

    added 2 new methods: Util.URI.extract_domain() and Util.URI.make_domain_relative()

--- loki_2.0/js/Util.URI.js
+++ loki_2.0/js/Util.URI.js
@@ -35,3 +35,29 @@ Util.URI.strip_https_and_http = function(uri)
 {
 	return uri.replace(new RegExp('^https?:', ''), '');
 };
+
+/**
+ * Extracts the domain name from the URI.
+ * @param	uri	the URI
+ * @return	the domain name or null if an invalid URI was provided
+ */
+Util.URI.extract_domain = function(uri)
+{
+	var match = Util.URI.uri_pattern.exec(uri);
+	return (!match || !match[4]) ? null : match[4].toLowerCase();
+};
+
+/**
+ * Makes the given URI relative to its domain
+ * (i.e. strips the protocol and domain).
+ */
+Util.URI.make_domain_relative = function(uri)
+{
+	return uri.replace(Util.URI.protocol_host_pattern, '');
+}
+
+Util.URI.uri_pattern =
+	new RegExp('^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?',
+	'i');
+Util.URI.protocol_host_pattern =
+	new RegExp('^(([^:/?#]+):)?(//([^/?#]*))?', 'i');
