87978568dbb222e6dd657daa589bd8ee2af5b0ff
Matt Ryan <mryan@carleton.edu>
2007-04-18 22:29:09 +0000

    moved to force_secure_if_available()

--- reason_4.0/lib/core/scripts/page_types/view_page_type_info.php
+++ reason_4.0/lib/core/scripts/page_types/view_page_type_info.php
@@ -8,10 +8,7 @@
 	$top_link = '<a href="#top" class="top">Return to top</a>';
 
 	reason_include_once( 'function_libraries/user_functions.php' );
-	if(!on_secure_page())
-        { 
-                force_secure();
-        }
+        force_secure_if_available();
         $current_user = check_authentication();
 	if (!user_is_a( get_user_id ( $current_user ), id_of('admin_role') ) )
 	{
