892e5321465e889e0507b24b64c28fe0a41276eb
Matt Ryan <mryan@carleton.edu>
2007-04-18 18:10:48 +0000

    Image URLs have the protocol portion removed when (and only when) image is served under the same domain as the editor

--- loki_2.0/js/UI.Image_Masseuse.js
+++ loki_2.0/js/UI.Image_Masseuse.js
@@ -9,21 +9,60 @@ UI.Image_Masseuse = function()
 {
 	var self = this;
 	Util.OOP.inherits(self, UI.Masseuse);
+			
+	this.init = function(loki)
+	{
+		this.superclass.init.call(this, loki);
+		this._unsecured = new RegExp('^http:', '');
+		return this;
+	};
 
 	/**
 	 * Massages the given node's descendants.
 	 */
 	this.massage_node_descendants = function(node)
 	{
-		var images = node.getElementsByTagName('IMG');
-		for ( var i = images.length - 1; i >= 0; i-- )
-		{
-			if ( images[i].getAttribute('src') != null ) //&& images[i].href == null )
-			{
-				var fake = self.get_fake_elem(images[i]);
-				images[i].parentNode.replaceChild(fake, images[i]);
-			}
+		self.secure_node_descendants(node);
+	};
+	
+	this.secure_node_descendants = function(node)
+	{
+		Util.Array.for_each(node.getElementsByTagName('IMG'),
+			self.secure_node, self);
+	};
+	
+	this.secure_node = function(img)
+	{
+		var placeholder = self.get_fake_elem(img);
+		if (placeholder.src !== img.src)
+			img.parentNode.replaceChild(placeholder, img);
+	};
+	
+	this.get_fake_elem = function(img)
+	{
+		var src = img.getAttribute('src');
+		if (src == null)
+			return;
+		
+		if (self._unsecured.test(src)) {
+			if (Util.URI.extract_domain(src) == self._loki.editor_domain())
+				new_src = Util.URI.strip_https_and_http(src);
+			else if (self._loki.settings.sanitize_unsecured)
+				new_src = self._loki.settings.base_uri +
+					'images/insecure_image.gif';
+			else
+				return img;
+			
+			var placeholder = img.ownerDocument.createElement('IMG');
+			placeholder.title = img.title;
+			placeholder.alt = img.alt;
+			placeholder.setAttribute('loki:src', img.src);
+			placeholder.src = new_src;
+			
+			return placeholder;
 		}
+		
+		return img;
 	};
 
 	/**
@@ -31,80 +70,28 @@ UI.Image_Masseuse = function()
 	 */
 	this.unmassage_node_descendants = function(node)
 	{
-		var dummies = node.getElementsByTagName('SPAN');
-		for ( var i = dummies.length - 1; i >= 0; i-- )
-		{
-			if ( dummies[i].getAttribute('loki:is_really_an_image_whose_src_is') != null )
-			{
-				var real = self.get_real_elem(dummies[i]);
-				dummies[i].parentNode.replaceChild(real, dummies[i])
-			}
-		}
+		Util.Array.for_each(node.getElementsByTagName('IMG'),
+			self.unmassage_node, self);
 	};
-
-	/**
-	 * Returns a fake element for the given image.
-	 */
-	this.get_fake_elem = function(image)
+	
+	this.unmassage_node = function(img)
 	{
-		if ( document.all ) // XXX bad
-		{
-			/*
-			var dummy = image.ownerDocument.createElement('IMG');
-			Util.Element.add_class(dummy, 'loki__named_image'); // changed while in if (false)
-			dummy.title = image.name;
-			dummy.setAttribute('loki:is_really_an_image_whose_src_is', image.name);
-			dummy.src = self._loki.settings.base_uri + 'images/nav/image.gif';
-			dummy.width = 12;
-			dummy.height = 12;
-			return dummy;
-			*/
-			return image;
-		}
-		else
-		{
-			// get width and height
-			image.style.position = 'absolute';
-			image.style.left = '-10000px';
-			image.ownerDocument.documentElement.appendChild(image);
-			var width = image.clientWidth;
-			var height = image.clientHeight;
-			image.ownerDocument.documentElement.removeChild(image);
-
-			var dummy = image.ownerDocument.createElement('A');
-			Util.Element.add_class(dummy, 'loki__image');
-			//dummy.className = 'loki__image';
-			dummy.title = image.title;
-			dummy.alt = image.alt;
-			dummy.setAttribute('loki:is_really_an_image_whose_src_is', image.src);
-			dummy.setAttribute('style', 'display: inline-block; width: ' + width + 'px; height: ' + height + 'px; line-height: ' + height + 'px; overflow: hidden; padding-left: ' + width + 'px; background-image: url("' + image.src + '"); background-position: bottom; background-repeat: no-repeat;');
-			//dummy.style.width = width + 'px';
-			//dummy.style.lineHeight = height + 'px';
-			//dummy.innerHTML = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-			//dummy.style.background = 'url("' + image.src + '");';
-			return dummy;
-		}
+		var real = self.get_real_elem(img);
+		if (real.src != img.src)
+			img.parentNode.replaceChild(real, img);
 	};
-
-	/**
-	 * If the given fake element is really fake, returns the appropriate 
-	 * real image. Else, returns null.
-	 */
-	this.get_real_elem = function(dummy)
+	
+	this.get_real_elem = function(img)
 	{
-		if ( dummy != null )
-		{
-			var image_src = dummy.getAttribute('loki:is_really_an_image_whose_src_is');
-			if ( image_src != null )
-			{
-				var image = dummy.ownerDocument.createElement('IMG');
-				image.title = dummy.title;
-				image.src = image_src;
-				//image.width = 12;
-				//image.height = 12;
-				return image;
-			}
-		}
-		return null;
+		var src = img.getAttribute('loki:src');
+		if (src == null)
+			return img;
+		
+		var real = img.ownerDocument.createElement('IMG');
+		real.title = img.title;
+		real.alt = img.alt;
+		real.src = src;
+		
+		return real;
 	};
 };
