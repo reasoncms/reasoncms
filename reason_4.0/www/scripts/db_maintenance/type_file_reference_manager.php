4dc179ed32a201fce532bf86ee58399f56ff1628
Nathan White <nwhite@carleton.edu>
2007-04-23 22:03:10 +0000

    stub for type_file_reference_manager.php ... i realized it did not have a stub yet

---
+++ reason_4.0/www/scripts/db_maintenance/type_file_reference_manager.php
@@ -0,0 +1,8 @@
+<?php
+	/**
+	 * Stub for the type file reference manager script
+	 * No additional code should be placed in this file
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/db_maintenance/type_file_reference_manager.php' );
+?>
