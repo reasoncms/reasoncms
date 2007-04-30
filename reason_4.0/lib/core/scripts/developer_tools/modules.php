6254e5bc3b5876673111b31b49930310bb7aa1fd
Nathan White <nwhite@carleton.edu>
2007-04-30 23:13:35 +0000

    changed code to speed things up drastically - was previously searching across all entities for each section of each page type ... not necessary.

--- reason_4.0/lib/core/scripts/developer_tools/modules.php
+++ reason_4.0/lib/core/scripts/developer_tools/modules.php
@@ -6,41 +6,43 @@
  */
 
 include_once( 'reason_header.php' );
-reason_include_once( 'minisite_templates/page_types.php' );
-reason_include_once( 'function_libraries/URL_History.php' );
-
 reason_include_once( 'function_libraries/user_functions.php' );
-force_secure_if_available();
-$current_user = check_authentication();
+reason_include_once( 'minisite_templates/page_types.php' );
+reason_include_once( 'classes/entity_selector.php');
+
+$current_user = reason_require_authentication();
 if (!user_is_a( get_user_id ( $current_user ), id_of('admin_role') ) )
 {
 	die('<h1>Sorry.</h1><p>You do not have permission to view modules.</p><p>Only Reason users who have the Administrator role may do that.</p></body></html>');
 }
 
-connectDB( REASON_DB );
 $page_types = $GLOBALS['_reason_page_types'];
 $pages = array();
 $modules = array();
+
+$es = new entity_selector();
+$es->add_type(id_of('minisite_page'));
+$es->limit_tables('page_node');
+$es->limit_fields('custom_page');
+$result = $es->run_one();
+
+foreach ($result as $k=>$mypage)
+{
+	$page_type_value = $mypage->get_value('custom_page');
+	if (empty($page_type_value)) $page_type_value = 'default';
+	$reason_page_types[$page_type_value][$k] = 'true';
+}
+
 foreach( $page_types AS $page_type => $type )
 {
-	if( $page_type != 'default' )
-	{
-		$type = array_merge( $type, $page_types[ 'default' ] );
-	}
 	foreach( $type AS $section => $module_info )
 	{
 		$module = is_array( $module_info ) ? $module_info[ 'module' ] : $module_info;
 		if( !empty( $module ) )
 		{
-			$q = 'SELECT e.id id FROM entity e, page_node p WHERE e.id = p.id AND e.state = "Live" AND p.custom_page = "'.$page_type.'"';
-			$r = db_query( $q, 'Unable to get page for this page type' );
-			while( $row = mysql_fetch_assoc( $r ) )
+			if (isset($reason_page_types[$page_type]))
 			{
-				$pages[ $page_type ][ $row['id' ]] = true;
-			}
-			if( !empty( $pages[ $page_type ] ) )
-			{
-				$modules[ $module ][ $page_type ] = $pages[ $page_type ];
+				$modules[$module][$page_type] = $reason_page_types[$page_type];
 			}
 		}
 	}
@@ -52,19 +54,19 @@ echo "<th>Module</th>\n";
 echo "<th>Pages using this module (approx)</th>\n";
 echo "<th>Random Page Using this module</th>\n";
 echo "</tr>\n";
-foreach( $modules AS $module => $page_types )
+foreach( $modules AS $module => $my_page_types )
 {
 	echo "<tr>\n";
 	echo "<td>$module</td>\n";
 	$page_total = 0;
 	$tmp_pages = array();
-	foreach( $page_types AS $page_type => $module_pages )
+	foreach( $my_page_types AS $page_type => $module_pages )
 	{
 		$page_total += count( $module_pages );
 		$tmp_pages = array_merge( array_keys( $module_pages ), $tmp_pages );
 	}
-	reset( $page_types );
-	list( ,$pt ) = each( $page_types );
+	reset( $my_page_types );
+	list( ,$pt ) = each( $my_page_types );
 	reset( $pt );
 	list( $page_id, ) = each( $pt );
 	echo "<td>$page_total</td>\n";
