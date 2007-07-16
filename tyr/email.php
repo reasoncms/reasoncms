26aa8f9be62e2947a4d7ca91bbd394a1ec984f87
Matt Ryan <mryan@carleton.edu>
2007-07-16 04:48:31 +0000

    added base64 encoding to email subjects so that we can pass UTF-8 in subject lines; added documentation; altered send() to return the result of the mail() function (e.g. boolean success)

--- tyr/email.php
+++ tyr/email.php
@@ -53,59 +53,111 @@ class Email
 		$this->_htmlbody = $htmlbody;
 	}
 
+	/**
+	 * Send the Email
+	 *
+	 * @return boolean Accepted for delivery
+	 */
 	function send() {
 		$multipart_boundary = uniqid('the_multipart_boundary_is_');
+		
+		$crlf = "\r\n" ;
 	
-		$email =
-			 "From: " . $this->_froms . "\n" .
-			 "Bcc: " . $this->_tos . "\n" .
-			 "Reply-To: " . $this->_replytos . "\n" .
-			 "MIME-version: 1.0\n";
+		$additional_headers =
+			 'From: ' . $this->_froms . $crlf .
+			 'Bcc: ' . $this->_tos . $crlf .
+			 'Reply-To: ' . $this->_replytos . $crlf .
+			 'MIME-version: 1.0'.$crlf;
+		
+		$email = '';
 
 		if ( !empty($this->_htmlbody) ) {
-			$email .= 'Content-Type: multipart/alternative; boundary="' . $multipart_boundary . '"' . "\n"; // define boundary
-			$email .= "\nThis is a MIME message. If you are reading this text, you may want to consider changing to a mail reader or gateway that understands how properly to handle MIME multipart messages.\n\n"; // preamble
-			$email .= "--" . $multipart_boundary . "\n"; // first boundary
+			$additional_headers .= 'Content-Type: multipart/alternative; boundary="' . $multipart_boundary . '"' . $crlf; // define boundary
+			$email .= "\n".'This is a MIME message. If you are reading this text, you may want to consider changing to a mail reader or gateway that understands how properly to handle MIME multipart messages.'.$crlf."\n"; // preamble
+			$email .= "--" . $multipart_boundary . $crlf; // first boundary
+			$email .= 'Content-Type: text/plain; charset=UTF-8'.$crlf;
+			$email .= 'Content-Disposition: inline'.$crlf;
 		}
-	
-		$email .= "Content-Type: text/plain; charset=UTF-8\n";
-		$email .= "Content-Disposition: inline";
+		else
+		{
+			$additional_headers .= 'Content-Type: text/plain; charset=UTF-8'.$crlf;
+			$additional_headers .= 'Content-Disposition: inline'.$crlf;
+		}
 	
 		$email .= "\n\n" . $this->_txtbody;
 	
 		if ( !empty($this->_htmlbody) ) {
 			$email .= "\n\n--" . $multipart_boundary . "\n";
-			$email .= "Content-Type: text/html; charset=UTF-8\n";
-			$email .= "Content-Disposition: inline\n\n";
+			$email .= 'Content-Type: text/html; charset=UTF-8'.$crlf;
+			$email .= 'Content-Disposition: inline'.$crlf."\n";
 			$email .= $this->_htmlbody;
 			$email .= "\n\n--" . $multipart_boundary . "--\n"; //last boundary
 		}
+		$subject = $this->mb_mime_header_encode($this->_subject);
+		echo $additional_headers.$email;
+		$success = mail( '', $subject, '', $additional_headers.$email );
+		return $success;
+		die();
+	}
 	
-		mail( '', $this->_subject, '', $email );
+	/**
+	 * Encode a multibyte string as a MIME header
+	 *
+	 * mb_encode_mimeheader is naive and therefore unusable (it splits multibyte characters) so this is a more robust replacement.
+	 * This function uses base64 encoding, which is slightly heftier than quoted printable in many settings,
+	 * but has the advantage of being a predictable length (which is important in this case!)
+	 *
+	 * @param string $string
+	 * @param string $encoding the encoding of the string; pass a null value to use the current mb_internal_encoding
+	 * @param string $linefeed the chars to be placed at the end of lines in the encoding
+	 * @return string The encoded string
+	 */
+	function mb_mime_header_encode( $string, $encoding = 'UTF-8', $linefeed = "\r\n " )
+	{
+		if(!$encoding) $encoding = mb_internal_encoding();
+		$encoded = '';
+		
+		while($length = mb_strlen($string))
+		{
+			$encoded .= '=?'.$encoding.'?B?'
+				. base64_encode(mb_substr($string,0,24,$encoding))
+				. '?='.$linefeed;
+		
+			$string = mb_substr($string,24,$length,$encoding);
+		}
+		
+		return trim($encoded);
 	}
 
 
-	// Paramater $addresses can be any of the following:
-	//   a valid email address,
-	//   an username in the directory,
-	//   a comma-delimited combination of addresses and/or usernames, or
-	//   an array of address and/or usernames.
-	// 
-	// This function works as follows: For each address, first treats
-	// the address as a netid and tries to find a corresponding
-	// address in the directory (if this fails, it assumes that the address
-	// was intended as an address rather than a username); second, checks
-	// whether the address is valid
-	// 
-	// If the address is invalid, it is sent to the webmaster and an error is triggered
-	function _prettify_addresses($addresses) {
-
-		$pretty_addresses = '';
+	/**
+	 * Take mixed username/netids and email addresses and resolve into "clean" array of good-looking email addresses
+	 *
+	 * By good-looking we mean that they satisfy this regex: /^([-.]|\w)+@([-.]|\w)+\.([-.]|\w)+$/i
+	 *
+	 * This function works as follows: For each address, first treats
+	 * the address as a netid and tries to find a corresponding
+	 * address in the directory (if this fails, it assumes that the address
+	 * was intended as an address rather than a username); second, checks
+	 * whether the address is valid
+	 * 
+	 * If the address is invalid, the webmaster is included in the recipient list and an error is triggered
+	 *
+	 * @param mixed $addresses can be any of the following: 1) a valid email address, 2) a username in the directory, 3) a comma-delimited combination of addresses and/or usernames, or 4) an array of addresses and/or usernames.
+	 * @return string $pretty_addresses Comma separated email addresses
+	 *
+	 * @todo add ability to handle addresses that include names like this: "Jane Smith" <janesmith@example.com>
+	 **/
+	function _prettify_addresses($addresses)
+	{
 		if ( !is_array($addresses) )
 			$addresses = explode(',', $addresses);
-		foreach ( $addresses as $address ) {
+		$pretty_address_array = array();
+		foreach ( $addresses as $address )
+		{
 			$address = trim($address);
-			if ( !empty($address) ) {
+			if ( !empty($address) )
+			{
 				$dir = new directory_service();
 				$result = $dir->search_by_attribute('ds_username', $address, array('ds_email'));
 				$dir_value = $dir->get_first_value('ds_email');
@@ -114,15 +166,15 @@ class Email
 				if ($num_results <= 0)
 				{
 					trigger_error('The address ' . $address . ' is invalid - sending e-mail to ' . WEBMASTER_EMAIL_ADDRESS . ' instead.');
-					$pretty_addresses .= WEBMASTER_EMAIL_ADDRESS . ', ';
+					$pretty_address_array[] = WEBMASTER_EMAIL_ADDRESS;
 				}
 				else
 				{
-					$pretty_addresses .= $address . ', ';
+					$pretty_address_array[] = $address;
 				}
 			}
 		}
-
+		$pretty_addresses = implode(', ',$pretty_address_array);
 		return $pretty_addresses;
 	}
 }
