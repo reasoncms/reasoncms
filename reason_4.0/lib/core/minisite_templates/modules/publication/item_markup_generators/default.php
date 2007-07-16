bdf81422e24db8f61a4b5ad8d57cf2e7d968d748
Matt Ryan <mryan@carleton.edu>
2007-07-16 04:36:32 +0000

    reworked markup generator to move away from submodule system and most markup (except for that in forms) is now in this module

--- reason_4.0/lib/core/minisite_templates/modules/publication/item_markup_generators/default.php
+++ reason_4.0/lib/core/minisite_templates/modules/publication/item_markup_generators/default.php
@@ -7,11 +7,12 @@ reason_include_once( 'minisite_templates/modules/publication/markup_generator.ph
 *
 *  @package reason
 *  @subpackage minisite_modules
-*  @author Meg Gibbs
+*  @author Meg Gibbs, Matt Ryan, Nate White
 *
 */
 class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 {
+	var $item;
 	var $variables_needed = array('item',
 								  'comment_group',
 								  'comment_group_helper',
@@ -23,72 +24,15 @@ class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 								  'site',
 								  'current_issue',
 								  'current_section',
-								  'publication'
+								  'publication',
+								  'item_events',
+								  'item_images',
+								  'item_assets',
+								  'item_categories',
+								  'item_comments',
+								  'comment_form_markup',
+								  'commenting_status',
 								);
-
-	/**
-	* List of all the submodules for this module with the default parameters for each submodule.
-	* This should be accessed through the get_submodules() method, not directly.
-	* @var array
-	*/
-	var $submodules = array(    'news_comments_added'=>array('title'=>'Your comment has been added.',
-														'title_tag'=>'h4',
-														'wrapper_class'=>'commentAdded',
-													),
-								'news_content'=>array(	'title'=>'',
-														'title_tag'=>'',
-														'wrapper_class'=>'text',
-														'date_format' => 'F j, Y \a\t g:i a',
-													),
-								'news_related_events'=>array ( 'title'=>'Related Events',
-															   'title_tag'=>'h4',
-															   'wrapper_class'=>'relatedEvents'
-													),
-								'news_images'=>array(	'title'=>'Images',
-														'title_tag'=>'h4',
-														'wrapper_class'=>'images',
-													),
-								'news_assets'=>array(	'title'=>'Related Documents',
-														'title_tag'=>'h4',
-														'wrapper_class'=>'assets',
-													),
-								'news_related'=>array(	'title'=>'Related Stories',
-														'title_tag'=>'h4',
-														'wrapper_class'=>'relatedNews',
-													),
-								'news_categories_submodule'=>array(	'title'=>'Categories',
-														'title_tag'=>'h4',
-														'wrapper_class'=>'categories',
-													),
-								'news_display_comments'=>array(	'title'=>'Comments',
-													'title_tag'=>'h4',
-													'wrapper_class'=>'comments',
-													'date_format' => 'F j, Y \a\t g:i a',
-													'back_link' => '',
-												),
-								'news_add_comments'=>array(	'title'=>'Add a Comment',
-														'title_tag'=>'h4',
-														'public'=>false,
-														'wrapper_class'=>'addCommentForm',
-														'comments_moderated' => false, 
-														'group' => '',
-														'back_link' => '',
-													),
-								/*'news_return_to_list' => array ( 'title'=>'Return to',
-													 	 'wrapper_class'=>'returnToList',
-														), */
-								); 
-	/**
-	* List of submodules that should be called.
-	* @var array
-	*/
-	var $use_submodules = array();  // defined in init(), so that our default is to use everything defined in the $submodules array
-
-	/**
-	* The order in which submodules should be called.  If a submodule being used is not included in this array, it will be displayed after
-	* the submodules defined in this array - but you should include all submodules that you're using in this array anyway.
-	* @var array
-	*/							
 	var $submodule_order = array(   'news_comments_added',
 									'news_content',
 									'news_related_events',
@@ -101,23 +45,320 @@ class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 								);
 
 
-	function PublicationListMarkupGenerator ()
-	{
-	}
-
 	function additional_init_actions()
 	{
-		if(empty($this->use_submodules))
-			$this->use_submodules = array_keys($this->submodules);  //defined here so that we always default to using all available submodules.
-		$item = $this->passed_vars['item'];
-		$creator = $item->get_value('created_by');
+		/* if(empty($this->use_submodules))
+			$this->use_submodules = array_keys($this->submodules);  //defined here so that we always default to using all available submodules. */
+		$this->item = $this->passed_vars['item'];
+		$creator = $this->item->get_value('created_by');
 		if(empty($creator))
-			$this->update_entity_to_include_creator($item);
+			$this->update_entity_to_include_creator($this->item);
 	}
 
 	function run()
-	{	
-		$this->markup_string = $this->run_submodules();
+	{
+		$this->markup_string = '';
+		$this->markup_string .= '<div class="fullPost">';
+		$this->markup_string .= '<div class="primaryContent">'."\n";
+		if($this->should_show_comment_added_section())
+		{
+			$this->markup_string .= '<div class="commentAdded">'.$this->get_comment_added_section().'</div>'."\n";
+		}
+		$this->markup_string .= '<h3 class="postTitle">'.$this->item->get_value( 'release_title' ).'</h3>'."\n";
+		if($this->should_show_date_section())
+		{
+			$this->markup_string .= '<div class="date">'.$this->get_date_section().'</div>'."\n";
+		}
+		if($this->should_show_author_section())
+		{
+			$this->markup_string .= '<div class="author">'.$this->get_author_section().'</div>'."\n";
+		}
+		if($this->should_show_content_section())
+		{
+			$this->markup_string .= '<div class="text">'.$this->get_content_section().'</div>'."\n";
+		}
+		if($this->should_show_comments_section() || $this->should_show_comment_adder_section())
+		{
+			$this->markup_string .= $this->get_back_links_markup();
+		}
+		if($this->should_show_comments_section())
+		{
+			$this->markup_string .= '<div class="comments">'.$this->get_comments_section().'</div>'."\n";
+		}
+		if($this->should_show_comment_adder_section())
+		{
+			$this->markup_string .= '<div class="addCommentForm">'.$this->get_comment_adder_section().'</div>'."\n";
+		}
+		$this->markup_string .= '</div>'."\n";
+		if($this->should_show_related_events_section() || $this->should_show_images_section() || $this->should_show_assets_section() || $this->should_show_categories_section())
+		{
+			$this->markup_string .= '<div class="relatedItems">'."\n";
+			if($this->should_show_related_events_section())
+			{
+				$this->markup_string .= '<div class="relatedEvents">'.$this->get_related_events_section().'</div>'."\n";
+			}
+			if($this->should_show_images_section())
+			{
+				$this->markup_string .= '<div class="images">'.$this->get_images_section().'</div>'."\n";
+			}
+			if($this->should_show_assets_section())
+			{
+				$this->markup_string .= '<div class="assets">'.$this->get_assets_section().'</div>'."\n";
+			}
+			if($this->should_show_categories_section())
+			{
+				$this->markup_string .= '<div class="categories">'.$this->get_categories_section().'</div>'."\n";
+			}
+			$this->markup_string .= '</div>'."\n";
+		}
+		$this->markup_string .= '</div>';
+	}
+	
+	/**
+	 * Answers question: should the "Comment Added" section be displayed?
+	 *
+	 * Basically this should answer true if a comment was just posted (e.g. the request has a value for comment_posted_id)
+	 *
+	 * @return boolean
+	 */
+	function should_show_comment_added_section()
+	{
+		if(!empty($this->passed_vars['request']['comment_posted_id']))
+			return true;
+		else
+			return false;
+	}
+	
+	/**
+	 * Build the markup for the "Comment Added" section
+	 *
+	 * This section exists to inform the user that their comment was a) added, or b) held for review
+	 *
+	 * @return string 
+	 */
+	function get_comment_added_section()
+	{
+		$ret = '';
+		if($this->passed_vars['publication']->get_value('hold_comments_for_review') == 'yes')
+		{
+			$ret .= '<h4>Comments are being held for review on this publication.  Please check back later to see if your comment has been posted.</h4>';
+		}
+		else
+		{
+			$ret .= '<h4>Your comment has been added.</h4>';
+			$ret .= '<a href="#comment'.$this->passed_vars['request']['comment_posted_id'].'">Jump to your comment</a>';
+		}
+		return $ret;
+	}
+	function should_show_date_section()
+	{
+		if( $this->item->get_value( 'datetime' ) )
+			return true;
+		else
+			return false;
+	}
+	function get_date_section()
+	{
+		// todo: use publication date format setting
+		return prettify_mysql_datetime($this->item->get_value( 'datetime' ), 'F j, Y \a\t g:i a');
+	}
+	function should_show_author_section()
+	{
+		if( $this->item->get_value( 'author' ) )
+			return true;
+		else
+			return false;
+	}
+	function get_author_section()
+	{
+		return 'By '.$this->item->get_value( 'author' );
+	}
+	function should_show_content_section()
+	{
+		if($this->item->get_value('content') || $this->item->get_value('description'))
+			return true;
+		else
+			return false;
+	}
+	function get_content_section()
+	{
+		if( $this->item->get_value( 'content' ) )
+		{
+			return $this->alter_content($this->item->get_value( 'content' ) );
+		}
+		else
+		{
+			return $this->alter_content( $this->item->get_value('description') );
+		}
+	}
+	function alter_content($content)
+	{
+		if(strpos($content,'<h3') !== false || strpos($content,'<h4') !== false)
+		{
+			$content = tagSearchReplace($content, 'h4', 'h5');
+			$content = tagSearchReplace($content, 'h3', 'h4');
+		}
+		return $content;
+	}
+	
+	// Related events section
+	function should_show_related_events_section()
+	{
+		if(!empty($this->passed_vars['item_events']))
+			return true;
+		else
+			return false;
+	}
+	function get_related_events_section()
+	{
+		$str = '<h4>Related Events</h4>';
+		$str .= '<ul>';
+		foreach($this->passed_vars['item_events'] as $event)
+		{
+			$str .= '<li>';
+			if($event->get_value('event_url'))
+				$str .= '<a href="'.$event->get_value('event_url').'">'.$event->get_value('name').'</a>';
+			else
+				$str .= $event->get_value('name');
+			$str .= '</li>';
+		}
+		$str .= '</ul>';
+		return $str;
+	}
+	
+	// Images section
+	function should_show_images_section()
+	{
+		if(!empty($this->passed_vars['item_images']))
+			return true;
+		else
+			return false;
+	}
+	function get_images_section()
+	{
+		$str = '<h4>Images</h4>';
+		$str .= '<ul>';
+		foreach($this->passed_vars['item_images'] as $image)
+		{
+			$str .= '<li>';
+			ob_start();
+			$textonly = false;
+			if(!empty($this->passed_vars['request']['textonly']))
+				$textonly = true;
+			show_image( $image, false, true, true, '', $textonly );
+			$str .= ob_get_contents();
+			ob_end_clean();
+			$str .= '</li>';
+		}
+		$str .= '</ul>';
+		return $str;
+	}
+	
+	// Assets section
+	function should_show_assets_section()
+	{
+		if(!empty($this->passed_vars['item_assets']))
+			return true;
+		else
+			return false;
+	}
+	function get_assets_section()
+	{
+		reason_include_once( 'function_libraries/asset_functions.php' );
+		$str = '<h4>Related Documents</h4>';
+		$str .= make_assets_list_markup( $this->passed_vars['item_assets'], $this->passed_vars['site'] );
+		return $str;
+	}
+	
+	// Categories section
+	function should_show_categories_section()
+	{
+		if(!empty($this->passed_vars['item_categories']))
+			return true;
+		else
+			return false;
+	}
+	function get_categories_section()
+	{
+		$ret = '<h4>Categories</h4>';
+		$ret .= '<ul>';
+		foreach($this->passed_vars['item_categories'] as $category)
+		{
+			$ret .= '<li><a href="'.$category->get_value('category_url').'">'.$category->get_value('name').'</a></li>';
+		}
+		$ret .= '</ul>';
+		return $ret;
+	}
+	
+	// Comments section
+	function should_show_comments_section()
+	{
+		if(!empty($this->passed_vars['item_comments']))
+			return true;
+		else
+			return false;
+	}
+	function get_comments_section()
+	{
+		$ret = '<a name="comments"></a>';
+		$ret .= '<h4>Comments</h4>'."\n";
+		
+		if(!empty($this->passed_vars['item_comments']))
+		{
+			$ret .=  '<ul>';
+			foreach($this->passed_vars['item_comments'] as $comment)
+			{
+				$ret .=  '<li><a name="comment'.$comment->id().'"></a>';
+				// todo: use publication date format
+				$ret .=  '<div class="datetime">'.prettify_mysql_datetime($comment->get_value('datetime'), 'F j Y \a\t g:i a').'</div>';
+				$ret .= '<div class="author">'.$comment->get_value('author').'</div>';
+				$ret .= '<div class="commentContent">'.$comment->get_value('content').'</div>';
+				$ret .= '</li>';
+			}
+			$ret .= '</ul>';
+		}
+		else
+		{
+			$ret .= '<p>There are no comments yet for this post.</p>';
+		}
+		
+		return $ret;
+	}
+	
+	// Comment adder section
+	function should_show_comment_adder_section()
+	{
+		if($this->passed_vars['commenting_status'] == 'publication_comments_off')
+			return false;
+		else
+			return true;
+	}
+	function get_comment_adder_section()
+	{
+		$ret = '';
+		switch($this->passed_vars['commenting_status'])
+		{
+			case 'publication_comments_off':
+				break;
+			case 'item_comments_off':
+				$ret .= '<p>Comments for this post are turned off</p>';
+			case 'login_required':
+				$ret .= '<h4>Add a comment</h4>'."\n";
+				$ret .= '<p>Please <a href="'.REASON_LOGIN_URL.'"> login </a> to comment.</p>';
+				break;
+			case 'user_not_permitted':
+				$ret .= '<h4>Commenting Restricted</h4>'."\n";
+				$ret .= '<p>You do not currently have the rights to post a comment. If you would like to comment, please contact the site maintainer listed on this page.</p>';
+				break;
+			case 'open_comments':
+			case 'user_has_permission':
+				$ret .= '<h4>Add a comment</h4>'."\n";
+				$ret .= $this->passed_vars['comment_form_markup'];
+				break;
+			default:
+				trigger_error( 'commenting_status not an expected value ('.$this->passed_vars['commenting_status'].')' );
+		}
+		return $ret;
 	}
 
 	function update_entity_to_include_creator($entity)
