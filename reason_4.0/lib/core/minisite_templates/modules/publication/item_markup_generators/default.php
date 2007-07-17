433223f04eefa28438de1a662acac5a40e2492ee
Matt Ryan <mryan@carleton.edu>
2007-07-17 19:58:20 +0000

    removed submodule vestiges

--- reason_4.0/lib/core/minisite_templates/modules/publication/item_markup_generators/default.php
+++ reason_4.0/lib/core/minisite_templates/modules/publication/item_markup_generators/default.php
@@ -14,12 +14,9 @@ class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 {
 	var $item;
 	var $variables_needed = array('item',
-								  'comment_group',
-								  'comment_group_helper',
 								  'comment_moderation_state',
 								  'back_link',
 								  'back_to_section_link',
-								  'class_vars_pass_to_submodules',
 								  'request',
 								  'site',
 								  'current_issue',
@@ -33,22 +30,10 @@ class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 								  'comment_form_markup',
 								  'commenting_status',
 								);
-	var $submodule_order = array(   'news_comments_added',
-									'news_content',
-									'news_related_events',
-									'news_images',
-									'news_assets',
-									'news_related',
-									'news_categories_submodule',
-									'news_display_comments',
-									'news_add_comments',																		
-								);
 
 
 	function additional_init_actions()
 	{
-		/* if(empty($this->use_submodules))
-			$this->use_submodules = array_keys($this->submodules);  //defined here so that we always default to using all available submodules. */
 		$this->item = $this->passed_vars['item'];
 		$creator = $this->item->get_value('created_by');
 		if(empty($creator))
@@ -391,154 +376,6 @@ class PublicationItemMarkupGenerator extends PublicationMarkupGenerator
 			return $first->get_value('last_edited_by');
 		}
 	}
-	
-	/**	
-	* Used to modify the default paramaters in the submodule, or by children to add new submodules to the submodule array.  
-	* The submodule array should ALWAYS be accessed using this function.  
-	* @return array the submodule array
-	*/	
-	function get_submodules()
-	{
-		//set the defaults that can't be set in the global variable section
-		$this->submodules['news_add_comments']['comments_moderated'] = $this->passed_vars['comment_moderation_state'];
-		$this->submodules['news_add_comments']['group'] = $this->passed_vars['comment_group']; 
-
-		$this->submodules['news_add_comments']['back_link'] = $this->get_back_links_markup();
-		$this->submodules['news_display_comments']['back_link'] = $this->get_back_links_markup();
- 
-		$cgh = $this->passed_vars['comment_group_helper'];
-		if(!$cgh->group_has_members())
-		{
-			//the default comments_off_message says comments have been turned off for a POST; when no comments for entire pub, want no message to show up
-			$this->submodules['news_add_comments']['comments_off_message'] = ''; 
-		}
-		return $this->submodules;
-	}
-
-	/**	
-	* Displays the content of a news item by running the submodules defined in $use_submodules and 
-	* displaying their output in the order defined by $submodule_order. 
-	* @param entity news item
-	*/	  
-	function run_submodules()
-	{
-		$markup_string = '';
-		$item = $this->passed_vars['item'];
-		$submodules_output = array();
-		$site = $this->passed_vars['site'];
-		$submodule_params = $this->get_submodules();
-
-		//we don't care about the submodule order when we're just getting the output - it's all going into an array, anyway.
-		foreach($this->use_submodules as $sub_name)
-		{
-			$output = $this->get_submodule_output($item, $sub_name, $site);
-			if($output)
-			{
-				$submodules_output[$sub_name]  = $output;
-			}
-		}
-
-		//we do care about order when we're displaying the output, however.
-		if(!empty($submodules_output))
-		{
-			//so we check to see if anything in submodules doesn't have a place in the order of submodules
-			$unordered_submodules = array();
-			foreach($submodules_output as $sub_name => $output)
-			{
-				$class_name[] = 'contains' . ucfirst($this->submodules[$sub_name]['wrapper_class']);
-				if(!in_array($sub_name, $this->submodule_order))
-				{
-					$unordered_submodules[] = $sub_name;
-					trigger_error('The submodule "'.$sub_name.'" does not appear in the $submodule_order array',WARNING);
-				}
-			}
-		
-			if(count($submodules_output > 1))
-			{
-				$additional_class_text = ' multiple ' . implode($class_name, ' ');
-			}
-			else
-			{
-				$additional_class_text = '';
-			}
-			$markup_string .= '<div class="submodules'.$additional_class_text.'">'."\n";
-
-			//and then display the output in the defined order of submodules			
-			$submodule_order = array_merge($this->submodule_order, $unordered_submodules);
-			foreach($submodule_order as $sub_name)
-			{
-				if(array_key_exists($sub_name, $submodules_output))
-				{
-					$markup_string .=  '<div class="'.$submodule_params[$sub_name]['wrapper_class'].'">'."\n".$submodules_output[$sub_name].'</div>'."\n";
-				}
-			}
-			$markup_string .=  '</div>'."\n";
-			return $markup_string;
-		}
-	}
-
-	/**	
-	* Runs a given submodule and returns the output.
-	* @param entity news item
-	* @param string submodule name
-	* @param entity site entity for this site
-	* @return string submodule output
-	*/	
-	function get_submodule_output($item, $sub_name, $site)
-	{
-		$this->include_filename($sub_name);
-		$sub = new $sub_name();
-		$submodules = $this->get_submodules();
-		
-		$sub->pass_params($submodules[$sub_name]);
-		$sub->pass_site($site);
-		if(!empty($this->passed_vars['class_vars_pass_to_submodules']))
-		{
-			$add_vars = array();
-			foreach($this->passed_vars['class_vars_pass_to_submodules'] as $var)
-			{
-				if(!empty($this->$var))
-				{
-					$add_vars[$var] = $this->$var;
-				}
-				elseif(!empty($this->passed_vars[$var]))
-				{
-					$add_vars[$var] = $this->passed_vars[$var];
-				}
-			}
-			if(!empty($add_vars))
-			{
-				$sub->pass_additional_vars($add_vars);
-			}
-		}
-		$sub->init($this->passed_vars['request'], $item);
-		if($sub->has_content())
-		{
-			return $sub->get_content();
-		}
-		else
-			return false;
-	}
-
-	/**	
-	* Includes the file for a given submodule, or triggers an error if the file cannot be found.
-	* @param string name of submodule to be included
-	*/	
-	//this probably needs some more checks ... e.g., does the class exist?  compare to default minisite template
-	function include_filename($submodule_name)
-	{
-#		$base = $_SERVER[ 'DOCUMENT_ROOT' ]/*.'/global_stock/php/'*/;
-		$include = /*REASON_INC.'lib/core/*/'minisite_templates/modules/publication/submodules/'.$submodule_name;
-		if( file_exists( REASON_INC.'lib/core/'.$include.'.php' ) )
-		{
-			reason_include_once( $include.'.php' );
-		}
-		else
-		{
-			trigger_error('The submodule class file for "'.$submodule_name.'" cannot be found',WARNING);
-		}
-	}
-
 	
 	function get_back_links_markup()
 	{
