d09f1345b842c5508d8034e6c67299b6e1628909
Matt Ryan <mryan@carleton.edu>
2007-07-30 20:00:59 +0000

    script to update the name of the repeat field

---
+++ reason_4.0/www/scripts/upgrade/4.0b3_to_4.0b4/event_repeat_field_name_change.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b3_to_4.0b4 upgrade page
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b3_to_4.0b4/event_repeat_field_name_change.php' );
+?>
