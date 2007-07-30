433cc6e72cc15a0083b402120c41bd04e6768f05
Matt Ryan <mryan@carleton.edu>
2007-07-30 22:12:43 +0000

    fixed failure to implement search interface

--- reason_4.0/lib/core/minisite_templates/modules/events_archive.php
+++ reason_4.0/lib/core/minisite_templates/modules/events_archive.php
@@ -7,29 +7,13 @@ class EventsArchiveModule extends EventsModule
 {
 	var $show_calendar_grid = false;
 	var $show_views = false;
-	function init_list()
+	
+	function make_reason_calendar_init_array($start_date, $end_date = '', $view = '')
 	{
-		$this->today = date('Y-m-d');
-		$init_array = array();
-		$init_array['site'] = $this->parent->site_info;
-		$init_array['start_date'] = '1970-01-01 00:00:00';  // Make the calendar show all events
-		if(!empty($this->pass_vars['view']))
-			$init_array['view'] = $this->pass_vars['view'];
-		if(!empty($this->pass_vars['audience']))
-		{
-			$audiences = array($this->pass_vars['audience']);
-			$init_array['audiences'] = $audiences;
-		}
-		if(!empty($this->pass_vars['category']))
-		{
-			$category = new entity($this->pass_vars['category']);
-			$categories = array( $category->id()=>$category );
-			$init_array['categories'] = $categories;
-		}
-		if(!empty($this->ideal_count))
-			$init_array['ideal_count'] = $this->ideal_count;
-		$this->calendar = new reasonCalendar($init_array);
-		$this->calendar->run();
+		$array = parent::make_reason_calendar_init_array($start_date, $end_date, $view);
+		$array['start_date'] = '1970-01-01';
+		$array['view'] = 'all';
+		return $array;
 	}
 	function show_navigation()
 	{
