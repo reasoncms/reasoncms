12e3f13ac7de08e447feceb94ab75a125295491e
Nathan White <nwhite@carleton.edu>
2007-08-29 19:51:59 +0000

    changed paths to use HTML_PURIFIER_INC constant

--- htmlpurifier/htmlpurifier.php
+++ htmlpurifier/htmlpurifier.php
@@ -5,6 +5,6 @@
 	/**
 	 * Loads the HTML_Purifier class appropriate for the version of PHP
 	 */
-	if (carl_is_php5()) require_once( INCLUDE_PATH . 'htmlpurifier/htmlpurifier-2.1.1-strict/library/HTMLPurifier.auto.php' );
-	else require_once( INCLUDE_PATH . 'htmlpurifier/htmlpurifier-2.1.1/library/HTMLPurifier.auto.php' );
+	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.1-strict/library/HTMLPurifier.auto.php' );
+	else require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.1/library/HTMLPurifier.auto.php' );
 ?>
