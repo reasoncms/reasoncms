349b3a423b7c37fad5cc1541960b26467cc98e23
Nathan White <nwhite@carleton.edu>
2007-08-29 19:52:39 +0000

    changed paths to use the XML_PARSER_INC constant and removed reason header include

--- xml/xmlparser.php
+++ xml/xmlparser.php
@@ -1,9 +1,10 @@
 <?php
-	include_once('reason_header.php');
+	include_once('paths.php');
+	include_once(CARL_UTIL_INC . 'basic/misc.php');
 
 	/**
 	 * Loads the XML Parser class appropriate for the version of PHP
 	 */
-	if (carl_is_php5()) require_once( INCLUDE_PATH . 'xml/xmlparser5.php' );
-	else require_once( INCLUDE_PATH . 'xml/xmlparser4.php' );
+	if (carl_is_php5()) require_once( XML_PARSER_INC . 'xmlparser5.php' );
+	else require_once( XML_PARSER_INC . 'xmlparser4.php' );
 ?>
