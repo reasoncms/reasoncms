31bee7abda3fb9bc503d972686a10a25cd45544c
Nathan White <nwhite@carleton.edu>
2007-09-24 17:40:00 +0000

    changed to make full story link part of links div

--- reason_4.0/lib/core/minisite_templates/modules/publication/list_item_markup_generators/related_full_story_link.php
+++ reason_4.0/lib/core/minisite_templates/modules/publication/list_item_markup_generators/related_full_story_link.php
@@ -10,21 +10,45 @@ reason_include_once( 'minisite_templates/modules/publication/list_item_markup_ge
 
 class RelatedFullStoryLinkListItemMarkupGenerator extends RelatedListItemMarkupGenerator
 {
-	function get_description_markup()
+
+	function run ()
+	{	
+		$this->markup_string .= '<li>';
+		$this->markup_string .= $this->get_teaser_image_markup();
+		$this->markup_string .= $this->get_date_markup();
+		$this->markup_string .= $this->get_title_markup();
+		$this->markup_string .= $this->get_description_markup();
+		$this->markup_string .= $this->get_links_markup();
+		$this->markup_string .= '</li>';
+	}
+	
+	function get_links_markup()
+	{
+		$markup_string =  '<ul class="links">'."\n";
+		if(!empty($this->passed_vars['link_to_full_item']))
+			$markup_string .= $this->get_more_link_markup();
+		if(!empty($this->passed_vars['permalink']))
+			$markup_string .= $this->get_permalink_markup();
+		if(!empty($this->passed_vars['item_comment_count']))
+			$markup_string .= $this->get_comment_link_markup();
+		$markup_string .= '</ul>'."\n";
+		return $markup_string;
+	}
+
+	function get_more_link_markup()
 	{
 		$item = $this->passed_vars['item'];
-		$link_to_full_item = $this->passed_vars['link_to_full_item'];
-				
-		if (isset($link_to_full_item) && !empty($link_to_full_item))
+
+		$markup_string = '';
+		if($item->get_value('content') && isset($this->passed_vars['link_to_full_item']) &&  !empty($this->passed_vars['link_to_full_item']))
 		{
-			$link = ' <a href="' .$link_to_full_item. '" title="'.$item->get_value('release_title').'">';
-			$link .= 'full story';
-			$link .= '</a>';
+			$markup_string .=  '<li class="more">';
+			$markup_string .=  '<a href="' . $this->passed_vars['link_to_full_item'] .'" title="'.$item->get_value('release_title').'">';
+			$markup_string .=  'full story';
+			$markup_string .=  '</a>';
+			$markup_string .=  '</li>'."\n";
 		}
-		else $link = '';
-			
-		if($item->get_value('description'))
-			return '<div class="desc">'.$item->get_value('description').$link.'</div>'."\n";
+		return $markup_string;
 	}
 }
 ?>
