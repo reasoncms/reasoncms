9737a2015c374cf08030bfd8a86cd56035a622c5
Nathan White <nwhite@carleton.edu>
2007-10-23 18:50:17 +0000

    upgraded to 2.1.2 standalone - fewer files

--- htmlpurifier/htmlpurifier.php
+++ htmlpurifier/htmlpurifier.php
@@ -5,6 +5,6 @@
 	/**
 	 * Loads the HTML_Purifier class appropriate for the version of PHP
 	 */
-	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.1-strict/library/HTMLPurifier.auto.php' );
-	else require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.1/library/HTMLPurifier.auto.php' );
+	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.2-strict-standalone/HTMLPurifier.standalone.php' );
+	else require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.2-standalone/HTMLPurifier.standalone.php' );
 ?>
