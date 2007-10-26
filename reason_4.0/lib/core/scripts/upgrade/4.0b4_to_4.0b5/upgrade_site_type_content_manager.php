a595be98abe9fddd7d442e647a338a2942aafef5
Matt Ryan <mryan@carleton.edu>
2007-10-26 23:54:26 +0000

    fixed typo

--- reason_4.0/lib/core/scripts/upgrade/4.0b4_to_4.0b5/upgrade_site_type_content_manager.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b4_to_4.0b5/upgrade_site_type_content_manager.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>>Upgrade content manager for site types</title>
+<title>Upgrade content manager for site types</title>
 </head>
 
 <body>
