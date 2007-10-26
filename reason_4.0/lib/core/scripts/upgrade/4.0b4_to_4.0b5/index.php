dd3a0da1c94af75c83698455f19234920578b113
Matt Ryan <mryan@carleton.edu>
2007-10-26 23:48:22 +0000

    4.0 beta 4 to beta 5 upgrades

---
+++ reason_4.0/lib/core/scripts/upgrade/4.0b4_to_4.0b5/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Upgrade Reason from 4.0 Beta 4 to 4.0 Beta 5</title>
+</head>
+
+<body>
+<ul>
+<li><a href="upgrade_site_type_content_manager.php">Upgrade content manager for site types</a></li>
+</ul>
+</body>
+</html>
