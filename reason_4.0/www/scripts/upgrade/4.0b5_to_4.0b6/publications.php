3acde6dea0b1144fcb90ed2dea923299acf4f873
Matt Ryan <mryan@carleton.edu>
2007-11-03 21:47:29 +0000

    adding dir for b5->b6 upgrades and file for upgrading publications stuff

---
+++ reason_4.0/www/scripts/upgrade/4.0b5_to_4.0b6/publications.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the b5->b6 publications upgrader
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b5_to_4.0b6/publications.php' );
+?>
