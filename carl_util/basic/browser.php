cf5ae5d45b7766153b3dbc4fb7c879047bb4b1e7
Matt Ryan <mryan@carleton.edu>
2007-11-16 00:23:28 +0000

    added documentation

--- carl_util/basic/browser.php
+++ carl_util/basic/browser.php
@@ -1,18 +1,40 @@
 <?php
+/**
+ * @package carl_util
+ * @subpackage basic
+ */
+ 
+ /**
+  * Determine if the current user agent string matches a given regular expression
+  * @param string $regex
+  * @return integer 0 for no matches, 1 for a match
+  */
 	function match_ua( $regex )
 	{
 		return preg_match( '/'.$regex.'/', $_SERVER['HTTP_USER_AGENT'] );
 	}
 
+ /**
+  * Determine if the current browser is Mac Netscape Navigator 4.x
+  * @deprecated Deprecated since 15 November 2007, as this is not apparently used anywhere and is just dumb
+  * @todo remove this function entirely before official release
+  * @return boolean (now always returns false)
+  */
 	function is_mac_nn4()
 	{
-		if ( match_ua( 'Mac' ) && match_ua( 'Mozilla' ) && !( match_ua( 'MSIE' ) || match_ua( 'Netscape6' ) || match_ua( 'Netscape7' ) || match_ua( 'Gecko' ) ||  match_ua( 'Opera' ) || match_ua( 'OmniWeb' ) || match_ua( 'Safari' ) || match_ua( 'Lynx' ) ) )
-				return true;
-		else return false;
+		trigger_error('is_mac_nn4() is deprecated. Please remove any calls to this function from your code.');
+		return false;
 	}
 
+ /**
+  * Determine if the current ip address is on the Carleton College campus
+  * @deprecated Deprecated since 15 November 2007, as this is not apparently used anywhere and is Carleton-specific code
+  * @todo remove this function entirely before official release
+  * @return integer 0 (false); 1 (true)
+  */
 	function is_on_campus()
 	{
+		trigger_error('is_on_campus() is deprecated. Please remove any calls to this function from your code.');
 		return preg_match( '/^137/' , $_SERVER[ 'REMOTE_ADDR' ] );
 	}
 
@@ -23,6 +45,8 @@
 	 *   ARE enabled.  Custom strings can be passsed to the function for custom messages for both possibilities.
 	 *   Also note that there is no styling of the messages.  Any style should either be wrapped around the call to this
 	 *   function or should be passed in the strings to the function.
+	 *
+	 *	@todo reimplement in an XHTML strict-comliant manner
 	 */
 	function show_cookie_capability( $no_cookie_msg = '', $cookie_msg = '')
 	{
