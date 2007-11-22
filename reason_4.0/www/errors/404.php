ff5bb2f98ca587bc87c45b8268cad6844bb8cdd5
Matt Ryan <mryan@carleton.edu>
2007-11-22 16:18:34 +0000

    added documentation and changed mac newlinechars to unix newline chars

--- reason_4.0/www/errors/404.php
+++ reason_4.0/www/errors/404.php
@@ -1,1 +1,12 @@
-<?php	include_once('reason_header.php');	reason_include('errors/404.php');?>
+<?php
+/**
+ * Hook for the Reason 404 error page script
+ *
+ * This file exists in the web tree so that the "real" script can exist outside the web tree.
+ * It is a simple reason include of lib/[core|local]/errors/404.php
+ *
+ * @package reason
+ */
+	include_once('reason_header.php');
+	reason_include('errors/404.php');
+?>
