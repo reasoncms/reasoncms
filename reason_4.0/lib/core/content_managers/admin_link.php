404442372a7156cdebec46bdfeaab8ec714c04c8
Matt Ryan <mryan@carleton.edu>
2007-11-22 17:51:25 +0000

    improved documentation

--- reason_4.0/lib/core/content_managers/admin_link.php
+++ reason_4.0/lib/core/content_managers/admin_link.php
@@ -1,12 +1,23 @@
 <?php
+/**
+ * A content manager for admin link entities
+ * @package reason
+ * @subpackage content_managers
+ */
+ 
+ /**
+  * Define the class name so that the admin page can use this content manager
+  */
 	$GLOBALS[ '_content_manager_class_names' ][ basename( __FILE__) ] = 'adminLinkManager';
 
-	/**
-         * this content manager transparently fixes the situation where an admin link is specified as being
-	 * relative to the reason_http_base, but the url entered begins with a slash ... this strips the slash,
-         * thus preserving the consistency of the way admin links are stored in the database but without bothering
-	 * the user. at least this is the intention.
-         */
+/**
+ * A content manager for admin link entities
+ *
+ * this content manager transparently fixes the situation where an admin link is specified as being
+ * relative to the reason_http_base, but the url entered begins with a slash ... this strips the slash,
+ * thus preserving the consistency of the way admin links are stored in the database but without bothering
+ * the user. at least this is the intention.
+ */
 	class adminLinkManager extends ContentManager
 	{
 		function process()
