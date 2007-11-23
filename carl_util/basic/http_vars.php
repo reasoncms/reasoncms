840cb00d6b11440d94ffe75a8ee9cd99c9654a35
Matt Ryan <mryan@carleton.edu>
2007-11-23 19:57:23 +0000

    added documentation

--- carl_util/basic/http_vars.php
+++ carl_util/basic/http_vars.php
@@ -1,11 +1,24 @@
 <?php
-/*
- * merge HTTP_GET_VARS and HTTP_POST_VARS
+/**
+ * @package carl_util
+ * @subpackage basic
+ */
+
+/**
+ * Pre-php4 solution for before include_once() existed
  */
 if(!defined( '__HTTP_VARS' ))
 {
 	define( '__HTTP_VARS' , true);
 	
+	/**
+	 * Get merged HTTP_GET_VARS and HTTP_POST_VARS
+	 *
+	 * This must be a relic of an era (php3?) before $_REQUEST was available...?
+	 *
+	 * @deprecated
+	 * @todo Replace references to this function with $_REQUEST
+	 */
 	function get_http_vars()
 	{
 		/*
