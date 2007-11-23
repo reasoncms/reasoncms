9baab81566330911cf5e5ed881b6a3ff80006039
Matt Ryan <mryan@carleton.edu>
2007-11-23 20:09:12 +0000

    added documentation and eliminated reflected XSS vulnerability

--- carl_util/cache/get_cache_filename.php
+++ carl_util/cache/get_cache_filename.php
@@ -1,15 +1,29 @@
 <?php
+/**
+ * Simple script for determining what file to zap if you need to refresh a cache
+ * @package carl_util
+ * @subpackage cache
+ */
+ 
+ /**
+  * Include dependencies
+  */
 	include ('paths.php');
 	include_once( CARL_UTIL_INC . 'cache/cache.php' );
 	
-	echo '<html><head><title>Discover a Cache File for a particular URL</title></head><body>';
+ /**
+  * Run the script
+  */
+	echo '<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
+	echo '<title>Discover a Cache File for a particular URL</title></head><body>';
 	echo '<h1>Discover a Cache File for a particular URL</h1>';
 	echo '<p>You might want to double check before zapping a cache file.</p>';
-	echo '<form method="get"><input type="text" name="url"><input type="submit"></form></body></html>';
+	echo '<form method="get"><input type="text" name="url"><input type="submit"></form>';
 	if(!empty($_REQUEST['url']))
 	{
 		$pc = new PageCache;
-		echo '<p>URL: '.$_REQUEST['url'].'</p>';
+		echo '<p>URL: '.htmlspecialchars($_REQUEST['url'], ENT_COMPAT, 'UTF-8').'</p>';
 		echo '<p>Cache Filename: '.$pc->_get_cache_file( $_REQUEST['url'] ).'</p>';
 	}
+	echo '</body></html>';
 ?>
