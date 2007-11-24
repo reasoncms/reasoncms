e55a957a2e0d9a9db2617f29de1f678575749d59
Matt Ryan <mryan@carleton.edu>
2007-11-24 20:59:41 +0000

    added documentation and reworked code for better security

--- loki_1.0/dialogs/hel_link_get_anchors.php
+++ loki_1.0/dialogs/hel_link_get_anchors.php
@@ -1,20 +1,39 @@
 <?php
-
+/**
+ * @package loki_1
+ * @subpackage hel
+ */
+/**
+ * Include reason libraries
+ */
 include_once('reason_header.php');
 reason_include_once( 'function_libraries/url_utils.php');
 
-$string = get_reason_url_contents($_REQUEST['uri']);
-$string=eregi_replace("\r","",$string);
-$string=eregi_replace("\n","",$string);
-$string=strip_tags($string, "<a>");
-//$string=addslashes($string);
+/**
+ * Include the find_anchors_naive() function
+ */
+include_once('anchors_from_string.php');
 
+if(strpos($_REQUEST['uri'],'http://'.$_SERVER['HTTP_HOST']) === 0 || strpos($_REQUEST['uri'],'https://'.$_SERVER['HTTP_HOST']) === 0)
+{
+	$html = get_reason_url_contents($_REQUEST['uri']);
+	$anchors = find_anchors_naive($html);
+	$string = '';
+	foreach($anchors as $a)
+	{
+		$string .= '<a name="'.$a.'"></a>';
+	}
+	
+}
+else
+{
+	$string = 'not an http or https URL on the current host';
+}
 echo $string;
-
 ?>
 <script type="text/javascript">
 window.addEventListener("load", function (event) {
 	window.parent.refresh_named_anchors_contd( document.getElementsByTagName('a'),
-											   '<?php echo addslashes($_REQUEST['selected_named_anchor']); ?>' )
+											   '<?php if(!empty($_REQUEST['selected_named_anchor'])) echo addslashes(htmlspecialchars($_REQUEST['selected_named_anchor'], ENT_QUOTES)); ?>' )
 } , true);
 </script>
