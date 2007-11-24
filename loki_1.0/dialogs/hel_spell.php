2eba43ff946aecb51fc1d0159287e00549e7becf
Matt Ryan <mryan@carleton.edu>
2007-11-24 21:01:39 +0000

    added documentation and sanitized inputs

--- loki_1.0/dialogs/hel_spell.php
+++ loki_1.0/dialogs/hel_spell.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package loki_1
+ * @subpackage hel
+ */
+/**
+ * Include reason libraries
+ */
 include_once("reason_header.php");
 
 header('Content-Type: application/vnd.mozilla.xul+xml');
@@ -68,7 +75,7 @@ FINIS;
 <![CDATA[
 
 try {
-	var editor_obj = opener.<?php echo $_REQUEST['editor_obj'] ?>;
+	var editor_obj = opener.<?php echo htmlspecialchars($_REQUEST['editor_obj'],ENT_QUOTES,'UTF-8'); ?>;
 	var sc;
 } catch(e) {}
 
