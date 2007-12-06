5d331dba26880457b8c4e3c00253e90e705ac18a
Matt Ryan <mryan@carleton.edu>
2007-12-06 20:15:29 +0000

    added documentation and patched security hole

--- loki_1.0/dialogs/lokiLoading.php
+++ loki_1.0/dialogs/lokiLoading.php
@@ -1,12 +1,18 @@
 <?php
-
+/**
+ * @package loki_1
+ * @subpackage loki
+ */
+/**
+ * set dialog
+ */
 if ( !empty($_REQUEST['dialog']) )
 	$inner = "lokiLoadingInner.php?dialog=" . urlencode($_REQUEST['dialog']);
 else
 	die('Please provide a <em>dialog</em> to be displayed.');
 
 if ( !empty($_REQUEST['window_title']) )
-	$window_title = $_REQUEST['window_title'];
+	$window_title = htmlspecialchars($_REQUEST['window_title'], ENT_QUOTES);
 else
 	die('Please provide a <em>window_title</em> to be displayed.');
 
