2e241375552684699f8cf341986de3fbeeae74f0
Nathan White <nwhite@carleton.edu>
2007-12-20 00:58:49 +0000

    added understanding of parameter limit_by_page_categories - when set to true, assets displayed will be only the assets that are related to the page and related to a category that is related to the page. The assets will be displayed under an h4 that denotes the category

--- reason_4.0/lib/core/minisite_templates/modules/assets.php
+++ reason_4.0/lib/core/minisite_templates/modules/assets.php
@@ -1,63 +1,121 @@
 <?php
 
 	reason_include_once( 'minisite_templates/modules/default.php' );
+	reason_include_once( 'function_libraries/asset_functions.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'AssetsModule';
 	
 	class AssetsModule extends DefaultMinisiteModule
 	{
 		var $es;
+		var $assets;
+		var $assets_by_category;
+		
 		var $acceptable_params = array(
 			'show_fields'=>array(),
 			'date_format'=>'',
+			'limit_by_page_categories'=>false,
 			'order'=>'',
 		);
+		
 		function init( $args = array() ) // {{{
 		{
+			$this->site = new entity($this->site_id);
 			$this->es = new entity_selector();
 			$this->es->description = 'Selecting assets for this page';
 			$this->es->set_env( 'site', $this->site_id);
 			$this->es->add_type( id_of('asset') );
-			$this->es->add_right_relationship( $this->parent->cur_page->id(), relationship_id_of('page_to_asset') );
+			$this->es->add_right_relationship( $this->page_id, relationship_id_of('page_to_asset') );
+			if ($this->params['limit_by_page_categories'])
+			{
+				$this->es->enable_multivalue_results();
+				$this->es->add_left_relationship_field('asset_to_category', 'entity', 'id', 'cat_id'); // grab category ids
+			}
 			if(!empty($this->params['order']))
 			{
 				$this->es->set_order( $this->params['order'] );
 			}
 			else
 			{
-				$this->es->add_rel_sort_field( $this->parent->cur_page->id(), relationship_id_of('page_to_asset'));
+				$this->es->add_rel_sort_field( $this->page_id, relationship_id_of('page_to_asset'));
 				$this->es->set_order( 'rel_sort_order' );
 			}
-			$this->assets = $this->es->run_one();
+			$result = $this->es->run_one();
+			
+			if ($result)
+			{
+				if ($this->params['limit_by_page_categories'])
+				{
+					$this->assets_by_category =& $this->init_by_category($result);
+				}
+				if (!$this->assets_by_category)
+				{
+					$this->assets =& $result;
+				}
+			}
 		} // }}}
+		
+		/**
+		 * Grab categories and for each page category, build a reference to a subset of the page assets
+		 */
+		function &init_by_category(&$page_assets)
+		{
+			$assets_by_category = false;
+			
+			$cat_es = new entity_selector($this->site_id);
+			$cat_es->set_env( 'site', $this->site_id );
+			$cat_es->add_type( id_of('category_type') );
+			$cat_es->limit_tables('entity');
+			$cat_es->limit_fields('entity.name');
+			$cat_es->add_right_relationship( $this->page_id, relationship_id_of('page_to_category') );
+			$cat_es->add_rel_sort_field( $this->page_id, relationship_id_of('page_to_category'));
+			$cat_es->set_order( 'rel_sort_order' );
+			$result = $cat_es->run_one();
+			
+			if ($result) 
+			{
+				$asset_ids = array_keys($page_assets);
+				$cat_ids = array_keys($result);
+				foreach ($asset_ids as $asset_id)
+				{
+					$item =& $page_assets[$asset_id];
+					$asset_cat_ids = (is_array($item->get_value('cat_id'))) ? $item->get_value('cat_id') : array($item->get_value('cat_id'));
+					$cat_intersect = array_intersect($asset_cat_ids, $cat_ids);
+					if (!empty($cat_intersect))
+					foreach ($cat_intersect as $cat_id)
+					$stack[$cat_id][$asset_id] =& $item;
+				}
+				foreach ($cat_ids as $cat_id)
+				{
+					if (isset($stack[$cat_id])) $assets_by_category[$cat_id] =& $stack[$cat_id];
+				}
+			}
+			else
+			{
+				
+			}
+			return $assets_by_category;
+		}
+		
 		function has_content() // {{{
 		{
-			if( $this->assets )
-				return true;
-			else
-				return false;
+			if( $this->assets || $this->assets_by_category) return true;
+			else return false;
 		} // }}}
 		function run() // {{{
 		{
-			$site = new entity( $this->parent->site_id );
-			reason_include_once( 'function_libraries/asset_functions.php' );
-			if(!empty($this->params['show_fields']) && !empty($this->params['date_format']))
+			$markup = '';
+			
+			if ($this->assets_by_category)
 			{
-				$markup = make_assets_list_markup( $this->assets, $site, $this->params['show_fields'], $this->params['date_format'] );
+				foreach ($this->assets_by_category as $category_id=>$assets)
+				{
+					$category = new entity( $category_id );
+					$markup .= '<h4>' . $category->get_value('name') . '</h4>';
+					$markup .= $this->get_asset_markup($assets);
+				}
 			}
-			elseif(!empty($this->params['show_fields']))
-			{
-				$markup = make_assets_list_markup( $this->assets, $site, $this->params['show_fields'] );
-			}
-			elseif(!empty($this->params['date_format']))
-			{
-				trigger_error('assetsModule::run(): the show_fields parameter must be passed to the assetsModule for the date_format parameter to work');
-				$markup = make_assets_list_markup( $this->assets, $site );
-			}
-			else
-			{
-				$markup = make_assets_list_markup( $this->assets, $site );
-			}
+			else $markup = $this->get_asset_markup($this->assets);
 			
 			if(!empty($markup))
 			{
@@ -67,6 +125,29 @@
 				echo '</div>'."\n";
 			}
 		} // }}}
+		
+		function get_asset_markup($assets)
+		{
+			if(!empty($this->params['show_fields']) && !empty($this->params['date_format']))
+			{
+				$markup = make_assets_list_markup( $assets, $this->site, $this->params['show_fields'], $this->params['date_format'] );
+			}
+			elseif(!empty($this->params['show_fields']))
+			{
+				$markup = make_assets_list_markup( $assets, $this->site, $this->params['show_fields'] );
+			}
+			elseif(!empty($this->params['date_format']))
+			{
+				trigger_error('assetsModule::run(): the show_fields parameter must be passed to the assetsModule for the date_format parameter to work');
+				$markup = make_assets_list_markup( $assets, $this->site );
+			}
+			else
+			{
+				$markup = make_assets_list_markup( $assets, $this->site );
+			}
+			return $markup;
+		}
+		
 		function last_modified()
 		{
 			if( $this->has_content() )
