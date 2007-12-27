d23cb5c80c5c9fd2a2093306337f66286b3ecd62
Nathan White <nwhite@carleton.edu>
2007-12-27 21:38:25 +0000

    added functions to support multiple quotes, grabbing of quotes in rel_sort_order

--- reason_4.0/lib/core/classes/quote_helper.php
+++ reason_4.0/lib/core/classes/quote_helper.php
@@ -6,9 +6,9 @@ reason_include_once( 'classes/object_cache.php' );
 /**
  * Quote helper
  *
- * Retrives unused quotes from the cache and database according to site_id, page_id, category_ids.
+ * Retrives unused quotes according to site_id and page_id
  *
- * Can be setup to exclude unavailable_quotes from the set of quotes returned.
+ * Can be setup to exclude unavailable_quotes from the set of quotes returned and to use a cache.
  *
  * Utilized by the quote module and the quote_retrieval script.
  *
@@ -32,29 +32,37 @@ reason_include_once( 'classes/object_cache.php' );
  {
 	var $site_id;
 	var $page_id;
+	
 	var $unavailable_quote_ids = array();
-	var $ignore_categories = false;
+	var $page_category_mode = false;
+	var $cache_lifespan = 0;
 	
 	var $quote;
 	var $quote_pool;
 	
- 	function QuoteHelper($site_id = NULL, $page_id = NULL, $unavailable_quote_ids = NULL)
+ 	function QuoteHelper($site_id = NULL, $page_id = NULL, $unavailable_quote_ids = NULL, $page_category_mode = NULL, $cache_lifespan = NULL)
  	{
  		if (isset($site_id)) $this->set_site_id($site_id);
  		if (isset($page_id)) $this->set_page_id($page_id);
  		if (isset($unavailable_quote_ids)) $this->set_unavailable_quote_ids($unavailable_quote_ids);
+ 		if (isset($page_category_mode)) $this->set_page_category_mode($page_category_mode);
+ 		if (isset($cache_lifespan)) $this->set_cache_lifespan($cache_lifespan);
  	}
  	
  	function init()
  	{
  		$this->init_from_cache();
-		if (empty($this->quote_pool) && ($this->quote_pool === false)) $this->init_from_database(); // there is no cache
+		if (!isset($this->quote_pool)) $this->init_from_database(); // there is no cache
  	}
  	
  	function init_from_cache()
 	{
-		$cache = new ReasonObjectCache($this->get_cache_id(), 86400);
-		$this->quote_pool =& $cache->fetch();
+		$cache_lifespan = $this->get_cache_lifespan();
+		if ($cache_lifespan > 0)
+		{
+			$cache = new ReasonObjectCache($this->get_cache_id(), $this->get_cache_lifespan());
+			$this->quote_pool =& $cache->fetch();
+		}
 	}
 	
 	function init_from_database()
@@ -66,17 +74,44 @@ reason_include_once( 'classes/object_cache.php' );
  			$es->limit_tables(array('meta'));
  			$es->limit_fields('meta.description');
  			$es->add_right_relationship( $this->page_id, relationship_id_of('page_to_quote') );
- 			if (!$this->ignore_categories) $this->_limit_by_category($es);
- 			$this->quote_pool = $es->run_one();
+ 			$es->add_rel_sort_field( $this->page_id, relationship_id_of('page_to_quote'), 'rel_sort_order');
+ 			$es->set_order( 'rel_sort_order ASC' );
+ 			$result = $es->run_one();
+ 			$result_array = ($result) ? $result : array();
+ 			$extra_results = $this->init_from_categories($result);
+ 			$this->quote_pool = $result + $extra_results;
  			$this->set_cache();
  		}
  		else
  		{
- 			trigger_error('The page_id and site_id must be available to determine category ids');
+ 			trigger_error('The page_id and site_id must be available to find quotes');
  		}
  	}
  	
- 	function &get_quotes()
+ 	function init_from_categories(&$already_selected)
+	{
+		if ($this->page_category_mode)
+		{
+			$cat_es = new entity_selector($this->site_id);
+			$cat_es->add_type( id_of('category_type') );
+			$cat_es->limit_tables();
+			$cat_es->limit_fields();
+			$cat_es->add_right_relationship ($this->page_id, relationship_id_of( 'page_to_category' ) );
+			$cat_result = $cat_es->run_one();
+			if (!empty($cat_result))
+			{
+				$es = new entity_selector($this->site_id);
+				$es->add_type( id_of('quote_type') );
+				$es->set_env('site', $this->site_id);
+				$es->add_left_relationship_field( 'quote_to_category', 'entity', 'id', 'cat_id', array_keys($cat_result));
+				if (!empty($already_selected)) $es->add_relation('entity.id NOT IN ('.implode(array_keys($already_selected)).')');
+				$result = $es->run_one();
+			}
+		}
+		return (!empty($result)) ? $result : array();
+	}
+ 	
+ 	function &get_quote_pool()
  	{
  		if (isset($this->quote_pool))
 		{
@@ -90,7 +125,7 @@ reason_include_once( 'classes/object_cache.php' );
  	
  	function get_unavailable_quotes()
  	{
- 		$quotes =& $this->get_quotes();
+ 		$quotes =& $this->get_quote_pool();
  		foreach ($this->unavailable_quote_ids as $id)
  		{
  			$unavailable[$id] =& $quotes[$id];
@@ -100,7 +135,7 @@ reason_include_once( 'classes/object_cache.php' );
  	
  	function get_available_quotes()
  	{
- 		$quotes =& $this->get_quotes();
+ 		$quotes =& $this->get_quote_pool();
  		$available_ids = array_diff(array_keys($quotes), $this->unavailable_quote_ids);
  		foreach ($available_ids as $id)
  		{
@@ -109,9 +144,40 @@ reason_include_once( 'classes/object_cache.php' );
  		return (!empty($available)) ? $available : false;
  	}
  	
+ 	function &get_quotes($num, $rand_flag = false)
+ 	{
+ 		if ($rand_flag)
+ 		{
+ 			return $this->get_random_quotes($num);
+ 		}
+ 		else
+ 		{
+ 			$index = 0;
+ 			$quotes =& $this->get_quote_pool();
+ 			foreach ($quotes as $k=>$v)
+ 			{
+ 				if ($index == $num) break;
+ 				$quote_set[$k] =& $quotes[$k];
+ 				$index++;
+ 			}
+ 			return $quote_set;
+ 		}
+ 	}
+ 	function &get_random_quotes($num)
+ 	{
+ 		for ($i=0; $i<$num; $i++)
+ 		{
+ 			$quote =& $this->get_random_quote();
+ 			$id = $quote->id();
+ 			$this->set_unavailable_quote_id($id);
+ 			$quotes[$id] =& $quote;
+ 		}
+ 		return $quotes;
+ 	}
+ 	
  	function &get_random_quote()
  	{
- 		$quotes =& $this->get_quotes();
+ 		$quotes =& $this->get_quote_pool();
  		if (!empty($quotes))
  		{
  			$available_quotes = $this->get_available_quotes();
@@ -144,26 +210,35 @@ reason_include_once( 'classes/object_cache.php' );
  	{
  		$this->page_id = $page_id;
  	}
- 		
- 	function set_categories_ids($cat_ids)
- 	{
- 		$this->category_ids = $cat_ids;
- 	}
  	
- 	function set_ignore_categories($boolean)
+ 	function set_page_category_mode($val)
  	{
- 		$this->ignore_categories = $boolean;
+ 		$this->page_category_mode = ($val);
  	}
 
  	function set_unavailable_quote_ids($unavailable_quote_ids)
  	{
  		$this->unavailable_quote_ids = $unavailable_quote_ids;
  	}
+ 	
+ 	function set_unavailable_quote_id($unavailable_quote_id)
+ 	{
+ 		$this->unavailable_quote_ids[] = $unavailable_quote_id;
+ 	}
 		
 	function set_cache()
 	{
-		$cache = new ReasonObjectCache($this->get_cache_id());
-		$cache->set($this->quote_pool);
+		if ($this->get_cache_lifespan() > 0)
+		{
+			$cache = new ReasonObjectCache($this->get_cache_id());
+			$cache->set($this->quote_pool);
+		}
+	}
+	
+	function set_cache_lifespan($seconds)
+	{
+		$ls = turn_into_int($seconds);
+		$this->cache_lifespan = $seconds;
 	}
 		
 	function get_cache_id()
@@ -171,25 +246,15 @@ reason_include_once( 'classes/object_cache.php' );
 		return md5('quote_cache_site_' . $this->site_id . '_page_' . $this->page_id);
 	}
 	
-	function _limit_by_category(&$es)
+	function get_cache_lifespan()
 	{
-		if (!isset($this->category_ids))
-		{
-			$cat_es = new entity_selector($this->site_id);
-			$cat_es->add_type( id_of('category_type') );
-			$cat_es->limit_tables();
-			$cat_es->limit_fields();
-			$cat_es->add_right_relationship ($this->page_id, relationship_id_of( 'page_to_category' ) );
-			$cat_result = $cat_es->run_one();
-			if (!empty($cat_result))
-			{
-				$this->set_category_ids = array_keys($cat_result);
-			}
-		}
-		if (!empty($this->category_ids))
-		{
-			$es->add_left_relationship_field( 'quote_to_category', 'entity', 'id', 'cat_id', $this->category_ids);
-		}
+		return $this->cache_lifespan;
+	}
+	
+	function clear_cache()
+	{
+		$cache = new ReasonObjectCache($this->get_cache_id());
+		$cache->clear();
 	}
  }
  
