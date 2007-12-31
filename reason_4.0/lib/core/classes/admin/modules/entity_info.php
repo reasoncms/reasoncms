a2684146521d641bf8bcb6b6efc6d2ca31959288
Matt Ryan <mryan@carleton.edu>
2007-12-31 17:57:29 +0000

    module for providing basic info about an entity, regardless of site

---
+++ reason_4.0/lib/core/classes/admin/modules/entity_info.php
@@ -0,0 +1,44 @@
+<?php
+	reason_include_once('classes/admin/modules/default.php');
+	class EntityInfoModule extends DefaultModule// {{{
+	{
+		function EntityInfoModule( &$page ) // {{{
+		{
+			$this->admin_page =& $page;
+		} // }}}
+		function init() // {{{
+		{
+			$this->admin_page->title = 'Get Basic Entity Information';
+		} // }}}
+		function run() // {{{
+		{
+			if(!user_is_a($this->admin_page->user_id, id_of('admin_role')))
+			{
+				echo '<p>Sorry; only admin users can get entity information.</p>'."\n";
+				return;
+			}
+			if(!empty($this->admin_page->request['entity_id_test']))
+			{
+				$id = $this->admin_page->request['entity_id_test'];
+				settype($id, 'integer');
+			}
+			echo '<form method="get" action="?"><label for="entity_id_test">Entity ID:</label> <input type="text" name="entity_id_test" id="entity_id_test" value="'.$id.'"/><input type="submit" value="submit" /><input type="hidden" name="cur_module" value="EntityInfo" /></form>';
+			if(!empty($id))
+			{
+				$entity = new entity($id);
+				if($entity->get_values())
+				{
+					$owner = $entity->get_owner();
+					echo '<p>Owner site: '.$owner->get_value('name').' (ID: '.$owner->id().')</p>'."\n";
+					echo '<p>Entity name: '.$entity->get_display_name().'</p>'."\n";
+					echo '<p>Entity data:</p>';
+					pray($entity->get_values());
+				}
+				else
+				{
+					echo '<p>The Reason ID '.$id.' does not belong to a real entity. It may have been deleted.</p>';
+				}
+			}
+		} // }}}
+	} // }}}
+?>
