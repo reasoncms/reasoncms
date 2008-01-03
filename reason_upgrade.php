b6226d906cb755aaca49a2f92d9038b713f0856c
Nathan White <nwhite@carleton.edu>
2008-01-03 23:38:23 +0000

    added link to Reason beta 5 to reason beta 6 upgrade scripts

--- reason_upgrade.php
+++ reason_upgrade.php
@@ -1,5 +1,6 @@
 <h1>Upgrade Reason</h1>
 <ul>
+<li><a href="./reason_4.0/www/scripts/upgrade/4.0b5_to_4.0b6">Reason Beta 5 to Reason Bate 6</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b4_to_4.0b5">Reason Beta 4 to Reason Beta 5</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b3_to_4.0b4">Reason Beta 3 to Reason Beta 4</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b1_and_b2_to_4.0b3">Reason Beta 1 or Beta 2 to Reason Beta 3</a></li>
