7ab2c1bb7963c5d4a25a40c36aee7d463e0cc417
Nathan White <nwhite@carleton.edu>
2008-01-21 23:52:05 +0000

    added ntoes on new settings

--- reason_4.0/lib/core/scripts/upgrade/4.0b5_to_4.0b6/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b5_to_4.0b6/index.php
@@ -17,7 +17,26 @@
 </head>
 <body>
 <h2>Upgrade Reason from 4.0 Beta 5 to 4.0 Beta 6</h2>
+<h3>Settings Changes</h3>
+<p>There are several new settings and changes to the settings files. Use the new settings files included with the Reason 4 Beta 6 package or add/update the
+following settings:
+<h4>package_settings.php</h4>
+<ul>
+<li>LOKI_2_INC - changed from INCLUDE_PATH.LOKI_2_DIRNAME.'/' to INCLUDE_PATH.LOKI_2_DIRNAME.'/helpers/php/' to reflect updated Loki 2 directory structure</li>
+<li>JQUERY_DIRNAME, JQUERY_INC, JQUERY_HTTP_PATH, JQUERY_URL: <p>Add the following lines to package_settings.php to setup jQuery constants:</p>
+<ul>
+<li>define('JQUERY_DIRNAME', 'jquery');</li>
+<li>define('JQUERY_INC',INCLUDE_PATH.JQUERY_DIRNAME.'/');</li>
+<li>define('JQUERY_HTTP_PATH',REASON_PACKAGE_WEB_AVAILABLE_HTTP_PATH.JQUERY_DIRNAME.'/');</li>
+<li>define('JQUERY_URL',JQUERY_HTTP_PATH.'jquery_latest.js');</li>
+</ul></li>
+</ul>
+<h4>reason_settings.php</h4>
+<ul>
+<li>REASON_DEFAULT_TIMEZONE - setting should be defined as a <a href="http://www.php.net/manual/en/timezones.php">php timezone</a>. Needed only for php5 installations.</li>
+</ul>
 <h3>Scripts to Run</h3>
+<p>The following scripts should be run to upgrade a Reason 4 Beta 5 database to Reason 4 Beta 6. If you setup Reason with the database distributed with Reason 4 Beta 6 you do not need to run these scripts.</p>
 <ul>
 <li><a href="publications.php">Upgrade publications framework</a></li>
 <li><a href="thor_db_structure_fix.php">Update thor tables</a></li>
