481802bd08da7dd53525bdeb347abd2d96d233e9
Matt Ryan <mryan@carleton.edu>
2008-02-13 00:14:00 +0000

    stubs for beta 6 => beta 7 upgrades

---
+++ reason_4.0/www/scripts/upgrade/4.0b6_to_4.0b7/index.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b6_to_4.0b7 upgrade script index
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b6_to_4.0b7/index.php' );
+?>
