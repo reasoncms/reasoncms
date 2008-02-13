bec1d162cac1edf82f14bb4d5e3ebeab646cb5a7
Matt Ryan <mryan@carleton.edu>
2008-02-13 00:13:12 +0000

    new scripts for beta6 to beta7 upgrades

---
+++ reason_4.0/lib/core/scripts/upgrade/4.0b6_to_4.0b7/index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Upgrade Reason from 4.0 beta 6 to beta 7
+ *
+ * @package reason
+ * @subpackage scripts
+ */
+
+/**
+ * Start script
+ */
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Upgrade Reason from 4.0 Beta 6 to 4.0 Beta 7</title>
+</head>
+<body>
+<h2>Upgrade Reason from 4.0 Beta 6 to 4.0 Beta 7</h2>
+<h3>Scripts to Run</h3>
+<p>The following scripts should be run to upgrade a Reason 4 Beta 6 database to Reason 4 Beta 7. If you setup Reason with the database distributed with Reason 4 Beta 6 you do not need to run these scripts.</p>
+<ul>
+<li><a href="site_parent_rel.php">Upgrade parent-child site relationship</a></li>
+<li><a href="new_themes.php">Add new themes</a></li>
+</ul>
+</body>
+</html>
