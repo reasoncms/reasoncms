3e38c3161f417815e9ff239303a2f8328b5b726f
Matt Ryan <mryan@carleton.edu>
2008-02-21 20:06:59 +0000

    upgrade script for custom footers

---
+++ reason_4.0/www/scripts/upgrade/4.0b6_to_4.0b7/custom_footer.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b6_to_4.0b7 upgrade script custom_footers.php
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b6_to_4.0b7/custom_footer.php' );
+?>
