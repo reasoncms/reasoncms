cc28dc363491584c3a3a58710d9ae6e2c0c88b68
Matt Ryan <mryan@carleton.edu>
2008-04-05 16:27:40 +0000

    switched to privs-based permissions

--- reason_4.0/lib/core/scripts/stats/stats.php
+++ reason_4.0/lib/core/scripts/stats/stats.php
@@ -40,9 +40,9 @@ if (defined('UNIVERSAL_CSS_PATH') && UNIVERSAL_CSS_PATH != '')
 	reason_include_once( 'function_libraries/user_functions.php' );
 	force_secure_if_available();
 	$current_user = check_authentication();
-	if (!user_is_a( get_user_id ( $current_user ), id_of('admin_role') ) )
+	if (!reason_user_has_privs( get_user_id ( $current_user ), 'view_sensitive_data' ) )
 	{
-		die('<h1>Sorry.</h1><p>You do not have permission to view page type info.</p><p>Only Reason users who have the Administrator role may do that.</p></body></html>');
+		die('<p>You do not have permission to view Reason stats.</p><p>Only Reason users who have sensitive data viewing privileges may do that.</p></body></html>');
 	}
 	
 	connectDB( REASON_DB );
