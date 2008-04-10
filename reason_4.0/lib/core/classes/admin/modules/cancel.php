bd11d43c79842a60f48f056289a3e7073dfeb8a1
Matt Ryan <mryan@carleton.edu>
2008-04-10 16:41:15 +0000

    added privs check, simplified some logic, and added a check so that items get deleting only if their current state is pending

--- reason_4.0/lib/core/classes/admin/modules/cancel.php
+++ reason_4.0/lib/core/classes/admin/modules/cancel.php
@@ -11,21 +11,14 @@
 		{
 			if( !empty( $this->admin_page->id ) )
 			{
-				/* figure out if this is a new entity and store the answer */
-				
 				$temp = new entity( $this->admin_page->id,false );
-				if( $temp->get_value( 'new' ) )
-					$this->new_entity = true;
-				else
-					$this->new_entity = false;
-
-			if( $this->new_entity )
-				delete_entity( $this->admin_page->id );
+				if( $temp->get_value( 'new' ) && $temp->get_value( 'state' ) == 'Pending' && reason_user_has_privs($this->admin_page->user_id,'delete_pending') )
+				{
+					delete_entity( $this->admin_page->id );
+				}
 			}
-			
 			if( !empty( $this->admin_page->request[ CM_VAR_PREFIX.'type_id' ] ) )
 			{
-				// associate this new entity with the original entity if this is new
 				$old_vars = array();	
 				foreach( $this->admin_page->request AS $key => $val )
 					if( substr( $key, 0, strlen( CM_VAR_PREFIX ) ) == CM_VAR_PREFIX )
