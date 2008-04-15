5a6f0895630b4e345aa495ddd34e6edbc6107f5e
Nathan White <nwhite@carleton.edu>
2008-04-15 21:45:26 +0000

    stub for publicator migrator wizard

---
+++ reason_4.0/www/scripts/developer_tools/publication_migrator.php
@@ -0,0 +1,8 @@
+<?php
+	/**
+	 * Stub for the developer publication_migrator script
+	 * No additional code should be placed in this file
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/developer_tools/publication_migrator.php' );
+?>
