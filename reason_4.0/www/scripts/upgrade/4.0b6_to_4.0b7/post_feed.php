de8146a091f9307bc5c309d4a9905b4a94497297
Nathan White <nwhite@carleton.edu>
2008-07-11 23:51:47 +0000

    upgrade script for feed changes

---
+++ reason_4.0/www/scripts/upgrade/4.0b6_to_4.0b7/post_feed.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b6_to_4.0b7 upgrade script post_feed.php
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b6_to_4.0b7/post_feed.php' );
+?>
