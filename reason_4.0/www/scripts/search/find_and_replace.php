2ede50661eea263c9d69c393f9a7584d2a4ef6b6
Nathan White <nwhite@carleton.edu>
2008-08-07 19:53:50 +0000

    first commit stub for find and replace script

---
+++ reason_4.0/www/scripts/search/find_and_replace.php
@@ -0,0 +1,8 @@
+<?php
+	/**
+	 * Stub for the Find And Replace script
+	 * No additional code should be placed in this file
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/search/find_and_replace.php' );
+?>
