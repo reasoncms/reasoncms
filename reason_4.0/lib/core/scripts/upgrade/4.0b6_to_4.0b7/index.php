d6e73497f014a6ad58b4e4791c4f606ac1f61f25
Nathan White <nwhite@carleton.edu>
2008-08-14 20:09:19 +0000

    added link to upgrade thor forms

--- reason_4.0/lib/core/scripts/upgrade/4.0b6_to_4.0b7/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b6_to_4.0b7/index.php
@@ -30,6 +30,7 @@
 <li><a href="site_parent_rel.php">Upgrade parent-child site relationship</a></li>
 <li><a href="new_themes.php">Add new themes</a></li>
 <li><a href="post_feed.php">Upgrade news feed generator</a></li>
+<li><a href="forms.php">Upgrade thor form infrastructure</a></li>
 <li><a href="kill_cruft.php">Kill database cruft</a></li>
 </ul>
 <h3>Scripts to Maybe Run</h3>
