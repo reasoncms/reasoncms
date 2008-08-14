058380db74939cf3ace29862434c80706107aa54
Nathan White <nwhite@carleton.edu>
2008-08-14 20:02:25 +0000

    updated ThorAdmin and DiscoThorAdmin to use ThorCore

--- thor/thor_admin.php
+++ thor/thor_admin.php
@@ -8,262 +8,281 @@
  *
  * Provides a view of thor form data with filtering, and view/edit/delete row action support
  *
+ * This class is a bit dirty - but works - table admin should accept data model objects or something instead of needing extension
+ *
+ * @todo add support for data models to table admin and deprecate me
  */
 include_once ( 'paths.php' );
 include_once ( CARL_UTIL_INC . 'db/table_admin.php' );
+include_once( THOR_INC . 'thor.php');
+include_once( TYR_INC . 'tyr.php');
 
 class ThorAdmin extends TableAdmin
 {
+	var $_thor_core;
+	
 	/**
 	 * @var array extra fields to display that are not actually thor elements described in xml
 	 * @todo we should grab this from the ThorCore probably
 	 */
-	var $extra_fields = array('id', 'submitted_by', 'submitter_ip', 'date_created');
+	var $extra_fields = array('id', 'submitted_by', 'submitter_ip', 'date_created', 'date_modified');
 	
 	function ThorAdmin()
 	{
 	}
 
-	function init_thor_admin($xml, $table_name)
+	function set_thor_core(&$thor_core)
 	{
-		$this->set_xml($xml);
-		$this->set_db_conn(THOR_FORM_DB_CONN);
-		$this->set_table_name($table_name);
-		$this->set_filename_frag($table_name);
-		
-		// grab information from the table and setup display value array
-		if ($this->_check_table_exists())
+		$this->_thor_core =& $thor_core;
+	}
+	
+	function &get_thor_core()
+	{
+		return $this->_thor_core;
+	}
+	
+	function init_thor_admin($thor_core = '')
+	{
+		if (empty($thor_core)) $thor_core =& $this->get_thor_core();
+		if (is_object($thor_core))
 		{
-			$this->thor_build_display_values();
-			
-			$this->thor_pass_display_names_to_admin_form();
-			
-			// grab the request and set parameters accordingly
-			$this->_set_params_from_request();
+			$this->set_db_conn(THOR_FORM_DB_CONN);
+			$this->set_table_name($thor_core->get_thor_table());
+			$this->set_filename_frag($thor_core->get_thor_table());
 		
-			// call appropriate init
-			if (isset($this->table_action)) $this->init_action();
-			elseif (isset($this->table_row_action) && isset($this->table_action_id) && $this->verify_table_action_id()) $this->init_row_action();
-			else $this->init_default();
+			// grab information from the table and setup display value array
+			if ($this->_check_table_exists())
+			{
+				$this->thor_build_display_values();
+			
+				// grab the request and set parameters accordingly
+				$this->_set_params_from_request();
+		
+				// call appropriate init
+				if (isset($this->table_action)) $this->init_action();
+				elseif (isset($this->table_row_action) && isset($this->table_action_id) && $this->verify_table_action_id()) $this->init_row_action();
+				else $this->init_default();
+			}
 		}
-	}
-	
-	function set_xml($xml)
-	{
-		$this->_xml = $xml;
-	}
-	
-	function get_xml()
-	{
-		return $this->_xml;
+		else trigger_error('ThorAdmin needs to be provided with a thor_core object in order to run');
 	}
 	
 	function thor_build_display_values()
 	{
-		$xml = $this->get_xml();
-		$form_xml_obj = new XMLParser($xml);
-		$form_xml_obj->Parse();
-		$display_values = array();
-		foreach ($form_xml_obj->document->tagChildren as $k=>$v)
-		{
-			$tagname = is_object($v) ? $v->tagName : '';
-			if (method_exists($this, '_build_display_'.$tagname))
-			{
-				$build_function = '_build_display_'.$tagname;
-				$display_values = array_merge($display_values, $this->$build_function($v));
-			}
-		}
-		foreach ($this->extra_fields as $field_name)
-		{
-			$display_values[$field_name]['label'] = prettify_string($field_name);
-			$display_values[$field_name]['type'] = 'text';
-		}
-		$this->_display_values = (isset($display_values)) ? $display_values : array();
-	}
-	
-	function thor_pass_display_names_to_admin_form()
-	{
-		$form =& $this->get_admin_form();
-		if (!$form) $form = new DiscoDefaultAdmin();
-		foreach ($this->_display_values as $k=>$v)
-		{
-			$field_display_names[$k] = $v['label'];
-		}
-		$form->field_display_names = $field_display_names;
-	}
-	
-	function _delete_data()
-	{
-		// connect with table database defined in settings.php3
-		connectDB($this->get_db_conn());
-		$q = 'DROP TABLE ' . $this->get_table_name();
-		$res = mysql_query( $q ) or mysql_error();//or trigger_error( 'Error: mysql error in Thor Data delete - URL ' . get_current_url() . ': '.mysql_error() );
-  		connectDB($this->get_orig_db_conn());
-		return $res;
+		// thor core method
+		$tc =& $this->get_thor_core();
+		$this->_display_values =& $tc->get_display_values();
 	}
 	
 	/**
-	 * Helper functions for _build_display_values()
-	 * @todo these should be in thor core I think
-	 * @access private
+	 * TableAdmin should take an optional data model instead of overloading a private method ...
 	 */
-	 
-	function _build_display_input($element_obj)
+	function _delete_data()
 	{
-		$element_attrs = $element_obj->tagAttrs;
-		$type = 'input';
-		$display_values[$element_attrs['id']] = array('label' => $element_attrs['label'], 'type' => $type);
-		return $display_values;
-	}
-
-	function _build_display_hidden($element_obj)
-	{
-		$element_attrs = $element_obj->tagAttrs;
-		$type = 'hidden';
-		$display_values[$element_attrs['id']] = array('label' => $element_attrs['label'], 'type' => $type);
-		return $display_values;
-	}
- 
-	function _build_display_textarea($element_obj)
-	{
-		$element_attrs = $element_obj->tagAttrs;
-		$type = 'textarea';
-		$display_values[$element_attrs['id']] = array('label' => $element_attrs['label'], 'type' => $type);
-		return $display_values;
-	}
-
-	function _build_display_checkboxgroup($element_obj)
-	{
-		$element_children = $element_obj->tagChildren;
-		$type = 'checkbox';
-		foreach ($element_children as $element_child) 
-		{
-			$child_attrs = $element_child->tagAttrs;
-			$label = $child_attrs['label'];
-			$display_values[$child_attrs['id']] = array('label' => $label, 'type' => $type);
-		}
-		return $display_values;
+		$tc =& $this->get_thor_core();
+		return $tc->delete_table();
 	}
 	
-	function _build_display_radiogroup($element_obj)
-	{
-		$element_attrs = $element_obj->tagAttrs;
-		$element_children = $element_obj->tagChildren;
-		$id = $element_attrs['id'];
-		$label = $element_attrs['label'];
-		$type = 'radiogroup';
-		foreach ($element_children as $element_child)
-		{
-			$child_attrs =& $element_child->tagAttrs;
-			$options[] = $child_attrs['value'];
-		}
-		$display_values[$id] = array('label' => $label, 'type' => $type, 'options' => $options);
-		return $display_values;
-	}
 
-	function _build_display_optiongroup($element_obj)
-	{
-		$element_attrs = $element_obj->tagAttrs;
-		$element_children = $element_obj->tagChildren;
-		$id = $element_attrs['id'];
-		$label = $element_attrs['label'];
-		$type = 'optiongroup';
-		foreach ($element_children as $element_child)
-		{
-			$child_attrs =& $element_child->tagAttrs;
-			$options[] = $child_attrs['value'];
-		}
-		$display_values[$id] = array('label' => $label, 'type' => $type, 'options' => $options);
-		return $display_values;
-	}
 }
 
-class DiscoThorAdmin extends DiscoDefaultAdmin
-{
-	function DiscoThorAdmin()
+/**
+ * Default Thor Admin Form
+ *
+ * Provides basic view / edit / create / delete privileges on a thor table
+ *
+ * @author Nathan White
+ */
+
+ class DiscoThorAdmin extends Disco
+ {			
+ 	/**
+ 	 * The class requires a thor_core object
+ 	 */
+	var $_thor_core;
+
+	/**
+	 * The real or spoofed netid
+	 */
+	var $_user_netid;
+	
+	/**
+	 * Contains the current table action - if any
+	 */
+	var $table_action;
+	
+	/**
+	 * Contains the current table action id - if any
+	 */
+	var $table_action_id;
+	
+	/**
+	 * Contains the primary key column name
+	 */
+	var $primary_key_column;
+	
+	/**
+	 * Default sort field
+	 */
+	var $default_sort_field;
+	
+	/**
+	 * DiscoThorAdmin requires a reference to a thor_core object to work properly, get display names, etc
+	 */
+	function set_thor_core(&$thor_core)
 	{
+		$this->_thor_core =& $thor_core;
 	}
 	
-	//function authorize()
-	//{
-	//	return true;
-	//}
-	
-	//function set_form_id($id)
-	//{
-	//	$this->form_id = $id;
-	//}
-	
-	//function get_form_id()
-	//{
-	//	return $this->form_id;
-	//}
-	
-	function pre_show_form_edit()
+	function &get_thor_core()
 	{
-		$id = $this->get_id();
+		return $this->_thor_core;
+	}
+	
+	/**
+	 * DiscoThorAdmin requires a reference to a real or spoofed netid
+	 */
+	function set_user_netid($user_netid)
+	{
+		$this->_user_netid = $user_netid;
+	}
+	
+	function &get_user_netid()
+	{
+		return $this->_user_netid;
+	}
+	
+	// if we do not have a thor_core object, take it from thor_admin
+	function setup_form(&$thor_admin)
+	{
+		$this->set_action($thor_admin->get_table_row_action());
+		$this->set_action_id($thor_admin->get_table_action_id());
+		$this->set_primary_key_column($thor_admin->get_primary_key());
+		if (!$this->get_thor_core()) $this->set_thor_core($thor_admin->get_thor_core());
+		
+		$tc =& $this->get_thor_core();
+		$tc->append_thor_elements_to_form($this);
+		$tc->apply_values_for_primary_key_to_form($this, $this->get_action_id(), $this->get_primary_key_column());
+	}
+	
+	function on_every_time()
+	{
+		$action = $this->get_action();
+		$id = $this->get_action_id();
+		
+		if ($action == 'view')
+		{
+			$this->on_every_time_view();
+		}
+		elseif ($action == 'edit')
+		{
+			$this->on_every_time_edit();
+		}
+		elseif ($action == 'delete')
+		{
+			$this->on_every_time_delete();
+		}
+	}
+	
+	/**
+	 * Grab the data (minus id) from thor core and display as a list
+	 */
+	function on_every_time_view()
+	{
+		$id = $this->get_action_id();
+		$tc =& $this->get_thor_core();
+		echo '<h3>Viewing row id ' . $id . '</h3>';
+		$link = carl_make_link(array('table_row_action' => '', 'table_action_id' => ''));
+		echo '<p><a href="'.$link.'">Return to summary form data</a></p>';
+			
+		$data = $tc->get_output_data_for_primary_key($this->get_action_id(), 'id', array('id')); // exclude only the id field
+		if ($data)
+		{
+			// we are going to use Tyr to format this up though it is a little silly ... 
+			$tyr = new Tyr();
+			$html = $tyr->make_html_table($data, false);
+			echo $html;
+		}
+		else echo '<p>No data to display for this row</p>';
+		
+		// turn off display of the form
+		$this->show_form = false;
+	}
+	
+	function on_every_time_edit()
+	{
+		$id = $this->get_action_id();
 		echo '<h3>Editing row id ' . $id . '</h3>';
 		$link = carl_make_link(array('table_row_action' => '', 'table_action_id' => ''));
 		echo '<p><a href="'.$link.'">Return to summary form data</a></p>';
 	}
 	
-	function pre_show_form_delete()
-	{
-		$id = $this->get_id();
-		echo '<h3>Are you sure you want to delete row id ' . $id . '?</h3>';
-	}
-	
-	function pre_show_form_view()
-	{
-		$id = $this->get_id();
-		echo '<h3>Viewing row id ' . $id . '</h3>';
-		$link = carl_make_link(array('table_row_action' => '', 'table_action_id' => ''));
-		echo '<p><a href="'.$link.'">Return to summary form data</a></p>';
-	}
-	
-	function on_every_time()
-	{
-		$this->remove_element('id');
-		$this->setup_display_names();
-		parent::on_every_time();
-	}
-	
 	function on_every_time_delete()
 	{
+		$tc =& $this->get_thor_core();
+		$id = $this->get_action_id();
+		{
+			$elements = $this->get_element_names();
+			foreach ($elements as $element_name)
+			{
+				$this->remove_element($element_name);
+			}
+		}
+		echo '<h3>Are you sure you want to delete row id ' . $id . '?</h3>';
 		$this->actions = array('delete' => 'Confirm Delete', 'cancel' => 'Cancel');
-		$element_names = $this->get_element_names();
-		foreach ($element_names as $element)
+		$data = $tc->get_output_data_for_primary_key($this->get_action_id(), 'id', array('id')); // exclude only the id field
+		if ($data)
 		{
-			$this->change_element_type($element, 'solidtext');
+			// we are going to use Tyr to format this up though it is a little silly ... 
+			$tyr = new Tyr();
+			$html = $tyr->make_html_table($data, false);
+			echo $html;
 		}
+		else echo '<p>No data to display for this row</p>';
 	}
 	
-	function setup_display_names()
+	function run_error_checks()
 	{
-		$element_names = $this->get_element_names();
-		foreach ($element_names as $element)
-		{
-			if (isset($this->field_display_names[$element]));
-			$this->set_display_name($element, $this->field_display_names[$element]);
-		}
+		parent::run_error_checks();
 	}
 	
+	function process()
+	{
+		if ($this->get_action() == 'new') $this->process_new();
+		elseif ($this->get_action() == 'edit') $this->process_edit();
+		elseif ($this->get_action() == 'delete') $this->process_delete();
+	}		
+	function process_default() { parent::process(); }
+	
+	function process_new() 
+	{
+		$tc = $this->get_thor_core();
+		$values = $tc->get_thor_values_from_form($this);
+		$values['submitted_by'] = reason_check_authentication();
+		$values['submitter_ip'] = $_SERVER['REMOTE_ADDR'];
+		$values['date_created'] = get_mysql_datetime();
+		$tc->insert_values($values);
+	}
+	
+	/**
+	 * @todo invoke ThorCore to save edits
+	 */
+	function process_edit()
+	{ 
+		$tc = $this->get_thor_core();
+		$values = $tc->get_thor_values_from_form($this);
+		$tc->update_values_for_primary_key($this->get_action_id(), $values);
+	}
+	
+	/**
+	 * invoke ThorCore to delete the row
+	 */
 	function process_delete()
 	{
 		if ($this->chosen_action == 'delete')
 		{
-			$id_to_delete = $this->get_id();
-			$qry = 'DELETE FROM ' . $this->get_table_name() . ' WHERE '.$this->id_column_name.' = '.$id_to_delete;
-			$result = db_query($qry, 'The delete query failed');
-			
-			$qry = 'SELECT COUNT(*) as totalfound FROM ' . $this->get_table_name();
-			$result = db_query($qry, 'The count query failed');
-			$result = mysql_fetch_assoc($result);
-			if ($result['totalfound'] == 0)
-			{
-				$q = 'DROP TABLE ' . $this->get_table_name();
-				$res = mysql_query( $q ) or mysql_error();//or trigger_error( 'Error: mysql error in Thor Data delete - URL ' . get_current_url() . ': '.mysql_error() );
-			}
+			$tc = $this->get_thor_core();
+			$tc->delete_by_primary_key($this->get_action_id());
 		}
 	}
 	
@@ -271,6 +290,46 @@ class DiscoThorAdmin extends DiscoDefaultAdmin
 	{
 		$link = carl_make_redirect(array('table_row_action'=>'', 'table_action_id'=>''));
 		return $link;
+	}
+	
+	function set_primary_key_column($column)
+	{
+		$this->primary_key_column = $column;
+	}
+
+	function get_primary_key_column()
+	{
+		return $this->primary_key_column;
+	}
+	
+	function set_action($action)
+	{
+		$this->table_action = $action;
+	}
+
+	function set_action_id($action_id)
+	{
+		$this->table_action_id = $action_id;
+	}
+
+	function get_action()
+	{
+		return $this->table_action;
+	}
+	
+	function get_action_id()
+	{
+		return $this->table_action_id;
+	}
+	
+	function get_default_sort_order()
+	{
+		return $this->default_sort_order;
+	}
+	
+	function get_default_sort_field()
+	{
+		return $this->default_sort_field;
 	}
 }
 ?>
