9bea7cc7b88a61025eb9a58b80ef5ceab4c7289e
Nathan White <nwhite@carleton.edu>
2008-08-20 21:00:25 +0000

    fixed link to 4b6 to 4b7 updates

--- reason_upgrade.php
+++ reason_upgrade.php
@@ -1,6 +1,6 @@
 <h1>Upgrade Reason</h1>
 <ul>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b5_to_4.0b6">Reason Beta 6 to Reason Beta 7</a></li>
+<li><a href="./reason_4.0/www/scripts/upgrade/4.0b6_to_4.0b7">Reason Beta 6 to Reason Beta 7</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b5_to_4.0b6">Reason Beta 5 to Reason Beta 6</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b4_to_4.0b5">Reason Beta 4 to Reason Beta 5</a></li>
 <li><a href="./reason_4.0/www/scripts/upgrade/4.0b3_to_4.0b4">Reason Beta 3 to Reason Beta 4</a></li>
