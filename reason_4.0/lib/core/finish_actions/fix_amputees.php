31b2b69e1c41b7f5555d2d21ba0d91efffe6f216
Nathan White <nwhite@carleton.edu>
2008-08-20 20:43:33 +0000

    new finish action to fix_amputees - is run whenever a type is finished ... written because when an entity table to added to a type the amputees need to be fixed for things to work properly.

---
+++ reason_4.0/lib/core/finish_actions/fix_amputees.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This script contains a class fixes amputees for the current type.
+ */
+ 
+$GLOBALS['_finish_action_classes'][ basename( __FILE__) ] = 'fixAmputees';
+reason_include_once('finish_actions/default.php');
+reason_include_once('classes/url_manager.php');
+reason_include_once('classes/amputee_fixer.php');
+
+/**
+ * Fix Amputees finish action class
+ * Instantiates an amputee fixer and fixes amputees for the current type.
+ * @author Nathan White
+ * @date August 20, 2008
+ */
+class fixAmputees extends defaultFinishAction
+{
+	/**
+	 * fix the amputees
+	 */
+	function run()
+	{
+		$fixer = new AmputeeFixer();
+		$fixer->fix_amputees($this->vars['type_id']);
+	}
+}
+?>
