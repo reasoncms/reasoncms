a1a59de6baadf3a2efa8fd14d765fde82b2d91e9
Nathan White <nwhite@carleton.edu>
2008-09-30 19:39:24 +0000

    loki 2 feed of news items on publications - uses the sitewide news feed with no limits on number of items, and also uses a 3 minute cache

--- reason_4.0/lib/core/feeds/editor_links_for_news.php
+++ reason_4.0/lib/core/feeds/editor_links_for_news.php
@@ -1,33 +1,17 @@
 <?php
 
-/* This is the news feed for Loki */
+/* This is the news feed for Loki - it uses the sitewide news feed, but adds a 3 minute cache to help with sites 
+that have a large number of news items, and also does not limit the number of items shown */
 
 include_once( 'reason_header.php' );
-reason_include_once( 'feeds/news.php' );
+reason_include_once( 'feeds/sitewide_news.php' );
+reason_include_once( 'function_libraries/url_utils.php' );
 $GLOBALS[ '_feed_class_names' ][ basename( __FILE__, '.php' ) ] = 'editorNewsFeed';
 
-class editorNewsFeed extends newsFeed
+class editorNewsFeed extends sitewideNewsFeed
 {
-	function alter_feed()
-	{
-		if($this->site_specific)
-		{
-			$this->feed->set_item_field_map('title','id');
-			$this->feed->set_item_field_handler( 'title', 'make_title', true );
-			$this->feed->set_item_field_map('author','');
-			$this->feed->set_item_field_map('description','');
-			$this->feed->set_item_field_map('pubDate','');	
-			
-			$this->feed->es->add_relation( 'show_hide.show_hide = "show"' );
-			$this->feed->es->set_order( 'datetime DESC' );
-			$this->feed->es->add_relation( 'status.status != "pending"' );
-			$this->feed->es->set_num( 100000 );
-		}
-		else
-		{
-			$this->feed->es->add_relation( '1 = 2' );
-		}
-	}
+	var $num_to_display;
+	var $cache_lifespan = 180;
 }
 
 ?>
