51232a16a988a992679ce93443486d7a4978cd3a
Nathan White <nwhite@carleton.edu>
2008-10-01 21:57:42 +0000

    fixed typo referencing reason 4 beta 6 and also added some info on change to REASON_DEFAULT_ALLOWED_TAGS, and link to change log update

--- reason_4.0/lib/core/scripts/upgrade/4.0b6_to_4.0b7/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b6_to_4.0b7/index.php
@@ -22,8 +22,12 @@
    may need to be configured for your environment. The object caching system now supports caching using memcached or mysql, as well
    as caching to the file system. By default, the object cache is configured to use file system caching to the REASON_LOG_DIR (in the Reason
    environment, or to /tmp/ if the reason_header has not been loaded.</p>
+<p>The setting REASON_DEFAULT_ALLOWED_TAGS in previous distributions of Reason contained an odd assortment of tags. You may 
+want to update this setting in reason_settings.php to more closely match the set of XHTML compliant tags. More information can be found in an <a 
+href="https://apps.carleton.edu/opensource/reason/developers/changes/?story_id=399475">April 4, 2008 Reason change log post</a>.</p>
 <h3>Scripts to Run</h3>
-<p>The following scripts should be run to upgrade a Reason 4 Beta 6 database to Reason 4 Beta 7. If you setup Reason with the database distributed with Reason 4 Beta 6 you do not need to run these scripts.</p>
+<p>The following scripts should be run to upgrade a Reason 4 Beta 6 database to Reason 4 Beta 7. If you setup Reason with the database distributed 
+with Reason 4 Beta 7 you do not need to run these scripts.</p>
 <ul>
 <li><a href="page_access.php">Add the page access allowable relationship</a></li>
 <li><a href="custom_footer.php">Add capability to customize site footers</a></li>
