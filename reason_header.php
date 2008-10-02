53c9dc33e00f0b66e3d5052afc8b98524b48ebf5
Nathan White <nwhite@carleton.edu>
2008-10-02 20:56:22 +0000

    added output buffering to reason_header if not already started - fixes redirection breaks when no error handler settings are setup

--- reason_header.php
+++ reason_header.php
@@ -3,6 +3,7 @@
  * This file should be used to bring in the Reason libraries.
  * It must be included in any script execution for Reason to work.
  */
+	if (ob_get_level() == 0) ob_start();
 	include_once('paths.php');
 	include_once( REASON_INC.'header.php' );
 ?>
