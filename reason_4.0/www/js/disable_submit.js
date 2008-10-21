5e276cb03f946819e65fb244e95397989858c47d
Nathan White <nwhite@carleton.edu>
2008-10-21 20:03:42 +0000

    jquerified

--- reason_4.0/www/js/disable_submit.js
+++ reason_4.0/www/js/disable_submit.js
@@ -1,85 +1,76 @@
 /**
- * Resets ths submit state in case of a timeout
+ * Disable submit javascript - when a form is submitted, replaces the submit button with a "Please Wait" message.
  *
- * @param form_name the form to be reset
- */
-function reset_submit_state(form_name)
-{
-  var form = document.forms[form_name];
-  var inputs = form.getElementsByTagName("input");
-
-  for (var i=0; i < inputs.length; i++) {
-    if (inputs[i].type=="submit") {
-      enable(inputs[i]);
-    }
-    if (inputs[i].type=="hidden" && inputs[i].__replacement)
-      inputs[i].parentNode.removeChild(inputs[i]);
-  }
-}
-
-/**
- * Enables the submit button and removes the replacement
+ * Features include:
  *
- * @param submit_button the submit button to enable
- */
-function enable(submit_button)
-{
-  if (submit_button.__original) {
-    enabled_submit = submit_button.__original.cloneNode(true);
-    submit_button.parentNode.replaceChild(enabled_submit, submit_button);
-  }
-}
-
-/**
- * Disables the submit button in the form
+ * - forms can be specified with a name, id, or class
+ * - a reset time (in milliseconds) can optionally be provided
  *
- * @param event the onsubmit event which contains how the form was submitted
- * @param wait_time how long to disable to form (in milliseconds); defaults to 5000
- *  Usage: <form name="login_form" method="method" action="login_form" id="login_form" onsubmit="disable_submit(event, 1000)")>
- */
+ * @author Nathan White
+ * @todo ensure keypress submissions are properly handled (i think they are)
+ * @requires jQuery
+ */ 
 
-function disable_submit(event, wait_time)
+$(document).ready(function()
 {
-  var submitted_button = event.explicitOriginalTarget.name;
-  var form = event.target;
-  var wait_time = (wait_time == null) ? 5000 : wait_time;
-
-  var inputs = form.getElementsByTagName("input");
-  for (var i=0; i < inputs.length; i++) {
-    if (inputs[i].type=="submit") {
-      disable(inputs[i], submitted_button);
-    }
-  }
-  var buttons = form.getElementsByTagName("button");
-  for (var i=0; i < buttons.length; i++) {
-    disable(buttons[i], submitted_button);
-  }
-
-  setTimeout("reset_submit_state('" + form.name +"');", wait_time);
-}
-
-/**
- * Disable the submit button and creates a replacement
- *
- * @param submit_button the submit button to disable
- * @param submitted-button the name of the button used to submit the form
- */
-function disable(submit_button, submitted_button)
-{
-  if (submit_button.name == submitted_button) {
-    button_replacement = document.createElement("input");
-    button_replacement.type = "hidden";
-    button_replacement.__replacement = true;
-    button_replacement.name = submit_button.name;
-    button_replacement.value = submit_button.value;
-    submit_button.parentNode.appendChild(button_replacement);
-  }
-
-  disabled_button = document.createElement("input");
-  disabled_button.type = "submit";
-  disabled_button.disabled = true;
-  disabled_button.value = "Please wait...";
-
-  submit_button.parentNode.replaceChild(disabled_button, submit_button);
-  disabled_button.__original = submit_button;
-}
+	var js_src = $('script[src*=disable_submit]:first').attr("src");
+	
+	// we grab and validate user input
+	var valid_names = verifyString(queryString( "name", js_src ));
+	var valid_ids = verifyString(queryString( "id", js_src ));
+	var valid_classes = verifyString(queryString( "class", js_src ));
+	var valid_reset_time = parseInt(queryString ('reset_time', js_src), 10);
+	var selector = Array();
+	
+	if (valid_names) selector[selector.length] = $.map(valid_names.split(","), function (item) { return "form[name='" + item + "']"; }).toString();
+	if (valid_ids) selector[selector.length] = $.map(valid_ids.split(","), function (item) { return "form#" + item; }).toString();
+	if (valid_classes) selector[selector.length] = $.map(valid_classes.split(","), function (item) { return "form." + item; }).toString();
+	
+	if (selector.length > 0) $(selector.toString()).submit(function() // sets up a submit event for valid forms
+	{
+		if (valid_reset_time)
+		{
+			var buttons = new Array();
+			buttons["text"] = new Array();
+			buttons["refs"] = new Array();
+		}
+		
+		$("input[type=submit]", this).each(function(index)
+		{
+			if (valid_reset_time)
+			{
+				buttons["refs"][index] = $(this);
+				buttons["text"][index] = ($(this).attr("value"));
+				setTimeout( function() { enableSubmit(buttons["refs"][index], buttons["text"][index]); }, valid_reset_time);
+			}
+			disableSubmit(this, "Please wait...");
+		});
+	});
+	
+	function enableSubmit(button, value)
+	{
+		$(button).attr("value",value);
+		$(button).attr("disabled",false);
+	}
+	
+	function disableSubmit(button, value)
+	{
+		$(button).attr("value",value);
+		$(button).attr("disabled",true);
+	}
+	
+	function queryString( key, url )
+	{
+		if ( (url.search( RegExp( "[?&]" + key + "=([^&$]*)", "i" ) )) > -1 ) return RegExp.$1;
+		else return false;
+	}
+	
+	function verifyString( string )
+	{
+		if (string)
+		{
+			if (string.match(/^[a-z0-9,_-]*$/i)) return string;
+		}
+		return false;
+	}
+});
