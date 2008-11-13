d6050c17e5daf0b1389f24c36f896226b0a221a1
Nathan White <nwhite@carleton.edu>
2008-11-13 00:01:37 +0000

    first commit scripts for 4.0b7_to_4.0b8 upgrade

---
+++ reason_4.0/www/scripts/upgrade/4.0b7_to_4.0b8/index.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b7_to_4.0b8 upgrade script index
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b7_to_4.0b8/index.php' );
+?>
