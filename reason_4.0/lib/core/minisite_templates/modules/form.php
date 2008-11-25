e6bf6e85e1447100b7ae909a85bc7abeae09d115
Nathan White <nwhite@carleton.edu>
2008-11-25 19:02:28 +0000

    added support for admin_view in page type - also deprecated the thor_success method of form confirmation in favor of submission_key

--- reason_4.0/lib/core/minisite_templates/modules/form.php
+++ reason_4.0/lib/core/minisite_templates/modules/form.php
@@ -11,14 +11,15 @@
 	 * Reason Form Module - intended to be used to build interfaces around thor or custom forms, while maintaining backwards
 	 * compatibility with Reason's old thor form module.
 	 *
-	 * Common usage would involve instantiation of a model, view (optional), and controller. If no parameters are provided, 
-	 * then the default thor form model, controller, and view (as specified in the content manager) will be used.
+	 * Common usage would involve instantiation of a model, view (optional), admin_view (optional) and controller.
+	 *
+	 * If no parameters are provided, then the default thor form model, view, and admin_view, and controller will be used.
 	 *
 	 * The model is passed a reference to the module at the time of instantiation, so that head items, the cur_page object, 
 	 * or other items available to the module can be localized into the model. The controller handles cleanup rules and request
 	 * variables, just like a module would. Essentially, the controller serves as a sub-module.
 	 *
-	 * The controller itself will be provided the view if it is provided in a parameter, but the view is optional.
+	 * The controller itself will be provided the view and admin view if these are provided parameter, but they are optional.
 	 *
 	 * @author Nathan White
 	 */
@@ -31,6 +32,7 @@
 		var $acceptable_params = array('form_model' => false,
 									   'form_controller' => false,
 									   'form_view' => false,
+									   'form_admin_view' => false,
 									   'force_login' => false);
 		
 		function _init_legacy()
@@ -91,7 +93,10 @@
 			if (!isset($this->form_model))
 			{
 				$default_model_filename = (defined('REASON_FORMS_THOR_DEFAULT_MODEL')) ? REASON_FORMS_THOR_DEFAULT_MODEL : 'thor.php';
-				$model_filename = (!empty($this->params['form_model'])) ? $this->params['form_model'] : $default_model_filename;
+				if (!empty($this->params['form_model']) && is_array($this->params['form_model'])) $model_filename = $this->params['form_model']['model'];
+				elseif (!empty($this->params['form_model'])) $model_filename = $this->params['form_model'];
+				else $model_filename = $default_model_filename;
+				
 				if (reason_file_exists('minisite_templates/modules/form/models/'.$model_filename))
 				{
 					reason_include_once('minisite_templates/modules/form/models/'.$model_filename);
@@ -137,17 +142,19 @@
 				else trigger_error('The forms module was unable to load a controller - the controller_filename in get_form_controller is ' . $controller_filename, FATAL);
 				$model =& $this->get_form_model();
 				$view =& $this->get_form_view();
+				$admin_view =& $this->get_form_admin_view();
 				$controller_name = $GLOBALS['_form_controller_class_names'][basename($controller_filename, '.php')];
 				$controller = new $controller_name();
 				$controller->set_model($model);
 				if ($view) $controller->set_view($view);
+				if ($admin_view) $controller->set_admin_view($admin_view);
 				$this->form_controller =& $controller;
 			}
 			return $this->form_controller;
 		}
 		
 		/**
-		 * Get the form view from page type parameter if it exists - in many cases (like thor) the controller may handle view selection.		
+		 * Get the form view from page type parameter if it exists - in some cases (like thor) the controller may handle view selection.		
 		 */
 		function &get_form_view()
 		{
@@ -168,6 +175,7 @@
 					{
 						include_once($view_filename);
 					}
+					else trigger_error('The forms module was unable to load the view specified (' . $view_filename . ')', FATAL);
 					$view_name = $GLOBALS['_form_view_class_names'][basename($view_filename, '.php')];
 					$view = new $view_name();
 					$model =& $this->get_form_model();
@@ -177,6 +185,40 @@
 				$this->form_view =& $view;
 			}
 			return $this->form_view;
+		}
+		
+		/**
+		 * Get the form view from page type parameter if it exists - in some cases (like thor) the controller may handle view selection.		
+		 */
+		function &get_form_admin_view()
+		{
+			if (!isset($this->form_admin_view))
+			{
+				$admin_view_filename = (!empty($this->params['form_admin_view'])) ? $this->params['form_admin_view'] : false;
+				if ($admin_view_filename)
+				{
+					if (reason_file_exists('minisite_templates/modules/form/admin_views/'.$admin_view_filename))
+					{
+						reason_include_once('minisite_templates/modules/form/admin_views/'.$admin_view_filename);
+					}
+					elseif (reason_file_exists($admin_view_filename))
+					{
+						reason_include_once($admin_view_filename);
+					}
+					elseif (file_exists($admin_view_filename))
+					{
+						include_once($admin_view_filename);
+					}
+					else trigger_error('The forms module was unable to load the admin view specified (' . $admin_view_filename . ')', FATAL);
+					$admin_view_name = $GLOBALS['_form_admin_view_class_names'][basename($admin_view_filename, '.php')];
+					$admin_view = new $admin_view_name();
+					$model =& $this->get_form_model();
+					$admin_view->set_model($model);
+				}
+				else $admin_view = false;
+				$this->form_admin_view =& $admin_view;
+			}
+			return $this->form_admin_view;
 		}
 		
 		/**
@@ -194,6 +236,12 @@
 			if (isset($_REQUEST['mode']) && $_REQUEST['mode'] == 'data_view')
 			{
 				$redirect = carl_make_redirect(array('mode' => '', 'form_admin_view' => 'true'));
+				header("Location: " . $redirect);
+				exit;
+			}
+			if (isset($_REQUEST['thor_success']) && $_REQUEST['thor_success'] == 'true')
+			{
+				$redirect = carl_make_redirect(array('thor_success' => '', 'submission_key' => 'true'));
 				header("Location: " . $redirect);
 				exit;
 			}
