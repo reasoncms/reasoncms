a1f5e53fecc60430d12c0ad9558eaf482960ee97
Nathan White <nwhite@carleton.edu>
2008-12-09 00:23:50 +0000

    updated so that registration slot entities can be sorted even on sites that do not have the event type

--- reason_4.0/lib/core/content_sorters/registration_slot.php
+++ reason_4.0/lib/core/content_sorters/registration_slot.php
@@ -14,15 +14,18 @@
 			if( !empty( $this->admin_page->request[ 'event_id' ] ) )
 			{
 				$event = new entity($this->admin_page->request[ 'event_id']);
-				$this->admin_page->title = 'Sorting Registration Slots  of "' . $event->get_value('name') . '"';
+				$this->admin_page->title = 'Sorting Registration Slots of "' . $event->get_value('name') . '"';
 			}
 		} // }}}
 		function update_es( $es ) // {{{
 		{
-			$es->add_right_relationship_field( 'event_type_to_registration_slot_type' , 'entity' , 'id' , 'event_id' );
-			$es->add_right_relationship_field( 'event_type_to_registration_slot_type' , 'entity' , 'name' , 'event_name' );
 			if( !empty( $this->admin_page->request[ 'event_id' ] ) )
-				$es->add_relation( '__entity__.id = ' . $this->admin_page->request[ 'event_id' ] ); 
+			{
+				$es->add_right_relationship_field( 'event_type_to_registration_slot_type' , 'entity' , 'id' , 'event_id' );
+				$es->add_right_relationship_field( 'event_type_to_registration_slot_type' , 'entity' , 'name' , 'event_name' );
+				if( !empty( $this->admin_page->request[ 'event_id' ] ) )
+					$es->add_relation( '__entity__.id = ' . $this->admin_page->request[ 'event_id' ] ); 
+			}
 			return $es;
 		} // }}}
 		
@@ -36,24 +39,26 @@
 			
 			//should adjust so that can't rearrange slots for events that have only one or no registration slots.
 			//also, probably not for past events either.
-			foreach( $values AS $event_id => $event )
+			if ($values)
 			{
-				$es2 = new entity_selector( $this->admin_page->site_id );
-				$es2->add_type( id_of( 'registration_slot_type' ) );
-				$es2->add_right_relationship($event_id, relationship_id_of('event_type_to_registration_slot_type'));
-				$numSlots = $es2->get_one_count();
-				
-				if($numSlots > 1)
+				foreach( $values AS $event_id => $event )
 				{
-					$date = $event->get_value('datetime');
-					$name = 'Sort slots for ' . $event->get_value('name') . ' - ' . prettify_mysql_datetime($date);
-					$link = $this->admin_page->make_link( array( 'event_id' => $event->id() , 'default_sort' => false ) , true );
-					$links[ $name ] = $link;
+					$es2 = new entity_selector( $this->admin_page->site_id );
+					$es2->add_type( id_of( 'registration_slot_type' ) );
+					$es2->add_right_relationship($event_id, relationship_id_of('event_type_to_registration_slot_type'));
+					$numSlots = $es2->get_one_count();
+				
+					if($numSlots > 1)
+					{
+						$date = $event->get_value('datetime');
+						$name = 'Sort slots for ' . $event->get_value('name') . ' - ' . prettify_mysql_datetime($date);
+						$link = $this->admin_page->make_link( array( 'event_id' => $event->id() , 'default_sort' => false ) , true );
+						$links[ $name ] = $link;
+					}
 				}
+				$this->links = $links;
+				return( $this->links );
 			}
-
-			$this->links = $links;
-			return( $this->links );
 		} // }}}
 	}
 ?>
