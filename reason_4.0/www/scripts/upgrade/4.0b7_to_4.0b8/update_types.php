c79b2fa537a996f31447d339ec1e5ab7db5f0ce3
Nathan White <nwhite@carleton.edu>
2008-12-30 21:21:27 +0000

    stub for update_types.php

---
+++ reason_4.0/www/scripts/upgrade/4.0b7_to_4.0b8/update_types.php
@@ -0,0 +1,7 @@
+<?php
+        /**
+         * Stub for the 4.0b7_to_4.0b8 update_types upgrade script
+         */
+        include_once( 'reason_header.php' );
+        reason_include_once( 'scripts/upgrade/4.0b7_to_4.0b8/update_types.php' );
+?>
