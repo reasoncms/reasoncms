4bf76ae6afa49f95088c0bd1bf07bcb464e9bb8d
Nathan White <nwhite@carleton.edu>
2009-01-06 00:46:16 +0000

    prep for easier creation of local version

--- reason_4.0/lib/core/content_managers/quote.php
+++ reason_4.0/lib/core/content_managers/quote.php
@@ -3,14 +3,25 @@
 
 	class QuoteManager extends ContentManager
 	{
+		var $fields_to_remove = array('rating');
+		var $field_order = array('name', 'unique_name', 'content', 'description', 'author', 'keywords');
+		
 		function alter_data()
 		{
 			$this->set_comments('description', form_comment('If provided, the short version may be used by modules that need to display quotes in a limited amount of space.'));
 			$this->set_display_name('description', 'Quotation Text (Short Version)');
 			$this->set_element_properties('description', array('rows' => 3));
 			$this->set_display_name('content', 'Quotation Text');
-			$this->set_order(array('name', 'unique_name', 'content', 'description', 'author', 'keywords'));
 			$this->add_required('content');
+			
+			if (!empty($this->fields_to_remove))
+			{
+				foreach ($this->fields_to_remove as $field)
+				{
+					$this->remove_element($field);
+				}
+			}
+			$this -> set_order ($this->field_order);
 		}
 	}
 ?>
