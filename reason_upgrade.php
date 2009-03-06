463b1b0cde6c74f9fd95a63e8049e192ef0eb9fe
Nathan White <nwhite@carleton.edu>
2009-03-06 02:19:31 +0000

    changed to redirect to upgrade.php in www folder

--- reason_upgrade.php
+++ reason_upgrade.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * The reason_upgrade.php script now lives in the reason www folder and is called upgrade.php.
+ * The reason_upgrade.php script now lives in the reason www folder and is called upgrade.php
  *
  * @deprecated 
  * @todo remove by Reason 4 RC 1
  */
-include('./reason_4.0/www/upgrade.php');
+
+$path = dirname($_SERVER['REQUEST_URI']) . '/reason_4.0/www/upgrade.php';
+header("Location: " . $path);
+exit;
 ?>
