64b5edca6cdb4417989ce146ede0329b3407250f
Nathan White <nwhite@carleton.edu>
2009-03-06 02:01:50 +0000

    now just includes the version in the www folder

--- reason_upgrade.php
+++ reason_upgrade.php
@@ -1,8 +1,9 @@
-<h1>Upgrade Reason</h1>
-<ul>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b6_to_4.0b7">Reason Beta 6 to Reason Beta 7</a></li>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b5_to_4.0b6">Reason Beta 5 to Reason Beta 6</a></li>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b4_to_4.0b5">Reason Beta 4 to Reason Beta 5</a></li>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b3_to_4.0b4">Reason Beta 3 to Reason Beta 4</a></li>
-<li><a href="./reason_4.0/www/scripts/upgrade/4.0b1_and_b2_to_4.0b3">Reason Beta 1 or Beta 2 to Reason Beta 3</a></li>
-</ul>
+<?php
+/**
+ * The reason_upgrade.php script now lives in the reason www folder and is called upgrade.php.
+ *
+ * @deprecated 
+ * @todo remove by Reason 4 RC 1
+ */
+include('./reason_4.0/www/upgrade.php');
+?>
