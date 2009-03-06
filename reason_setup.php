6b175d8648bdf8effc2ae6b36741b9330bd2de00
Nathan White <nwhite@carleton.edu>
2009-03-06 02:19:13 +0000

    changed to redirect to setup.php in www folder

--- reason_setup.php
+++ reason_setup.php
@@ -1,9 +1,12 @@
 <?php
 /**
- * The reason_setup.php script now lives in the reason www folder and is called setup.php.
+ * The reason_setup.php script now lives in the reason www folder and is called setup.php
  *
  * @deprecated 
  * @todo remove by Reason 4 RC 1
  */
-include('./reason_4.0/www/setup.php');
+
+$path = dirname($_SERVER['REQUEST_URI']) . '/reason_4.0/www/setup.php';
+header("Location: " . $path);
+exit;
 ?>
