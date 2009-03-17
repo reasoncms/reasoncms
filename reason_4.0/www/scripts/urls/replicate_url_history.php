efc8824dca7333b32f51b415df98205f59cd98c9
Matt Ryan <mryan@carleton.edu>
2009-03-17 17:33:05 +0000

    stub for URL history replication script

---
+++ reason_4.0/www/scripts/urls/replicate_url_history.php
@@ -0,0 +1,8 @@
+<?php
+	/**
+	 * Stub for the URL history replication script
+	 * No additional code should be placed in this file
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/urls/replicate_url_history.php' );
+?>
