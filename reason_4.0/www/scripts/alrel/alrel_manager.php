82e27141116924156ccaad3892d406d0d4a44c0a
Matt Ryan <mryan@carleton.edu>
2009-03-29 04:22:15 +0000

    added better package info

--- reason_4.0/www/scripts/alrel/alrel_manager.php
+++ reason_4.0/www/scripts/alrel/alrel_manager.php
@@ -1,6 +1,8 @@
 <?php
 	/**
 	 * Stub for the Allowable Relationship Manager
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/alrel/alrel_manager.php' );
--- reason_4.0/www/scripts/alrel/del_alrel.php
+++ reason_4.0/www/scripts/alrel/del_alrel.php
@@ -2,6 +2,8 @@
 	/**
 	 * Stub for the Allowable Relationship Deleter
 	 * No additional code should be placed in this file
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/alrel/del_alrel.php' );
