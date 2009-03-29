4950aeed6fadbee4de6321903b2bbc987bd6280e
Matt Ryan <mryan@carleton.edu>
2009-03-29 05:25:31 +0000

    added basic documentation

--- reason_4.0/lib/core/minisite_templates/modules/av_with_filters.php
+++ reason_4.0/lib/core/minisite_templates/modules/av_with_filters.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 	/**
+ 	 * Include the base class & register module with Reason
+ 	 */
 	reason_include_once( 'minisite_templates/modules/av.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'AvWithFiltersModule';
 
+	/**
+	 * Minisite module that displays media works & media files with a search box and category filter
+	 */
 	class AvWithFiltersModule extends AvModule
 	{
 		var $use_filters = true;
