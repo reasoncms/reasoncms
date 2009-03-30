963947c099ed39ca8b55121be5ed3490dbfcb934
Matt Ryan <mryan@carleton.edu>
2009-03-30 23:39:22 +0000

    improved documentation

--- reason_4.0/lib/core/minisite_templates/modules/form_content.php
+++ reason_4.0/lib/core/minisite_templates/modules/form_content.php
@@ -1,9 +1,25 @@
 <?php
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
 
+	/**
+	 * Register the module with Reason and include the parent class
+	 */
 	reason_include_once( 'minisite_templates/modules/content.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'FormContentModule';
 
+	/**
+	 * A minisite module that only displays the current page's content if the page is not responding to a form submission
+	 *
+	 * When used in conjunction with a form module, this hides the page's content on form submission
+	 * so that the form's thank you message is not buried onder the page content
+	 *
+	 * @todo use cleanup rules instead of directly looking at $_REQUEST
+	 * @todo report has_content state
+	 */
 	class FormContentModule extends ContentModule
 	{
 		function run()
