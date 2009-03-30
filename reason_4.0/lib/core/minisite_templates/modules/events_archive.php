a0cd4ae90a96e11075836586d557bdaf906aac47
Matt Ryan <mryan@carleton.edu>
2009-03-30 04:12:20 +0000

    improved documentation

--- reason_4.0/lib/core/minisite_templates/modules/events_archive.php
+++ reason_4.0/lib/core/minisite_templates/modules/events_archive.php
@@ -1,8 +1,22 @@
 <?php 
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+ /**
+  * include the base class and register the module with Reason
+  */
 	reason_include_once( 'minisite_templates/modules/events.php' );
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'EventsArchiveModule';
 
-
+/**
+ * A minisite module that shows a chronological listing of all events on the site, 
+ * beginning with the oldeest event on the site
+ *
+ * This module is useful for short-term sites that deal with defined events; when the time frame is
+ * over this module acts as an archive of the events that were listed on the site.
+ */
 class EventsArchiveModule extends EventsModule
 {
 	var $show_calendar_grid = false;
