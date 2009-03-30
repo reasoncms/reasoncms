c8380e9456e1c56e7bfa2dd3da6bc76c8a61669d
Matt Ryan <mryan@carleton.edu>
2009-03-30 04:20:25 +0000

    phpdocified documentation

--- reason_4.0/lib/core/minisite_templates/modules/default.php
+++ reason_4.0/lib/core/minisite_templates/modules/default.php
@@ -1,48 +1,120 @@
 <?php
-
-	// get the name of the file, without all the path information
-	// and without the .php suffix and set the name of the class
-	// to that index of this global array
+	/**
+	 * @package reason
+	 * @subpackage minisite_modules
+	 */
+	
+	/**
+	 * Register the module with Reason
+	 *
+	 * Get the name of the file, without all the path information
+	 * and without the .php suffix and set the name of the class
+	 * to that index of this global array
+	 */
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'DefaultMinisiteModule';
 
+	/**
+	 * The base minisite module
+	 *
+	 * This class defines the API of minisite modules, and serves as a base
+	 * class for other minisite modules
+	 *
+	 * This class is not completely abstract; it contains the prep_args and 
+	 * handle_params utilities
+	 */
 	class DefaultMinisiteModule
 	{
 		var $cleanup_rules = array();
-		// the cleaned up array of request variables
+		/**
+		 * the cleaned up array of request variables
+		 * @var array
+		 */
 		var $request = array();
-		// page id as passed from the template
+		/**
+		 * page id as passed from the template
+		 * @var integer
+		 */
 		var $page_id;
-		// site id as passed from the template
+		/**
+		 * site id as passed from the template
+		 * @var integer
+		 */
 		var $site_id;
-		// current page entity (this is the cur_page object from the pages collection from the template
+		/**
+		 * current page entity (this is the cur_page object from the pages collection from the template
+		 * @var object (entity)
+		 */
 		var $cur_page;
-		// Reason_Session object passed if this module is editable
+		/**
+		 * Reason_Session object passed if this module is editable
+		 * @var object
+		 */
 		var $session = null;
-		// textonly variable from the template
+		/**
+		 * textonly variable from the template
+		 * @var mixed (null until set as boolean value)
+		 */
 		var $textonly = null;
 		
-		// allowable parameters for this module
-		// IMPORTANT: this array must contain fully specified keys AND values.
+		/**
+		 * allowable parameters for this module
+		 *
+		 * IMPORTANT: this array must contain fully specified keys AND values.
+		 *
+		 * @var array
+		 */
 		var $acceptable_params = array();
+		/**
+		 * Parameters that are to be inherited by classes that extend the current class
+		 *
+		 * Structured the same way as acceptable_params
+		 *
+		 * @var array
+		 */
 		var $base_params = array();
-		// params as keys with values as values
+		/**
+		 * params as keys with values as values
+		 *
+		 * These are the parameters set up in the page types file
+		 *
+		 * Note that only those in acceptable_params or base_params will be set
+		 *
+		 * @var array
+		 */
 		var $params;
 		
-		// see if arguments have been parsed
+		/**
+		 * see if arguments have been parsed
+		 * @var boolean
+		 */
 		var $_prepped = false;
-		// copy of original arguments
+		/**
+		 * copy of original arguments
+		 * @deprecated
+		 * @var array
+		 */
 		var $_args = array();
 		
-		///////////////////////////////////////////////////////////////
-		// $parent is DEPRECATED.  Only for compatibility.
-		// this used to be the way to access page_id, site_id, etc.
-		// but that method sucks.  so we just pass down the right
-		// information now.  which is better
-		
+		/**
+		 * $parent is DEPRECATED.  Only for compatibility.
+		 *
+		 * this used to be the way to access page_id, site_id, etc.
+		 * but that method sucks.  so we just pass down the right
+		 * information now.  which is better
+		 *
+		 * @deprecated
+		 * @var object minisite_template
+		 * @todo Remove use of $this->parent from core minisite modules
+		 */
 		var $parent;
-		///////////////////////////////////////////////////////////////
 		
-		// convenience method that the template calls. sets up the variables from the template
+		/**
+		 * convenience method that the template calls. sets up the variables from the template
+		 * @param array $args
+		 * @todo is there anything necessary about this? Shouldn't classes that 
+		 *       instantiate modules just call something like set_site, set_page, etc?
+		 *       This seems unnecessarily dangerous and complex.
+		 */
 		function prep_args( $args = array() ) // {{{
 		{
 			// only run this method once.  for compatibility, it is called by init().
@@ -66,8 +138,15 @@
 				$this->_prepped = true;
 			}
 		} // }}}
-		// takes in the list of parameters set up by the page_types array.  checks to see if the module has defined each
-		// key as a valid parameter to accept.  if it is, adds the value to the internal array for module usage.
+		/**
+		 * takes in the list of parameters set up by the page_types array. 
+		 *
+		 * checks to see if the module has defined each
+		 * key as a valid parameter to accept.  if it is, adds the value to the 
+		 * internal array for module usage.
+		 *
+		 * @param array $params
+		 */
 		function handle_params( $params ) // {{{
 		{
 			// run through acceptable_params and collect the real keys.  If there is an index that
@@ -100,56 +179,100 @@
 				}
 			}
 		} // }}}
-		// hook to run some code and load some objects before get_cleanup_rules() is called
+		/**
+		 * hook to run some code and load some objects before get_cleanup_rules() is called
+		 */
 		function pre_request_cleanup_init() // {{{
 		{
 		} // }}}
-		// called by the template to do any of the DB heavy lifting.  if a module is going to get data from some source,
-		// it should always do it here.  we should NOT be retrieving or munging data from other methods.
+		/**
+		 * called by the template to do any of the DB heavy lifting.
+		 *
+		 * if a module is going to get data from some source,
+		 * it should always do it here.  we should NOT be retrieving
+		 * or munging data from other methods.
+		 *
+		 * @param array $args This is param is kind of a legacy thing; it does not appear to
+		 *                    be used much, if at all
+		 */
 		function init( $args = array() ) // {{{
 		{
 		} // }}}
-		// called when editing is available for any specific editable needs
+		/**
+		 * called when editing is available for any specific editable needs
+		 * @param object $session
+		 */
 		function init_editable( $session ) // {{{
 		{
 			$this->session = $session;
 		} // }}}
-		// returns a boolean value determining whether someone can edit the page or not.  this method is for performing
-		// checks on authentication if they need to be made.  for instance, this is where you would check to see if the
-		// logged in user is related to the page with an authentication relationship.  it could even be used to perform
-		// special actions for a module that doesn't use the usual method of authentication.
+		/**
+		 * returns a boolean value determining whether someone can edit the page or not. 
+		 *
+		 * this method is for performing
+		 * checks on authentication if they need to be made.  for instance, this 
+		 * is where you would check to see if the
+		 * logged in user is related to the page with an authentication relationship.
+		 * it could even be used to perform
+		 * special actions for a module that doesn't use the usual method of authentication.
+		 *
+		 * @return boolean
+		 */
 		function can_edit() // {{{
 		{
 			return false;
 		} // }}}
-		// returns a boolean to determine if this module should even be run at all
+		/**
+		 * returns a boolean that reports on whether the module will have any content to display
+		 *
+		 * @return boolean
+		 */
 		function has_content() // {{{
 		{
 			return true;
 		} // }}}
-		// basic version simply returns the variable.  more complicated actions can be done here if needed
+		/**
+		 * Gets the cleanup rules so that inputs can be validated before being set
+		 *
+		 * basic version simply returns the variable.  more complicated actions can be done here if needed
+		 * @return array
+		 */
 		function get_cleanup_rules() // {{{
 		{
 			return $this->cleanup_rules;
 		} // }}}
-		// the basic run function to display this module.  this is when the template is in non-editing mode
+		/**
+		 * the basic run function to display this module.
+		 *
+		 * this is called when the template is in non-editing mode
+		 */
 		function run() // {{{
 		{
 			echo 'I am the default minisite frontend module<br />';
 		} // }}}
-		// displays the editable version of the page.  there is a good chance that this method will handle all states of
-		// an editable form
+		/**
+		 * displays the editable version of the page.
+		 *
+		 * there is a good chance that this method will handle all states of
+		 * an editable form
+		 */
 		function run_editable() // {{{
 		{
 			$this->run();
 		} // }}}
-		// used by the template to determine the most recent modification time
+		/**
+		 * Get the datetime of the most recently modified displayed item
+		 *
+		 * used by the template to determine the most recent modification time
+		 *
+		 * @return mixed false if not available; otherwise return a string containing a MySQL-formatted datetime
+		 */
 		function last_modified() // {{{
 		{
 			return false;
 		} // }}}
 		/**
-		 * Provides (x)HTML documentation of the module
+		 * Get (x)HTML documentation of the module
 		 * @return mixed null if no documentation available, string if available
 		 */
 		function get_documentation()
@@ -158,7 +281,7 @@
 		}
 		
 		/**
-		 * Provides sample module output
+		 * Get sample module output
 		 * @return mixed null if no sample output available, string if available
 		 */
 		function get_sample_output()
