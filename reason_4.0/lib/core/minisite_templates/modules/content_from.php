6038c23264050f028e345ffb250cc1944fcbe311
Matt Ryan <mryan@carleton.edu>
2009-03-30 04:22:42 +0000

    improved documentation

--- reason_4.0/lib/core/minisite_templates/modules/content_from.php
+++ reason_4.0/lib/core/minisite_templates/modules/content_from.php
@@ -1,9 +1,21 @@
 <?php
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
 
+	/**
+	 * Include base class & register module with Reason
+	 */
 	reason_include_once( 'minisite_templates/modules/content_base.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'ContentFromModule';
 
+	/**
+	 * A minisite module that outputs the content of a uniquely named entity
+	 *
+	 * Use the parameter unique_name to specify which entity's content should be output
+	 */
 	class ContentFromModule extends ContentModule
 	{
 		var $acceptable_params = array(
