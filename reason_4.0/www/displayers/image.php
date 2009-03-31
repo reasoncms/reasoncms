c16f3a8c8283b7966af5e67dbf8da1430c110488
Matt Ryan <mryan@carleton.edu>
2009-03-31 16:20:01 +0000

    added documentation

--- reason_4.0/www/displayers/image.php
+++ reason_4.0/www/displayers/image.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * Stub for the image popup generator
+ * @package reason
+ * @subpackage popups
+ */
 	$reason_session = false;
 	include_once( 'reason_header.php' );
 	reason_include_once( 'popups/image_popup.php' );
