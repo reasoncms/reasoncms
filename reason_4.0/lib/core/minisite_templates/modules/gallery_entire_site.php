3aa3aa318db83fad4f387c88c091cf3e54640efb
Matt Ryan <mryan@carleton.edu>
2009-03-31 16:32:37 +0000

    added documentation

--- reason_4.0/lib/core/minisite_templates/modules/gallery_entire_site.php
+++ reason_4.0/lib/core/minisite_templates/modules/gallery_entire_site.php
@@ -1,8 +1,25 @@
 <?php
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+ 	/**
+ 	 * Include parent class & register module with Reason
+ 	 */
 	reason_include_once( 'minisite_templates/modules/gallery.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'GalleryAllModule';
 	
+	/**
+	 * A Minisite module that shows all images on the current site
+	 *
+	 * This module is deprecated.
+	 * Use gallery2, setting the "entire_site" parameter to true, instead.
+	 * 
+	 * @deprecated
+	 * @todo move out of the core
+	 */
 	class GalleryAllModule extends GalleryModule
 	{
 		
