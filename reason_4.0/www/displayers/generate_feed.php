61d8b6520fa6caecdde17fc67af033c09ee85585
Matt Ryan <mryan@carleton.edu>
2009-03-31 16:19:49 +0000

    added documentation

--- reason_4.0/www/displayers/generate_feed.php
+++ reason_4.0/www/displayers/generate_feed.php
@@ -2,6 +2,8 @@
 	/**
 	 * Stub for the Generate Feed (generate_feed.php) script
 	 * No additional code should be placed in this file
+	 * @package reason
+	 * @subpackage feeds
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/feeds/generate_feed.php' );
