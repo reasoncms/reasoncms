ae3dbf493d9307f024057165f46116304ca2113d
Matt Ryan <mryan@carleton.edu>
2009-03-31 21:10:03 +0000

    added documentation

--- reason_4.0/lib/core/minisite_templates/modules/navigation_top.php
+++ reason_4.0/lib/core/minisite_templates/modules/navigation_top.php
@@ -1,9 +1,19 @@
 <?php
-
+/**
+ * @package reason
+ * @subpackage minisite_templates
+ */
+	
+	/**
+	 * Include parent class; register module with Reason
+	 */
 	reason_include_once( 'minisite_templates/modules/default.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'NavigationTopModule';
 	
+	/**
+	 * A minisite module that presents the "top nav" (e.g. tab navigation) of the current navigation class
+	 */
 	class NavigationTopModule extends DefaultMinisiteModule
 	{
 		function has_content()
