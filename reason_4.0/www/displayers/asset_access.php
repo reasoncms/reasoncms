99146ff806c15850ef5c865ea708c5848079e651
Matt Ryan <mryan@carleton.edu>
2009-03-31 16:19:55 +0000

    added documentation

--- reason_4.0/www/displayers/asset_access.php
+++ reason_4.0/www/displayers/asset_access.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Stub for asset access script
+ * @package reason
+ * @subpackage scripts
+ */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/assets/asset_access_handler.php' );
 ?>
