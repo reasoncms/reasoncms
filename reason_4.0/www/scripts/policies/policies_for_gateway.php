6cfe1de0a0235509f226c08e1126fb4061d877ed
Matt Ryan <mryan@carleton.edu>
2009-04-01 21:27:35 +0000

    improved documentation

--- reason_4.0/www/scripts/policies/policies_for_gateway.php
+++ reason_4.0/www/scripts/policies/policies_for_gateway.php
@@ -1,7 +1,13 @@
 <?php
 	/**
 	 * Stub for the Gateway Policies script
+	 *
 	 * No additional code should be placed in this file
+	 *
+	 * @todo move out of Reason core -- this is Carleton-specific code
+	 *
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/policies/policies_for_gateway.php' );
