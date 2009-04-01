7d8246a7342043f19fcbabf12dca1c4c34c6ec6c
Matt Ryan <mryan@carleton.edu>
2009-04-01 18:43:28 +0000

    added documentation

--- reason_4.0/lib/core/minisite_templates/modules/page_description.php
+++ reason_4.0/lib/core/minisite_templates/modules/page_description.php
@@ -1,9 +1,20 @@
 <?php
-
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+	/**
+	 * Register module with Reason and include dependencies
+	 */
 	reason_include_once( 'minisite_templates/modules/default.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'PageDescriptionModule';
 
+	/**
+	 * A minisite module that displays the description of the current page
+	 * (normally only displayed in a meta tag or in listings on other pages.)
+	 */
 	class PageDescriptionModule extends DefaultMinisiteModule
 	{
 		function has_content()
