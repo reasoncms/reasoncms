3f9a30f4bf3a47510c903ec37257d6e593d9095a
Matt Ryan <mryan@carleton.edu>
2009-04-01 22:53:50 +0000

    added package documentation

--- reason_4.0/www/scripts/db_minimize/minimize.php
+++ reason_4.0/www/scripts/db_minimize/minimize.php
@@ -1,6 +1,8 @@
 <?php
 	/**
 	 * Stub for the Instance Minimizer
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/db_minimize/minimize.php' );
