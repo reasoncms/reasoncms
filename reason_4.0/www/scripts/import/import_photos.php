99f0aab4507f5f3649ca432369c4e123b79a3a8d
Matt Ryan <mryan@carleton.edu>
2009-04-01 21:27:05 +0000

    improved package documentation

--- reason_4.0/www/scripts/import/import_photos.php
+++ reason_4.0/www/scripts/import/import_photos.php
@@ -1,7 +1,11 @@
 <?php
 	/**
 	 * Stub for the Import Photos script
+	 *
 	 * No additional code should be placed in this file
+	 *
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/import/import_photos.php' );
