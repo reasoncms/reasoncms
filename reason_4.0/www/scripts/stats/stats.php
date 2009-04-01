556f218c479ab180ea7bc9522006c53725ab0a78
Matt Ryan <mryan@carleton.edu>
2009-04-01 22:54:17 +0000

    added package documentation

--- reason_4.0/www/scripts/stats/stats.php
+++ reason_4.0/www/scripts/stats/stats.php
@@ -1,7 +1,11 @@
 <?php
 	/**
 	 * Stub for the stats script
+	 *
 	 * No additional code should be placed in this file
+	 *
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/stats/stats.php' );
