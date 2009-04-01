d4751eab0426e9f3420d2601d7425548b9f55095
Matt Ryan <mryan@carleton.edu>
2009-04-01 18:53:21 +0000

    added documentation

--- reason_4.0/lib/core/minisite_templates/modules/page_title.php
+++ reason_4.0/lib/core/minisite_templates/modules/page_title.php
@@ -1,9 +1,19 @@
 <?php
-
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+	/**
+	 * Register module with Reason and include dependencies
+	 */
 	reason_include_once( 'minisite_templates/modules/default.php' );
 
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'PageTitleModule';
 
+	/**
+	 * A minisite module that displays the title of the current page
+	 */
 	class PageTitleModule extends DefaultMinisiteModule
 	{
 		function has_content()
