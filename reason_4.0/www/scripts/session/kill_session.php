07350e53ff7a11b041b015c04398bfe5d112f783
Matt Ryan <mryan@carleton.edu>
2009-04-01 22:53:03 +0000

    added package documentation

--- reason_4.0/www/scripts/session/kill_session.php
+++ reason_4.0/www/scripts/session/kill_session.php
@@ -1,7 +1,11 @@
 <?php
 	/**
 	 * Stub for the kill session script
+	 *
 	 * No additional code should be placed in this file
+	 *
+	 * @package reason
+	 * @subpackage scripts
 	 */
 	include_once( 'reason_header.php' );
 	reason_include_once( 'scripts/session/kill_session.php' );
