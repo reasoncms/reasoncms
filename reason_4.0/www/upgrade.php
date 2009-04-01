96244777209c97a3542dc2ddca2aaa40d4882086
Matt Ryan <mryan@carleton.edu>
2009-04-01 21:25:44 +0000

    added phpdoc package info & improved HTML

--- reason_4.0/www/upgrade.php
+++ reason_4.0/www/upgrade.php
@@ -1,9 +1,22 @@
+<?php
+/**
+ * @package reason
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Upgrade Reason</title>
+</head>
+<body>
 <h1>Upgrade Reason</h1>
 <ul>
-<li><a href="./scripts/upgrade/4.0b7_to_4.0b8">Reason Beta 7 to Reason Beta 8</a></li>
-<li><a href="./scripts/upgrade/4.0b6_to_4.0b7">Reason Beta 6 to Reason Beta 7</a></li>
-<li><a href="./scripts/upgrade/4.0b5_to_4.0b6">Reason Beta 5 to Reason Beta 6</a></li>
-<li><a href="./scripts/upgrade/4.0b4_to_4.0b5">Reason Beta 4 to Reason Beta 5</a></li>
-<li><a href="./scripts/upgrade/4.0b3_to_4.0b4">Reason Beta 3 to Reason Beta 4</a></li>
-<li><a href="./scripts/upgrade/4.0b1_and_b2_to_4.0b3">Reason Beta 1 or Beta 2 to Reason Beta 3</a></li>
+<li><a href="./scripts/upgrade/4.0b7_to_4.0b8">Reason 4.0 Beta 7 to Beta 8</a></li>
+<li><a href="./scripts/upgrade/4.0b6_to_4.0b7">Reason 4.0 Beta 6 to Beta 7</a></li>
+<li><a href="./scripts/upgrade/4.0b5_to_4.0b6">Reason 4.0 Beta 5 to Beta 6</a></li>
+<li><a href="./scripts/upgrade/4.0b4_to_4.0b5">Reason 4.0 Beta 4 to Beta 5</a></li>
+<li><a href="./scripts/upgrade/4.0b3_to_4.0b4">Reason 4.0 Beta 3 to Beta 4</a></li>
+<li><a href="./scripts/upgrade/4.0b1_and_b2_to_4.0b3">Reason 4.0 Beta 1 or 2 to Beta 3</a></li>
 </ul>
+</body>
+</html>
