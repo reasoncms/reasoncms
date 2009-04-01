2eefba7fcc01246f3a263b46019c43c2c222c50c
Matt Ryan <mryan@carleton.edu>
2009-04-01 21:26:29 +0000

    improved documentation

--- reason_4.0/www/admin/index.php
+++ reason_4.0/www/admin/index.php
@@ -1,5 +1,9 @@
 <?
-// This is a stub that includes the admin index.php page
+/**
+ * This is a stub that includes the admin index.php page
+ * @package reason
+ * @subpackage admin
+ */
 include_once( 'reason_header.php' );
 reason_include_once( 'admin/index.php' );
 ?>
