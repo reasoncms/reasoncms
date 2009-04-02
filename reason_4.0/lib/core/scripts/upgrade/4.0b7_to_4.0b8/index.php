533fc1dbf2703a68181e1a8708ade71a21870b56
Matt Ryan <mryan@carleton.edu>
2009-04-02 19:23:04 +0000

    added script to update the ordering of images on images module pages

--- reason_4.0/lib/core/scripts/upgrade/4.0b7_to_4.0b8/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b7_to_4.0b8/index.php
@@ -21,9 +21,11 @@
 <p>Reason 4 Beta 8 changes the rel_name field for "owns" and "borrows" allowable relationships to be unique. This allows entity selector queries
 to be a bit simpler, and allows the relationship_id_of method to work for all allowable relationships. This is a major change; code distributed 
 with Reason 4 Beta 8 will be unreliable on a Reason 4 Beta 7 database that has not been upgraded.</p>
-<h3>New Setting</h3>
-<p>Reason 4 Beta 8 introduces a new setting called DISABLE_REASON_ADMINISTRATIVE_INTERFACE. You should make sure the setting is defined in the
-reason_settings.php used in your reason instance. You can copy and paste the following:</p>
+<h3>New Settings</h3>
+<p>Reason 4 Beta 8 introduces two new settings:</p>
+<ol>
+<li>DISABLE_REASON_ADMINISTRATIVE_INTERFACE. You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:<br />
 <textarea rows="8" cols="100">
 /**
  * DISABLE_REASON_ADMINISTRATIVE_INTERFACE
@@ -34,6 +36,20 @@ reason_settings.php used in your reason instance. You can copy and paste the fol
  */
 define('DISABLE_REASON_ADMINISTRATIVE_INTERFACE', false);
 </textarea>
+</li>
+<li>REASON_ASSET_MAX_UPLOAD_SIZE_MEGS. You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:<br />
+<textarea rows="8" cols="100">
+/**
+ * REASON_ASSET_MAX_UPLOAD_SIZE_MEGS
+ * The largest size that uploaded Reason assets can be, in megabytes.
+ * Note that Reason will use the smallest of these three values:
+ * post_max_size in php.ini, upload_max_filesize in php.ini, and this setting.
+ */
+define( 'REASON_ASSET_MAX_UPLOAD_SIZE_MEGS',  50 );
+</textarea>
+</li>
+</ol>
 <h3>Scripts to Run</h3>
 <ul>
 
@@ -42,9 +58,10 @@ define('DISABLE_REASON_ADMINISTRATIVE_INTERFACE', false);
 echo '<li>Update the allowable relationship table structure (NOT READY YET)</li>';
 ?>
 
-<li><a href="remove_rewrite_finish_actions.php">Removes rewrite finish actions that are no longer needed</a></li>
-<li><a href="update_types.php">Updates several Reason types with new fields / deletes obsolete fields</a></li>
+<li><a href="remove_rewrite_finish_actions.php">Remove rewrite finish actions that are no longer needed</a></li>
+<li><a href="update_types.php">Update several Reason types with new fields / deletes obsolete fields</a></li>
 <li><a href="database_cleanup.php">Perform database cleanup and maintenance</a></li>
+<li><a href="image_ordering.php">Update the sort_order value for the images module</a></li>
 </ul>
 </body>
 </html>
