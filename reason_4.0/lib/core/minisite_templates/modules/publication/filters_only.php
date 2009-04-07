0f65ff8591031f862045b890553c2c49d1fb74d9
Matt Ryan <mryan@carleton.edu>
2009-04-07 18:20:19 +0000

    improved documentation

--- reason_4.0/lib/core/minisite_templates/modules/publication/filters_only.php
+++ reason_4.0/lib/core/minisite_templates/modules/publication/filters_only.php
@@ -1,14 +1,31 @@
 <?php
-
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+/**
+ * Include parent class
+ */
 reason_include_once('minisite_templates/modules/publication/module.php');
-/* PLEASE NOTE: the blog directory is being hard-coded in this array. If the file is moved, or if the directory is renamed, this module will stop working. */
+
+/**
+ * Register the module with Reason
+ *
+ * PLEASE NOTE: the blog directory is being hard-coded in this array. If the file is moved, or if the directory is renamed, this module will stop working.
+ */
 $GLOBALS[ '_module_class_names' ][ 'publication/'.basename( __FILE__, '.php' ) ] = 'PublicationFiltersOnlyModule';
 
 /**
  * Show just the filters for a blog
+ *
  * Developed so that the filters could be contained in a different place on the page
  * than the rest of the blog module
+ *
  * @author Matt Ryan & Henry Gross
+ * @todo Develop a better way to break publication parts up into separate modules -- this solution
+ *       is fairly hacky, requirs "neutering" of things like crumb and feed modification, and will
+ *       give poor performance (lots of doubling of work going on here.)
  */
 class PublicationFiltersOnlyModule extends PublicationModule
 {
