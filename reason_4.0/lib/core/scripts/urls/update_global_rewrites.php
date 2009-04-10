6e480fafe7f68dd54b857bc5385b993bc7eb305e
Matt Ryan <mryan@carleton.edu>
2009-04-10 23:14:37 +0000

    tightened security (cli access only now) and added ability to provide unique name instead of site ID; also improved output

--- reason_4.0/lib/core/scripts/urls/update_global_rewrites.php
+++ reason_4.0/lib/core/scripts/urls/update_global_rewrites.php
@@ -1,26 +1,63 @@
 <?php
+/**
+ * A command-line script to update .htaccess files for a given site.
+ *
+ * In a terminal application, type:
+ *
+ * php -f update_global_rewrites.php -d include_path=/path/to/reason/package/ site_unique_name
+ *
+ * Alternately, you can enter the ID of the site instead of the unique name:
+ *
+ * php -f update_global_rewrites.php -d include_path=/path/to/reason/package/ 123456
+ *
+ * Note: This script will not run in a browser. Use update_urls.php instead.
+ *
+ * @package reason
+ * @subpackage scripts
+ */
+	
+	/**
+	 * Include the URL manager
+	 */
+	include_once('reason_header.php');
 	reason_include_once( 'classes/url_manager.php' );
 
-	// determine if this is from the web or from the command line or specified otherwise
-	// the _SERVER['_'] still seems to work.  it may be a hack.
-	if( !empty( $_SERVER[ '_' ] ) )
-		$site_id = $argv[1];	// get the site_id from the command line
-	elseif( !empty( $rewrite_site_id ) )
-		$site_id = $rewrite_site_id;	// get site id from the context of the script that called this one
+	// determine if this is from the web or from the command line
+	if( php_sapi_name() == 'cli' )
+	{
+		if(isset($argv[1]))
+		{
+			if(is_numeric($argv[1]))
+				$site_id = (integer) $argv[1];	// get the site_id from the command line
+			else
+				$site_id = id_of( $argv[1] );
+		}
+		else
+		{
+			echo 'Please specify a site as the first argument.'."\n";
+			echo 'e.g. (where "123456" is the ID of the site to update):'."\n";
+			echo 'php -f update_global_rewrites.php -d include_path=/path/to/reason/package/ 123456'."\n";
+			echo 'or...'."\n";
+			echo 'php -f update_global_rewrites.php -d include_path=/path/to/reason/package/ site_unique_name'."\n";
+			exit( 0 );
+		}
+	}
 	else
-		$site_id = !empty( $_REQUEST[ 'site_id' ] ) ? $_REQUEST[ 'site_id' ] : ''; // get site_id from _REQUEST
-
-	if( empty( $site_id ) )
-		trigger_error( 'No site ID in update rewrites script.  This is bad.',HIGH );
+	{
+		header('HTTP/1.1 401 Unauthorized');
+		echo 'This script may only be invoked from the command line.'."\n";
+		exit();
+	}
+	if(empty($site_id))
+	{
+		echo 'Invalid site id or unique name provided.'."\n";
+		exit( 0 );
+	}
 
 	$um = new url_manager( $site_id, true );
 
 	$um->update_rewrites();
 
-	// if we are a shell script, exit cleanly
-	if( !empty( $_SERVER[ '_' ] ) )
-	{
-		'url shells script done'."\n";
-		exit( 0 );
-	}
+	echo 'Rewrites complete'."\n";
+	exit( 0 );
 ?>
