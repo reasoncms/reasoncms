7e9220d7c231674d20887f85c1d4cfc7a16c31e0
Matt Ryan <mryan@carleton.edu>
2009-04-10 23:27:17 +0000

    improved phpdocs

--- reason_4.0/lib/core/minisite_templates/tabbed.php
+++ reason_4.0/lib/core/minisite_templates/tabbed.php
@@ -1,9 +1,9 @@
 <?php
 
-/** Tabbed Template
- * Uses the Basic Tabs Navigation to provide the top level of site as tabs
- * @author Matt Ryan
+/**
+ * Tabbed Template
  * @package reason
+ * @subpackage minisite_templates
  */
 
 // include the MinisiteTemplate class
@@ -11,8 +11,15 @@ reason_include_once( 'minisite_templates/default.php' );
 // this variable must be the same as the class name
 $GLOBALS[ '_minisite_template_class_names' ][ basename( __FILE__) ] = 'tabbedTemplate';
 
-
 reason_include_once( 'minisite_templates/nav_classes/basic_tabs.php' );
+
+/**
+ * Tabbed template
+ *
+ * Uses the Basic Tabs Navigation to provide the top level of site as tabs
+ *
+ * @author Matt Ryan
+ */
 class tabbedTemplate extends MinisiteTemplate
 {
 	var $nav_class = 'BasicTabsNavigation';
