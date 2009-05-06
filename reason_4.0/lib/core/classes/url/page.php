af8caf8dbd5f85e15a4ae1e748131d32ec3717e9
Nathan White <nwhite@carleton.edu>
2009-05-06 22:38:41 +0000

    major changes to add optional optimization possibilities if page entities are available

--- reason_4.0/lib/core/classes/url/page.php
+++ reason_4.0/lib/core/classes/url/page.php
@@ -24,7 +24,7 @@ include_once(CARL_UTIL_INC . 'basic/url_funcs.php');
  * with page_id we keep the API for the class simple. It is best to always just use get_url, which makes a
  * "best guess" about protocol.
  *
- * Usage:
+ * Basic Usage:
  *
  * <code>
  * $reason_page_url = new reasonPageURL();
@@ -32,7 +32,16 @@ include_once(CARL_UTIL_INC . 'basic/url_funcs.php');
  * $url = $reason_page_url->get_url();
  * </code>
  *
- * One instance can be used for repeated operations - just change the page_id.
+ * Advanced Usage:
+ * 
+ * There are some optimizations that can be applied for advanced users. Most notably, if you have a minisite page 
+ * or a set of minisite pages already, you can provide them to the reasonPageURL class. If those entities have values 
+ * for parent_id and/or owner_id preset, those values will be used for further optimization. See methods:
+ *
+ * - provide_page_entity
+ * - provide_page_entities
+ *
+ * One instance of this class can and should be used for repeated operations - just change the page_id.
  *
  * One bit of oddness is how to handle pages that aren't really pages, but just containers for URLs. What we do 
  * is to just return the value in the url field if it is populated regardless of what get_url method is called.
@@ -44,11 +53,6 @@ include_once(CARL_UTIL_INC . 'basic/url_funcs.php');
  */
 class reasonPageURL extends reasonURL
 {
-	/**
-	 * Improves speed when lots of urls are being grabbed on a single site, slight hit otherwise
-	 */
-	var $find_children_of_parents_dynamically = false;
-	
 	/**
 	 * Returns an absolute url with our best guess at the appropriate protocol
 	 * 
@@ -111,35 +115,134 @@ class reasonPageURL extends reasonURL
 	}
 	
 	/**
+	 * You can provide entities to the class in order to speed things up.
+	 *
+	 * @param object minisite_page entity
+	 * @return void
+	 */
+	function provide_page_entity(&$page)
+	{
+		$page_entities =& $this->_get_page_entities();
+		if (!isset($page_entities[$page->id()])) $page_entities[$page->id()] = $page;
+	}
+	
+	/**
+	 * You can provide entities to the class in order to speed things up - this should rarely be necessary but might help in cases 
+	 * where large batches of pages are selected with a single entity selector, and you want to get urls for those entities.
+	 *
+	 * @param array of minisite_page entity objects
+	 * @return void
+	 */
+	function provide_page_entities(&$pages)
+	{
+		$page_entities =& $this->_get_page_entities();
+		$page_entities = $pages;
+	}
+
+	/**
+	 * Grab the page entity with URL information (builds it if necessary)
+	 *
+	 * @param int page_id
+	 * @return object page entity with URL information
+	 */
+	function &get_page($page_id)
+	{
+		static $pages;
+		if (!isset($pages[$page_id]) || !$this->use_static_cache())
+		{
+			$pages[$page_id] = $this->_build_page($page_id);
+		}
+		$page =& $pages[$page_id];
+		return $page;
+	}
+	
+	/**
+	 * We use a DBSelector to construct a super basic query to quickly get the parent.
+	 *
+	 * This method probably ought to be a method directly on minisite page entities ... but alas we aren't there yet.
+	 * For efficiency sake, we could eliminate this if the parent was stored on the page entity and not across a relationship
+	 *
+	 * @access private
+	 * @param object minisite_page entity
+	 * @return int parent entity id for a minisite page
+	 */
+	function _get_parent_id(&$page)
+	{
+		if (!$page->has_value('parent_id'))
+		{
+			$d = new DBselector();
+			$d->add_table( 'r', 'relationship' );
+			$d->add_field( 'r', 'entity_b', 'parent_id' );
+			$d->add_relation( 'r.type = ' . relationship_id_of('minisite_page_parent'));
+			$d->add_relation( 'r.entity_a = ' . $page->id() );
+			$result = db_query( $d->get_query() , 'Error getting parent ID.' );
+			if( $row = mysql_fetch_assoc($result))
+			{
+				$page->set_value('parent_id', $row['parent_id']);
+			}
+			else
+			{
+				$page->set_value('parent_id', false);
+			}
+		}
+		return $page->get_value('parent_id');
+	}
+	
+	/**
+	 * We use a DBSelector to construct a super basic query to quickly get the owner id.
+	 *
+	 * This method probably ought to be a method directly on minisite page entities ... but alas we aren't there yet.
+	 * For efficiency sake, we could eliminate this if the owner was stored on the page entity and not across a relationship
+	 *
+	 * @access private
+	 * @param object minisite_page entity
+	 * @return int owner id for a minisite page
+	 */
+	function _get_owner_id(&$page)
+	{
+		if (!$page->has_value('owner_id'))
+		{
+			$d = new DBselector();
+			$d->add_table( 'r', 'relationship' );
+			$d->add_field( 'r', 'entity_a', 'owner_id' );
+			$d->add_relation( 'r.type = ' . $this->_get_site_owns_minisite_page_relationship_id());
+			$d->add_relation( 'r.entity_b = ' . $page->id() );
+			$result = db_query( $d->get_query() , 'Error getting owner ID.' );
+			if( $row = mysql_fetch_assoc($result))
+			{
+				$page->set_value('owner_id', $row['owner_id']);
+			}
+			else
+			{
+				$page->set_value('owner_id', false);
+			}
+		}
+		return $page->get_value('owner_id');
+	}
+	
+	/**
 	 * Build a page entity with some custom values
 	 *
 	 * - relative_url
 	 * - domain
+	 * - parent_id
+	 * - owner_id
 	 *
+	 * @access private
 	 * @return object augmented page entity
 	 */
 	function _build_page($page_id)
 	{
-		$es = new entity_selector();
-		$es->limit_tables('page_node');
-		$es->add_type(id_of('minisite_page'));
-		$es->add_relation("entity.id = " . $page_id);
-		$page_result = $es->run_one();
-		$page = ($page_result) ? reset($page_result) : false;
+		$page =& $this->_get_page_entity($page_id);
 		if ($page && !$page->get_value('url')) // lets populate our custom values
 		{
-			if ($parent_id = $this->get_parent_id($page_id))
+			if ($parent_id = $this->_get_parent_id($page)) // this populates parent_id on the entity
 			{
 				if ($parent_id == $page_id) // if i'm my own parent i am a root and my relative_url is the site_url
 				{
-					$es = new entity_selector();
-					$es->limit_tables('site');
-					$es->add_type(id_of('site'));
-					$es->add_left_relationship($page_id, $this->_get_site_owns_minisite_page_relationship_id());
-					$site_result = $es->run_one();
-					$site = ($site_result) ? reset($site_result) : false;
-					if ($site)
+					if ($site_id = $this->_get_owner_id($page)) // this populates owner_id on the entity
 					{
+						$site =& $this->_get_site_entity($site_id);
 						$domain = ($site->has_value('domain') && $site->get_value('domain')) ? $site->get_value('domain') : REASON_HOST;
 						$page->set_value('relative_url', str_replace("//", "/", $site->get_value('base_url')));
 						$page->set_value('domain', $domain);
@@ -162,7 +265,7 @@ class reasonPageURL extends reasonURL
 					}
 					else
 					{
-						trigger_error('the parent entity (should have id ' . $parent_id . ') could not be found for page_id ' . $page_id);
+						trigger_error('a live parent entity (should have id ' . $parent_id . ') could not be found for page_id ' . $page_id);
 						return false;
 					}
 				}
@@ -180,7 +283,7 @@ class reasonPageURL extends reasonURL
 		}
 		else
 		{
-			trigger_error('a page with id ' . $page_id . ' could not be found in the reason database.');
+			trigger_error('a live page with id ' . $page_id . ' could not be found in the reason database.');
 			return false;
 		}
 		return $page;
@@ -190,6 +293,7 @@ class reasonPageURL extends reasonURL
 	 * Until Reason 4 Beta 9 when we have actual relationship unique names - we have to use relationship finder functions to get
 	 * the correct owns relationship between the site and minisite_page.
 	 *
+	 * @access private
 	 * @return int site owns minisite page relationship id
 	 * @todo zap me when relationship_id_of('site_to_minisite_page') works as it ought to
 	 */
@@ -209,60 +313,58 @@ class reasonPageURL extends reasonURL
 	}
 	
 	/**
-	 * We use a DBSelector to construct a super basic query to quickly get the parent.
-	 *
-	 * For efficiency sake, we could eliminate this if the parent was stored on the page entity and not across a relationship
-	 * @return int parent entity id for a minisite page
+	 * @access private
 	 */
-	function get_parent_id($page_id)
+	function &_get_page_entity($page_id)
 	{
-		static $page_to_parent;
-		if (!isset($page_to_parent[$page_id]) || !$this->use_static_cache())
+		$page_entities =& $this->_get_page_entities();
+		if (!isset($page_entities[$page_id]) || !$this->use_static_cache())
 		{
-			$d = new DBselector();
-			$d->add_table( 'r', 'relationship' );
-			$d->add_field( 'r', 'entity_b', 'parent_id' );
-			$d->add_relation( 'r.type = ' . relationship_id_of('minisite_page_parent'));
-			$d->add_relation( 'r.entity_a = ' . $page_id );
-			$result = db_query( $d->get_query() , 'Error getting parent ID.' );
-			if( $row = mysql_fetch_assoc($result))
-			{
-				$parent_id = $row['parent_id'];
-				if ($this->find_children_of_parents_dynamically) // this makes the most sense when grabbing lots of urls on a single site
-				{
-					// lets find the children of the parent
-					$d2 = new DBselector();
-					$d2->add_table( 'r', 'relationship' );
-					$d2->add_field( 'r', 'entity_a', 'page_id' );
-					$d2->add_relation( 'r.type = ' . relationship_id_of('minisite_page_parent'));
-					$d2->add_relation( 'r.entity_b = ' . $row['parent_id'] );
-					$result2 = db_query( $d2->get_query() , 'Error getting child page ids of page '.$row['parent_id']);
-					while ($row2 = mysql_fetch_assoc($result2))
-					{
-						$page_to_parent[$row2['page_id']] = $parent_id;
-					}
-				}
-				else $page_to_parent[$page_id] = $parent_id;
-			}
-			else
-			{
-				$page_to_parent[$page_id] = false;
-			}
+			$es = new entity_selector();
+			$es->limit_tables(array('page_node', 'url'));
+			$es->add_type(id_of('minisite_page'));
+			$es->add_relation("entity.id = " . $page_id);
+			$result = $es->run_one();
+			$page_entities[$page_id] = ($result) ? reset($result) : false;
 		}
-		return $page_to_parent[$page_id];
+		return $page_entities[$page_id];
+	}
+
+	/**
+	 * @access private
+	 */
+	function &_get_page_entities()
+	{
+		static $entities;
+		if (!isset($entities)) $entities = array();
+		return $entities;
 	}
 	
 	/**
-	 * @return object page entity
+	 * @access private
 	 */
-	function &get_page($page_id)
+	function &_get_site_entity($site_id)
 	{
-		static $pages;
-		if (!isset($pages[$page_id]) || !$this->use_static_cache())
+		$site_entities =& $this->_get_site_entities();
+		if (!isset($site_entities[$site_id]) || !$this->use_static_cache())
 		{
-			$pages[$page_id] = $this->_build_page($page_id);
+			$es = new entity_selector();
+			$es->limit_tables('site');
+			$es->add_type(id_of('site'));
+			$es->add_relation("entity.id = " . $site_id);
+			$result = $es->run_one();
+			$site_entities[$site_id] = ($result) ? reset($result) : false;
 		}
-		$page =& $pages[$page_id];
-		return $page;
+		return $site_entities[$site_id];
+	}
+	
+	/**
+	 * @access private
+	 */
+	function &_get_site_entities()
+	{
+		static $entities;
+		if (!isset($entities)) $entities = array();
+		return $entities;
 	}
 }
