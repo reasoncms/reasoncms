17d232463b78e18089f5181149838bcbc7bb42c4
Nathan White <nwhite@carleton.edu>
2009-05-28 19:34:45 +0000

    updated to fix rows with external url ids that are being actively used

--- reason_4.0/lib/core/scripts/upgrade/4.0b7_to_4.0b8/tame_url_history.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b7_to_4.0b8/tame_url_history.php
@@ -51,10 +51,19 @@ class tameURLHistory
 		$this->reason_user_id = $reason_user_id;
 		
 		// The updates
-		$this->remove_external_urls_from_url_history();
+		//$this->remove_external_urls_from_url_history();
+		$this->repair_external_url_problem();
 	}
 	
-	function remove_external_urls_from_url_history()
+	/**
+	 * Find pages that are actually external URLs - if the external url is the most recent timestamp for an entry, keep track of its details.
+	 *
+	 * Delete all the external URL page_id entries from the table, and then insert corrected ones for the external urls that were being used
+	 * as the most recent timestamp. The corrected entries use the parent page_id instead, which is what the external url entries would typically
+	 * resolve to. This approach keeps the history working as intended, while removing the external url entries that are no longer put into the
+	 * table in Reason 4 Beta 8
+	 */
+	function repair_external_url_problem()
 	{
 		// first we grab all the minisite page_ids that are actually URLs
 		$es = new entity_selector();
@@ -70,31 +79,93 @@ class tameURLHistory
 			$dbs->add_table('URL_history');
 			$dbs->add_relation('page_id IN ("'.implode('","',array_keys($result)).'")');
 			$rows = $dbs->run();
-			if ($rows)
+			foreach ($rows as $row) // lets keep the most recent timestamp for each URL - we might want to transform these
 			{
-				foreach ($rows as $row)
-				{
-					$todelete[] = $row['id'];
-				}
-				if (isset($todelete))
-				{
-					$qry = 'DELETE FROM URL_history WHERE id IN ("'.implode('","',$todelete).'")';
-					if ($this->mode == 'test')
-					{
-						echo '<p>Would delete ' . count($todelete) . ' rows with this query:</p>';
-						echo $qry;
-					}
-					elseif ($this->mode == 'run')
-					{
-						db_query($qry, 'Could not delete rows from URL_history');
-						echo '<p>Deleted ' . count($todelete) . ' rows with this query:</p>';
-						echo $qry;
-					}
-					return true;
-				}
+				$url = $row['url'];
+				$id = $row['id'];
+				$urls[$id] = $url;
+				$rows_by_id[$id] = $row;
 			}
 		}
-		echo '<p>There do not appear to be any external_urls that need to be deleted from the URL_history table</p>';
+		if (isset($urls))
+		{
+			$unique_urls = array_unique($urls);
+			foreach ($unique_urls as $url) // check URL history table to see if any of these URLs currently resolve to external URL pages
+			{
+				$query =  'SELECT * FROM URL_history WHERE url ="' . addslashes ( $url ) . '" AND deleted="no" ORDER BY timestamp DESC limit 1';
+				$result = db_query($query, 'error in query');
+				$latest_row = ($result) ? mysql_fetch_assoc($result) : false;
+				if ($latest_row)
+				{
+					$latest_row_id = $latest_row['id'];
+					if (isset($urls[$latest_row_id]))
+					{
+						$needs_fixin[] = $latest_row_id;
+					}
+				}
+			}
+			if (isset($needs_fixin))
+			{
+				foreach ($needs_fixin as $url_history_id_to_update)
+				{
+					$d = new DBselector();
+					$d->add_table( 'r', 'relationship' );
+					$d->add_field( 'r', 'entity_b', 'parent_id' );
+					$d->add_relation( 'r.type = ' . relationship_id_of('minisite_page_parent'));
+					$d->add_relation( 'r.entity_a = ' . $rows_by_id[$url_history_id_to_update]['page_id'] );
+					$result = db_query( $d->get_query() , 'Error getting parent ID.' );
+					if( $myrow = mysql_fetch_assoc($result))
+					{
+						$fixer[$url_history_id_to_update]['deleted'] = $rows_by_id[$url_history_id_to_update]['deleted'];
+						$fixer[$url_history_id_to_update]['timestamp'] = $rows_by_id[$url_history_id_to_update]['timestamp'];
+						$fixer[$url_history_id_to_update]['url'] = $rows_by_id[$url_history_id_to_update]['url'];
+						$fixer[$url_history_id_to_update]['page_id'] = $myrow['parent_id'];
+						$fixer[$url_history_id_to_update]['old_page_id'] = $rows_by_id[$url_history_id_to_update]['page_id'];
+						$fixer[$url_history_id_to_update]['new_page_id'] = $myrow['parent_id'];
+					}
+				}
+			}
+			
+			if (isset($fixer)) // lets build the sql to run
+			{
+				$fixer_sql = 'INSERT INTO `URL_history` (`id`, `url`, `page_id`, `timestamp`, `deleted`) VALUES ';
+				$fixcount = 0;
+				foreach ($fixer as $row_id => $fields)
+				{
+					$notfirst = (!isset($notfirst)) ? false : true;
+					if ($notfirst) $fixer_sql .= ", ";
+					$fixer_sql .= "(".$row_id.", '".$fields['url']."', ".$fields['page_id'].", ".$fields['timestamp'].", '".$fields['deleted']."')";
+					$fixcount++;
+				}
+			}
+			$deleter_sql = 'DELETE FROM URL_history WHERE id IN ("'.implode('","',array_keys($rows_by_id)).'")';
+			if ($this->mode == 'test')
+			{
+				echo '<p>Would delete ' . count($rows_by_id) . ' rows with this query:</p>';
+				echo $deleter_sql;
+			}
+			elseif ($this->mode == 'run')
+			{
+				db_query($deleter_sql, 'Could not delete rows from URL_history');
+				echo '<p>Deleted ' . count($rows_by_id) . ' rows with this query:</p>';
+				echo $deleter_sql;
+			}
+			if ($this->mode == 'test' && isset($fixer_sql))
+			{
+				echo '<p>Would restore ' . $fixcount . ' rows with updated page_ids this query:</p>';
+				echo $fixer_sql;
+			}
+			elseif ($this->mode == 'run' && isset($fixer_sql))
+			{
+				db_query($fixer_sql, 'Could not restore rows to URL_history');
+				echo '<p>Restored ' . $fixcount . ' rows with this query:</p>';
+				echo $fixer_sql;
+			}
+		}
+		else
+		{
+			echo '<p>There are no rows in the URL_history table that resolve to external urls - you may have already run this script</p>';
+		}
 	}
 }
 
