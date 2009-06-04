0f2452b51e43a4ee3d5d18513d5d72793f2c0cc7
Nathan White <nwhite@carleton.edu>
2009-06-04 22:06:46 +0000

    stub for entity_info.php

---
+++ reason_4.0/www/scripts/developer_tools/entity_info.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * Stub for the developer entity info script
+	 *
+	 * No additional code should be placed in this file
+	 *
+	 * @package reason
+	 * @subpackage scripts
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/developer_tools/entity_info.php' );
+?>
