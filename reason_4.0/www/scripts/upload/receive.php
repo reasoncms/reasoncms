e4d194ed8dbb037e20415dffb69246d280b9018f
Nathan White <nwhite@carleton.edu>
2009-07-29 18:25:25 +0000

    first commit receive / destory scripts for javascript upload

---
+++ reason_4.0/www/scripts/upload/receive.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Stub for the "background upload" script.
+ * No additional code should be placed in this file.
+ *
+ * @package reason
+ * @subpackage scripts
+ */
+
+include_once 'reason_header.php';
+reason_require('scripts/upload/receive.php');
