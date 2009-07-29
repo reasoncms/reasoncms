0d757578222b1c2b08b93fa0bf42f10a2d1323e8
Nathan White <nwhite@carleton.edu>
2009-07-29 18:22:32 +0000

    fixed bug where rows would not be displayed after a row with an error was submitted

--- reason_4.0/www/js/import_photos.js
+++ reason_4.0/www/js/import_photos.js
@@ -4,27 +4,25 @@
  * 1. Makes sure the next upload field is shown anytime an upload field is updated
  * 2. On load, ensure there is an upload field without a file or error available
  *
- * @requires Jquery
+ * @requires jQuery
  * @author Nathan White
  */
 
-$(document).ready(function()
-{
-	//hide_fields(); // hide the fields to start with
+jQuery(function($) {
 	var upload_rows = $("tr[id^='upload'][id$='Row']");
-	var error_last_index = $(upload_rows).index($("tr.error:last")[0]);
-	var file_last_index = $(upload_rows).index($("tr:has(input[name*='tmp_file']):last")[0]);
+	var error_last_index = upload_rows.index($("tr.error:last"));
+	var file_last_index = upload_rows.index($("tr:has(.uploaded_file):last"));
 	
-	if (file_last_index > error_last_index) var hide_after_index = (file_last_index + 1);
-	else if (error_last_index > "-1") var hide_after_index = error_last_index;
-	else var hide_after_index = 0;
+	var hide_after_index = Math.max(0, error_last_index, file_last_index + 1);
 	
-	$(upload_rows).each(function()
-	{
+	upload_rows.each(function() {
 		var index = $(upload_rows).index(this); // what is the index in the set of upload field rows?
-		if (index > hide_after_index) $(this).hide();
-		$(this).change(function() // change event to show next index
-		{
+		if (index > hide_after_index)
+			$(this).hide();
+		
+		// Show the next element when the value of the INPUT in this row
+		// changes.
+		$("input[type=file]", this).change(function() {
 			$(upload_rows).eq(index+1).show();
 		});
 	});
