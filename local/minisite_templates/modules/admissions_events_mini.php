70d724512b5f18b0ec22d10c0f53e0a1c6def239
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-09-16 15:15:48 +0000

    commiting changes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@90 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/admissions_events_mini.php
+++ local/minisite_templates/modules/admissions_events_mini.php
@@ -94,7 +94,7 @@ class miniEventsModule extends EventsModule
 	{
 	}
 
-	function show_event_list_item_standard( $event_id, $day ) // {{{
+	function show_event_list_item_standard( $event_id, $day )
         {
 	// Override - don't want time on admissions home page
                 //if($this->show_times && substr($this->events[$event_id]->get_value( 'datetime' ), 11) != '00:00:00')
@@ -105,7 +105,7 @@ class miniEventsModule extends EventsModule
                 echo '">';
                 echo $this->events[$event_id]->get_value( 'name' );
                 echo '</a>';
-        } // }}}
+        }
 
 }
 ?>
