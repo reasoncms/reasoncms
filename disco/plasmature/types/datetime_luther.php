2ea986e5a51bc4d9d005ee88487e19eb0fea6bd6
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-09-28 19:10:09 +0000

    adding disco/plasmature/types/datetime_luther.php
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@108 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ disco/plasmature/types/datetime_luther.php
@@ -0,0 +1,80 @@
+<?php
+reason_include_once('disco/plasmature/types/datetime.php');
+
+require_once PLASMATURE_TYPES_INC."default.php";
+require_once PLASMATURE_TYPES_INC."text.php";
+require_once PLASMATURE_TYPES_INC."hidden.php";
+require_once PLASMATURE_TYPES_INC."options.php";
+
+
+include_once('paths.php');
+include_once(PLASMATURE_TYPES_INC."datetime.php");
+
+class textDateTimeNoSecondsType extends textDateTimeType {
+	var $type = 'textDateTimeNoSeconds';
+	var $date_format = 'Y-m-d H:i';
+	var $use_fields = array( 'month', 'day', 'year','hour','minute','ampm',);
+
+
+	/*
+function get_year_display($year_val = '')
+	{
+	    // The classes on the year display activate the JavaScript
+	    // date picker.
+	    $class = ($this->use_picker)
+	        ? 'split-date fill-grid statusformat-l-cc-sp-d-sp-F-sp-Y'
+	        : null;
+		return $this->_get_display('year', $year_val, null, ' / ', 4,
+		    $class);
+	}
+*/
+	function get_hour_display($hour_val = '')
+	{
+		$str = '';
+		$str .= '<select id="'.$this->name.'hourElement" name="'.$this->name.'[hour]">';
+		$str .= '<option value="1"'.($hour_val == '1' ? ' selected="selected"': '').'>1</option>';
+		$str .= '<option value="2"'.($hour_val == '2' ? ' selected="selected"': '').'>2</option>';
+		$str .= '<option value="3"'.($hour_val == '3' ? ' selected="selected"': '').'>3</option>';
+		$str .= '<option value="4"'.($hour_val == '4' ? ' selected="selected"': '').'>4</option>';
+		$str .= '<option value="5"'.($hour_val == '5' ? ' selected="selected"': '').'>5</option>';
+		$str .= '<option value="6"'.($hour_val == '6' ? ' selected="selected"': '').'>6</option>';
+		$str .= '<option value="7"'.($hour_val == '7' ? ' selected="selected"': '').'>7</option>';
+		$str .= '<option value="8"'.($hour_val == '8' ? ' selected="selected"': '').'>8</option>';
+		$str .= '<option value="9"'.($hour_val == '9' ? ' selected="selected"': '').'>9</option>';
+		$str .= '<option value="10"'.($hour_val == '10' ? ' selected="selected"': '').'>10</option>';
+		$str .= '<option value="11"'.($hour_val == '11' ? ' selected="selected"': '').'>11</option>';
+		$str .= '<option value="12"'.($hour_val == '12' ? ' selected="selected"': '').'>12</option>';
+		$str .= '</select>';
+		return $str;
+		//return $this->_get_display($str, $hour_val, 'HH',
+		 //   '&nbsp;&nbsp; at ');
+	}
+	function get_minute_display($minute_val = '')
+	{
+	    $str = ' ';
+		$str .= '<select id="'.$this->name.'minuteElement" name="'.$this->name.'[minute]">';
+		$str .= '<option value="00"'.($minute_val == '00' ? ' selected="selected"': '').'>00</option>';
+		$str .= '<option value="15"'.($minute_val == '15' ? ' selected="selected"': '').'>15</option>';
+		$str .= '<option value="30"'.($minute_val == '30' ? ' selected="selected"': '').'>30</option>';
+		$str .= '<option value="45"'.($minute_val == '45' ? ' selected="selected"': '').'>45</option>';
+		$str .= '</select>';
+		return $str;
+	}
+/*
+	function get_second_display($second_val = '')
+	{
+		return $this->_get_display('second', $second_val, 'SS', ' : ');
+	}
+*/
+	function get_ampm_display($ampm_val)
+	{
+		$str = ' ';
+		$str .= '<select id="'.$this->name.'ampmElement" name="'.$this->name.'[ampm]">';
+		$str .= '<option value="am"'.($ampm_val == 'am' ? ' selected="selected"': '').'>AM</option>';
+		$str .= '<option value="pm"'.($ampm_val == 'pm' ? ' selected="selected"': '').'>PM</option>';
+		$str .= '</select>';
+		return $str;
+	}
+	
+}
+?>
