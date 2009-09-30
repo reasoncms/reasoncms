2ed7b80cd36bab80e98489299f3e7ed7c949cb08
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-09-30 20:51:29 +0000

    adding twitter module
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@113 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/twitter.php
@@ -0,0 +1,26 @@
+<?php
+	reason_include_once( 'minisite_templates/modules/default.php' );
+	
+	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'twitterModule';
+	
+	class twitterModule extends DefaultMinisiteModule
+	{
+		function init( $args = array() )
+		{
+			
+		}
+		function has_content()
+		{
+			return true;
+		}
+		function run()
+		{
+			echo '<div class="test">Hello World</div>'."\n";
+			echo '<div id="twtr-profile-widget"></div>'."\n";
+			echo '<script src="http://widgets.twimg.com/j/1/widget.js"></script>'."\n";
+			echo '<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">'."\n";
+			echo '<script>'."\n";
+			echo 'new TWTR.Widget({profile: true,id: \'twtr-profile-widget\',loop: true,width: 225,height: 300,theme:{shell: {background: \'#3082af\',color: \'#ffffff\'},tweets: {background: \'#ffffff\',color: \'#444444\',links: \'#1985b5\'}}}).render().setProfile(\'slylth\').start();	</script>';
+		}
+	}
+?>
