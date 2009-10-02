cc61afccf90974dc63e4f10cb9cf4eb1b0f59c63
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-02 21:53:29 +0000

    changes to luther_datetime.php
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@120 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- disco/plasmature/types/luther_datetime.php
+++ disco/plasmature/types/luther_datetime.php
@@ -1,29 +1,31 @@
 <?php
 
-include_once('paths.php');
+//include_once('paths.php');
 include_once(PLASMATURE_TYPES_INC . "datetime.php");
 
 class textDateTimeNoSecondsType extends textDateTimeType
 {
 	var $type = 'textDateTimeNoSeconds';
 	var $date_format = 'Y-m-d H:i';
+	var $use_fields = array( 'month', 'day', 'year','hour','minute','ampm',);
+	var $datepicker_arg = 'split-date fill-grid-no-select disable-days-7 statusformat-l-cc-sp-d-sp-F-sp-Y opacity-99 disable-20091126 disable-20091127 disable-20091128 disable-20091212 disable-20091219 disable-xxxx1224 disable-xxxx1225 disable-xxxx1226 disable-xxxx1231 disable-xxxx0101 disable-xxxx0102 disable-20100130 disable-20100320 disable-20100327 disable-20100402 disable-20100403 disable-20100531 disable-20100603 disable-20100515 disable-20100522 disable-20100529 disable-20100605 disable-20100612 disable-20100619 disable-20100626 disable-20100703 disable-20100710 disable-20100717 disable-20100724 disable-20100731 disable-20100807 disable-20100814 disable-20100821 disable-20100828 disable-20100904 disable-20100911 range-low-today range-high-20100912'; //the class argument for the JavaScript datepicker
 	var $type_valid_args = array(   'prepopulate',
 									#'date_format',
 									'year_max',
 									'year_min',
 									'use_picker',
-									'use_fields'
+									'datepicker_arg',
 								);
-	var $use_fields = array( 'month', 'day', 'year','hour','minute','ampm',);
 
 
 	
-function get_year_display($year_val = '')
+
+	function get_year_display($year_val = '')
 	{
 	    // The classes on the year display activate the JavaScript
 	    // date picker.
 	    $class = ($this->use_picker)
-	        ? 'split-date fill-grid statusformat-l-cc-sp-d-sp-F-sp-Y'
+	        ? 'split-date fill-grid-no-select disable-days-7 statusformat-l-cc-sp-d-sp-F-sp-Y opacity-99 disable-20091126 disable-20091127 disable-20091128 disable-20091212 disable-20091219 disable-xxxx1224 disable-xxxx1225 disable-xxxx1226 disable-xxxx1231 disable-xxxx0101 disable-xxxx0102 disable-20100130 disable-20100320 disable-20100327 disable-20100402 disable-20100403 disable-20100531 disable-20100603 disable-20100515 disable-20100522 disable-20100529 disable-20100605 disable-20100612 disable-20100619 disable-20100626 disable-20100703 disable-20100710 disable-20100717 disable-20100724 disable-20100731 disable-20100807 disable-20100814 disable-20100821 disable-20100828 disable-20100904 disable-20100911 range-low-today range-high-20100912'
 	        : null;
 		return $this->_get_display('year', $year_val, null, ' / ', 4,
 		    $class);
@@ -32,7 +34,7 @@ function get_year_display($year_val = '')
 
 	function get_hour_display($hour_val = '')
 	{
-		$str = '';
+		$str = ' at ';
 		$str .= '<select id="'.$this->name.'hourElement" name="'.$this->name.'[hour]">';
 		$str .= '<option value="1"'.($hour_val == '1' ? ' selected="selected"': '').'>1</option>';
 		$str .= '<option value="2"'.($hour_val == '2' ? ' selected="selected"': '').'>2</option>';
@@ -63,22 +65,5 @@ function get_year_display($year_val = '')
 		$str .= '</select>';
 		return $str;
 	}
-/*
-	function get_second_display($second_val = '')
-	{
-		return $this->_get_display('second', $second_val, 'SS', ' : ');
-	}
-*/
-	function get_ampm_display($ampm_val)
-	{
-		$str = ' ';
-		$str .= '<select id="'.$this->name.'ampmElement" name="'.$this->name.'[ampm]">';
-		$str .= '<option value="am"'.($ampm_val == 'am' ? ' selected="selected"': '').'>AM</option>';
-		$str .= '<option value="pm"'.($ampm_val == 'pm' ? ' selected="selected"': '').'>PM</option>';
-		$str .= '</select>';
-		return $str;
-	}
-	
 }
 ?>
-
