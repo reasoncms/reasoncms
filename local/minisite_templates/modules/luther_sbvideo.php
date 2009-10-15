947281e5e046b48bcbc239a66ebadf4b563e90ec
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-15 14:44:07 +0000

    added luther_sbvideo.php
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@137 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/luther_sbvideo.php
@@ -0,0 +1,46 @@
+<?php
+	reason_include_once( 'minisite_templates/modules/image_sidebar.php' );
+
+	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'LutherSbvideoModule';
+	
+	class LutherSbvideoModule extends ImageSidebarModule
+	{
+		var $es;
+		var $images;
+
+		var $acceptable_params = array(
+		'num_to_display' => '',
+		'caption_flag' => true,
+		'rand_flag' => false,
+		'order_by' => '' );
+
+		function init( $args = array() )
+		{
+			parent::init( $args );
+			$head_items =& $this->parent->head_items;
+		}
+		
+		function run()
+		{
+			$die = isset( $this->die_without_thumbmail ) ? $this->die_without_thumbnail : false;
+			$popup = isset( $this->show_popup_link ) ? $this->show_popup_link : true;
+			$desc = isset( $this->description ) ? $this->description : true;
+			$text = isset( $this->additional_text ) ? $this->additional_text : "";
+			
+			if ( !empty($this->parent->textonly) )
+				echo '<p>Sidebar Video</p>'."\n";
+			
+			echo '<div id="bannerleft">'."\n";
+			foreach( $this->images AS $id => $image )
+			{
+				if (preg_match("/video/", $image->get_value('keywords')))
+				{
+					$url = WEB_PHOTOSTOCK . $id . '.' . $image->get_value('image_type');
+					echo $image->get_value("content");
+					//break;
+				}
+			}
+			echo "</div>\n";
+		}
+	}
+?>
