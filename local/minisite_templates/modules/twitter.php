1295ce522b4efc0093be588a381dbb5f7c0452c5
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-16 20:02:19 +0000

    moving twitter module with lutherultimate as the username
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@141 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/twitter.php
+++ local/minisite_templates/modules/twitter.php
@@ -20,7 +20,7 @@
 			echo '<script src="http://widgets.twimg.com/j/1/widget.js"></script>'."\n";
 			echo '<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">'."\n";
 			echo '<script>'."\n";
-			echo 'new TWTR.Widget({profile: true,id: \'twtr-profile-widget\',loop: true,width: 225,height: 300,theme:{shell: {background: \'#3082af\',color: \'#ffffff\'},tweets: {background: \'#ffffff\',color: \'#444444\',links: \'#1985b5\'}}}).render().setProfile(\'slylth\').start();	</script>';
+			echo 'new TWTR.Widget({profile: true,id: \'twtr-profile-widget\',loop: true,width: 225,height: 300,theme:{shell: {background: \'#3082af\',color: \'#ffffff\'},tweets: {background: \'#ffffff\',color: \'#444444\',links: \'#1985b5\'}}}).render().setProfile(\'lutherultimate\').start();	</script>';
 		}
 	}
 ?>
