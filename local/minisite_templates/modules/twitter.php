6fbf697cb842d69dca0df47c6721a013e983dba2
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-16 22:32:48 +0000

    twtter modifications
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@149 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/twitter.php
+++ local/minisite_templates/modules/twitter.php
@@ -17,37 +17,37 @@
 		{
 			echo '<div id="twtr-profile-widget"></div>'."\n";
 			echo '<script src="http://widgets.twimg.com/j/2/widget.js"></script>'."\n";
-			echo '<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">'."\n";
+			//echo '<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">'."\n";
 			echo '<script>'."\n";
 			echo 'new TWTR.Widget({
-  version: 2,
-  type: \'profile\',
-  rpp: 6,
-  interval: 6000,
-  width: 239,
-  height: 300,
-  theme: {
-    shell: {
-      background: \'#ffffff\',
-      color: \'#b4bec9\'
-    },
-    tweets: {
-      background: \'#ffffff\',
-      color: \'#000000\',
-      links: \'#1985b5\'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: true,
-    live: true,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    behavior: \'default\'
-  }
-}).render().setUser(\'lutherultimate\').start();
-</script>';
+  				version: 2,
+			  	type: \'profile\',
+				rpp: 6,
+				interval: 6000,
+				width: 239,
+				height: 300,
+				theme: {
+				    shell: {
+				      background: \'#ffffff\',
+				      color: \'#b4bec9\'
+				    },
+				    tweets: {
+				      background: \'#ffffff\',
+				      color: \'#000000\',
+				      links: \'#1985b5\'
+    				}
+			  },
+			  features: {
+			    scrollbar: false,
+			    loop: true,
+			    live: true,
+			    hashtags: true,
+			    timestamp: true,
+			    avatars: false,
+			    behavior: \'default\'
+			  }
+			}).render().setUser(\'lutherultimate\').start();
+			</script>';
 		}
 	}
 ?>
