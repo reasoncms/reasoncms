7bd44fc0d46468d283a85cacd21aec1f5bf6f522
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-16 21:20:28 +0000

    twtter modifications
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@147 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/page_types_local.php
+++ local/minisite_templates/page_types_local.php
@@ -247,7 +247,7 @@ $GLOBALS['_reason_page_types_local'] = array(
 		'twitter' => array(
 			'sub_nav'=> 'twitter',
 			'sub_nav_2' => 'blurb',
-			'sub_nav_3' => 'blurb',
+			//'sub_nav_3' => 'blurb',
 		),
 );
 
