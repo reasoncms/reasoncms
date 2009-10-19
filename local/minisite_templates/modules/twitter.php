df0d7749b3672f8202c00f00307102763559b4c3
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-19 15:18:41 +0000

    removing commented code
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@150 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/twitter.php
+++ local/minisite_templates/modules/twitter.php
@@ -51,36 +51,3 @@
 		}
 	}
 ?>
-
-
-<script src="http://widgets.twimg.com/j/2/widget.js"></script>
-<script>
-new TWTR.Widget({
-  version: 2,
-  type: \'profile\',
-  rpp: 4,
-  interval: 10000,
-  width: 239,
-  height: 300,
-  theme: {
-    shell: {
-      background: \'#333333\',
-      color: \'#ffffff\'
-    },
-    tweets: {
-      background: \'#000000\',
-      color: \'#ffffff\',
-      links: \'#4aed05\'
-    }
-  },
-  features: {
-    scrollbar: false,
-    loop: true,
-    live: false,
-    hashtags: true,
-    timestamp: true,
-    avatars: false,
-    behavior: 'default'
-  }
-}).render().setUser(\'lutherultimate\').start();
-</script>
