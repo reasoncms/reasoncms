79ccf01cafd9ebb316809785f551b91ba2059e5f
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2009-10-23 21:50:48 +0000

    added texttimepublic type
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@165 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- disco/plasmature/types/datetime.php
+++ disco/plasmature/types/datetime.php
@@ -64,4 +64,31 @@ class textDateTimePublicType extends textDateTimeType
 		return $str;
 	}
 }
+
+class textTimePublicType extends textDateTimePublicType
+{
+	public $type = 'textTimePublic';
+	public $use_fields = array('hour','minute','ampm',);
+	
+	function get_hour_display($hour_val = '')
+	{
+		$str .= '<select id="'.$this->name.'hourElement" name="'.$this->name.'[hour]">';
+		$str .= '<option value="--"'.($hour_val == '--' ? ' selected="selected"': '').'>--</option>';
+		$str .= '<option value="1"'.($hour_val == '1' ? ' selected="selected"': '').'>1</option>';
+		$str .= '<option value="2"'.($hour_val == '2' ? ' selected="selected"': '').'>2</option>';
+		$str .= '<option value="3"'.($hour_val == '3' ? ' selected="selected"': '').'>3</option>';
+		$str .= '<option value="4"'.($hour_val == '4' ? ' selected="selected"': '').'>4</option>';
+		$str .= '<option value="5"'.($hour_val == '5' ? ' selected="selected"': '').'>5</option>';
+		$str .= '<option value="6"'.($hour_val == '6' ? ' selected="selected"': '').'>6</option>';
+		$str .= '<option value="7"'.($hour_val == '7' ? ' selected="selected"': '').'>7</option>';
+		$str .= '<option value="8"'.($hour_val == '8' ? ' selected="selected"': '').'>8</option>';
+		$str .= '<option value="9"'.($hour_val == '9' ? ' selected="selected"': '').'>9</option>';
+		$str .= '<option value="10"'.($hour_val == '10' ? ' selected="selected"': '').'>10</option>';
+		$str .= '<option value="11"'.($hour_val == '11' ? ' selected="selected"': '').'>11</option>';
+		$str .= '<option value="12"'.($hour_val == '12' ? ' selected="selected"': '').'>12</option>';
+		$str .= '</select>';
+		return $str. ' : ';
+	}
+	
+}
 ?>
