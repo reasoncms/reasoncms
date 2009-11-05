0115c0baecfad14a072b32b50a76282150ee061d
Nathan White <nwhite@carleton.edu>
2009-11-05 21:37:31 +0000

    updated php5 version of htmlpurifer to 4.0.0

--- htmlpurifier/htmlpurifier.php
+++ htmlpurifier/htmlpurifier.php
@@ -5,6 +5,6 @@
 	/**
 	 * Loads the HTML_Purifier class appropriate for the version of PHP
 	 */
-	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-3.1.1-standalone/HTMLPurifier.standalone.php' );
+	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-4.0.0-standalone/HTMLPurifier.standalone.php' );
 	else require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.5-standalone/HTMLPurifier.standalone.php' );
 ?>
