4efa590e85d3f21a5af03aecf321130ec9e8db7f
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-02-16 16:30:27 +0000

    added anthro key and secret to flickr
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@247 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/luther_flickr_slideshow.php
+++ local/minisite_templates/modules/luther_flickr_slideshow.php
@@ -14,7 +14,8 @@
        			require("phpFlickr/phpFlickr.php");
 
 			// flickr username, api key, and secret must be included in array below:
-			$flickr_account = array("luthercollegemedia" => array("5b298e650817ac77f14054abfc722b01", "f8a94f21e063f110"));
+			$flickr_account = array("luthercollegemedia" => array("5b298e650817ac77f14054abfc722b01", "f8a94f21e063f110"),
+			"lc.anthrolab" => array("6725551efcba6082283094d49a0c807f", "3dff123f2a053c3a"));
 
 			$site_id = $this->site_id;
 			$es = new entity_selector( $site_id );
