7af9c69584b7b7cc0e4d36a20b1cfe6eae0a9ea8
Matt Ryan <mryan@carleton.edu>
2010-02-27 00:03:50 +0000

    stub for the unique name sanity check script

---
+++ reason_4.0/www/scripts/db_maintenance/unique_name_sanity_check.php
@@ -0,0 +1,10 @@
+<?php
+	/**
+	 * Stub for the unique name sanity check script
+	 * No additional code should be placed in this file
+	 * @package reason
+	 * @subpackage scripts
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/db_maintenance/unique_name_sanity_check.php' );
+?>
