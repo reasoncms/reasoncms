baf542b46dd4a4793fe31818220267d169b30993
Matt Ryan <mryan@carleton.edu>
2010-03-02 00:46:41 +0000

    updated to use new crumb handling approach

--- reason_4.0/lib/core/minisite_templates/modules/policy_related.php
+++ reason_4.0/lib/core/minisite_templates/modules/policy_related.php
@@ -56,7 +56,10 @@ class RelatedPolicyModule extends PolicyModule
 			if(array_key_exists($this->request[ 'policy_id' ], $this->values))
 			{
 				$this->policy = new entity( $this->request[ 'policy_id' ] );
-				$this->parent->add_crumb( $this->policy->get_value( 'name' ) , '?policy_id=' . $this->request[ 'policy_id' ] );
+				if($crumbs = &$this->get_crumbs())
+				{
+					$crumbs->add_crumb( $this->policy->get_value( 'name' ) , '?policy_id=' . $this->request[ 'policy_id' ] );
+				}
 			}
 			else
 			{
