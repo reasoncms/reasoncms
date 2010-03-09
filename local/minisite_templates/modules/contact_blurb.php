9abbfed4e8f8e74110e2b7eada3d1f4a92f25b28
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-03-09 22:30:07 +0000

    moved div and contact info header into if statement
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@276 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/contact_blurb.php
+++ local/minisite_templates/modules/contact_blurb.php
@@ -7,15 +7,16 @@
 	{
 		function run() // {{{
 		{
-			echo '<div class="contact-info">'."\n";
-			echo '<h3>Contact Information</h3>'."\n";
 			$i = 0;
 			foreach( $this->blurbs as $blurb )
 			{
 				$i++;
 	
-				if (preg_match("/[Cc]ontact [Ii]nformation/", $blurb->get_value('name')))
-				echo $blurb->get_value('content');
+				if (preg_match("/[Cc]ontact [Ii]nformation/", $blurb->get_value('name'))){
+					echo '<div class="contact-info">'."\n";
+					echo '<h3>Contact Information</h3>'."\n";
+					echo $blurb->get_value('content');
+				}
 			}
 			echo '</div>'."\n";
 		} 
