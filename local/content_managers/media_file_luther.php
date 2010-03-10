822961a4a9510ae207d9aa8d612b6645b826462d
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-03-10 21:59:57 +0000

    initial add- adding comment for youTube format
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@277 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/content_managers/media_file_luther.php
@@ -0,0 +1,15 @@
+<?php
+reason_include_once( 'content_managers/media_file.php3');
+
+$GLOBALS[ '_content_manager_class_names' ][basename(__FILE__) ] = 'lutherAvFileManager';
+
+class lutherAvFileManager extends avFileManager
+{
+	function alter_data()
+	{
+		parent::alter_data();
+		
+		
+		$this->add_comments('media_format', form_comment('Note: <img src="/images/youtube_16.jpg"/> videos are Flash Multimedia (.swf) format.'));
+	}
+}
