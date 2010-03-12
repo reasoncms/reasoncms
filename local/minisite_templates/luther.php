70dfaedf88de1a9f0522b07877f8ee80d6e2b950
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-03-12 23:43:37 +0000

    add this changes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@280 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/luther.php
+++ local/minisite_templates/luther.php
@@ -138,8 +138,12 @@ class LutherTemplate extends MinisiteTemplate
 		}
 		echo '<!-- AddThis Button BEGIN -->'."\n";
 		echo '<div class="addthis_toolbox addthis_default_style">'."\n";
-		echo '<a href="http://www.addthis.com/bookmark.php?v=250&amp;pub=lutheraddthis" class=
-"addthis_button_compact">Share</a>'."\n";
+		echo '<a href="http://www.addthis.com/bookmark.php?v=250&amp;pub=lutheraddthis" class="addthis_button_compact"></a>'."\n";
+		echo '<span class="addthis_separator">|</span>'."\n";
+		echo '<a class="addthis_button_facebook"></a>'."\n";
+		echo '<a class="addthis_button_twitter"></a>'."\n";
+		echo '<a class="addthis_button_email"></a>'."\n";
+		echo '<a class="addthis_button_print"></a>'."\n";
 		echo '</div>'."\n";
 		echo '<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pub=lutheraddthis"></script>'."\n";
 		echo '<!-- AddThis Button END -->'."\n";
@@ -152,6 +156,10 @@ class LutherTemplate extends MinisiteTemplate
 
                 $this->run_section( 'imagetop' );
 		$this->luther_breadcrumbs();
+					if (!$this->luther_add_this_complete)
+					{
+						$this->luther_add_this();
+					}
 
                 if ($this->has_content( 'main_head' ))
                 {
