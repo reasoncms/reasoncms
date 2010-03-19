f4b2d3269623648c0073b93df8a4bbfdb2789a5b
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-03-19 23:34:21 +0000

    removed hard coding from lutherultimate moved to new twitter_feed_type
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@291 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/twitter.php
+++ local/minisite_templates/modules/twitter.php
@@ -15,39 +15,47 @@
 		}
 		function run()
 		{
-			echo '<div id="twtr-profile-widget">'."\n";
-			echo '<script src="http://widgets.twimg.com/j/2/widget.js"></script>'."\n";
-			//echo '<link href="http://widgets.twimg.com/j/1/widget.css" type="text/css" rel="stylesheet">'."\n";
-			echo '<script>'."\n";
-			echo 'new TWTR.Widget({
-  				version: 2,
-			  	type: \'profile\',
-				rpp: 6,
-				interval: 6000,
-				width: 239,
-				height: 300,
-				theme: {
-				    shell: {
-				      background: \'#ffffff\',
-				      color: \'#b4bec9\'
-				    },
-				    tweets: {
-				      background: \'#ffffff\',
-				      color: \'#000000\',
-				      links: \'#1985b5\'
-    				}
-			  },
-			  features: {
-			    scrollbar: false,
-			    loop: true,
-			    live: true,
-			    hashtags: true,
-			    timestamp: true,
-			    avatars: false,
-			    behavior: \'default\'
-			  }
-			}).render().setUser(\'lutherultimate\').start();
-			</script></div>';
+			$site_id = $this->site_id;
+			
+			$es = new entity_selector( $site_id );
+			$es->add_type( id_of( 'twitter_feed_type' ) );
+			$twitter_info = $es->run_one();
+
+			foreach ($twitter_info as $info)
+			{
+				echo '<div id="twtr-profile-widget">'."\n";
+				echo '<script src="http://widgets.twimg.com/j/2/widget.js"></script>'."\n";
+				echo '<script>'."\n";
+				echo 'new TWTR.Widget({
+	  				version: 2,
+				  	type: \'profile\',
+					rpp: ' . $info->get_value('twitter_posts') . ',
+					interval: 6000,
+					width: 239,
+					height: 200,
+					theme: {
+					    shell: {
+					      background: \'#ffffff\',
+					      color: \'#b4bec9\'
+					    },
+					    tweets: {
+					      background: \'#ffffff\',
+					      color: \'#000000\',
+					      links: \'#1985b5\'
+	    				}
+				  },
+				  features: {
+				    scrollbar: false,
+				    loop: true,
+				    live: true,
+				    hashtags: true,
+				    timestamp: true,
+				    avatars: false,
+				    behavior: \'default\'
+				  }
+				}).render().setUser(\'' . $info->get_value('name') . '\').start();
+				</script></div>';
+			}
 		}
 	}
 ?>
