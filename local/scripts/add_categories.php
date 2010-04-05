20a4f3eb4668c4629725c4c80354ab8cdcdbe668
(no author) <(no author)@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-04-05 21:55:36 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@306 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/scripts/add_categories.php
+++ local/scripts/add_categories.php
@@ -2,44 +2,15 @@
 include_once('reason_header.php');
 reason_include_once('function_libraries/relationship_finder.php');
 reason_include_once('function_libraries/admin_actions.php');
-// get site owns category relationship
-//$owns_id = relationship_finder(id_of('site'), id_of('category_type'));
 
-// select all categories on master admin site
-//$es = new entity_selector();
-//$es->add_type(id_of('category_type'));
-//$es->add_right_relationship(id_of('admissions'), $owns_id);
-//$result = $es->run_one();
-//pray ($result);
 
-// simpler way
-//$es = new entity_selector(id_of('admissions'));
-//$es->add_type(id_of('category_type'));
-//$result2 = $es->run_one();
-//pray ($result2);
-
-// select all sites with category ids
-// relationship is "owns" entity a is site, entity b is category
-// ummm ... this does not work because of the issue where tons of relationships are named "owns"
-// it will work in Reason 4 Beta 9 ...
-//$es = new entity_selector();
-//$es->add_type(id_of('site'));
-//$es->enable_multivalue_results();
-//$es->limit_tables();
-//$es->limit_fields();
-//$es->add_left_relationship_field('owns', 'entity', 'id', 'cat_id');
-//$es->add_left_relationship_field('owns', 'meta', 'description', 'cat_description');
-//$result3 = $es->run_one();
-//pray($result3);
-//echo 'hi';
-
-// this code causes each site to borrow all categories from the master admin site
+// this code causes each site to borrow all categories from the Luther Home site
 $es = new entity_selector();
 $es->add_type(id_of('site'));
-$es->add_relation('entity.id != ' . id_of('master_admin'));
+$es->add_relation('entity.id != ' . id_of('luther_home'));
 $sites = $es->run_one();
 
-$cat_es = new entity_selector(id_of('master_admin'));
+$cat_es = new entity_selector(id_of('luther_home'));
 $cat_es->add_type(id_of('category_type'));
 $cat_result = $cat_es->run_one();
 $cats = array_keys($cat_result);
