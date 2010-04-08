7e6eab8e028b44acc0f3104a767703d9863755e3
Nathan White <nwhite@carleton.edu>
2010-04-08 19:11:30 +0000

    added some documentation to try to convey that you cannot necessarily go from reason 4 beta pre 7 to reason 4 beta 8 without downloading the fileset corresponding to the release to which you want to upgrade

--- reason_4.0/www/upgrade.php
+++ reason_4.0/www/upgrade.php
@@ -9,14 +9,22 @@
 <title>Upgrade Reason</title>
 </head>
 <body>
-<h1>Upgrade Reason</h1>
+<h1>Upgrading Reason</h1>
+<p>Each new version of Reason includes a set of scripts that you should 
+run to update your database to work with the latest version of the Reason code base. 
+The scripts are designed to be used from one release to the next; you cannot necessarily update 
+a Reason database across multiple steps after downloading the most current code base.</p>
+<p>If you have trouble upgrading and you are using an old version of Reason, try downloading 
+the point release after the one you are currently using and upgrade incrementally.</p>
+<p><a href="http://apps.carleton.edu/opensource/reason/download/">Reason download page</a></p>
+<h2>Reason Upgrade Scripts</h2>
 <ul>
-<li><a href="./scripts/upgrade/4.0b7_to_4.0b8">Reason 4.0 Beta 7 to Beta 8</a></li>
-<li><a href="./scripts/upgrade/4.0b6_to_4.0b7">Reason 4.0 Beta 6 to Beta 7</a></li>
-<li><a href="./scripts/upgrade/4.0b5_to_4.0b6">Reason 4.0 Beta 5 to Beta 6</a></li>
-<li><a href="./scripts/upgrade/4.0b4_to_4.0b5">Reason 4.0 Beta 4 to Beta 5</a></li>
-<li><a href="./scripts/upgrade/4.0b3_to_4.0b4">Reason 4.0 Beta 3 to Beta 4</a></li>
-<li><a href="./scripts/upgrade/4.0b1_and_b2_to_4.0b3">Reason 4.0 Beta 1 or 2 to Beta 3</a></li>
+<li><a href="./scripts/upgrade/4.0b7_to_4.0b8/index.php">Reason 4.0 Beta 7 to Beta 8</a></li>
+<li><a href="./scripts/upgrade/4.0b6_to_4.0b7/index.php">Reason 4.0 Beta 6 to Beta 7</a></li>
+<li><a href="./scripts/upgrade/4.0b5_to_4.0b6/index.php">Reason 4.0 Beta 5 to Beta 6</a></li>
+<li><a href="./scripts/upgrade/4.0b4_to_4.0b5/index.php">Reason 4.0 Beta 4 to Beta 5</a></li>
+<li><a href="./scripts/upgrade/4.0b3_to_4.0b4/index.php">Reason 4.0 Beta 3 to Beta 4</a></li>
+<li><a href="./scripts/upgrade/4.0b1_and_b2_to_4.0b3/index.php">Reason 4.0 Beta 1 or 2 to Beta 3</a></li>
 </ul>
 </body>
 </html>
