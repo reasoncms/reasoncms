d1218c5a47aaeef6ba3ddc098f0a6f97cb64dd68
wilburb <wilburb@wilburweb.com>
2010-04-23 23:14:04 +0000

    testing
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@338 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/app_dev_on_call.php
+++ local/minisite_templates/modules/app_dev_on_call.php
@@ -91,7 +91,8 @@ function getPerson($client, $startDate, $endDate, $currentHour, $currentMinute)
        $startMinute = $startTime[1];
        
        // if we haven't reached the end hour or we are currently living the end hour
-       echo '</br>' . $event->title->text . ' ' . $startHour . ' ' . $endHour . ' ' . $currentHour;
+       //echo '</br>' . $event->title->text . ' ' . $startHour . ' ' . $endHour . ' ' . $currentHour;
+       echo 'current hour: '.$currentHour;
        if (($endHour >= $currentHour) and ($startHour <= $currentHour)) {
            //  if the end hour is the current hour we better check minutes
            if ($endHour == $currentHour) {
