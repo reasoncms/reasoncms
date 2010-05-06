3cafce957fbd520d4f2f06a40f2261a8784fc34a
Matt Ryan <mryan@carleton.edu>
2010-05-06 21:14:07 +0000

    added inclusion of dependency (reason header)

--- reason_4.0/lib/core/function_libraries/file_finders.php
+++ reason_4.0/lib/core/function_libraries/file_finders.php
@@ -4,7 +4,12 @@
  * @package reason
  * @subpackage function_libraries
  */
- 
+
+/**
+ * Include dependencies
+ */
+include_once( 'reason_header.php' );
+	
 /**
  * Finds files in both the core and local directories and merges them into one listing
  * @author Matt Ryan
