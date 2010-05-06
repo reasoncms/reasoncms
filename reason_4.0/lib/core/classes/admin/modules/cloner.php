0618e391e3b61af2ed83a60c7779f4a339da80a6
Matt Ryan <mryan@carleton.edu>
2010-05-06 21:25:35 +0000

    added more information about this half-baked module

--- reason_4.0/lib/core/classes/admin/modules/cloner.php
+++ reason_4.0/lib/core/classes/admin/modules/cloner.php
@@ -12,6 +12,27 @@
 	 * Cloner Module
 	 * A hidden module that only admins can use to clone entities
 	 * This will need to be made much more robust if we are to put it out for general consumption
+	 *
+	 * Part of the reason it isn't ready for prime time is that it only
+	 * modifies the database; if the item has any representation on the
+	 * filesystem (like images, assets, sites, etc.) it won't duplicate them;
+	 * also, strings that need to be different -- like filenames for assets
+	 * or base urls for sites -- are not changed, so the database can be put
+	 * into a bad state accidentally.
+	 * 
+	 * It also duplicates *all* relationships for the item, which is not
+	 * always desirable -- for example, if you duplicate an image you might
+	 * not want a duplicate of that image to all of a sudden appear on all
+	 * the sidebars where the image appeared. Or a site which borrowed the
+	 * original will all of a sudden also be borrowing its duplicate. It also
+	 * doesn't respect allowable relationship rules, so if the are
+	 * one-to-many or many-to-one relationships involved, you might end up
+	 * with the db in a bad state -- for example, if you duplicate a group
+	 * that is attached to a page, the page will now have two groups attached
+	 * to it, which is not supposed to happen.
+	 * 
+	 * So, it's sort of a loose cannon of an admin module. Until somebody has
+	 * the time to make it less dangerous, use it with care. :)
 	 * @autho Matt Ryan
 	 * @date 2006-05-26
 	 */
@@ -36,6 +57,7 @@
 				else
 				{
 					echo '<p>This will make a duplicate of the current entity.<p>'."\n";
+					echo '<p>Note that this will not duplicate anything on the filesystem, like images, assets, site folders, etc., and that it might duplicate relationships you might not want duplicated. Use this module with care!</p>'."\n";
 					echo '<p>Do you want to do that?</p>'."\n";
 				}
 				echo '<ul><li><a href="'.$this->admin_page->make_link( array( 'clone' => 'true' )).'">Yes</a></li><li><a href="'.$this->admin_page->make_link( array( 'cur_module' => 'Editor' )).'">No</a></li></ul>'."\n";
