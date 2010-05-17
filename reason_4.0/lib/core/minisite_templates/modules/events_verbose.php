d81894ccec7e84bf3314fd9a8a1c45865936fc1d
Matt Ryan <mryan@carleton.edu>
2010-05-17 14:28:54 +0000

    used the list_type parameter instead of overloading the show_event_list_item method

--- reason_4.0/lib/core/minisite_templates/modules/events_verbose.php
+++ reason_4.0/lib/core/minisite_templates/modules/events_verbose.php
@@ -1,37 +1,25 @@
-<?php 
+<?php
+/**
+ * @package reason
+ * @subpackage minisite_modules
+ */
+ 
+ /**
+  * include the base class and register the module with Reason
+  */
 	reason_include_once( 'minisite_templates/modules/events.php' );
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'VerboseEventsModule';
 
-
+/**
+ * A minisite module that includes additional information in listings (inc. description & location)
+ */
 class VerboseEventsModule extends EventsModule
 {
-	
-	function show_event_list_item( $event_id, $day ) // {{{
+	function handle_params( $params )
 	{
-		$link = $this->construct_link(array('event_id'=>$this->events[$event_id]->id(),'date'=>$day,'view'=>$this->calendar->get_view()));
-		//echo '<p class="name">';
-		echo '<a href="'.$link.'">';
-		echo $this->events[$event_id]->get_value( 'name' );
-		echo '</a>';
-		echo '<ul>'."\n";
-		if($this->events[$event_id]->get_value( 'description' ))
-		{
-			echo '<li>';
-			echo $this->events[$event_id]->get_value( 'description' );
-			echo '</li>'."\n";
-		}
-		$time_loc = array();
-		if(substr($this->events[$event_id]->get_value( 'datetime' ), 11) != '00:00:00')
-			$time_loc[] = prettify_mysql_datetime( $this->events[$event_id]->get_value( 'datetime' ), $this->list_time_format );
-		if($this->events[$event_id]->get_value( 'location' ))
-			$time_loc[] = $this->events[$event_id]->get_value( 'location' );
-		if (!empty($time_loc))
-		{
-			echo '<li>';
-			echo implode(', ',$time_loc);
-			echo '</li>'."\n";
-		}
-		echo '</ul>'."\n";
-	} // }}}
+		$this->acceptable_params['list_type'] = 'verbose';
+		parent::handle_params( $params );
+	}
+	
 }
 ?>
