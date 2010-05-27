e6423451b809a9e2f949055e657b66feb53416fa
brifishjones <jonebr01@gmail.com>
2010-05-27 00:00:05 +0000

    added alumni office to flickr
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@473 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/luther_flickr_slideshow.php
+++ local/minisite_templates/modules/luther_flickr_slideshow.php
@@ -18,6 +18,7 @@
 			"lc.anthrolab" => array("6725551efcba6082283094d49a0c807f", "3dff123f2a053c3a"),
 			"luthertkd" => array("2e80ed6a3ce811d45607da933862c0e5", "140daaf7539dd293"),
 			"lutherbookshop" => array("72d5d704e16ae8cefcfab73e046d9129", "30fd8bc6d7fb815c"),
+			"Luther_College_Alumni_Office" => array("d97a9bcb0bef39692eb8b0ac97c8887f", "b0d82d76b43bc6d6"),
 			"luthercollegechemistry" => array("4fe2ce650080400958007a505b980522", "cef37676c4424ae1"));
 
 			$site_id = $this->site_id;
