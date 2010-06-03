9b47970456d0e93e27a796b5262e162ca442c1fa
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-06-03 18:20:55 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@502 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/norse_calendar.php
@@ -0,0 +1,61 @@
+<?php
+	reason_include_once( 'minisite_templates/modules/default.php' );
+
+	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'norseCalendarModule';
+
+	class norseCalendarModule extends DefaultMinisiteModule
+	{
+		function init( $args = array() )
+		{
+
+		}
+		function has_content()
+		{
+			return true;
+		}
+		function run()
+		{
+			$site_id = $this->site_id;
+
+			$es = new entity_selector( $site_id );
+			$es->add_type( id_of( 'twitter_feed_type' ) );
+			$twitter_info = $es->run_one();
+
+			foreach ($twitter_info as $info)
+			{
+				echo '<div id="twtr-profile-widget">'."\n";
+				echo '<script src="http://widgets.twimg.com/j/2/widget.js"></script>'."\n";
+				echo '<script>'."\n";
+				echo 'new TWTR.Widget({
+	  				version: 2,
+				  	type: \'profile\',
+					rpp: ' . $info->get_value('twitter_posts') . ',
+					interval: 6000,
+					width: 239,
+					height: 200,
+					theme: {
+					    shell: {
+					      background: \'#ffffff\',
+					      color: \'#b4bec9\'
+					    },
+					    tweets: {
+					      background: \'#ffffff\',
+					      color: \'#000000\',
+					      links: \'#1985b5\'
+	    				}
+				  },
+				  features: {
+				    scrollbar: false,
+				    loop: true,
+				    live: true,
+				    hashtags: true,
+				    timestamp: true,
+				    avatars: false,
+				    behavior: \'default\'
+				  }
+				}).render().setUser(\'' . $info->get_value('name') . '\').start();
+				</script></div>';
+			}
+		}
+	}
+?>
