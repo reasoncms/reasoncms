8a312daef90c074cb1be8cbbdfccc491d59f34df
Nathan White <nwhite@carleton.edu>
2010-06-07 21:21:43 +0000

    simplified language, added methods get_activation_params, get_deactivation_params, available_for_module, active_for_module, is_available

--- reason_4.0/lib/core/classes/inline_editing.php
+++ reason_4.0/lib/core/classes/inline_editing.php
@@ -15,7 +15,7 @@ include_once( 'reason_header.php' );
  * - disable / enable inline editing
  * - get information on whether or not inline editing is enabled
  * - do any modules allow inline editing?
- * - does the user have permission for inline editing?
+ * - is inline editing available now for a module?
  *
  * If the constant REASON_ALLOWS_INLINE_EDITING is set to false, modules can still register but the following methods will always return false
  *
@@ -29,6 +29,14 @@ include_once( 'reason_header.php' );
 class ReasonInlineEditing
 {
 	var $modules;
+	
+	/**
+	 * Key on the query string used to identify the module that is active for inline editing
+	 *
+	 * @access private
+	 * @var string
+	 */
+	var $_module_identifier_key = 'inline_edit';
 	
 	/**
 	 * Constructor - should never be used directly - user get_reason_inline_editing() to get an instance of the class for a reason page_id
@@ -47,32 +55,123 @@ class ReasonInlineEditing
 	 * During the init phase, modules should register themselves with the class if they support inline editing
 	 *
 	 * @param obj module
-	 * @param boolean user_can_inline_edit
+	 * @param boolean available
 	 * @return int key
 	 */
-	function register_module(&$module, $user_can_inline_edit)
+	function register_module(&$module, $available)
 	{
 		$item['module'] =& $module;
-		$item['user_can_inline_edit'] = ($this->reason_allows_inline_editing()) ? $user_can_inline_edit : false;
-		$this->modules[] = $item;
+		$item['available'] = ($this->reason_allows_inline_editing()) ? $available : false;
+		$this->modules[$module->identifier] = $item;
 		$keys = array_keys($this->modules);
 		return array_pop($keys);
 	}
+		
+	/**
+	 * Returns true if a registered module allows inline editing (regardless of whether inline editing is enabled in the session or not).
+	 *
+	 * @return boolean
+	 */
+	function is_available()
+	{
+		$modules =& $this->get_registered_modules();
+		if (!empty($modules))
+		{
+			foreach ($modules as $v)
+			{
+				if ($this->available_for_module($v['module'], false)) return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Return true if a registered module is available for inline editing. By default, we additionally check if inline editing is enabled
+	 * in the session, but this check can be turned off in order to check if a module "would" allow inline editing if enabled.
+	 *
+	 * @param object module
+	 * @param boolean check_is_enabled default true
+	 * @return boolean
+	 */
+	function available_for_module($module, $check_is_enabled = true)
+	{
+		if ($this->reason_allows_inline_editing())
+		{
+			if ( !$check_is_enabled || ($this->is_enabled()))
+			{
+				$modules =& $this->get_registered_modules();
+				$identifier = $module->identifier;
+				if ( !empty($identifier) && isset($modules[$identifier]['available'])) return $modules[$identifier]['available'];
+			}
+		}
+		return false;
+	}
+	
+	function active_for_module($module)
+	{
+		$identifier = $module->identifier;
+		if (isset($_REQUEST[$this->_module_identifier_key]) && ($_REQUEST[$this->_module_identifier_key] == $identifier)) return true;
+		return false;
+	}	
+	
+	function get_activation_params($module)
+	{
+		$identifier = $module->identifier;
+		return array($this->_module_identifier_key => $identifier);
+	}
+	
+	function get_deactivation_params($module)
+	{
+		$identifier = $module->identifier;
+		return array($this->_module_identifier_key => '');
+	}
 	
 	/**
-	 * Return true if user can inline edit the module identified by key. If a key is not provided, return true if any module allows inline editing.
+	 * Checks to see if reason allows inline editing and if inline editing is enabled for the reason session.
 	 *
-	 * @param int key - a key referencing a module in $this->modules - the key was probably by the return value of register_module.
-	 * @return boolean whether or not user can inline edit
+	 * @return boolean or NULL if session is not started
 	 */
-	function has_inline_edit_privs( $key = NULL )
+	function is_enabled()
 	{
 		if (!$this->reason_allows_inline_editing()) return false;
-		$modules =& $this->get_registered_modules();
-		if ($key != NULL) return (isset($modules[$key]['user_can_inline_edit'])) ? $modules[$key]['user_can_inline_edit'] : false;
-		elseif ($modules) foreach ($modules as $k => $v)
+		$session =& get_reason_session();
+		if ($session->exists() && $session->has_started())
 		{
-			if ($v['user_can_inline_edit']) return true;
+			return ($session->get('inline_editing') == 'enabled');
+		}
+		return NULL;
+	}
+	
+	/**
+	 * Enable inline editing - sets session value inline_editing to enabled.
+	 *
+	 * @return boolean success or failure
+	 */	
+	function enable()
+	{
+		if (!$this->reason_allows_inline_editing()) return false;
+		$session =& get_reason_session();
+		if ($session->exists() && $session->has_started())
+		{
+			$session->set('inline_editing', "enabled");
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Disable inline editing - sets session value inline_editing to disabled.
+	 *
+	 * @return boolean success or failure
+	 */	
+	function disable()
+	{
+		if (!$this->reason_allows_inline_editing()) return false;
+		$session =& get_reason_session();
+		if ($session->exists() && $session->has_started())
+		{
+			$session->set('inline_editing', "disabled");
+			return true;
 		}
 		return false;
 	}
@@ -86,22 +185,6 @@ class ReasonInlineEditing
 		return $this->modules;
 	}
 	
-	/**
-	 * Checks to see if reason allows inline editing and if inline editing is enabled for the reason session.
-	 *
-	 * @return boolean or NULL if session is not started
-	 */
-	function is_inline_editing_enabled()
-	{
-		if (!$this->reason_allows_inline_editing()) return false;
-		$session =& get_reason_session();
-		if ($session->exists() && $session->has_started())
-		{
-			return ($session->get('inline_editing') == 'enabled');
-		}
-		return NULL;
-	}
-
 	/**
 	 * Returns the value of the constant REASON_ALLOWS_INLINE_EDITING or true if the constant is undefined.
 	 *
@@ -123,40 +206,6 @@ class ReasonInlineEditing
 			}
 		}
 		return $this->_reason_allows_inline_editing;
-	}
-	
-	/**
-	 * Enable inline editing - sets session value inline_editing to enabled.
-	 *
-	 * @return boolean success or failure
-	 */	
-	function enable_inline_editing()
-	{
-		if (!$this->reason_allows_inline_editing()) return false;
-		$session =& get_reason_session();
-		if ($session->exists() && $session->has_started())
-		{
-			$session->set('inline_editing', "enabled");
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Disable inline editing - sets session value inline_editing to disabled.
-	 *
-	 * @return boolean success or failure
-	 */	
-	function disable_inline_editing()
-	{
-		if (!$this->reason_allows_inline_editing()) return false;
-		$session =& get_reason_session();
-		if ($session->exists() && $session->has_started())
-		{
-			$session->set('inline_editing', "disabled");
-			return true;
-		}
-		return false;
 	}
 }
 
