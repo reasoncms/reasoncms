ba19dfa7041b02da1df6e20231197a215e3e8b21
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-06-11 18:36:51 +0000

    more changes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@541 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/mobile_footer.php
+++ local/minisite_templates/modules/mobile_footer.php
@@ -1,28 +1,21 @@
 <?php
-	reason_include_once( 'minisite_templates/modules/default.php' );
+reason_include_once( 'minisite_templates/modules/default.php' );
 
-	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'LutherFooterModule';
+$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'LutherFooterModule';
 
-	class LutherFooterModule extends DefaultMinisiteModule
-	{
-		function init( $args = array() )
-		{
+class LutherFooterModule extends DefaultMinisiteModule {
+    function init( $args = array() ) {
 
-		}
-		function has_content()
-		{
-			return true;
-		}
-		function run()
-		{
-			echo '<div id="foot">'."\n";
-                        echo '<center> Email burkaa01@luther.edu with Questions/Comments/Suggestions</center>'."\n";
-
-  			echo '<center><div>'."\n";
-  			echo '<div>Copyright '.date("Y").' &#8226'.'Luther College'."\n".'</div>';
-      			echo '<span class="street-address">700 College Drive Decorah,IA 52101</span>'."\n";
-	                echo '<div>Phone: 563-387-2000 or 800-4 LUTHER (<span class="tel">800-458-8437</span>)</div></div></center>'."\n";
-			google_analytics();
-		}
-	}
+    }
+    function has_content() {
+        return true;
+    }
+    function run() {
+        echo '<div id="foot">'."\n";
+        echo '<center><p></p>'."\n";
+        echo '<div>&#169 '.date("Y").'Luther College 563-387-2000 or 800-458-8437'."\n".'</div>';
+        echo '</center>'."\n";
+        google_analytics();
+    }
+}
 ?>
