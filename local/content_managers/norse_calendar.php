e60a091ea6e05df50f27967214be22b6a65e31ac
slylth <slylth@gmail.com>
2010-06-14 16:53:26 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@564 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/content_managers/norse_calendar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package reason
+ * @subpackage content_managers
+ */
+	/**
+	 * Register content manager with Reason
+	 */
+	$GLOBALS[ '_content_manager_class_names' ][ basename( __FILE__) ] = 'NorseCalendar';
+
+	/**
+	 * A content manager for text blurbs
+	 */
+	class NorseCalendar extends ContentManager
+	{
+		function alter_data()
+		{
+			$this->set_display_name('name', 'Norsekey Username');
+			$this->set_comments('name', form_comment('Enter the username the of Norse Calendar you wish to display.'));
+			
+		/*
+	$this->set_order(
+				array(
+					'name',
+					'twitter_username',
+					'twitter_posts',
+				)
+		);
+*/
+		}
+	}
+?>
