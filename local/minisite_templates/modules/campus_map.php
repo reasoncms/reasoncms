f480169f79dcaa8406a2f43a3a0b16db762ac308
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-06-16 23:37:23 +0000

    ordering
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@634 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/campus_map.php
+++ local/minisite_templates/modules/campus_map.php
@@ -90,7 +90,7 @@ class CampusMapModule extends DefaultMinisiteModule {
                         <option value="sampson">Sampson Hoffland Laboratories</option>
                     </select>
                 </td>
-                <p></p>
+                <p>blah</p>
                 <th>Departments:</th>
                 <td valign="top">
                     <select id="departments" name="departments" default="0" onchange="switchMenu(this.options[this.selectedIndex].value); buildingReset()">
