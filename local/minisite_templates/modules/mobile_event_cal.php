34f7fcff2bff3ac8d9f4a54a367e38764f41ddd2
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-06-28 15:41:21 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@824 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/mobile_event_cal.php
@@ -0,0 +1,20 @@
+<?php
+reason_include_once( 'minisite_templates/modules/default.php' );
+
+$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'MobileEventCalModule';
+
+class MobileEventCalModule extends DefaultMinisiteModule {
+    function init( $args = array() ) {
+        
+    }
+    function has_content() {
+        return true;
+    }
+    function run() {
+?>
+<iframe scrolling="no" height="600" frameborder="0" width="100%" style="border-width: 0pt;" src="https://www.google.com/calendar/hosted/luther.edu/embed?showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=luther.edu_7c6m91gqfh995svsqo7bqkvrf0%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago"></iframe>
+        
+<?php
+    }
+}
+?>
