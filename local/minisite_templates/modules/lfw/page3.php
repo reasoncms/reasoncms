c1b9f172f20698fb4f0fdfd21ba7e463da04abcc
slylth <slylth@gmail.com>
2010-06-30 20:15:48 +0000

    committing final changes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@857 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/lfw/page3.php
+++ local/minisite_templates/modules/lfw/page3.php
@@ -10,7 +10,7 @@
 //require_once( '/usr/local/webapps/reason/reason_package/carl_util/db/db.php' );
 include_once(TYR_INC . 'tyr.php');
 include_once('paths.php');
-class LFWConfirmation extends FormStep
+class LFWRegistrationConfirmation extends FormStep
 {
 	var $date_format = 'F, j Y';
 
@@ -20,38 +20,118 @@ class LFWConfirmation extends FormStep
 		$blurb = $this->get_thank_you_blurb();
 		echo '<div id="thankYouBlurb">' . $blurb . '</div>';
 		
-		$text = $this->controller->get_confirmation_text();
-		echo $text;
+		$title = $this->controller->get('title');
+		$first_name = $this->controller->get('first_name');
+		$middle_initial = $this->controller->get('middle_initial');
+		$last_name = $this->controller->get('last_name');
+		$street_address = $this->controller->get('street_address');
+		$city = $this->controller->get('city');
+		$state_province = $this->controller->get('state_province');
+		$zip = $this->controller->get('zip');
+		$home_phone = $this->controller->get('home_phone');
+		$office_phone = $this->controller->get('office_phone');
+		$cell_phone = $this->controller->get('cell_phone');
+		$email = $this->controller->get('e-mail');
+		$institution = $this->controller->get('institution');
+		$position_title = $this->controller->get('position_title');
+		$profession = $this->controller->get('profession');
+		$conference_fee = $this->controller->get('conference_fee');
+		$attend_banquet = $this->controller->get('attend_banquet');
+		$dietary_needs = $this->controller->get('dietary_needs');
+		$student_housing = $this->controller->get('student_housing');
+		$housing_gender = $this->controller->get('housing_gender');
+		$housing_student_type = $this->controller->get('housing_student_type');
 		
-		$this->email_alumni($text);
-		$this->email_registrant($blurb . $text);
+		$txt = '<div id="reviewLFWRegistration">'."\n";			
+		$txt .= '<ul>'."\n";
+		$txt .= '<li><strong>Date:</strong> '.date($this->date_format).'</li>'."\n";
+		//Format the name
+		$txt .= '<li>';
+		if ($title){
+			$txt .= '<strong>Name:</strong> '.$title.' ';
+		}
+		$txt .= $first_name.' ';
+		if ($middle_initial){ 
+			$txt .= $middle_initial.' ';
+		}
+		$txt .= $last_name.'</li>'."\n";
+		
+		$txt .= '<li><strong>Address:</strong>'."\n".$street_address."\n".$city.' '.$state_province.' '.$zip./*$country.*/'</li>'."\n";
+		if ($home_phone)
+		{
+			$txt .= '<li><strong>Home Phone:</strong> '.$home_phone.'</li>'."\n";
+		}
+		if ($office_phone)
+		{
+			$txt .= '<li><strong>Office Phone:</strong> '.$office_phone.'</li>'."\n";
+		}
+		if ($cell_phone)
+		{
+			$txt .= '<li><strong>Cell Phone:</strong> '.$cell_phone.'</li>'."\n";
+		}
+		$txt .= '<li><strong>Email:</strong> '.$email.'</li>'."\n";
+		if ($institution)
+		{
+			$txt .= '<li><strong>Institution:</strong> '.$institution.'</li>'."\n";
+		}
+		if($position_title)
+		{
+			$txt .= '<li><strong>Position Title:</strong> '.$position_title.'</li>'."\n";
+		}		
+		if ($profession )
+		{
+			$txt .= '<li><strong>Profession:</strong> '.$profession.'</li>'."\n";
+		}
+		if($conference_fee)
+		{
+			$txt .= '<li><strong>Conference Fee:</strong> $'.number_format( $conference_fee, 2, '.', ',' ).'</li>'."\n";
+		}
+		if ($attend_banquet == 'Yes')
+		{
+			$txt .= '<li><strong>Banquet Fee:</strong> $25</li>'."\n";
+		}
+		if($dietary_needs)
+		{
+			$txt .= '<li><strong>Special dietary needs:</strong> '.$dietary_needs.'</li>'."\n";
+		}
+		if ($student_housing == 'Yes')
+		{
+			$txt .= '<li><strong>Student Housing Needs:</strong> '.$housing_gender.', '.$housing_student_type.' for '.
+			$housing_nights.'</li>'."\n";
+		}
+		$txt .= '</ul>'."\n";
+		$txt .= '</div>'."\n";
+		
+		echo $txt;
+		$this->email_alumni($txt);
+		$this->email_registrant($blurb . $txt);
 		echo 'A copy of this confirmation has been sent to your email address.'."\n";
 				
 		connectDB('lfw_connection');
 		
 		$qstring = "INSERT INTO `registrants` SET  
-		title=".((!empty($this->trans_details['title'])) ? addslashes($this->trans_details['title']) : 'NULL').", 
-		first_name='".addslashes($this->trans_details['first_name'])."',
-		middle_initial='".((!empty($this->trans_details['middle_initial'])) ? addslashes($this->trans_details['middle_initial']) : 'NULL')."',
-		last_name='".addslashes($this->trans_details['last_name'])."', 
-		address='".addslashes($this->trans_details['address'])."',
-		city='".addslashes($this->trans_details['city'])."',
-		state_province = '".addslashes($this->trans_details['state_province'])."',
-		zip = '".addslashes($this->trans_details['zip'])."',
-		home_phone = '".((!empty($this->trans_details['home_phone'])) ? addslashes($this->trans_details['home_phone']) : 'NULL')."',
-		office_phone = '".((!empty($this->trans_details['office_phone'])) ? addslashes($this->trans_details['office_phone']) : 'NULL')."',
-		cell_phone = '".((!empty($this->trans_details['cell_phone'])) ? addslashes($this->trans_details['cell_phone']) : 'NULL')."',
-		email = '".addslashes($this->trans_details['e-mail'])."', 
-		institution='".((!empty($this->trans_details['institution'])) ? addslashes($this->trans_details['institution']) : 'NULL')."',
-		position_title='".((!empty($this->trans_details['position_title'])) ? addslashes($this->trans_details['position_title']) : 'NULL')."',
-		profession=".((!empty($this->trans_details['profession'])) ? addslashes($this->trans_details['profession']) : 'NULL').", 
-		conference_fee=".((!empty($this->trans_details['conference_fee'])) ? addslashes($this->trans_details['conference_fee']) : 'NULL').", 
-		attend_banquet=".((!empty($this->trans_details['attend_banquet'])) ? addslashes($this->trans_details['attend_banquet']) : 'NULL').",
-		dietary_needs='".((!empty($this->trans_details['dietary_needs'])) ? addslashes($this->trans_details['dietary_needs']) : 'NULL')."',
-		student_housing=".((!empty($this->trans_details['student_housing'])) ? addslashes($this->trans_details['student_housing']) : 'NULL').",
-		housing_gender=".((!empty($this->trans_details['housing_gender'])) ? addslashes($this->trans_details['housing_gender']) : 'NULL').",
-		housing_student_type=".((!empty($this->trans_details['housing_student_type'])) ? addslashes($this->trans_details['housing_student_type']) : 'NULL').",
-		housing_nights=".((!empty($this->trans_details['housing_nights'])) ? addslashes($this->trans_details['housing_nights']) : 'NULL')." ",
+		title='".((!empty($title)) ? addslashes($title) : 'NULL')."', 
+		first_name='".addslashes($first_name)."',
+		middle_initial='".((!empty($middle_initial)) ? addslashes($middle_initial) : 'NULL')."',
+		last_name='".addslashes($last_name)."', 
+		street_address='".addslashes($street_address)."',
+		city='".addslashes($city)."',
+		state_province = '".addslashes($state_province)."',
+		zip = '".addslashes($zip)."',
+		home_phone = '".((!empty($home_phone)) ? addslashes($home_phone) : 'NULL')."',
+		office_phone = '".((!empty($office_phone)) ? addslashes($office_phone) : 'NULL')."',
+		cell_phone = '".((!empty($cell_phone)) ? addslashes($cell_phone) : 'NULL')."',
+		email = '".addslashes($email)."', 
+		institution='".((!empty($institution)) ? addslashes($institution) : 'NULL')."',
+		institution_title='".((!empty($position_title)) ? addslashes($position_title) : 'NULL')."',
+		profession='".((!empty($profession)) ? addslashes($profession) : 'NULL')."', 
+		conference_fee='".((!empty($conference_fee)) ? addslashes($conference_fee) : 'NULL')."', 
+		attend_banquet='".((!empty($attend_banquet)) ? addslashes($attend_banquet) : 'NULL')."',
+		dietary_needs='".((!empty($dietary_needs)) ? addslashes($dietary_needs) : 'NULL')."',
+		student_housing='".((!empty($student_housing)) ? addslashes($student_housing) : 'NULL')."',
+		housing_gender='".((!empty($housing_gender)) ? addslashes($housing_gender) : 'NULL')."',
+		housing_student_type='".((!empty($housing_student_type)) ? addslashes($housing_student_type) : 'NULL')."',
+		housing_nights='".((!empty($housing_nights)) ? addslashes($housing_nights) : 'NULL')."'";
 		
 		
 		if(THIS_IS_A_DEVELOPMENT_REASON_INSTANCE || !empty( $this->_request[ 'tm' ] ) ){
@@ -73,7 +153,7 @@ class LFWConfirmation extends FormStep
 	
 	function email_alumni($text)
 	{
-		$mail = new Email('gilbertc@luther.edu', 'noreply@luther.edu','noreply@luther.edu', 'New Homecoming Registration '.date('mdY H:i:s'),strip_tags($text), $text);
+		$mail = new Email('slylth@gmail.com', 'noreply@luther.edu','noreply@luther.edu', 'New Homecoming Registration '.date('mdY H:i:s'),strip_tags($text), $text);
 		$mail->send();
 	}
 	function email_registrant($text)
