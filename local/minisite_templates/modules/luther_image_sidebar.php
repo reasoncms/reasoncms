2718668c1a3bcf5982c83096e80cf1d60c26a86b
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-06-30 15:50:24 +0000

    adding if site id == mobile site id
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@839 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/luther_image_sidebar.php
+++ local/minisite_templates/modules/luther_image_sidebar.php
@@ -35,7 +35,37 @@
 			{
 
                             if ($site_id == '276256'){
-                                return;
+                                if (!preg_match("/imagetop|bannerad|video|map/", $image->get_value('keywords')))
+				{
+					$url = WEB_PHOTOSTOCK . $id . '.' . $image->get_value('image_type');
+					$thumb = WEB_PHOTOSTOCK . $id . '_tn.' . $image->get_value('image_type');
+					$d = max($image->get_value('width'), $image->get_value('height')) / 125.0;
+					if (preg_match("/hide_caption/", $image->get_value('keywords')))
+					{
+						$caption = "";
+					}
+					elseif (preg_match("/[A-Za-z0-9]+/", $image->get_value('content')))
+					{
+						$caption = $image->get_value('content');
+					}
+					else
+					{
+						$caption = $image->get_value('description');
+					}
+					//echo "<div class=\"imageChunk\">";
+					//echo '<div class="imageChunk" style="width:' . $image->get_value('width')/4 .'px;">';
+					echo '<div class="figure" style="width:' . intval($image->get_value('width')/$d) .'px;">';
+                                        echo '<div class="figure" style="width:100%;">';
+					// show href to full size image with class and onclick for highslide
+                                        //showing site id: echo 'site id is this: '. $site_id . ' end';
+					echo '<a href="'. $url . '" class="highslide" onclick="return hs.expand(this, imageOptions)">';
+					echo '<img src="' . $thumb . '" border="0" alt="" title="Click to enlarge" />';
+					echo '</a>';
+
+					// show caption if flag is true
+					if ($this->params['caption_flag'] && $caption != "") echo $image->get_value('description') ;
+					echo "</div>\n";
+				}
                             }
                             else {
 				if (!preg_match("/imagetop|bannerad|video|map/", $image->get_value('keywords')))
@@ -59,7 +89,7 @@
 					//echo '<div class="imageChunk" style="width:' . $image->get_value('width')/4 .'px;">';
 					echo '<div class="figure" style="width:' . intval($image->get_value('width')/$d) .'px;">';
 					// show href to full size image with class and onclick for highslide
-                                        echo 'site id is this: '. $site_id . ' end';
+                                        //showing site id: echo 'site id is this: '. $site_id . ' end';
 					echo '<a href="'. $url . '" class="highslide" onclick="return hs.expand(this, imageOptions)">';
 					echo '<img src="' . $thumb . '" border="0" alt="' . $caption . '" title="Click to enlarge" />';
 					echo '</a>';
