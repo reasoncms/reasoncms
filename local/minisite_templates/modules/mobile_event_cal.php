120621b85452b572ace12941341bf07ff1c6ed81
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-07 21:32:37 +0000

    mobile_event
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@892 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/mobile_event_cal.php
+++ local/minisite_templates/modules/mobile_event_cal.php
@@ -14,7 +14,7 @@ class MobileEventCalModule extends DefaultMinisiteModule {
 ?>
 
 <div id="eventCal">
-<iframe scrolling="no" height="600" frameborder="0" width="100%" style="border-width: 0pt;" src="https://www.google.com/calendar/hosted/luther.edu/embed?showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=luther.edu_7c6m91gqfh995svsqo7bqkvrf0%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago"></iframe>
+<iframe scrolling="no" height="100%" frameborder="0" width="100%" style="border-width: 0pt;" src="https://www.google.com/calendar/hosted/luther.edu/embed?showTitle=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=luther.edu_7c6m91gqfh995svsqo7bqkvrf0%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FChicago"></iframe>
 </div>
 
 <?php
