565a84115cd33d18e7e282c90d761ac050a19761
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-13 17:17:48 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@908 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/mobile_visitor_home.php
@@ -0,0 +1,40 @@
+<?php
+reason_include_once( 'minisite_templates/modules/default.php' );
+$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'MobileIconHomeModule';
+
+class MobileIconHomeModule extends DefaultMinisiteModule {
+    function init( $args = array() ) {
+
+    }
+
+    function has_content() {
+        return true;
+    }
+
+    function run() {
+?>
+<p style=" margin:0; padding:5px 0 0 0;font-size:11px;font-family:Helvetica;font-weight:bold;text-align:center;color:red;">Welcome to Luther College!</p>
+
+<?php
+        echo '<div class="center">';
+        echo '<ol class="icon-menu">';
+        //echo '<li id="menu-cafcam">';
+        //echo '<a accesskey="3" href="https://reasondev.luther.edu/mobile/comingsoon/">Caf Cam</a>';
+        //echo '</li>';
+        echo '<li id="menu-map">';
+        echo '<a accesskey="3" href="map/">Campus Map</a>';
+        echo '</li>';
+        echo '<li id="menu-tour">';
+        echo '<a accesskey="4" href="tour/">Campus Tour</a>';
+        echo '</li>';
+        echo '<li id="menu-directions">';
+        echo '<a accesskey="5" href="directions/">Directions</a>';
+        echo '</li>';
+        echo '<li id="menu-home">';
+        echo '<a accesskey="11" href="http://reasondev.luther.edu/">Full Mobile</a>';
+        echo '</li>';
+        echo '</ol>';
+        echo '</div>';
+    }
+}
+?>
