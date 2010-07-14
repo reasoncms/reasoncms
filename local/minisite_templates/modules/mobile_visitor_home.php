050a40b4af5775fa2278ef068703ea897bd02ea5
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-14 18:50:16 +0000

    adding contact button to visitor page
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@921 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/mobile_visitor_home.php
+++ local/minisite_templates/modules/mobile_visitor_home.php
@@ -12,29 +12,30 @@ class MobileVisitorHomeModule extends DefaultMinisiteModule {
     }
 
     function run() {
-?>
-<p style=" margin:0; padding:5px 0 0 0;font-size:11px;font-family:Helvetica;font-weight:bold;text-align:center;color:red;">Welcome to Luther College!</p>
-
-<?php
+/*
+ * ?>
+ *
+ * <p style=" margin:0; padding:5px 0 0 0;font-size:11px;font-family:Helvetica;font-weight:bold;text-align:center;color:red;">Welcome to Luther College!</p>
+ *
+ * <?php
+ *
+ */
         echo '<div class="center">';
         echo '<ol class="icon-menu">';
-        //echo '<li id="menu-cafcam">';
-        //echo '<a accesskey="3" href="https://reasondev.luther.edu/mobile/comingsoon/">Caf Cam</a>';
-        //echo '</li>';
         echo '<li id="menu-map">';
-        echo '<a accesskey="3" href="map/">Campus Map</a>';
+        echo '<a accesskey="1" href="map/">Campus Map</a>';
         echo '</li>';
         echo '<li id="menu-tour">';
-        echo '<a accesskey="4" href="tour/">Campus Tour</a>';
+        echo '<a accesskey="2" href="tour/">Campus Tour</a>';
         echo '</li>';
         echo '<li id="menu-contact">';
-        echo '<a accesskey="5" href="/mobile/contact/">Contact</a>';
+        echo '<a accesskey="3" href="/mobile/contact/">Contact</a>';
         echo '</li>';
         echo '<li id="menu-directions">';
-        echo '<a accesskey="6" href="directions/">Directions</a>';
+        echo '<a accesskey="4" href="directions/">Directions</a>';
         echo '</li>';
         echo '<li id="menu-home">';
-        echo '<a accesskey="7" href="/mobile/">Full Mobile</a>';
+        echo '<a accesskey="5" href="/mobile/">Full Mobile</a>';
         echo '</li>';
         echo '</ol>';
         echo '</div>';
