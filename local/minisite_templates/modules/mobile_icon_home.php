ba6d1294cf53072603b9ae4f4b67069fdf89b29d
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-14 18:33:49 +0000

    adding admissions and visitors section logos/buttons
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@919 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/mobile_icon_home.php
+++ local/minisite_templates/modules/mobile_icon_home.php
@@ -12,47 +12,55 @@ class MobileIconHomeModule extends DefaultMinisiteModule {
     }
 
     function run() {
-?>
-<p style=" margin:0; padding:5px 0 0 0;font-size:11px;font-family:Helvetica;font-weight:bold;text-align:center;color:red;"><a style="color: red;"href="http://reasondev.luther.edu/mobile/visitor">Click for Visitor Experience</a></p>
-
-<?php
+/*
+ * ?>
+ * <p style=" margin:0; padding:5px 0 0 0;font-size:11px;font-family:Helvetica;font-weight:bold;text-align:center;color:red;"><a style="color: red;"href="http://reasondev.luther.edu/mobile/visitor">Click for Visitor Experience</a></p>
+ *
+ * <?php
+ */
         echo '<div class="center">';
         echo '<ol class="icon-menu">';
         //echo '<li id="menu-cafcam">';
         //echo '<a accesskey="3" href="https://reasondev.luther.edu/mobile/comingsoon/">Caf Cam</a>';
         //echo '</li>';
+        echo '<li id="menu-admissions">';
+        echo '<a accesskey="1" href="admissions/">Admissions</a>';
+        echo '</li>';
+        echo '<li id="menu-visitors">';
+        echo '<a accesskey="2" href="visitor/">Visitors</a>';
+        echo '</li>';
         echo '<li id="menu-calendar">';
-        echo '<a accesskey="2" href="calendar/">Calendar</a>';
+        echo '<a accesskey="3" href="calendar/">Calendar</a>';
         echo '</li>';
         echo '<li id="menu-map">';
-        echo '<a accesskey="3" href="map/">Campus Map</a>';
+        echo '<a accesskey="4" href="map/">Campus Map</a>';
         echo '</li>';
         echo '<li id="menu-tour">';
-        echo '<a accesskey="4" href="tour/">Campus Tour</a>';
+        echo '<a accesskey="5" href="tour/">Campus Tour</a>';
         echo '</li>';
         echo '<li id="menu-directions">';
-        echo '<a accesskey="5" href="directions/">Directions</a>';
+        echo '<a accesskey="6" href="directions/">Directions</a>';
         echo '</li>';
         echo '<li id="menu-directory">';
-        echo '<a accesskey="6" href="https://www.luther.edu/directory/m.directory/">Directory</a>';
+        echo '<a accesskey="7" href="https://www.luther.edu/directory/m.directory/">Directory</a>';
         echo '</li>';
         //echo '<li id="menu-find">';
         //echo '<a accesskey="7" href="https://find.luther.edu/">Find</a>';
         //echo '</li>';
         echo '<li id="menu-labstat">';
-        echo '<a accesskey="7" href="labs/">Lab Availability</a>';
+        echo '<a accesskey="8" href="labs/">Lab Availability</a>';
         echo '</li>';
         echo '<li id="menu-library_search">';
-        echo '<a accesskey="8" href="librarysearch/">Library Search</a>';
+        echo '<a accesskey="9" href="librarysearch/">Library Search</a>';
         echo '</li>';
         echo '<li id="menu-news">';
-        echo '<a accesskey="9" href="news/">News</a>';
+        echo '<a accesskey="10" href="news/">News</a>';
         echo '</li>';
         echo '<li id="menu-mail">';
-        echo '<a accesskey="10" href="http://mail.luther.edu/">Norse Mail</a>';
+        echo '<a accesskey="11" href="http://mail.luther.edu/">Norse Mail</a>';
         echo '</li>';
         echo '<li id="menu-home">';
-        echo '<a accesskey="11" href="http://www.luther.edu/">Full Site</a>';
+        echo '<a accesskey="12" href="http://www.luther.edu/">Full Site</a>';
         echo '</li>';
         echo '</ol>';
         echo '</div>';
