16773c933769db886c70917ac7b1d208cca2fad4
slylth <slylth@gmail.com>
2010-07-15 22:31:35 +0000

    took out a pray() which was showing "(empty)" on every page
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@930 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/norse_calendar.php
+++ local/minisite_templates/modules/norse_calendar.php
@@ -17,13 +17,14 @@ class norseCalendarModule extends DefaultMinisiteModule {
         $es->add_type( id_of( 'norse_calendar_type' ) );
         $es->add_right_relationship($this->cur_page->id(), relationship_id_of('page_to_norse_calendar'));
         $norse_calendar_info = $es->run_one();
-        pray($norse_calendar_info);
-
+		
+       	echo '<div class="norseCalendar">'."\n";
         foreach ($norse_calendar_info as $info) {
             echo '<meta name = "viewport" content = "width = device-width, height = device-height" />';
             echo '<iframe src="https://www.google.com/calendar/hosted/luther.edu/embed?showTitle=0&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src='. $info->get_value('name') .'%40luther.edu&amp;color=%23A32929&amp;ctz=America%2FChicago" style=" border-width:0 " width="100%" height="400" frameborder="0" scrolling="no"></iframe>';
             
         }
+        echo '</div>'."\n";
     }
 }
 ?>
