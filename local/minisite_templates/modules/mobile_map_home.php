6e9828d1dfad9850aaa32d923df397a805522a7b
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-16 17:41:07 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@934 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/mobile_map_home.php
@@ -0,0 +1,94 @@
+<?php
+reason_include_once( 'minisite_templates/modules/default.php' );
+$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'MobileMapHomeModule';
+
+class MobileMapHomeModule extends DefaultMinisiteModule {
+    function init( $args = array() ) {
+        
+    }
+    
+    function has_content() {
+        return true;
+    }
+    
+    function run() {
+        ?>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+    <style type="text/css">
+        #wrapper {
+            text-align:left;
+            margin:0 auto;
+        }
+        #wifi img{
+            z-index: 1;
+        }
+        
+        #link { color: blue; cursor:pointer; }
+        
+        #link:hover {
+            color: black;
+            background-color: #def ;
+        }
+        .containerdiv { float: left; position: relative; }
+        
+        .cornerimage { position: absolute; top: 0; left: 0; }
+        
+    </style>
+    <script type="text/javascript">
+        
+        function buildingReset() {
+            document.getElementById("buildings").selectedIndex = document.getElementById("buildings").getAttribute("default");
+        }
+        
+        function departmentReset() {
+            document.getElementById("departments").selectedIndex = document.getElementById("departments").getAttribute("default");
+        }
+        
+        function switchMenu(obj) {
+            hideAll();
+            var o = document.getElementById(obj);
+            o.style.display = '';
+        }
+        function show(obj) {
+            var o = document.getElementById(obj);
+            o.style.display = '';
+        }
+        function hide(obj) {
+            var o = document.getElementById(obj);
+            o.style.display = 'none';
+        }
+
+        function WinOpen() {
+            var url=document.redirect.selection.value
+            document.location.href=url
+
+        }
+        
+    </script>
+</head>
+        
+<body onload="buildingReset();departmentReset()">
+
+<form name="redirect1">
+<select id="buildings" name="buildings">
+  <option value="blank" selected></option>
+  <option value="olin/">Olin</option>
+  <option value="m.luther.edu">Test</option>
+</select>
+<input type=button value="Go" onClick="WinOpen();">
+</form>
+<form name="redirect2">
+<select id="departments" name="departments">
+  <option value="blank" selected></option>
+  <option value="http://www.java2s.com">Hi</option>
+  <option value="http://www.java2s.com">Bye</option>
+</select>
+<input type=button value="Go" onClick="WinOpen();">
+</form>
+
+</body>
+        <?php
+    }
+}
+?>
