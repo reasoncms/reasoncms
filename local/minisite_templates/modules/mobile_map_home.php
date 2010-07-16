5a2a454ef864c36001f4474ae57a50e6cd0c5e47
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-16 18:06:10 +0000

    adding some buildings
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@939 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/mobile_map_home.php
+++ local/minisite_templates/modules/mobile_map_home.php
@@ -4,13 +4,13 @@ $GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'MobileMapHo
 
 class MobileMapHomeModule extends DefaultMinisiteModule {
     function init( $args = array() ) {
-        
+
     }
-    
+
     function has_content() {
         return true;
     }
-    
+
     function run() {
         ?>
 <head>
@@ -23,28 +23,28 @@ class MobileMapHomeModule extends DefaultMinisiteModule {
         #wifi img{
             z-index: 1;
         }
-        
+
         #link { color: blue; cursor:pointer; }
-        
+
         #link:hover {
             color: black;
             background-color: #def ;
         }
         .containerdiv { float: left; position: relative; }
-        
+
         .cornerimage { position: absolute; top: 0; left: 0; }
-        
+
     </style>
     <script type="text/javascript">
-        
+
         function buildingReset() {
             document.getElementById("buildings").selectedIndex = document.getElementById("buildings").getAttribute("default");
         }
-        
+
         function departmentReset() {
             document.getElementById("departments").selectedIndex = document.getElementById("departments").getAttribute("default");
         }
-        
+
         function switchMenu(obj) {
             hideAll();
             var o = document.getElementById(obj);
@@ -68,29 +68,43 @@ class MobileMapHomeModule extends DefaultMinisiteModule {
             var url=document.redirect2.departments.value
             document.location.href=url
         }
-        
+
     </script>
 </head>
-        
+
 <body onload="buildingReset();departmentReset()">
 
 <th>Buildings:</th>
 <form name="redirect1">
-<select id="buildings" name="buildings">
-  <option value="blank" selected></option>
-  <option value="olin/">Olin</option>
-  <option value="m.luther.edu">Test</option>
-</select>
-<input type=button value="Go" onClick="WinOpen1();">
+    <select id="buildings" name="buildings">
+        <option value="blank" selected></option>
+        <option value="olin/">Olin</option>
+        <option value="koren/">Koren</option>
+        <option value="sampson/">Sampson Hoffland</option>
+    </select>
+    <input type=button value="Go" onClick="WinOpen1();">
 </form>
+<p></p>
 <th>Departments:</th>
 <form name="redirect2">
-<select id="departments" name="departments">
-  <option value="blank" selected></option>
-  <option value="http://www.java2s.com">Hi</option>
-  <option value="http://www.java2s.com">Bye</option>
-</select>
-<input type=button value="Go" onClick="WinOpen2();">
+    <select id="departments" name="departments">
+        <option value="blank" selected></option>
+        <option value="koren/">Africana Studies</option>
+        <option value="koren/">Anthropology</option>
+        <option value="koren/">Archaeology</option>
+        <option value="sampson/">Biology</option>
+        <option value="olin/">Business</option>
+        <option value="sampson/">Chemistry</option>
+        <option value="olin/">Computer Science</option>
+        <option value="olin/">Economics</option>
+        <option value="koren/">Education</option>
+        <option value="olin/">Math</option>
+        <option value="koren/">Political Science</option>
+        <option value="koren/">Sociology</option>
+        <option value="koren/">Social Work</option>
+        <option value="koren/">Women's and Gender Studies</option>
+    </select>
+    <input type=button value="Go" onClick="WinOpen2();">
 </form>
 
 </body>
