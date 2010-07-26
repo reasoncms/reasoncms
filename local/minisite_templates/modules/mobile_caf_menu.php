c695a00fa14f4609df7df7058da6c7047064d50f
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-07-26 16:30:21 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@968 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/mobile_caf_menu.php
@@ -0,0 +1,23 @@
+<?php
+reason_include_once( 'minisite_templates/modules/default.php' );
+$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'MobileCafMenuModule';
+
+class MobileCafMenuModule extends DefaultMinisiteModule {
+    function init( $args = array() ) {
+
+    }
+
+    function has_content() {
+        return true;
+    }
+
+    function run() {
+
+        
+        
+?>
+
+<?php
+    }
+}
+?>
