9b50b288ecb6e1a7e07b96ecc2549dddf796e4bb
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-08-16 23:30:22 +0000

    adding rooms to houseing
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1101 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/aaron_directory.php
+++ local/minisite_templates/modules/aaron_directory.php
@@ -503,7 +503,7 @@ class AaronDirectoryModule extends DefaultMinisiteModule {
                 echo "</td></tr>";
             }
             if (isset($data['studentresidencehallbldg'])) {
-                echo "<tr valign=top><td><b>Housing: </b></td><td>".$data['studentresidencehallbldg'][0]."</td></tr>";
+                echo "<tr valign=top><td><b>Housing: </b></td><td>".$data['studentresidencehallbldg'][0]." Room ".$data['studentresidencehallroom'][0]."</td></tr>";
             }
             if (isset($data['studentresidencehallphone'])) {
                 echo "<tr valign=top><td><b>Campus Phone: </b></td><td>".$data['studentresidencehallphone'][0]."</td></tr>";
