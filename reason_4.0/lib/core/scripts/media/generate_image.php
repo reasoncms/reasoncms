2192ef6e9060c55bf9a080ae58e07a2766a5a2f0
Nathan White <nwhite@carleton.edu>
2010-08-18 20:32:17 +0000

    first commit still needs to be implemented

---
+++ reason_4.0/lib/core/scripts/media/generate_image.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Serves up an image according to provided params.
+ *
+ * @package reason
+ * @subpackage scripts
+ */
+
+/**
+ * include dependencies
+ */
+include_once( 'reason_header.php' );
+
+// implement me.
+?>
