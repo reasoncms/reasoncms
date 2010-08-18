c92d5e299dab87789a1134318389732c632d2df8
Nathan White <nwhite@carleton.edu>
2010-08-18 20:28:46 +0000

    first commit stub that loads generate_image.php

---
+++ reason_4.0/www/displayers/feature_image.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Stub for the featured image generator
+ * @package reason
+ * @subpackage displayers
+ */
+ include_once('reason_header.php');
+ reason_include( 'scripts/media/generate_image.php');
+?>
