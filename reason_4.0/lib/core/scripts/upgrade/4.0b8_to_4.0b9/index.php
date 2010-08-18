83678592069655334cee39c18d3d8d0d9f3b683f
Mark Heiman <mheiman@carleton.edu>
2010-08-18 19:36:08 +0000

    added REASON_LOG_LOGINS setting info

--- reason_4.0/lib/core/scripts/upgrade/4.0b8_to_4.0b9/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b8_to_4.0b9/index.php
@@ -18,6 +18,23 @@
 </head>
 <body>
 <h2>Upgrade Reason from 4.0 Beta 8 to 4.0 Beta 9</h2>
+
+<h3>New Settings</h3>
+<p>Reason 4 Beta 9 introduces new settings:</p>
+<ol>
+<li>REASON_LOG_LOGINS. You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:<br />
+<textarea rows="8" cols="100">
+/**
+ * REASON_LOG_LOGINS
+ *
+ * The Reason login module can log all login and logout actions.  If you set this value to true,
+ * a log file will be populated at REASON_LOG_DIR/reason_login.log
+ */
+define('REASON_LOG_LOGINS', true);</textarea>
+</li>
+</ol>
+
 <h3>Scripts to Run</h3>
 <ul>
 <li><a href="feature.php">Adds the feature type to Reason</a></li>
