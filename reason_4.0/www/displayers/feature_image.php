be022a6b574148aa166c00586925861a7db97d3f
Nathan White <nwhite@carleton.edu>
2010-08-19 20:14:57 +0000

    removed this decided to go a different direction than on the fly image creation in php

--- reason_4.0/www/displayers/feature_image.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Stub for the featured image generator
- * @package reason
- * @subpackage displayers
- */
- include_once('reason_header.php');
- reason_include( 'scripts/media/generate_image.php');
-?>
