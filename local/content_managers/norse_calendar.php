3da18fdcea68e3bc7f05b0874d4074602267430d
slylth <slylth@gmail.com>
2010-08-21 00:55:51 +0000

    added text and error checking for 10 usernames
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1172 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/content_managers/norse_calendar.php
+++ local/content_managers/norse_calendar.php
@@ -13,10 +13,23 @@
 	 */
 	class NorseCalendar extends ContentManager
 	{
+		function run_error_checks()
+		{
+			$name_field = str_replace(' ','',$this->get_value('name'));
+			$name_array = explode(',',$name_field);
+			
+			if (sizeof($name_array) > 11)
+			{
+				$this->set_error('name', 'Please enter less than 10 usernames');
+			}
+			
+		
+		}
+		
 		function alter_data()
 		{
 			$this->set_display_name('name', 'Norsekey Username');
-			$this->set_comments('name', form_comment('Enter the username the of Norse Calendar you wish to display.'));
+			$this->set_comments('name', form_comment('Enter up to 10 usernames to display their Norse Calendars.'));
 			$this->add_required('default_view');
 			$this->change_element_type('default_view', 'radio_inline');
 			
