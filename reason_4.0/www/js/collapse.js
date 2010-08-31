ce2ec1824ff39764bff57f7d4d18aa99fc884ff4
Nathan White <nwhite@carleton.edu>
2010-08-31 20:54:45 +0000

    revamped to use jquery, improved documentation

--- reason_4.0/www/js/collapse.js
+++ reason_4.0/www/js/collapse.js
@@ -1,14 +1,21 @@
-///<summary>
-///Expand or collapse rows in Reason selection pages
-///with using a cookie to store the state during the brower session
-///</summary>
-///<remarks>
-///By: Henry Gross
-///April 23, 2007
-///</remarks>
+/*
+ * Expand or collapse rows in the Reason pages content manager. 
+ * Allows the use of cookies to store tree state, and auto-expands 
+ * to rows that are results of searches (highlighted rows). 
+ * 
+ * This is an overhauled and jQuerified version of a script 
+ * written by Henry Gross for use with Reason, April 23, 2007.  
+ * 
+ * @author Henry Gross (original version)
+ * @author Andrew Bacon (2010 revamp)
+ */
 
 $(document).ready(function()
 {
+
+	/** 
+	 * Just defining a few useful variables.
+	 */
 	var webPath = new String();
 	$("script[src$='/collapse.js']:first").each(function()
 	{
@@ -21,108 +28,104 @@ $(document).ready(function()
 	var OPEN_TOGGLER = webPath + "/ui_images/item_open.gif";
 	var tree;
 
-	//IE doesn't have this object, which is needed, so we are just creating what we need
-	if (!window.Node) {
-		Node = new Object();
-		Node.ELEMENT_NODE = 1;
+	/**
+	 * And now we run the thing!
+	 */
+	init();
+
+	/**
+	 * A simple wrapper for hiding an element and its children in the (NOT DOM) tree.
+	 * Adds a class which should be set to display: none;
+	 */
+	
+	function hideNode(nodes)
+	{
+		$(nodes).addClass('hidden');
+		$(nodes).each(function ()
+		{
+			hideNode(this.kids);
+		});
+	}
+	
+	/**
+	 * 'Collapses' a given node by assigning a css class which has 
+	 * { display: none; }, marks the internal representation of the node 
+	 * as being collapsed, changes the image, and stores the state of the
+	 * node in the cookie. 
+	 * 
+	 * @param object node the node to be collapsed.
+	 * @return void
+	 */
+	function collapse(node)
+	{
+		node.expand = false;
+		images = $('[class$="Toggler"] img', node);
+		images.attr("src", CLOSE_TOGGLER);
+		hideNode(node.kids)
+	//	$(node.kids).hide();
+		setCookie(node);
 	}
 
-	
-	// run the thing
-	init();
-	
-	///<summary>
-	///Collapse a node, hiding all of its descendents and setting the cookie appropriately
-	///</summary>
-	///<param name="node">The node to collapse</param>
-	function collapse(node) {
-		node.expanded = false;
-		images = node.getElementsByTagName("img");
-		if (images.length == 1)
-			if (images[0].parentNode.className.match("Toggler"))
-				images[0].src = CLOSE_TOGGLER;
+	function showNode(nodes)
+	{
+		$(nodes).removeClass('hidden');
+		$(nodes).each(function()
+		{
+			if (this.expand == true)
+				showNode(this.kids);
+		});
+	}
+
+	/**
+	 * 'Expands' a given node by removing the the css class with 
+	 * { display: none; }, marks the internal representation of the node 
+	 * as being expanded, changes the image, and stores the state of the
+	 * node in the cookie. 
+	 * 
+	 * @param object node the node to be collapsed.
+	 * @return void
+	 */	
+	function expand(node)
+	{
+		node.expand = true;
+		images = $('[class$="Toggler"] img', node);
+		images.attr("src", OPEN_TOGGLER);
+		showNode($(node.kids));
+	//	$(node.kids).show();
 		setCookie(node);
-		for (var i = 0; i < node.childs.length; i++)
-			hide(node.childs[i]);
-	}
+	};
 	
-	///<summary>
-	///Hides a node and all of its descendents
-	///</summary>
-	///<param name="node">The node to hide</param>
-	function hide(node) {
-		node.style.display = "none";
-		for (var i = 0; i < node.childs.length; i++)
-			hide(node.childs[i]);
-	}
-	
-	///<summary>
-	///Expands a node, showing all of its descendents (that are descended through only expanded nodes) and setting the cookie appropriately
-	///</summary>
-	///<param name="node">The node to expand</param>
-	function expand(node) {
-		node.expanded = true;
-		images = node.getElementsByTagName("img");
-		if (images.length == 1)
-			if (images[0].parentNode.className.match("Toggler"))
-				images[0].src = OPEN_TOGGLER;
-		setCookie(node);
-		for (var i = 0; i < node.childs.length; i++)
-			show(node.childs[i]);	
-	}
-	
-	///<summary>
-	///Shows a node and if its descendents are expanded, shows them too
-	///</summary>
-	///<param name="node">The node to show</param>
-	function show(node) {
-		node.style.display = "";
-		if (node.expanded)
-			for (var i = 0; i < node.childs.length; i++)
-				show(node.childs[i]);
-	}
-	
-	///<summary>
-	///Marks all the ancestors of a node to be expanded so that the node will be visible
-	///</summary>
-	///<param name="node">the node to be visible</param>
-	function expandTo(node) {
-		node = node.parent;
-		while (node != null && node.toExpand == false) {
-			node.toExpand = true;
-			node = node.parent;
+	/**
+	 * Expands all of a given node's direct ancestor nodes, so that we can
+	 * see it properly. For highlighted nodes from the search.
+	 * 
+	 * Keep in mind that we're talking about 'node' ancestry,
+	 * not DOM. 
+	 *
+	 * @param object node the node that we want to be able to see.
+	 */
+	function expandAncestors(node)
+	{
+		if (node.parent != undefined)
+		{
+			expand(node.parent);
+			expandAncestors(node.parent);
 		}
 	}
 	
-	///<summary>
-	///Traverse through the tree expanding the nodes that have been mmarked to be expanded
-	///</summary>
-	///<param name="node">The node to start expanding with</param>
-	function expandSelected(node) {
-		if (node.toExpand) {
-			node.expanded = true;
-			images = node.getElementsByTagName("img");
-			if (images.length == 1)
-				if (images[0].parentNode.className.match("Toggler"))
-					images[0].src = OPEN_TOGGLER;
-			for (var i = 0; i < node.childs.length; i++) {
-				node.childs[i].style.display = "";
-				if (node.childs[i].toExpand == true)
-					expandSelected(node.childs[i]);
-			}
-		}
-	}
-	
-	///<summary>
-	///Gets the value of the cookie that contains which rows are expanded, and the expanded value of the node specified
-	///</summary>
-	///<param name="node">The node to find the expanded value of</param>
-	///<returns>An array containing [0] The full value of the cookie containing which rows are expanded
-	///				[1] Everything until the expanded value of the specified node
-	///				[2] The expanded value of the specified node
-	///				[3] Everything after the expanded value of the specified node
-	///				or undefined if the cookie isn't set
-	///</returns>
+	/**
+	 * Loads in the cookie and slices it up twice: once to get only expansion
+	 * values, and then once to get only the value for the relevant node. 
+	 * 
+	 * Returns a four-member array:
+	 * [0] The full value of the cookie containing which rows are expanded
+	 * [1] Everything until the expanded value of the specified node
+	 * [2] The expanded value of the specified node
+	 * [3] Everything after the expanded value of the specified node,
+	 *     or undefined if the cookie isn't set
+	 * 
+	 * @param object node the node whose expanded value we want to get.
+	 */
 	function getCookie(node) {
 		var cookie;
 		var regex;
@@ -135,165 +138,148 @@ $(document).ready(function()
 		return regex.exec(cookie);
 	}
 	
-	///<summary>
-	///Set the expanded value of the specified node in the cookie, creating the cookie if needed
-	///</summary>
-	///<param name="node">The node to set the expanded value of in the cookie</param>
+	
+	/**
+	 * Load the cookie, then add the value of node.expand.
+	 * 
+	 * 
+	 * @param node The node that we're talking about
+	 * @return void
+	 */
 	function setCookie(node) {
 		var cookie;
-		
+		// Load the cookie.
 		cookie = getCookie(node);
+		// If no cookie is set for this node
 		if (cookie == undefined) {
 			cookie = COOKIE_REGEX.exec(document.cookie);
+			// AND no cookie is set for any node
 			if (cookie == undefined)
-				document.cookie = "expansion=" + node.id + "=" + node.expanded + "; path=/";
+				// Set a new cookie with this node.
+				document.cookie = "expansion=" + node.id + "=" + node.expand + "; path=/";
+			// If there are other nodes set,
 			else {
+				// Stick this one at the end and append the rest of the (non-collapse.js-related) cookies at the end. 
 				cookie = cookie[1];
-				document.cookie = "expansion=" + cookie + "," + node.id + "=" + node.expanded + "; path=/";
+				document.cookie = "expansion=" + cookie + "," + node.id + "=" + node.expand + "; path=/";
 			}
 		}
 		else {
-			document.cookie = "expansion=" + cookie[1] + node.expanded + cookie[3] + "; path=/";
+			// Stick this cookie in the middle. 
+			document.cookie = "expansion=" + cookie[1] + node.expand + cookie[3] + "; path=/";
 		}
 	}
 	
-	
-	
-	///<summary>
-	///Get the expanded value of the specified node from the cookie
-	///</summary>
-	///<param name="node">The node to find the expanded value of</param>
-	///<returns>True if the node is to be expanded, false if it is to be collapsed or if it doesn't exist in the cookie</returns>
+
+	/**
+	 * Is there an expand value in the cookie for this node? If not, return false.
+	 * If so, return its value.
+	 * 
+	 * @param node the node whose cookie we want to look for.  
+	 * @return bool the expand value of the node or undefined if it's not set. 
+	 */
 	function readCookie(node) {
-		var cookie;
-		
 		cookie = getCookie(node);
 		if (cookie == undefined)
 			return false;
-		else if (cookie[2] == "true")
-			return true
-		else if (cookie[2] == "false")
-			return false
+		else 
+			return cookie[2]
 	}
 	
-	///<summary>
-	///Add a click listener to the toggler links to toggle the expanded state of the row
-	///</summary>
-	function addListener(node) {
-		var links;
-		
-		links = node.getElementsByTagName("a");
-		for (var j = 0; j < links.length; j++)
-			if(links[j].className.match("Toggler") != null)
-				links[j].onclick = toggle;
-	}
 	
-	///<summary>
-	///Toggle the expanded state of the row based upon the click event received
-	///</summary>
-	///<param name="e">The click event which triggered the toggle</param>
-	function toggle(e) {
-		var target;
-		
-		target = (e != undefined) ? e.target : window.event.srcElement;
-		while (target.tagName.toLowerCase() != "tr")
-			target = target.parentNode;
-		if (target.expanded)
-			collapse(target);
-		else
-			expand(target);
-	}
-	
-	///<summary>
-	///Takes a html table and creates a treee showing the parent-child relationships of its rows.
-	///Also gets previous state from a cookie and only shows the rows that were previously visible.
-	///Will also expand so items that were searched for are visible.
-	///</summary>
-	///<param name="table"> A HTML table element to create a tree from</param>
 	function buildTree(table) {
-		for (var i = 0; i < table.childNodes.length; i++)
-			if (String(table.childNodes[i].tagName).toLowerCase() == "tbody") {
-				var node;
-				var currParent;
-	
-				node = table.childNodes[i].firstChild;
-				while (node != null) {
-					if (node.nodeType == Node.ELEMENT_NODE && node.tagName.toLowerCase() == "tr")
-						if (node.id.match("row")) {
-							node.key = node.id.substring(0, node.id.length - 3);
-							node.childs = new Array();
-							node.parent = null;
-							node.toExpand = false;
-							addListener(node);
-							if (readCookie(node)) {
-								node.expanded = true;
-								images = node.getElementsByTagName("img");
-								if (images.length == 1)
-									if (images[0].parentNode.className.match("Toggler"))
-										images[0].src = OPEN_TOGGLER;
-							}
-							else
-								node.expanded = false;
-							if (!node.className.match("childOf")) {
-								tree.push(node);
-								currParent = node;
-							} else {
-								var nodeClasses = node.className.split(" ");
-									for (var nodeClass in nodeClasses)
-									if (nodeClasses[nodeClass].substring(0, 7) == "childOf")
-										var parentKey = nodeClasses[nodeClass].substring(7);
-								while (parentKey != currParent.key)
-									currParent = currParent.parent;
-								node.parent = currParent;
-								currParent.childs.push(node);
-								currParent = node;
-							}
-							if (node.className.match("highlightRow"))
-								expandTo(node);
-						}
-					node = node.nextSibling;
+		$(table).children("tbody").each(function () {
+			var node;
+			var lastPossibleParent;
+			$(this).children('tr[id$="row"]').each(function () {
+				node = this;
+				id = $(this).attr("id")
+				node.key = id.substr(0, id.length - 3);
+				node.kids = new Array;
+				node.parent = null;
+			
+				// You need to bind to an event handler for open/close toggle here.
+
+				$('a[class$="Toggler"]', node).click(function (event) {
+					node = $(this).parent().parent()[0];
+					if (node.expand == true)
+					{
+						node.expand = false;
+						collapse(node);
+					} else {
+						node.expand = true;
+						expand(node);
+					}
+				});
+								
+				// If there's a cookie, check the cookie for the presence of this
+				// node. If it's there,
+				// --> node.expanded = true; 
+				// --> change image to OPEN_TOGGLER
+				// else set node.expanded = false;
+				if (readCookie(node) == "true")
+				{
+					node.expand = true;
+					images = $('[class$="Toggler"] img', node);
+					images.attr("src", OPEN_TOGGLER);
 				}
-			}
-		for (var i = 0; i < tree.length; i++) {
-			//printTree(tree[i]);
-			if (tree.length == 1)
-				tree[i].expanded = true;
-			syncTree(tree[i], true);
-			expandSelected(tree[i]);
-		}
+				
+				// Is the node a child of another node?
+				// If no, add it to the top of the tree and set it as the last possible parent!  
+				if (!$(this).attr("class").match('childOf'))
+				{
+					tree.push(node);
+					lastPossibleParent = node;
+				} else {
+					// Check the classes applied to the node. Find the 'childOf' element
+					var nodeClasses = $(node).attr("class").split(" ");
+					for (var nodeClass in nodeClasses)
+						if (nodeClasses[nodeClass].substring(0, 7) == "childOf")
+						// Get the entity id of the parent from the class.
+							var parentKey = nodeClasses[nodeClass].substring(7);
+					// Was the last node touched the parent? If not, try that node's parent!
+					while (lastPossibleParent.key != parentKey)
+						lastPossibleParent = lastPossibleParent.parent;
+					node.parent = lastPossibleParent;
+					lastPossibleParent.kids.push(node);
+					lastPossibleParent = node;
+				}
+			});
+				for (var i = 0; i < tree.length; i++) {
+					if (tree.length == 1)
+						tree[i].expand = true;
+					syncTree(tree[i], true);
+				}
+				$(".highlightRow").each(function () {
+					expandAncestors(this);
+				})
+		})
 	}
-	
-	///<summary>
-	///traverses the tree and hides or shows each node depending on its visibility
-	///</summary>
-	///<param name="node">the node to hide or show</param>
-	///<param name="show">True if the node should be visible</param>
+
+	/** 
+	 * Loops through the tree. Hides nodes with expand=false.
+	 * Run once after buildTree().
+	 * 
+	 * @param object node the node in question
+	 * @param show the value of the parent's node.expand. 
+	 */
 	function syncTree(node, show) {
 		if (!show)
-			node.style.display = "none";
-		for (var i = 0; i < node.childs.length; i++)
+			//$(node).hide();
+			$(node).addClass('hidden');
+		for (var i = 0; i < node.kids.length; i++)
 			if (!show)
-				syncTree(node.childs[i], false);
+				syncTree(node.kids[i], false);
 			else
-				syncTree(node.childs[i], node.expanded);
+				syncTree(node.kids[i], node.expand);
 	}
 	
-	///<summary>
-	///Print the tree depth-first starting from node using alerts
-	///</summary>
-	///<param name="node">the node to strat printing the tree from</param>
-	function printTree(node) {
-		alert (node.key)
-		for (var i = 0; i < node.childs.length; i++) {
-			printTree(node.childs[i]);
-		}
-	}
-	
-	///<summary>
-	///Things that need to be taken care of on load
-	///Loads the previous state, or expands it if it didn't have a prevous state
-	///Add listeners so that we can actually expand/collapse 
-	///</summary>
+	/**
+	 * A wrapper to do it all. Init some vars, then run buildTree
+	 * on the relevant elements.
+	 * 
+	 * @return void
+	 */
 	function init() {
 		var divs;
 		var rows;
@@ -301,13 +287,10 @@ $(document).ready(function()
 		
 		table = new Array();
 		tree = new Array();
-		divs = document.getElementsByTagName("div");
-		for (var i = 0; i < divs.length; i++)
-			if(divs[i].className == "list") {
-				var tables = divs[i].getElementsByTagName("table");
-				for (var j = 0; j < tables.length; j++)
-					buildTree(tables[j]);
-			}
+		tables = $("div.list table");
+		tables.each(function () {
+			buildTree(this);
+		});
 	}
 	
 
