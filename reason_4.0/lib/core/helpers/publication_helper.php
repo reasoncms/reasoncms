37715e27ea35d857bb31a305b63db947009a39f4
Nathan White <nwhite@carleton.edu>
2010-08-31 20:03:52 +0000

    support start and end dates

--- reason_4.0/lib/core/helpers/publication_helper.php
+++ reason_4.0/lib/core/helpers/publication_helper.php
@@ -15,6 +15,11 @@ reason_include_once( 'function_libraries/util.php' );
 
 class PublicationHelper extends entity
 {
+	var $start_date = false;
+	var $end_date = false;
+	var $date_changed = false;
+	var $limit = false;
+	var $limit_changed = false;
 	var $site_id;
 	var $issues;
 	
@@ -46,6 +51,16 @@ class PublicationHelper extends entity
 		return $this->site_id;
 	}
 	
+	function get_start_date()
+	{
+		return $this->start_date;
+	}
+	
+	function get_end_date()
+	{
+		return $this->end_date;
+	}
+	
 	/**
 	 * Return published items - published items are defined as:
 	 *
@@ -58,7 +73,8 @@ class PublicationHelper extends entity
 	 */
 	function &get_published_items( $limit_num = false, $force_refresh = false)
 	{
-		if (!isset($this->published_items) && !$force_refresh)
+		if ($limit_num != $this->limit) $this->limit_changed = true;
+		if ( !isset($this->published_items) || $this->date_changed || $this->limit_changed || $force_refresh )
 		{
 			$issued = ($this->get_value('has_issues') == 'yes');
 			$issues = ($issued) ? $this->get_published_issues() : false;
@@ -72,15 +88,54 @@ class PublicationHelper extends entity
 				$es->limit_fields(array('dated.datetime', 'status.status', 'show_hide.show_hide'));
 				$es->add_left_relationship( $this->id(), relationship_id_of('news_to_publication') );
 				$es->set_order('dated.datetime DESC');
+				if ($this->get_start_date()) $es->add_relation('dated.datetime >= "' . $this->get_start_date() .'"');
+				if ($this->get_end_date()) $es->add_relation('dated.datetime <= "' . $this->get_end_date() .'"'); 
 				if ($limit_num) $es->set_num($limit_num);
 				$es->add_relation("status.status != 'pending'");
 				if ($issues) $es->add_left_relationship_field( 'news_to_issue', 'entity', 'id', 'issue_id', array_keys($issues) );
 				$this->published_items = $es->run_one();
 			}
+			$this->date_changed = false;
+			$this->limit_changed = false;
+			$this->limit = $limit_num;
 		}
 		return $this->published_items;
 	}
+
+	/**
+	 * Specify a start date that limits what you care about for the publication
+	 *
+	 * @param string mysql_datetime format
+	 * @param normalize time to the start of the day (12:00 AM) - default true
+	 */
+	function set_start_date($start_date, $normalize = true)
+	{
+		if (is_mysql_datetime($start_date) || preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $start_date))
+		{
+			if ($normalize) $start_date = carl_date('Y-m-d 00:00:00', get_unix_timestamp($start_date));
+			$this->start_date = $start_date;
+			$this->date_changed = true;
+		}
+		else trigger_error('publication helper method set_start_date given a value that is not in mysql_datetime format - did not set the start date');
+	}
 	
+	/**
+	 * Specify an end date that limits what you care about for the publication
+	 *
+	 * @param string mysql_datetime format
+	 * @param normalize time to end of the day (11:59 PM) - default true
+	 */
+	function set_end_date($end_date, $normalize = true)
+	{
+		if (is_mysql_datetime($end_date) || preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $end_date))
+		{
+			if ($normalize) $end_date = carl_date('Y-m-d 11:59:59', get_unix_timestamp($end_date));
+			$this->end_date = $end_date;
+			$this->date_changed = true;
+		}
+		else trigger_error('publication helper method set_end_date given a value that is not in mysql_datetime format - did not set the end date');
+	}
+
 	function &get_published_issues()
 	{
 		$issues =& $this->get_issues();
