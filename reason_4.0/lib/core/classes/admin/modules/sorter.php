9af2d37f6a1b8a197df970e23fc9cfb5d88cdafc
Nathan White <nwhite@carleton.edu>
2010-08-31 21:09:40 +0000

    andrews updated javascript drag and drop sorter should fallback to work with js disabled as well

--- reason_4.0/lib/core/classes/admin/modules/sorter.php
+++ reason_4.0/lib/core/classes/admin/modules/sorter.php
@@ -8,6 +8,9 @@
   * Include the default module
   */
 	reason_include_once('classes/admin/modules/default.php');
+	include_once('carl_util/basic/url_funcs.php');
+	include_once(DISCO_INC . 'disco.php');
+	
 	
 	/**
 	 * An administrative module that provides a sorting widget for entities with the sort_order column
@@ -18,11 +21,11 @@
 	class sorter
 	{
 		var $admin_page;
-		function sorter( &$page ) // {{{
+		function sorter( &$page ) 
 		{
 			$this->admin_page = &$page;
-		} // }}}
-		function init() // {{{
+		} 
+		function init()
 		{
 			$es = $this->get_entity_selector();
 			$this->values = $es->run_one();
@@ -37,16 +40,16 @@
 					die();
 				}
 			}
-		} // }}}
-		function update_es( $es ) // {{{
+		} 
+		function update_es( $es ) 
 		{
 			return $es;
-		} // }}}
-		function show_extras() // {{{
+		} 
+		function show_extras() 
 		{
 			echo '&nbsp;';
-		} // }}}
-		function get_entity_selector() // {{{
+		} 
+		function get_entity_selector() 
 		{
 			$es = new entity_selector( $this->admin_page->site_id );
 			$es->add_type( $this->admin_page->type_id );
@@ -54,39 +57,25 @@
 			$es->set_sharing( 'owns' );
 			$es = $this->update_es( $es );
 			return $es;
-		} // }}}
-		function show_menu() // {{{
-		{
-			$num = count( $this->values );
-			if( $num )
-			{
-				$size = ( $num > 25 ? 25 : $num + 1 );
-				echo '<select multiple="multiple" size="'.$size.'" name="list2">';
-				foreach( $this->values AS $v )
-				{
-					echo '<option value="'.$v->id().'">'.strip_tags( $v->get_display_name() )."</option>\n";
-				}
-				echo '</select>';
-			}
-		} // }}}
-		function get_links() // {{{
+		} 
+		function get_links() 
 		{
 			$link = $this->admin_page->make_link( array( 'default_sort' => false ) , true );
 			$this->links = array( 'Sort All Items' => $link );
 			return $this->links;
-		} // }}}
-		function is_new() // {{{
+		} 
+		function is_new() 
 		{
 			if( empty( $this->admin_page->request[ 'default_sort' ] ) )
 				return false;
 			else
 				return true;
-		} // }}}
-		function show_links() // {{{
+		} 
+		function show_links() 
 		{
 			foreach( $this->links AS $name => $link )
 				echo '<a href="'.$link.'">'.$name."</a><br />\n";
-		} // }}}
+		} 
 	}
 
 	class SortingModule extends defaultModule
@@ -95,12 +84,15 @@
 		var $type_entity;
 		var $sorter;
 
-		function init() // {{{
+		function init() 
 		{
 			if(!reason_user_has_privs($this->admin_page->user_id, 'edit'))
 			{
 				return;
 			}
+			$this->admin_page->head_items->add_javascript(WEB_JAVASCRIPT_PATH .'jquery.mobile-tablednd.js');
+			$this->admin_page->head_items->add_javascript(WEB_JAVASCRIPT_PATH .'sorter.js');
+			$this->admin_page->head_items->add_stylesheet(REASON_HTTP_BASE_PATH . 'css/reason_admin/sorter.css');
 			$type_entity = new entity( $this->admin_page->type_id );
 			$this->type_entity = $type_entity;
 			$this->admin_page->title = 'Sorting ' . prettify_string( $type_entity->get_value( 'plural_name' ) );
@@ -114,8 +106,8 @@
 			else
 				$this->sorter = new sorter( $this->admin_page );
 			$this->sorter->init();
-		} // }}}
-		function run() // {{{
+		} 
+		function run() 
 		{
 			if(!reason_user_has_privs($this->admin_page->user_id, 'edit'))
 			{
@@ -125,131 +117,130 @@
 			$fields = get_fields_by_type( $this->admin_page->type_id );
 			if( is_array($fields) && in_array( 'sort_order' , $fields ) )
 			{
-				if( empty( $this->admin_page->request[ 'order' ] ) )
+				if( $this->sorter->is_new() )
 				{
-					if( $this->sorter->is_new() )
-					{
-						$this->sorter->show_links();
-					}
-					else
-					{
-						$this->show_top_code();
-						$this->show_bottom_code();
-					}
+					$this->sorter->show_links();
 				}
 				else
-					$this->set_order();
+				{
+					// Disco stuff goes here!
+					$sorterForm = new SorterForm;
+					$sorterForm->set_sorted_vals($this->sorter->values);
+					$sorterForm->run();
+//				var_dump(unhtmlentities( $_SESSION[ 'listers' ][ $this->admin_page->site_id ][ $this->admin_page->type_id ] ));
+				}
 			}
 			else
 				echo 'This type is not sortable.';
-		} // }}}
-		function show_top_code() // {{{
+		} 
+
+		function set_order() 
 		{
-		?>
-		<script language="JavaScript" type="text/javaScript">
-        <!-- Begin
-            function Moveup(dbox) 
-            {
-                for(var i = 0; i < dbox.options.length; i++) 
-                {
-                    if (dbox.options[i].selected && dbox.options[i] != "" && dbox.options[i] != dbox.options[0]) 
-                    {
-                        var tmpval = dbox.options[i].value;
-                        var tmpval2 = dbox.options[i].text;
-                        dbox.options[i].value = dbox.options[i - 1].value;
-                        dbox.options[i].text = dbox.options[i - 1].text
-                        dbox.options[i-1].value = tmpval;
-                        dbox.options[i-1].text = tmpval2;
-						
-						dbox.options[i-1].selected=true;
-						dbox.options[i].selected=false;
 
-						i = dbox.options.length;
-                    }
-                }
-            }
-            function Movedown(ebox) 
-            {
-                for(var i = 0; i < ebox.options.length; i++) 
-                {
-                    if (ebox.options[i].selected && ebox.options[i] != "" && ebox.options[i+1] != ebox.options[ebox.options.length]) 
-                    {
-                        var tmpval = ebox.options[i].value;
-                        var tmpval2 = ebox.options[i].text;
-                        ebox.options[i].value = ebox.options[i+1].value;
-                        ebox.options[i].text = ebox.options[i+1].text
-                        ebox.options[i+1].value = tmpval;
-                        ebox.options[i+1].text = tmpval2;
-						
-						ebox.options[i+1].selected=true;
-						ebox.options[i].selected=false;
-						
-						i = ebox.options.length;
-                    }
-                }
-            }
-			function cycle(ebox) {
-   			 var answer = '';
-    			for (var i = 0; i<ebox.options.length; i++) {
-    
-                	answer += ebox.options[i].value + ' ';
 
-    			}
-			document.sortForm.order.value = answer;
-			}
-			//  End -->
 
-            </script>
-
-		<?php
-		} // }}}
-		function show_bottom_code() // {{{
-		{
-		?>
-			<h5>To change the order of the list, simply highlight an item, then move it up and down using the arrows.</h5>
-  			<form method="post" name="sortForm" action="<?php echo htmlspecialchars($_SERVER[ 'PHP_SELF' ], ENT_QUOTES); ?>">
-            <table>
-            <tr>
-            <td>
-            <a href="#" onclick="javascript:Moveup(document.sortForm.list2);"><img src="<?php echo REASON_ADMIN_IMAGES_DIRECTORY; ?>arrow_up.gif" border="0" alt="up" /></a><br />
-            <a href="#" onclick="javascript:Movedown(document.sortForm.list2);"><img src="<?php echo REASON_ADMIN_IMAGES_DIRECTORY; ?>arrow_down.gif" border="0" alt="down" /></a><br />
-            </td>
-            <td>
-			<?php
-				$this->sorter->show_menu();		
-           	?>
-		   	</td>
-			<td>
-			<?php
-				$this->sorter->show_extras();
-			?>
-			</td>
-            </tr>
-            </table>
-			<input type="hidden" name="order" value="" />
-			<?php
-				foreach( $this->admin_page->request AS $k => $v )
-					echo '<input type="hidden" name="'.htmlspecialchars($k, ENT_QUOTES).'" value="'.htmlspecialchars($v, ENT_QUOTES).'" />' . "\n";
-			?>
-			<br /><br /><input type="submit" name="submit" value="Save Order" onclick="cycle(this.form.list2);" />
-            </form>
-		<?php
-		} // }}}
-		function set_order() // {{{
-		{
-			$ids = explode( ' ' ,  $this->admin_page->request[ 'order' ] );
-			$i = 1;
-			foreach( $ids AS $id )
-			{
-				if( $id )
+			if (!empty($items))
+				foreach( $items AS $id => $order )
 				{
-					$q = 'UPDATE sortable set sort_order = ' . $i . ' WHERE id = ' . $id;
-					db_query( $q , 'Error setting sort_order IN SortingModule::set_order()' );
-					$i++;
+						$q = 'UPDATE sortable set sort_order = ' . $order . ' WHERE id = ' . $id;
+						db_query( $q , 'Error setting sort_order IN SortingModule::set_order()' );
 				}
+			$this->admin_page->request['order'] = '';
+			
+			$redir = carl_make_redirect(array('order' => ''));
+			header('Location: ' . $redir);
+			exit();
+		}
+	}
+	
+	class SorterForm extends Disco
+	{
+		var $elements = array();
+		var $required = array();
+		var $sorted_values = array();
+		var $actions =  array('Save', 'Go Back');
+		
+		function set_sorted_vals($vals)
+		{
+			$this->sorted_values = $vals;
+		}
+		
+		function on_every_time()
+		{
+			$num = count($this->sorted_values);
+			$counter = 1;
+			foreach( $this->sorted_values AS $v )
+			{
+				$options = array();
+				for ($i = 1; $i<=$num; $i++)
+				{
+					if ($i != $counter)
+					{
+						if ($i != $counter + 1)
+						{
+							$tinyI = $i - .5;
+							$options["$tinyI"] = "Move before $i";
+						}
+					}
+					else 
+					{
+						$tinyI = $i - .5;
+						$default = $tinyI;
+						$options["$tinyI"] = "Don't move";
+						
+					}
+				}
+				$anotherTinyI = $i - .5;
+				$options["$anotherTinyI"] = "Move after " . ($i - 1);
+				$args['display_name'] = "$counter. " . strip_tags( $v->get_display_name() );
+				$args['options'] = $options;
+				$counter++;
+				$element_name = "sortOrder_{$v->get_value('id')}";
+				$this->add_element($element_name, 'select_no_sort', $args);
+				$this->set_value($element_name, $default);
 			}
-			header( 'Location: ' . unhtmlentities( $_SESSION[ 'listers' ][ $this->admin_page->site_id ][ $this->admin_page->type_id ] ) ); 
-			die();
-		} // }}}
+		}
+
+
+		function process()
+		{
+			if ($this->get_value('chosen_action') == '0')
+			{
+				$items = array();
+				foreach ($this->get_element_names() as $name)
+				{
+					if (preg_match("/^sortOrder_/i", $name))
+					{
+						$order = $this->get_value($name);
+						$name = preg_replace("/^sortOrder_/i", "", $name);
+						$items[$name] = $order;
+					}
+				}
+				asort($items);
+				$sort_order = 1;
+				foreach ($items as $k => $v)
+				{
+					$items[$k] = $sort_order;
+					$sort_order++;
+				}
+				
+				if (!empty($items))
+					foreach( $items AS $id => $order )
+					{
+							$q = 'UPDATE sortable set sort_order = ' . $order . ' WHERE id = ' . $id;
+							db_query( $q , 'Error setting sort_order IN SortingModule::set_order()' );
+					}
+			}
+
+		}
+		
+		function where_to() 
+		{
+			if ($this->get_chosen_action() == '0')
+				return @carl_make_redirect();
+			else 
+				return carl_make_redirect(array('cur_module'=>'Lister', 'state'=>'live'));
+		
+		}
 	}
 ?>
