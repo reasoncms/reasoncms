fd1fe02aa9dcccb7b3f33f69f350a4b013a8e31e
Nathan White <nwhite@carleton.edu>
2010-08-31 20:18:54 +0000

    first commit web facing aspects of the page type wizard

---
+++ reason_4.0/www/scripts/developer_tools/page_type_wizard/wizard.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * Stub for the page type wizard script.
+	 *
+	 * No additional code should be placed in this file
+	 *
+	 * @package reason
+	 * @subpackage scripts
+	 */
+	include_once( 'reason_header.php' );
+	reason_include_once( 'scripts/developer_tools/page_type_wizard/wizard.php' );
+?>
