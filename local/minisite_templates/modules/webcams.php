1ef61ba30fbb77c285c808e2420163adf015ab3e
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-09-08 00:04:04 +0000

    webcams test page
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1213 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/webcams.php
+++ local/minisite_templates/modules/webcams.php
@@ -19,11 +19,15 @@ class WebcamsModule extends DefaultMinisiteModule
       echo '<h2>Caf Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/caf_cam_link.png" width="200" height="150" /></a>';
       echo 'The Caf Cam is a great thing, this is a test.</p>';
+      echo '<h2>Main Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/weather_cam_link.png" width="200" height="150" /></a>';
-
+      echo 'This is a test.</p>';
+      echo '<h2>Olin Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/olin_cam_link.png" width="200" height="150" /></a>';
-
+      echo 'This is a test.</p>';
+      echo '<h2>Eagle Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/eagle_cam_link.png" width="200" height="150" /></a>';
+      echo 'This is a test.</p>';
   }
 }
 ?>
