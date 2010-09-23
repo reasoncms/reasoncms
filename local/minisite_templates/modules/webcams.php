2b04198ebd2bb89b86ed76985bcd880c9a55727b
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-09-23 23:51:20 +0000

    fixing up webcams (columns and icons)
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1261 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/webcams.php
+++ local/minisite_templates/modules/webcams.php
@@ -16,6 +16,30 @@ class WebcamsModule extends DefaultMinisiteModule
 
   function run()
   {
+      echo '<div style="float: left; width: 48%;">';
+
+      echo '<h2>Caf Cam</h2><p>';
+      echo '<a href="http://www.luther.edu/dining/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/caf_cam_link.png" width="200" height="150" /></a>';
+      echo '</p>';
+      echo '<h2>Eagle Cam</h2><p>';
+      echo '<a href="http://www.luther.edu/eaglecam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/eagle_cam_link.png" width="200" height="150" /></a>';
+      echo '</p>';
+
+      echo '</div>';
+
+
+      echo '<div style="float: right; width: 48%;">';
+
+      echo '<h2>KCRG Weather Cam</h2><p>';
+      echo '<a href="http://mesonet.agron.iastate.edu/data/camera/640x480/KCRG-013.jpg"><img src="https://reasondev.luther.edu/images/luther2010/webcams/weather_cam_link.png" width="200" height="150" /></a>';
+      echo '</p>';
+      echo '<h2>Olin Cam</h2><p>';
+      echo '<a href="http://www.luther.edu/computerscience/olinview/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/olin_cam_link.png" width="200" height="150" /></a>';
+      echo '</p>';
+
+      echo '</div>';
+
+      /*
       echo '<h2>Caf Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/caf_cam_link.png" width="200" height="150" /></a>';
       echo 'The Caf Cam is a great thing, this is a test.</p>';
@@ -28,6 +52,7 @@ class WebcamsModule extends DefaultMinisiteModule
       echo '<h2>Eagle Cam</h2><p>';
       echo '<a href="https://reasondev.luther.edu/x/webcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/eagle_cam_link.png" width="200" height="150" /></a>';
       echo 'This is a test.</p>';
+       */
   }
 }
 ?>
