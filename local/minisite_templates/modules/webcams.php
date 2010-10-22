65f294338fc67e4650b9194b15d401aea80a64a5
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2010-10-22 18:47:11 +0000

    updating webcams
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1290 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/webcams.php
+++ local/minisite_templates/modules/webcams.php
@@ -19,10 +19,10 @@ class WebcamsModule extends DefaultMinisiteModule
       echo '<div style="float: left; width: 48%;">';
 
       echo '<h2>Caf Cam</h2><p>';
-      echo '<a href="https://reasondev.luther.edu/dining/cafcam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/caf_cam_link.png" width="200" height="150" /></a>';
+      echo '<a href="https://www.luther.edu/dining/cafcam/"><img src="https://reason.luther.edu/images/luther2010/webcams/caf_cam_link.png" width="200" height="150" /></a>';
       echo '</p>';
       echo '<h2>Eagle Cam</h2><p>';
-      echo '<a href="http://www.luther.edu/eaglecam/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/eagle_cam_link.png" width="200" height="150" /></a>';
+      echo '<a href="http://www.luther.edu/eaglecam/"><img src="https://reason.luther.edu/images/luther2010/webcams/eagle_cam_link.png" width="200" height="150" /></a>';
       echo '</p>';
 
       echo '</div>';
@@ -31,10 +31,10 @@ class WebcamsModule extends DefaultMinisiteModule
       echo '<div style="float: right; width: 48%;">';
 
       echo '<h2>KCRG Weather Cam</h2><p>';
-      echo '<a href="http://mesonet.agron.iastate.edu/data/camera/640x480/KCRG-013.jpg"><img src="https://reasondev.luther.edu/images/luther2010/webcams/weather_cam_link.png" width="200" height="150" /></a>';
+      echo '<a href="http://mesonet.agron.iastate.edu/data/camera/640x480/KCRG-013.jpg"><img src="https://reason.luther.edu/images/luther2010/webcams/weather_cam_link.png" width="200" height="150" /></a>';
       echo '</p>';
       echo '<h2>Olin Cam</h2><p>';
-      echo '<a href="http://www.luther.edu/computerscience/olinview/"><img src="https://reasondev.luther.edu/images/luther2010/webcams/olin_cam_link.png" width="200" height="150" /></a>';
+      echo '<a href="http://www.luther.edu/computerscience/olinview/"><img src="https://reason.luther.edu/images/luther2010/webcams/olin_cam_link.png" width="200" height="150" /></a>';
       echo '</p>';
 
       echo '</div>';
