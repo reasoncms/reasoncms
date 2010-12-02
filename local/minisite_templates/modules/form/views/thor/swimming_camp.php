42605ae5a72950cb659d702e7715fb7596f0294f
slylth <slylth@gmail.com>
2010-12-02 00:53:47 +0000

    changes based on susan's form
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1329 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/swimming_camp.php
+++ local/minisite_templates/modules/form/views/thor/swimming_camp.php
@@ -14,10 +14,21 @@ class SwimmingCampThorForm extends CreditCardThorForm
 		$this->change_element_type($p_element,'radio_no_sort', array(
 			'display_name' => 'Camper Type',
 			'options' => array(
+/*
 				'$'.number_format(385,2,'.','')=>'Full Resident - $385.00 (All meals, housing, materials)',
-				'$'.number_format(240,2,'.','')=>'Commuter - $240.00 (materials only - no meals)')
+				'$'.number_format(50,2,'.','')=>'or Full Resident deposit - $50.00 (non-refundable)',
+				'$'.number_format(240,2,'.','')=>'Commuter - $240.00 (materials only - no meals)'),
+				'$'.number_format(50,2,'.','')=>'or Commuter deposit - $50.00 (non-refundable)'
+*/			
+				'Full' => 'Full Resident - $385.00 (All meals, housing, materials)',
+				'Full-deposit' =>'or Full Resident deposit - $50.00 (non-refundable)',
+				'Commuter' =>'Commuter - $240.00 (materials only - no meals)'),
+				'Commuter-deposit' =>'or Commuter deposit - $50.00 (non-refundable)'
+
 			)
-		);
-	}
+	 	);
+	 	
+	}	
+	$this->is_in_testing_mode = true;
 }
 ?>
