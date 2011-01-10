b3d5b89ee214012ed95c44e5a5e36a6f098a7515
Nathan White <nwhite@carleton.edu>
2011-01-10 18:32:44 +0000

    updated new settings info

--- reason_4.0/lib/core/scripts/upgrade/4.0b8_to_4.0b9/index.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0b8_to_4.0b9/index.php
@@ -22,21 +22,10 @@
 <h3>New Settings</h3>
 <p>Reason 4 Beta 9 introduces new settings - if you manually maintain your settings files you'll need to add them:</p>
 <ol>
-<li><strong>REASON_LOG_LOGINS</strong><p>You should make sure the setting is defined in the
-reason_settings.php used in your reason instance. You can copy and paste the following:</p>
-<textarea rows="8" cols="105">
-/**
- * REASON_LOG_LOGINS
- *
- * The Reason login module can log all login and logout actions.  If you set this value to true,
- * a log file will be populated at REASON_LOG_DIR/reason_login.log
- */
-define('REASON_LOG_LOGINS', true);</textarea><br/><br/>
-</li>
 <li><strong>REASON_PACKAGE_HTTP_BASE_PATH</strong><p>This identifies an alias in the web root that links to the new
 reason_package/www/ folder. This folder should contain items associated with the reason_package
 that need to be web accessible (such as the colorpicker javascript and css files used by the colorpicker
-plasmature object). You can copy and paste the following:</p>
+plasmature object). You can copy and paste the following into your package_settings.php file:</p>
 <textarea rows="10" cols="105">
 /**
  * REASON_PACKAGE_HTTP_BASE_PATH
@@ -47,7 +36,38 @@ plasmature object). You can copy and paste the following:</p>
  * The location of the reason_package www folder is /reason_package/www/
  */
 domain_define( 'REASON_PACKAGE_HTTP_BASE_PATH','/reason_package/');
-</textarea>
+</textarea><br/><br/>
+</li>
+<li><strong>REASON_LOG_LOGINS</strong><p>You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:</p>
+<textarea rows="8" cols="105">
+/**
+ * REASON_LOG_LOGINS
+ *
+ * The Reason login module can log all login and logout actions.  If you set this value to true,
+ * a log file will be populated at REASON_LOG_DIR/reason_login.log
+ */
+define('REASON_LOG_LOGINS', false);</textarea><br/><br/>
+</li>
+<li><strong>REASON_SIZED_IMAGE_DIR</strong><p>You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:</p>
+<textarea rows="7" cols="105">
+/**
+ * REASON_SIZED_IMAGE_DIR
+ *
+ * Full path to the directory where Reason's sized image class (sized_image.php) should store sized images.
+ */
+define('REASON_SIZED_IMAGE_DIR', REASON_INC.'/www/sized_images/');</textarea><br/><br/>
+</li>
+<li><strong>REASON_SIZED_IMAGE_DIR_WEB_PATH</strong><p>You should make sure the setting is defined in the
+reason_settings.php used in your reason instance. You can copy and paste the following:</p>
+<textarea rows="7" cols="105">
+/**
+ * REASON_SIZED_IMAGE_DIR_WEB_PATH
+ *
+ * Full path to the directory where Reason's sized image class (sized_image.php) should store sized images.
+ */
+define('REASON_SIZED_IMAGE_DIR_WEB_PATH', REASON_HTTP_BASE_PATH.'sized_images/');</textarea>
 </li>
 </ol>
 
