ad614a894fe5d29a8cef31b97c80683797f3be7b
slylth <slylth@gmail.com>
2011-01-27 18:28:26 +0000

    changes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1369 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/dorian_jh_camps/page1.php
+++ local/minisite_templates/modules/dorian_jh_camps/page1.php
@@ -13,6 +13,7 @@ class DorianJHCampsOneForm extends FormStep
 {
 	var $_log_errors = true;
 	var $error;
+        var $display_name = 'Camper Information';
 
 	var $elements = array(
 		'amount' => 'cloaked',
@@ -57,12 +58,23 @@ class DorianJHCampsOneForm extends FormStep
 			'type' => 'text',
 			'size' => 35,
 		),
+                'school' => 'text',
+                'grade' => array(
+                    'type' => 'text',
+                    'size' => 2,
+                    'display_name' => 'Grade completed by June 2011',
+                ),
+                'roomate_requested' => array(
+                    'type' => 'text',
+                    'comments' => 'Males, one name, Females, one or two names',
+                   // 'display_name' => 'Roomate&nbsp;Requested',
+                ),
                 'participation_header' => array(
 			'type' => 'comment',
 			'text' => '<h3>Participation</h3>',
 		),
 		'band_participant' => array(
-			'type' => 'checkbox_first',
+			'type' => 'checkboxfirst',
 			'display_name' => 'Will you play in band?',
 		),
 		'band_instrument' => array(
@@ -70,9 +82,8 @@ class DorianJHCampsOneForm extends FormStep
                     'display_name' => '&nbsp;',
                     'comments'=>'<div class="smallText comment">Instrument</div>',
                     ),
-		),
                 'orchestra_participant' => array(
-			'type' => 'checkbox_first',
+			'type' => 'checkboxfirst',
 			'display_name' => 'Will you play in orchestra?',
 		),
 		'orchestra_instrument' => array(
@@ -80,9 +91,8 @@ class DorianJHCampsOneForm extends FormStep
                     'display_name' => '&nbsp;',
                     'comments'=>'<div class="smallText comment">Instrument</div>',
                     ),
-		),
                 'jazz_participant' => array(
-			'type' => 'checkbox_first',
+			'type' => 'checkboxfirst',
 			'display_name' => 'Will you play in jazz band?',
 		),
 		'jazz_instrument' => array(
@@ -90,9 +100,8 @@ class DorianJHCampsOneForm extends FormStep
                     'display_name' => '&nbsp;',
                     'comments'=>'<div class="smallText comment">Instrument</div>',
                     ),
-		),
                 'orchestra_participant' => array(
-			'type' => 'checkbox_first',
+			'type' => 'checkboxfirst',
 			'display_name' => 'Will you play in band?',
 		),
 		'orchestra_instrument' => array(
@@ -100,92 +109,48 @@ class DorianJHCampsOneForm extends FormStep
                     'display_name' => '&nbsp;',
                     'comments'=>'<div class="smallText comment">Instrument</div>',
                     ),
+                'wind_choir_participant' => array(
+			'type' => 'checkboxfirst',
+			'display_name' => 'Will you play in wind choir?',
 		),
-		'unofficial' => array(
-			'type' => 'radio_inline_no_sort',
-			'display_name' => 'Would you like an <em>unofficial</em> transcript',
-			'options' => array('yes' => 'Yes', 'no' => 'No'),
+		'wind_choir_instrument' => array(
+                    'type' => 'text',
+                    'display_name' => '&nbsp;',
+                    'comments'=>'<div class="smallText comment">Instrument</div>',
+                    ),
+                 'brass_choir_participant' => array(
+			'type' => 'checkboxfirst',
+			'display_name' => 'Will you play in brass choir?',
 		),
-		'delivery_header' => array(
-			'type' => 'comment',
-			'text' => '<h3>Delivery</h3>',
-		),
-		'delivery' => array(
-			'type' => 'radio',
-			'display_name' => 'When to prepare transcripts',
-			'options' => array(
-				'now'=>'Send out as soon as possible<br>
-						(allow 48 hours processing time)',
-				'after current semester' => 'Wait until current semester grades are posted',
-				'after degree' => 'Wait until degree is posted',
-			),
-		),
+		'brass_choir_instrument' => array(
+                    'type' => 'text',
+                    'display_name' => '&nbsp;',
+                    'comments'=>'<div class="smallText comment">Instrument</div>',
+                    ),
+                 'private_lessons_header' => array(
+                     'type' => 'comment',
+                     'text' => '<h3>Private Lessons</h3>',
+                 ),
+                 'private_lesson_sets' => array(
+                     'type' => 'radio_inline_no_sort',
+                     'options' => array(1,2),
+                 ),
 	);
 
-	var $required = array('first_name', 'last_name', 'daytime_phone', 'e-mail', 'address', 'city', 'state_province', 'zip', 'delivery');
+	//var $required = array('first_name', 'last_name', 'daytime_phone', 'e-mail', 'address', 'city', 'state_province', 'zip', 'delivery');
 
-	var $display_name = 'Transcript Request Info';
 	var $error_header_text = 'Please check your form.';
 
 
 
 	// style up the form and add comments et al
-	function on_every_time()
-	{
-             $username = reason_check_authentication(); // this will force login
-
-            if ($username) {
-                    echo "true";
-                    $this->show_form = true;
-            } else {
-                    echo "False";
-                    echo '<div id ="transcriptLogin">'."\n";
-                    echo 'Access to this for is limited...lalala'."\n";
-                    echo '</div>'."\n";
-
-                    reason_require_authentication('form_login_msg');
-            }
-            echo $username;
-            $group = id_of('transcripts_group');
-            $has_access = (reason_user_is_in_group($username, $group));
-
-	}
-
 	function pre_show_form()
 	{
-		echo '<div id="transcriptRequestForm" class="pageOne">'."\n";
+		echo '<div id="dorianJHCampForm" class="pageOne">'."\n";
 	}
 	function post_show_form()
 	{
 		echo '</div>'."\n";
 	}
-
-	function needs_payment()
-	{
-	  	$amount = 0;
-	  	$official_number = $this->get_value('official');
-
-		if (isset($official_number))
-	  	{
-			$amount = $amount + ($official_number * 5);
-		}
-		$this->set_value('amount', $amount);
-
-		if ($amount == 0)
-		{
-			return 'TranscriptConfirmation';
-		}else{
-			return 'TranscriptPageTwoForm';
-		}
-	}
-        function  run_error_checks()
-        {
-            parent::run_error_checks();
-
-            if (!preg_match('/^\d+$/', $this->get_value(official)))
-            {
-                $this-> set_error($this->official, "Please enter a whole number");
-            }
-        }
 }
 ?>
