ebc4f6da534054cdbed3f312a6c7fea921ad4038
lucaswelper <lucaswelper@gmail.com>
2011-02-02 18:04:43 +0000

    added lessons toggle
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1394 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/dorian_jh_camps.js
+++ local/www/js/dorian_jh_camps.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
         hide_field('jazz_instrument');
         hide_field('wind_choir_instrument');
         hide_field('brass_choir_instrument');
+        hide_field('lesson_instrument_1');
+        hide_field('lesson_instrument_2');
 	$("input[name='band_participant']").change(function(){toggle_fields('band_participant','band_instrument')});
         $("input[name='orchestra_participant']").change(function(){toggle_fields('orchestra_participant','orchestra_instrument')});
         $("input[name='jazz_participant']").change(function(){toggle_fields('jazz_participant','jazz_instrument')});
@@ -21,18 +23,27 @@ $(document).ready(function() {
         $("input[name='brass_choir_participant']").change(function(){toggle_fields('brass_choir_participant','brass_choir_instrument')});
 
         //orchestra requirements
-        $("#period_oneElement").change(function() { checkRequirements('period_one'); });
-        $("#period_twoElement").change(function() { checkRequirements('period_two'); });
-        $("#period_four_firstElement").change(function() { checkRequirements('period_four'); });
-        $("#period_fiveElement").change(function() { checkRequirements('period_five'); });
-        $("#period_sixElement").change(function() { checkRequirements('period_six'); });
+        $("#period_oneElement").blur(function() { checkRequirements('period_one'); });
+        $("#period_twoElement").blur(function() { checkRequirements('period_two'); });
+        $("#period_four_firstElement").blur(function() { checkRequirements('period_four'); });
+        $("#period_fiveElement").blur(function() { checkRequirements('period_five'); });
+        $("#period_sixElement").blur(function() { checkRequirements('period_six'); });
 
+        //lesson choices
+        $("#radio_private_lessons_0").click(function(){chooseLessons();});
+        $("#radio_private_lessons_1").click(function(){chooseLessons();});
+        $("#radio_private_lessons_2").click(function(){chooseLessons();});
 });
 
 function hide_field(element)
 {
         element = "tr#"+element.replace(/_/g,"")+"Row";
         $(element).hide();
+}
+function show_field(element)
+{
+        element = "tr#"+element.replace(/_/g,"")+"Row";
+        $(element).show();
 }
 
 function toggle_fields(trigger, element)
@@ -104,4 +115,25 @@ function chooseChoir()
 {
     $("#period_twoElement").val('1').attr('selected',true);
     $("#period_sixElement").val('1').attr('selected',true);
+}
+
+function chooseLessons()
+{
+    var id = $("input[@name=testGroup]:checked").attr('id');
+
+    switch(id){
+        case 'radio_private_lessons_0':
+            hide_field('lesson_instrument_1');
+            hide_field('lesson_instrument_2');
+            break;
+        case 'radio_private_lessons_1':
+            show_field('lesson_instrument_1');
+            hide_field('lesson_instrument_2');
+            break;
+        case 'radio_private_lessons_2':
+            show_field('lesson_instrument_1');
+            show_field('lesson_instrument_2');
+            break;
+    }
+
 }
