aa02fb89cb7e07d7a8f1b952c5c68bea5bc67be3
lucaswelper <lucaswelper@gmail.com>
2011-02-03 00:21:35 +0000

    1/2/2011 2:42pm
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1400 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/dorian_jh_camps.js
+++ local/www/js/dorian_jh_camps.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
         hide_field('brass_choir_instrument');
         hide_field('lesson_instrument_1');
         hide_field('lesson_instrument_2');
-	$("input[name='band_participant']").change(function(){toggle_fields('band_participant','band_instrument')});
-        $("input[name='orchestra_participant']").change(function(){toggle_fields('orchestra_participant','orchestra_instrument')});
+	$("input[name='band_participant']").change(function(){toggle_fields('band_participant','band_instrument'); });
+        $("input[name='orchestra_participant']").change(function(){toggle_fields('orchestra_participant','orchestra_instrument'); });
         $("input[name='jazz_participant']").change(function(){toggle_fields('jazz_participant','jazz_instrument')});
-        $("input[name='wind_choir_participant']").change(function(){toggle_fields('wind_choir_participant','wind_choir_instrument')});
+        $("input[name='wind_choir_participant']").change(function(){toggle_fields('wind_choir_participant','wind_choir_instrument'); });
         $("input[name='brass_choir_participant']").change(function(){toggle_fields('brass_choir_participant','brass_choir_instrument')});
 
         //orchestra requirements
@@ -48,18 +48,35 @@ function show_field(element)
 
 function toggle_fields(trigger, element)
 {
-        trigger = "input#checkbox_"+trigger+":checked";
-        element = "tr#"+element.replace(/_/g,"")+"Row";
+        var trigger_element = "input#checkbox_"+trigger+":checked";
+        var element_row = "tr#"+element.replace(/_/g,"")+"Row";
 
-        var checked = $(trigger).val();
+        var checked = $(trigger_element).val();
 
         if (checked){
-            $(element).show();
+            $(element_row).show();
         }else{
-            $(element).hide();
+            $(element_row).hide();
+        }
+
+        switch(trigger){
+            case 'band_participant':
+                if(checked){ chooseBand(); }else{ clearBand(); }
+                break;
+            case 'orchestra_participant':
+                if(checked){ chooseOrchestra(); }else{ clearOrchestra(); }
+                break;
+            case 'jazz_participant':
+                if(checked){ chooseJazz(); }else{ clearJazz(); }
+                break;
+            case 'wind_choir_participant':
+                if(checked){ chooseChoir(); }else{ clearChoir(); }
+                break;
+            case 'brass_choir_participant':
+                if(checked){ chooseChoir(); }else{ clearChoir(); }
+                break;
         }
 }
-
 function checkRequirements(element)
 {
         var selected;
@@ -68,53 +85,143 @@ function checkRequirements(element)
             case 'period_one':
                     selected = $("select#period_oneElement").val();
                     switch(selected){
-                        case '1': chooseOrchestra(); break;
-                        case '2': chooseBand(); break;
+                        case 'orchestra': chooseOrchestra(); break;
+                        case 'concert_band': chooseBand(); break;
+                        default: chooseNone('period_one');
                     }
                     break;
             case 'period_two':
                     selected = $("select#period_twoElement").val();
                     switch(selected){
-                        case '1': chooseChoir(); break;
+                        case 'choir': chooseChoir(); break;
+                        default: chooseNone('period_two');
                     }
                     break;
             case 'period_four':
                     selected = $("select#period_four_firstElement").val();
                     switch(selected){
-                        case '1': chooseOrchestra(); break;
+                        case 'orchestra': chooseOrchestra(); break;
+                        default: chooseNone('period_four');
                     }
                     break;
             case 'period_five':
                     selected = $("select#period_fiveElement").val();
                     switch(selected){
-                        case '1': chooseOrchestra(); break;
-                        case '2': chooseBand(); break;
+                        case 'orchestra': chooseOrchestra(); break;
+                        case 'concert_band': chooseBand(); break;
+                        default: chooseNone('period_five');
                     }
                     break;
             case 'period_six':
                     selected = $("select#period_sixElement").val();
                     switch(selected){
-                        case '1': chooseChoir(); break;
+                        case 'choir': chooseChoir(); break;
+                        default: chooseNone('period_six');
                     }
                     break;
         }
 }
 function chooseOrchestra()
 {
-    $("#period_oneElement").val('1').attr('selected',true);
-    $("#period_four_firstElement").val('1').attr('selected',true);
-    $("#period_fiveElement").val('1').attr('selected',true);
+        $("#period_oneElement").val('orchestra').attr('selected',true);
+        $("#period_four_firstElement").val('orchestra').attr('selected',true);
+        $("#period_fiveElement").val('orchestra').attr('selected',true);
+}
+function clearOrchestra()
+{
+        if($("#period_oneElement option[value='orchestra']").attr('selected')){
+            $("#period_oneElement").get(0).selectedIndex = 0;
+        }
+        if($("#period_four_firstElement option[value='orchestra']").attr('selected')){
+            $("#period_four_firstElement").get(0).selectedIndex = 0;
+        }
+        if($("#period_fiveElement option[value='orchestra']").attr('selected')){
+            $("#period_fiveElement").get(0).selectedIndex = 0;
+        }
 }
 function chooseBand()
 {
-    $("#period_oneElement").val('2').attr('selected',true);
-    $("#period_four_firstElement").get(0).selectedIndex = 0;
-    $("#period_fiveElement").val('2').attr('selected',true);
+        $("#period_oneElement").val('concert_band').attr('selected',true);
+        if($("#period_four_firstElement option[value='orchestra']").attr('selected')){
+            $("#period_four_firstElement").get(0).selectedIndex = 0;
+        }
+        $("#period_fiveElement").val('concert_band').attr('selected',true);
+}
+function clearBand()
+{
+        if($("#period_oneElement option[value='concert_band']").attr('selected')){
+            $("#period_oneElement").get(0).selectedIndex = 0;
+        }
+        if($("#period_fiveElement option[value='concert_band']").attr('selected')){
+            $("#period_fiveElement").get(0).selectedIndex = 0;
+        }
 }
 function chooseChoir()
 {
-    $("#period_twoElement").val('1').attr('selected',true);
-    $("#period_sixElement").val('1').attr('selected',true);
+    $("#period_twoElement").val('choir').attr('selected',true);
+    $("#period_sixElement").val('choir').attr('selected',true);
+}
+function clearChoir()
+{
+        if($("#period_twoElement option[value='choir']").attr('selected')){
+            $("#period_twoElement").get(0).selectedIndex = 0;
+        }
+        if($("#period_sixElement option[value='choir']").attr('selected')){
+            $("#period_sixElement").get(0).selectedIndex = 0;
+        }
+}
+function chooseJazz()
+{
+    $("#period_three_firstElement").val('jazz_band_blue').attr('selected',true);
+    $("#period_four_firstElement").val('jazz_band_white').attr('selected',true);
+}
+function clearJazz()
+{
+        if($("#period_three_firstElement option[value='jazz_band_blue']").attr('selected')){
+            $("#period_three_firstElement").get(0).selectedIndex = 0;
+        }
+        if($("#period_four_firstElement option[value='jazz_band_white']").attr('selected')){
+            $("#period_four_firstElement").get(0).selectedIndex = 0;
+        }
+}
+function chooseNone(period)
+{
+    switch(period){
+        case 'period_one':
+            if($("#period_four_firstElement option[value='orchestra']").attr('selected')){
+                $("#period_four_firstElement").get(0).selectedIndex = 0;
+            }
+            if($("#period_fiveElement option[value='orchestra']").attr('selected') || $("#period_fiveElement option[value='concert_band']").attr('selected')){
+                $("#period_fiveElement").get(0).selectedIndex = 0;
+            }
+            break;
+        case 'period_two':
+            if($("#period_sixElement option[value='choir']").attr('selected')){
+                $("#period_sixElement").get(0).selectedIndex = 0;
+            }
+            break;
+        case 'period_four':
+            if($("#period_oneElement option[value='orchestra']").attr('selected')){
+                $("#period_oneElement").get(0).selectedIndex = 0;
+            }
+            if($("#period_fiveElement option[value='orchestra']").attr('selected') || $("#period_fiveElement option[value='concert_band']").attr('selected')){
+                $("#period_fiveElement").get(0).selectedIndex = 0;
+            }
+            break;
+        case 'period_five':
+            if($("#period_oneElement option[value='orchestra']").attr('selected') || $("#period_oneElement option[value='concert_band']").attr('selected')){
+                $("#period_oneElement").get(0).selectedIndex = 0;
+            }
+            if($("#period_four_firstElement option[value='orchestra']").attr('selected')){
+                $("#period_four_firstElement").get(0).selectedIndex = 0;
+            }
+            break;
+        case 'period_six':
+            if($("#period_twoElement option[value='choir']").attr('selected')){
+                $("#period_twoElement").get(0).selectedIndex = 0;
+            }
+            break;
+    }
 }
 
 function chooseLessons()
