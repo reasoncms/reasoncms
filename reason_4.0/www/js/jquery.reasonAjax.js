07923abce5d86b65b628a7698b83388fce5408dc
Nathan White <nwhite@carleton.edu>
2011-02-11 20:29:11 +0000

    place for some helper functions for reason ajax stuff

---
+++ reason_4.0/www/js/jquery.reasonAjax.js
@@ -0,0 +1,29 @@
+/**
+ * reasonAjax Utility
+ *
+ * For now, we are just declaring a method get_module_identifier.
+ *
+ * get_module_identifier takes a jquery object and parses out a module_identifier from the class attribute.
+ *
+ * @author Nathan White
+ */
+ 
+(function($)
+{
+	$.reasonAjax = {};
+	$.reasonAjax.get_module_identifier = function( obj )
+	{
+		var reason_module_identifier_substring = 'module_identifier-';
+		var classes = obj.attr('class').split(" ");
+		var module_identifier = '';
+		$.each(classes, function(i)
+		{
+			if (classes[i].substring(0,reason_module_identifier_substring.length) == reason_module_identifier_substring)
+			{
+				module_identifier = classes[i].substring(reason_module_identifier_substring.length);
+				return false;
+			}
+		});
+		return module_identifier;
+	}
+})(jQuery);
