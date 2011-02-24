51b13f08de4b4fbf6f91297e8dd51954ad794308
Nathan White <nwhite@carleton.edu>
2011-02-24 16:40:01 +0000

    moved logic about setting the format out to the base class

--- reason_4.0/lib/core/classes/api/api.php
+++ reason_4.0/lib/core/classes/api/api.php
@@ -3,39 +3,25 @@ include_once('reason_header.php');
 include_once(CARL_UTIL_INC . 'api/api.php');
 
 /**
- * Reason API contains application logic specific to providing APIs for Reason.
+ * Reason API sets a few defaults on CarlUtilAPI - specifically:
  *
- * - Sets content type according to $_REQUEST['format'] in the constructor.
+ * - Add support for dynamically setting the content_type_request_key if "format" is in the request.
+ * - Set up json as the only supported content type.
  *
  * NOTE: This API framework is very new to Reason, considered beta, and subject to change.
+ *
+ * This class should not be extended - it is designed for a a Reason Module or other use case
+ * where application logic outside the class looks at the user request, sets up the appropriate
+ * status code and content.
+ *
+ * A more full featured API should extend the CarlUtilAPI base class.
  *
  * @version .1 
  * @author Nathan White
  */
-class ReasonAPI extends CarlUtilAPI
+final class ReasonAPI extends CarlUtilAPI
 {
-	/**
-	 * We set the content type according to $_REQUEST['format'] provided it contains safe characters.
-	 *
-	 * @param mixed array or string
-	 * @return array
-	 */
-	function __construct($support_types = NULL)
-	{
-		static $validated_format_request;
-		parent::__construct($support_types);
-		if (!isset($validated_format_request))
-		{
-			$validated_format_request = '';
-			if (isset($_REQUEST['format']) && check_against_regexp($_REQUEST['format'], array('safechars')))
-			{
-				$validated_format_request = $_REQUEST['format'];
-			}
-		}
-		if (!empty($validated_format_request))
-		{
-			$this->set_content_type($validated_format_request);
-		}
-	}	
+	protected $content_type_request_key = 'format';
+	protected $supported_content_types = array('json');
 }
 ?>
