227facae276a6e1d71875e860c61c614c8daf6df
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-03-02 23:43:04 +0000

    initial commit
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1471 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/dir_img.php
+++ local/minisite_templates/modules/dir_img.php
@@ -1,11 +1,6 @@
 <?php
-require_once("./functions.php");
-
-if (isset($_SESSION['logged_in'])) {
-  if ($_SESSION['load_img']) {
-    header("Content-type: image/jpg");
-    readfile($photo_dir.$_GET['user'].".jpg");
-    $_SESSION['load_img'] = 0;
-  }
-}
+//if (isset($_SESSION['logged_in'])) {
+  header("Content-type: image/jpg");
+  readfile($photo_dir.$_GET['user'].".jpg");
+//}
 ?>
