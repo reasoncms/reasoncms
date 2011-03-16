6a061f32072f4bac8f1be06f29a0f7d68ca89a1a
slylth <slylth@gmail.com>
2011-03-16 23:16:41 +0000

    modifications to athlete content manager
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1496 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/content_managers/athlete.php
+++ local/content_managers/athlete.php
@@ -1,4 +1,5 @@
 <?php
+include_once '/usr/local/webapps/reason/reason_package_local/disco/plasmature/types/athletics.php';
 /**
  * @package reason
  * @subpackage content_managers
@@ -9,7 +10,7 @@
 	$GLOBALS[ '_content_manager_class_names' ][ basename( __FILE__) ] = 'Athlete';
 
 	/**
-	 * A content manager for text blurbs
+	 * A content manager for Luther College athletes and sports rosters
 	 */
 	class Athlete extends ContentManager
 	{
@@ -21,6 +22,7 @@
 			$this->set_comments('name', form_comment('A name for internal reference. E.g. the athlete\'s full name'));
 			$this->change_element_type('unique_name', 'hidden');
 			$this->change_element_type('author', 'hidden');
+			$this->change_element_type('no_share', 'hidden');
 
 			if ($site_name == 'sport_baseball_men' || $site_name == 'sport_basketball_men' ||
 				$site_name == 'sport_football_men' || $site_name == 'sport_soccer_men' ||
@@ -50,8 +52,9 @@
 
 			$this->set_display_name('athlete_class_year', 'Class Year');
 
-			$this->set_display_name('athlete_hometown', 'Hometown');
-			$this->set_comments('athlete_hometown', form_comment('Enter City, State (Use AP abbreviation for state. E.g. Iowa, Minn., Wis.)'));
+			$this->set_display_name('athlete_hometown_city', 'Hometown City');
+			$this->add_element('athlete_hometown_state', 'state_province', array('display_name' => 'Hometown State')); 
+			//$this->set_comments('athlete_hometown', form_comment('Enter City, State (Use AP abbreviation for state. E.g. Iowa, Minn., Wis.)'));
 			$this->set_display_name('athlete_high_school', 'High School');
 			$this->add_element('athlete_letter', 'checkbox', array('display_name' => 'Letter', 'description' => 'check if letter winner'));
 			$this->add_element('athlete_captain', 'checkbox', array('display_name' => 'Captain', 'description' => 'check if team captain'));
@@ -62,12 +65,34 @@
 				$site_name == 'sport_basketball_women' || $site_name == 'sport_soccer_women' || 
 				$site_name == 'sport_softball_women' || $site_name == 'sport_volleyball_women')
 			{
-				$this->set_display_name('athlete_position_event', 'Position');
-			}
-			else if ($site_name == 'sport_swimming_men' || $site_name == 'sport_track_men' ||
-				$site_name == 'sport_swimming_women' || $site_name == 'sport_swimming_women') 
-			{
-				$this->set_display_name('athlete_position_event', 'Event');
+				if ($site_name == 'sport_baseball_men' || $site_name == 'sport_softball_women')
+				{
+					$this->add_element('athlete_position_event', 'baseball_softball_positions', array('display_name' => 'Position'));
+				}
+				else if ($site_name == 'sport_basketball_men' || $site_name == 'sport_basketball_women')
+				{
+					$this->add_element('athlete_position_event', 'basketball_positions', array('display_name' => 'Position'));
+				}
+				else if ($site_name == 'sport_football_men')
+				{
+					$this->add_element('athlete_position_event', 'football_positions', array('display_name' => 'Position'));
+				}
+				else if ($site_name == 'sport_soccer_men' || $site_name == 'sport_soccer_women')
+				{
+					$this->add_element('athlete_position_event', 'soccer_positions', array('display_name' => 'Position'));
+				}
+				else if ($site_name == 'sport_volleyball_women')
+				{
+					$this->add_element('athlete_position_event', 'volleyball_positions', array('display_name' => 'Position'));
+				}
+				else if ($site_name == 'sport_swimming_men' || $site_name == 'sport_swimming_women')
+				{
+					$this->add_element('athlete_position_event', 'swimming_events', array('display_name' => 'Event'));
+				}
+				else if ($site_name == 'sport_track_men' || $site_name == 'sport_track_women')
+				{
+					$this->add_element('athlete_position_event', 'track_events', array('display_name' => 'Event'));
+				}
 			}
 			else
 			{
@@ -108,6 +133,9 @@
 
 			$this->add_required('athlete_first_name');
 			$this->add_required('athlete_last_name');
+
+			// lokify the content box
+			$this->change_element_type('content' , html_editor_name($this->admin_page->site_id), html_editor_params($this->admin_page->site_id, $this->admin_page->user_id));
 			
 			$this->set_order(
 				array(
@@ -116,6 +144,9 @@
 					'athlete_first_name',
 					'athlete_last_name',
 					'athlete_gender',
+					'athlete_class_year',
+					'athlete_hometown_city',
+					'athlete_hometown_state',
 				)
 			);
 
