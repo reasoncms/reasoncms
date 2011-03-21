8fe2d889f7b8dc20686363ac62871dc83b11cd54
Nathan White <nwhite@carleton.edu>
2011-03-21 20:37:06 +0000

    fixed phpdoc comments to be accurate

--- reason_4.0/www/scripts/upgrade/4.0b8_to_4.0b9/feature.php
+++ reason_4.0/www/scripts/upgrade/4.0b8_to_4.0b9/feature.php
@@ -1,9 +1,9 @@
 <?php
-        /**
-         * Stub for the 4.0b7_to_4.0b8 update_types upgrade script
-	 * @package reason
-	 * @subpackage scripts
-         */
-        include_once( 'reason_header.php' );
-        reason_include_once( 'scripts/upgrade/4.0b8_to_4.0b9/feature.php' );
+/**
+ * Stub for the 4.0b8_to_4.0b9 feature upgrade script
+ * @package reason
+ * @subpackage scripts
+ */
+include_once( 'reason_header.php' );
+reason_include_once( 'scripts/upgrade/4.0b8_to_4.0b9/feature.php' );
 ?>
