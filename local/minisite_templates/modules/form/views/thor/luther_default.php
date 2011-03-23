1cfa5db5cd21a8ce7e655626a3630439f829f74b
slylth <slylth@gmail.com>
2011-03-23 18:51:11 +0000

    added additional defaults
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1504 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/luther_default.php
+++ local/minisite_templates/modules/form/views/thor/luther_default.php
@@ -28,19 +28,35 @@ class LutherDefaultThorForm extends DefaultThorForm
 		if ($this->get_element_name_from_label('Gender'))
 			$this->change_element_type($this->get_element_name_from_label('Gender'), 'radio_inline',
 				array('options' => array('m' => 'Male', 'f'=>'Female')));
+		if ($this->get_element_name_from_label('gender'))
+			$this->change_element_type($this->get_element_name_from_label('gender'), 'radio_inline',
+				array('options' => array('m' => 'Male', 'f'=>'Female')));
 		
 		if ($this->get_element_name_from_label('Sex'))
 			$this->change_element_type($this->get_element_name_from_label('Sex'), 'radio_inline',
 				array('options' => array('m' => 'Male', 'f'=>'Female')));
-		
+				
+		if ($this->get_element_name_from_label('sex'))
+			$this->change_element_type($this->get_element_name_from_label('sex'), 'radio_inline',
+				array('options' => array('m' => 'Male', 'f'=>'Female')));
+				
 		if ($this->get_element_name_from_label('State'))
 			$this->change_element_type($this->get_element_name_from_label('State'), 'state');
+			
+		if ($this->get_element_name_from_label('state'))
+			$this->change_element_type($this->get_element_name_from_label('state'), 'state');
 		
 		if ($this->get_element_name_from_label('State/Province'))
 			$this->change_element_type($this->get_element_name_from_label('State/Province'), 'state_province');
+			
+		if ($this->get_element_name_from_label('state/province'))
+			$this->change_element_type($this->get_element_name_from_label('state/province'), 'state_province');
 
         if ($this->get_element_name_from_label('Country'))
             $this->change_element_type ($this->get_element_name_from_label('Country'), 'country', array('default' => 'United States'));
+            
+        if ($this->get_element_name_from_label('country'))
+            $this->change_element_type ($this->get_element_name_from_label('country'), 'country', array('default' => 'United States'));
 	}	
 }
 ?>                      
