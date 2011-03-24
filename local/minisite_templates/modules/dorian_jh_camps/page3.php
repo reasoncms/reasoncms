2d39562abe6a0e4bd2adb45b085ee533f69efeb6
slylth <slylth@gmail.com>
2011-03-24 17:56:14 +0000

    sending additional billing information to paypal
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1508 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/dorian_jh_camps/page3.php
+++ local/minisite_templates/modules/dorian_jh_camps/page3.php
@@ -21,7 +21,7 @@ class DorianJHCampsThreeForm extends FormStep
 	var $expense_budget_number = '10-202-60500-51111';
         // $expense_budget_number stays the same, per Renee Lillibridge and Karen Dallenbach (1/27/11)
 	var $revenue_budget_number = '10-000-08520-22000';
-        // $revenue_budget_number  before June 1, 10-000-08520-22000
+        // $revenue_budget_number  before June 1, 10-000-08520-22000	
         // $revenue_budget_number on or after June 1, 10-000-08520-40221
 
 	var $transaction_comment = 'Dorian Camps';
@@ -281,6 +281,12 @@ class DorianJHCampsThreeForm extends FormStep
 			$this->set_error('billing_address', 'Please enter your full billing address if the address
                             you entered on the previous page was not the billing address for your credit card.');
 		}
+                if($this->get_value('billing_address') == 'entered') {
+                    $this->set_value('billing_street_address', $this->controller->get('address'));
+                    $this->set_value('billing_city', $this->controller->get('city'));
+                    $this->set_value('billing_state_province', $this->controller->get('state_province'));
+                    $this->set_value('billing_zip', $this->controller->get('zip'));
+                }
 		
 		
 		// Process credit card
@@ -329,8 +335,14 @@ class DorianJHCampsThreeForm extends FormStep
 				$expiration_mmyy,
 				$this->revenue_budget_number,
 				$this->get_value('credit_card_name'),
-				$this->transaction_comment,
-				$this->expense_budget_number
+				$this->expense_budget_number,
+                                $this->transaction_comment,
+                                $this->get_value('billing_street_address'),
+                                $this->get_value('billing_city'),
+                                $this->get_value('billing_state_province'),
+                                $this->get_value('billing_zip'),
+                                $this->controller->get('e-mail'),
+                                $this->controller->get('home_phone')
 			);
 			
 			//$this->helper->build_transactions_array();
