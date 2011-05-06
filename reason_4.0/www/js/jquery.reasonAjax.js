69d9880d91be3d4fba18caeafcf2d41548f38822
Nathan White <nwhite@carleton.edu>
2011-05-06 21:32:01 +0000

    added unserialize convenience function

--- reason_4.0/www/js/jquery.reasonAjax.js
+++ reason_4.0/www/js/jquery.reasonAjax.js
@@ -26,4 +26,18 @@
 		});
 		return module_identifier;
 	}
+	
+	$.reasonAjax.unserialize = function( str )
+	{
+		var ret = {},
+		seg = str.replace(/^\?/,'').split('&'),
+		len = seg.length, i = 0, s;
+		for (;i<len;i++)
+		{
+			if (!seg[i]) { continue; }
+			s = seg[i].split('=');
+			ret[s[0]] = s[1];
+		}
+		return ret;
+	}
 })(jQuery);
