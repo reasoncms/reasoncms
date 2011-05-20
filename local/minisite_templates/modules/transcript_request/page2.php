cefb72a538905270f1173a6b6f898b1c4576bf82
slylth <slylth@gmail.com>
2011-05-20 01:47:23 +0000

    added 	$this->is_in_testing_mode = true; to test on production
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1590 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/transcript_request/page2.php
+++ local/minisite_templates/modules/transcript_request/page2.php
@@ -133,6 +133,7 @@ class TranscriptPageTwoForm extends FormStep
 		{
 			$this->is_in_testing_mode = false;
 		}
+                	$this->is_in_testing_mode = true;
 		
 		$this->change_element_type('credit_card_expiration_year','numrange',array('start'=>date('Y'),'end'=>(date('Y')+15),'display_name' => 'Expiration Year'));
 	}
