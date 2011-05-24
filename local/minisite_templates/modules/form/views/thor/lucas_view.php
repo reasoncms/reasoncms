d3cbc6998a36300e9ce21e6bb1a367c1c97d63bb
lucaswelper <lucaswelper@gmail.com>
2011-05-24 23:50:49 +0000

    updated lucas_view.php
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1601 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/lucas_view.php
+++ local/minisite_templates/modules/form/views/thor/lucas_view.php
@@ -1,4 +1,6 @@
 <?
+reason_include_once('minisite_templates/modules/form/views/thor/luther_default.php');
+
 include_once(WEB_PATH . 'stock/pfproclass.php');
 $GLOBALS['_form_view_class_names'][basename(__FILE__, '.php')] = 'LucasViewThorForm';
 
