28832c560b3702dde50ccde5e58ad9a426ccedcc
lucaswelper <lucaswelper@gmail.com>
2011-05-24 23:35:16 +0000

    new form view for testing
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1598 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/form/views/thor/lucas_view.php
@@ -0,0 +1,16 @@
+<?
+include_once(WEB_PATH . 'stock/pfproclass.php');
+$GLOBALS['_form_view_class_names'][basename(__FILE__, '.php')] = 'LucasViewThorForm';
+
+class LucasViewThorForm extends LutherDefaultThorForm {
+
+    function on_every_time() {
+        parent::on_every_time();
+
+        $herp_element = $this->get_element_name_from_label('herp');
+
+        $this->change_element_type($herp_element, 'commentWithLabelType');
+        
+    }
+}
+?>
