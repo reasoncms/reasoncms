9ed35118638027909a037ae62f995575eedba4ce
slylth <slylth@gmail.com>
2011-05-27 00:34:27 +0000

    initial add with correct data
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1620 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/open_id.php
+++ local/minisite_templates/modules/open_id.php
@@ -1,7 +1,4 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?
+echo '<iframe src="http://luthertest2.rpxnow.com/openid/embed?token_url=http%3A%2F%2Fkatie.luther.edu%2Fnewtoken.php"
+    scrolling="no" frameBorder="no" allowtransparency="true" style="width:400px;height:240px"></iframe>';
 ?>
