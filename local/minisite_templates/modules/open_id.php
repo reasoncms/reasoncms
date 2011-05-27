7ed4959b5669ddbe5472aad1f054335f4a021bb5
slylth <slylth@gmail.com>
2011-05-27 06:32:27 +0000

    put the iframe in the a module class and registered it with reason
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1621 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/open_id.php
+++ local/minisite_templates/modules/open_id.php
@@ -1,4 +1,16 @@
 <?
-echo '<iframe src="http://luthertest2.rpxnow.com/openid/embed?token_url=http%3A%2F%2Fkatie.luther.edu%2Fnewtoken.php"
+reason_include_once('minisite_templates/modules/default.php');
+$GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'OpenIDModule';
+
+class OpenIDModule extends DefaultMinisiteModule {
+    function run() {
+        $url = get_current_url();
+        //$parts = parse_url($url);
+        //$url = $parts['scheme'] . '://' . $parts['host'] . '/login/?dest_page=' . $parts['scheme'] . '://' . $parts['host'] . $parts['path'];
+        echo $url;
+        
+        echo '<iframe src="http://luthertest2.rpxnow.com/openid/embed?token_url=' . $url . '"
     scrolling="no" frameBorder="no" allowtransparency="true" style="width:400px;height:240px"></iframe>';
+    }
+}
 ?>
