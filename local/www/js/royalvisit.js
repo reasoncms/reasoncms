8c2bcf432080505484856e140a5575dc484dadd4
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-06-01 00:23:24 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1627 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/www/js/royalvisit.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function() {
+    
+    	$("tr#id9B34063023Row").hide();   //Guest Name
+
+    
+    
+});
+
+
+
