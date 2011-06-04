ee36e936440e8377cc31572314978129ab11ea1e
lucaswelper <lucaswelper@gmail.com>
2011-06-04 01:31:09 +0000

    openid & status working
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1648 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/open_id.php
+++ local/minisite_templates/modules/open_id.php
@@ -3,14 +3,37 @@ reason_include_once('minisite_templates/modules/default.php');
 $GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'OpenIDModule';
 
 class OpenIDModule extends DefaultMinisiteModule {
+
+    var $sess;
+
     function run() {
         $url = get_current_url();
+        $next_url = $_GET['next'];
+        if ($next_url){
+            $url = 'https://reasondev.luther.edu/reason/open_id/new_token.php?next=' . $next_url;
+        }else{
+            $url = 'https://reasondev.luther.edu/reason/open_id/new_token.php';
+        }
+        
         //$parts = parse_url($url);
         //$url = $parts['scheme'] . '://' . $parts['host'] . '/login/?dest_page=' . $parts['scheme'] . '://' . $parts['host'] . $parts['path'];
-        echo $url;
+        //echo $url;
         
-        echo '<iframe src="http://luthertest2.rpxnow.com/openid/embed?token_url=' . $url . '"
+        echo '<iframe src="https://luthertest2.rpxnow.com/openid/embed?token_url=' . $url . '"
     scrolling="no" frameBorder="no" allowtransparency="true" style="width:400px;height:240px"></iframe>';
+
+        $this->sess =& get_reason_session();
+        if( $this->sess->exists( ) ) {
+                if( !$this->sess->has_started() )
+                        $this->sess->start();
+                        //echo "<br />>>>>> STARTED SESSION <<<<<";
+        }
+
+//        echo '<br /> session name: ' . $this->sess->sess_name;
+//        echo "<br /> session id: " . session_id();
+//        echo '<br /> openid_id:' . $this->sess->get('openid_id');
+//        echo '<br /> openid_provider:' . $this->sess->get('openid_provider');
+//        echo '<br /> openid_name:' . $this->sess->get('openid_name');
     }
 }
 ?>
