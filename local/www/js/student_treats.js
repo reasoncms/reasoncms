6049275193760724fd5a7a5c9343c154322d6c31
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-06-10 17:49:55 +0000

    unhide occasion #1
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1657 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/student_treats.js
+++ local/www/js/student_treats.js
@@ -1,6 +1,96 @@
+
 /* 
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
+$(document).ready(function() {
+    
+   
+   toggle_treat_orders();
+   
+    $(".words:contains('Payment Amount')").parent().change(function(){
+        toggle_treat_orders();
+        
+    });
+    
+     
+});
+
+function toggle_treat_orders(){
+    
+    $(".words:contains('#2')").parent().hide();
+    $(".words:contains('#3')").parent().hide();
+    $(".words:contains('#4')").parent().hide();
+    $(".words:contains('#5')").parent().hide();
+    $(".words:contains('#6')").parent().hide();
+    
+    
+    
+    
+         
+    //occassion #1
+    if ($("#radio_id_554I3v48rI_0:checked").val() == '$20 - 1 treat'){
+        
+        $(".words:contains('#1')").parent().show(); 
+       
+        
+    }
+    
+
+    else if ($("#radio_id_554I3v48rI_1:checked").val() == '$40 - 2 treats') {
+        
+        
+        $(".words:contains('#1')").parent().show();
+        $(".words:contains('#2')").parent().show(); 
+        
+    }
+    
+    else if ($("#radio_id_554I3v48rI_2:checked").val() == '$60 - 3 treats'){
+       
+        
+        $(".words:contains('#1')").parent().show();
+        $(".words:contains('#2')").parent().show(); 
+        $(".words:contains('#3')").parent().show();  
+    }
+    
+    
+    else if ($("#radio_id_554I3v48rI_3:checked").val() == '$80 - 4 treats'){
+        
+        
+        $(".words:contains('#1')").parent().show();
+        $(".words:contains('#2')").parent().show(); 
+        $(".words:contains('#3')").parent().show();  
+        $(".words:contains('#4')").parent().show();
+    }
+    
+    else if ($("#radio_id_554I3v48rI_4:checked").val() == '$100 - 5 treats'){
+        
+        
+        $(".words:contains('#1')").parent().show();
+        $(".words:contains('#2')").parent().show(); 
+        $(".words:contains('#3')").parent().show(); 
+        $(".words:contains('#4')").parent().show();  
+        $(".words:contains('#5')").parent().show();  
+    }
+    
+    else if ($("#radio_id_554I3v48rI_5:checked").val() == '$120 - 6 treats'){
+       
+        
+        $(".words:contains('#1')").parent().show();
+        $(".words:contains('#2')").parent().show(); 
+        $(".words:contains('#3')").parent().show(); 
+        $(".words:contains('#4')").parent().show();  
+        $(".words:contains('#5')").parent().show();
+        $(".words:contains('#6')").parent().show(); 
+    }
+    
+ 
+    
+}
+
+    
+    
+    
 
 
