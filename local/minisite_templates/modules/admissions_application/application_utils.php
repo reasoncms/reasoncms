7a3cc17b1463e0e2e870105709a7c172809fea18
lucaswelper <lucaswelper@gmail.com>
2011-06-14 20:42:29 +0000

    create new record for openid on get_applicant_data
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1684 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/admissions_application/application_utils.php
+++ local/minisite_templates/modules/admissions_application/application_utils.php
@@ -9,7 +9,6 @@ function check_open_id(&$the_form) {
 
     if ($the_form->sess->get('openid_id')) {
         $the_form->openid_id = $the_form->sess->get('openid_id');
-        echo $the_form->openid_id;
         return $the_form->openid_id;
     } else {
         return false;
@@ -17,19 +16,25 @@ function check_open_id(&$the_form) {
 }
 
 function get_applicant_data($openid, &$the_form) {
+    echo '<br />openid: ' . $openid . '<br />';
     connectDB('admissions_applications_connection');
     $qstring = "SELECT * FROM `applicants` WHERE `open_id`='" . addslashes($openid) . "' ";
 //            $qstring = "SELECT * FROM `applicants` WHERE `open_id`='". $openid . "' ";
 
     $results = db_query($qstring);
 
+    if (mysql_num_rows($results) < 1) {
+        $qstring = "INSERT INTO `applicants` (`open_id`)  VALUES ('" . addslashes($openid) . "'); ";
+        $results = mysql_query($qstring) or die(mysql_error());
+        $qstring = "SELECT * FROM `applicants` WHERE `open_id`='" . addslashes($openid) . "' ";
+        $results = db_query($qstring);
+    }
+
     while ($row = mysql_fetch_array($results, MYSQL_ASSOC)) {
         foreach ($the_form->get_element_names() as $element) {
             if (array_key_exists($element, $row)) {
                 $the_value = $row[$element];
-//                echo $the_value . 'BLANK????? <br>';
                 $the_form->set_value($element, $the_value);
-                echo $element . '=' . $row[$element] . '<br>';
             }
         }
     }
@@ -37,6 +42,7 @@ function get_applicant_data($openid, &$the_form) {
 }
 
 function set_applicant_data($openid, &$the_form) {
+
     connectDB('admissions_applications_connection');
     echo '<br>' . addslashes($openid) . '<br>';
     $qstring = "SELECT * FROM `applicants` WHERE `open_id`='" . addslashes($openid) . "' ";
@@ -52,8 +58,12 @@ function set_applicant_data($openid, &$the_form) {
         foreach ($the_form->get_element_names() as $element) {
             if (array_key_exists($element, $row)) {
                 $qstring .= $element . "='";
-                if ($the_form->get_value($element)) {
-                    $qstring .= addslashes($the_form->get_value($element));
+                if (is_null($the_form->get_value($element)) <> True) {
+                    if (is_array($the_form->get_value($element))) {
+                        $qstring .= addslashes(implode(',', $the_form->get_value($element)));
+                    } else {
+                        $qstring .= addslashes($the_form->get_value($element));
+                    }
                 } else {
                     $qstring .= NULL;
                 }
@@ -62,8 +72,7 @@ function set_applicant_data($openid, &$the_form) {
         }
         $qstring = rtrim($qstring, ' ,');
         $qstring .= " WHERE `open_id`= '" . addslashes($openid) . "' ";
-//        echo $qstring;
-//        die;
+        //die($qstring);
     }
     $qresult = db_query($qstring);
     connectDB(REASON_DB);
