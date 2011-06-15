5c7e4d1bec6c859b7ccd6559d6a2ecd31b1867e8
lucaswelper <lucaswelper@gmail.com>
2011-06-15 00:30:03 +0000

    modify ceeb lookup query to allow multiple strings, searching name, city and state
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1686 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/autocomplete/ceeb.php
+++ local/www/autocomplete/ceeb.php
@@ -12,7 +12,32 @@ mysql_select_db($dbname);
 /* If connection to database, run sql statement. */
 if ($conn)
 {
-	$fetch = mysql_query("SELECT * FROM ceeb where name like '%" . mysql_real_escape_string($_GET['term']) . "%'");
+        //split input term into base parts:  'decorah high' into 'decorah' and 'high'
+        $cleaned_term = mysql_real_escape_string($_GET['term']);
+        $remove_chars = array(",", "(", ")", "-");
+        $cleaned_term = str_replace($remove_chars, " ", $cleaned_term);
+        $term_parts = split(" ", $cleaned_term, 10);
+
+        $qstring = "SELECT * FROM ceeb where";
+        $num_parts = count($term_parts);
+        $i = 0;
+        foreach($term_parts as $part){
+            $i++;
+            $qstring .= " (name like '%" . $part . "%' or";
+            $qstring .= " city like '%" . $part . "%' or";
+            $qstring .= " state like '%" . $part . "%')";
+            if($i <> $num_parts){
+                 $qstring .= " and";
+            }else{
+                $qstring .= " LIMIT 20;";
+            }
+        }
+
+   //     $row_array['id'] = "testing";
+   //     $row_array['value'] = $qstring;
+
+        $fetch = mysql_query($qstring);
+	//$fetch = mysql_query("SELECT * FROM ceeb where name like '%" . mysql_real_escape_string($_GET['term']) . "%'");
         //echo $fetch;
 	/* Retrieve and store in array the results of the query.*/
 
@@ -20,7 +45,7 @@ if ($conn)
 		//$row_array['current_hs_ceeb'] = $row['ceeb'];
                 $row_array['id'] = $row['ceeb'];
 		//$row_array['current_hs_name'] = $row['name'];
-                $row_array['value'] = $row['name'];
+                $row_array['value'] = $row['name'] . " (" . $row['city'] . ", " . $row['state'] . ")";
 		//$row_array['current_hs_address'] = $row['addr1'];
 		//$row_array['addr2'] = $row['addr2'];
 		//$row_array['current_hs_city'] = $row['city'];
