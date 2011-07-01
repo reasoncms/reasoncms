ac25f7ea2750fe10b444bb749504d25583b43e92
slylth <slylth@gmail.com>
2011-07-01 19:24:46 +0000

    changed cost of transcripts to $5
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1788 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/transcript_request/page1.php
+++ local/minisite_templates/modules/transcript_request/page1.php
@@ -67,7 +67,7 @@ class TranscriptPageOneForm extends FormStep {
         ),
         'official_comment' => array(
             'type' => 'comment',
-            'text' => 'Official transcripts cost $4 per transcript for Alumni'
+            'text' => 'Official transcripts cost $5 per transcript for Alumni'
         ),
 //        'official_type' => array(
 //            'type' => 'radio_inline_no_sort',
@@ -244,7 +244,7 @@ class TranscriptPageOneForm extends FormStep {
        $deliver_time = $this->get_value('delivery_time');
 
        if ($this->get_value('student_status') == 'Alumni') {
-            $pay_amount = $pay_amount + ($official_number * 4);
+            $pay_amount = $pay_amount + ($official_number * 5);
         }
         $this->set_value('amount', $pay_amount);
 
