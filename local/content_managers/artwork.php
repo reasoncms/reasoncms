174efe682dbe7b0200db02e8a4ba6b89f28b3ae5
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-01 22:47:57 +0000

    created artwork content
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1794 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/content_managers/artwork.php
@@ -0,0 +1,13 @@
+<?php
+
+$GLOBALS['_content_manager_class_names'][basename(__FILE__)] = 'LutherArtwork';
+
+class LutherTwitterFeed extends ContentManager {
+
+    function alter_data() {
+        
+    }
+
+}
+
+?>
