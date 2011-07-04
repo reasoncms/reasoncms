fbb8425118f3f9d650252b85b2993f99ddf19ff3
slylth <slylth@gmail.com>
2011-07-04 23:29:44 +0000

    checking to make sure a user lands here correctly if an open_id and submit_date are set, then the thank_you_blurb will show otherwise it will display a friendly message for those with only an open_id set or send them to ApplicationPageOne if neither is set (new user)
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1802 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/admissions_application/confirmation.php
+++ local/minisite_templates/modules/admissions_application/confirmation.php
@@ -35,10 +35,65 @@ class ApplicationConfirmation extends FormStep {
     // style up the form and add comments et al
     function on_every_time() {
         $this->show_form = false;
+        /*
+         * check to see if applicants with a set open_id are landing here prematurely
+         * if so, display an error message with the required fields and break
+         */
+        $open_id = check_open_id($this);
+        connectDB('admissions_applications_connection');
+        $qstring = "SELECT `submit_date` FROM `applicants` WHERE `open_id`='" .addslashes($open_id) . "' ";
+        $results = db_query($qstring);
+        $row = mysql_fetch_array($results, MYSQL_ASSOC);
+        if ($row['submit_date'] == '0000-00-00 00:00:00'){
+            $i = 0;
+            $error_div = "";
+            foreach ($this->controller->forms as $name => $form) {
+                $error_header = "<div style='width:655px;border:1px solid red;border-radius:5px;background-color:#FFB2B2;padding:5px;margin:5px;'>
+                <span style='font-weight:bold;'>Required fields: " . $form->display_name . "</span>&nbsp;&nbsp;";
+                $error_footer = "</div>";
+                $i++;
+                switch ($i) {
+                    case 1:
+                        $validation = $p1_valid = validate_page1($form);
+                        break;
+                    case 2:
+                        $validation = $p2_valid = validate_page2($form);
+                        break;
+                    case 3:
+                        $validation = $p3_valid = validate_page3($form);
+                        break;
+                    case 4:
+                        $validation = $p4_valid = validate_page4($form);
+                        break;
+                    case 5:
+                        $validation = $p5_valid = validate_page5($form);
+                        break;
+                    case 6:
+                        $validation = $p6_valid = validate_page6($form);
+                        break;
+                }
+                if (!$validation['valid']) {
+                    $error_div .= $error_header;
+                    foreach ($validation as $val_key => $val_value) {
+                        $error_div .= " <a href='/admissions/apply/?_step=" . $name . "#" . $val_key . "_error'>" . $val_value . "</a>&nbsp;&nbsp; ";
+                    }
+                }
+                $error_div .= $error_footer;
+            }
 
-        //check if an open_id is set
-        // if not, send them back to the first page
-        if (check_open_id($this)) {
+            if (!$p1_valid['valid'] || !$p2_valid['valid'] || !$p3_valid['valid'] || !$p4_valid['valid'] || !$p5_valid['valid'] || !$p6_valid['valid']) {
+                echo '<p>Looks like you\'ve landed here accidentally</p>';
+                echo 'Please complete the following required fields to submit your application:' . $error_div;
+                break;
+            }
+        }
+        connectDB(REASON_DB);
+        /*
+         * check if an open_id is set and that a submit_date is set
+         * if so, display the thank you blurb
+         * if not, send them back to the first page
+         */
+        if (($open_id) && (strcasecmp($row['submit_date'], '0000-00-00 00:00:00') <> 0)) {
             echo $this->get_thank_you_blurb();
         } else {
             echo 'How\'d you get here?';
