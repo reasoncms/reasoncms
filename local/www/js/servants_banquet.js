1de60c439fa361b88b6067e9a5d7da8968cb22d1
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-08 23:26:39 +0000

    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1840 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/servants_banquet.js
+++ local/www/js/servants_banquet.js
@@ -20,6 +20,13 @@ $(document).ready(function(){
             $("#idI191dyUD00Row").hide();
             
         }
+        
+        if ($("#radio_id_7a3toi91Ps_0:checked").val() == 'Yes'){
+            
+            $("#id1e83H1WmM0Row").show();
+            $("#id1kCS7B75P6Row").show();
+            $("#idI191dyUD00Row").show();
+        }
     
     
     })
