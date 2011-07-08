64c2cd8c1d5100361651e6a62fa7fb4e9cd7aa70
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-08 22:47:43 +0000

    created php file
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1832 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/form/views/thor/servants_banquet.php
@@ -0,0 +1,33 @@
+<?php
+
+reason_include_once('minisite_templates/modules/form/views/thor/luther_default.php');
+$GLOBALS['_form_view_class_names'][basename(__FILE__, '.php')] = 'ServantsBanquetForm';
+
+class ServantsBanquetForm extends LutherDefaultThorForm {
+
+    function on_every_time() {
+        parent::on_every_time();
+
+        echo '<script type="text/javascript" src="/reason/js/servants_banquet.js"></script>';
+        
+    }
+}
+
+function pre_error_check_actions() {
+        parent::pre_error_check_actions();
+
+        $guest_first_name = $this->get_element_name_from_label('Guest First Name');
+        $guest_last_name = $this->get_element_name_from_label('Guest Last Name');
+        $guest_choice = $this->get_element_name_from_label('Guest Entree Choice');
+        
+
+
+        if ($this->get_value_from_label('Are you bringing a guest?') == 'Yes' ) {
+
+            $this->add_required($guest_first_name);
+            $this->add_required($guest_last_name);
+            $this->add_required($guest_choice);
+        }
+}
+
+?>
