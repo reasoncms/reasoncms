2862d2d0c2b9762936b87d459af48561184b0c38
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-13 18:38:59 +0000

    google map java file create
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1888 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/google_map.js
+++ local/www/js/google_map.js
@@ -8,48 +8,17 @@ function initialize()
 
 {
     
-    var latlng = new google.maps.LatLng(9.931544168615512,76.27632894178791);
-    var opt =
-        { 
-        center:latlng,
-        zoom:10,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableAutoPan:false,
-        navigationControl:true,
-        navigationControlOptions: {style:google.maps.NavigationControlStyle.SMALL },
-        mapTypeControl:true,
-        mapTypeControlOptions: {style:google.maps.MapTypeControlStyle.DROPDOWN_MENU}
-    };
-    var map = new google.maps.Map(document.getElementById("map"),opt);
-    var marker= new google.maps.Marker({
-        position: new google.maps.LatLng(9.931544168615512,76.27632894178791),
-        title: "CodeGlobe",
-        clickable: true,
-        map: map
-    });
+    var map;
 
-
-    var infiwindow = new google.maps.InfoWindow(
-    {
-        content: " I am here! "
-
-    });
-
-
-    google.maps.event.addListener(marker,'mouseover',function(){
-        infiwindow.open(map,marker);
-    });
-    google.maps.event.addListener(marker,'mouseout',function(){
-        infiwindow.close(map,marker);
-    });
-
-
-}
-function test(event)
-{
-
-    alert( event.latLng.lat());
-    alert(event.latLng.lng());
+    function initialize() {
+        var haightAshbury = new google.maps.LatLng(37.7699298, -122.4469157);
+        var mapOptions = {
+            zoom: 12,
+            center: haightAshbury,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
+        };
+        map =  new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+    }
 
 }
     
