4bacdb0f64719e7be2814d1357cd3422ec6cb18b
lucaswelper <lucaswelper@gmail.com>
2011-07-13 11:22:55 +0000

    *incomplete* tooltip for switching accounts
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1884 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/open_id_status.php
+++ local/minisite_templates/modules/open_id_status.php
@@ -18,12 +18,59 @@ class OpenIDStatusModule extends DefaultMinisiteModule {
         }
 
         if ($this->sess->get('openid_id')){
-            echo "Welcome, " . $this->sess->get('openid_name') . ".";
-            echo "&nbsp;&nbsp;If you think you started already, click ";
-            echo "<a href='" . $openid_url . "'>HERE</a>";
-            echo " to choose a different account.<br />&nbsp;";
-        } else {
 
+            if ($_SESSION['openid_profile']['name']['formatted']){
+                $welcome_name = $_SESSION['openid_profile']['name']['formatted'];
+            }else{
+                $welcome_name = $_SESSION['openid_profile']['displayName'];
+            }
+            
+            if($_SESSION['openid_profile']['photo']){
+                $openid_image = "<img src='" . $_SESSION['openid_profile']['photo'] . "' style='height:1em;display:inline;' />";
+            }else{
+                $openid_image = "";
+            }
+            
+            //echo "XXXX " . $_SESSION['openid_profile']['providerName'] . " XXXX";
+            
+            if($_SESSION['openid_profile']['providerName']){
+                $provider_image = "<img src='/reason/open_id/";
+                switch($_SESSION['openid_profile']['providerName']){
+                    case 'Twitter':
+                        $provider_image .= "twitter.png";
+                        break;
+                    case 'Google':
+                        $provider_image .= "google.png";
+                        break;
+                    case 'Yahoo!':
+                        $provider_image .= "yahoo.png";
+                        break;
+                    case 'Aol':
+                        $provider_image .= "aol.png";
+                        break;
+                    case 'Windows Live':
+                        $provider_image .= "windows.png";
+                        break;
+                    case 'Facebook':
+                        $provider_image .= "facebook.png";
+                        break;
+                    default:
+                        $provider_image .= "openid.png";
+                        break;
+                }
+                $provider_image .= "' style='height:1em;display:inline;' />";
+            }else{
+                $provider_image = "";
+            }
+
+            echo "Welcome, " . $provider_image . " " . $welcome_name . ".&nbsp;&nbsp;";
+            echo "<span id='missing_info_tip'>Missing previously completed information?</span>";
+            echo "<div class='tooltip_box'>";
+            echo "You may have started your application using a different login.";
+            echo "<br /><a href='" . $openid_url . "'>Try logging in using a different account</a>";
+            echo "</div><br />&nbsp;";
+        } else {
+            //error?
         }
 
 //        echo '<br /> session name: ' . $this->sess->sess_name;
