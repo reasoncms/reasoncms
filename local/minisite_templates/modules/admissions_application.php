6bcf0a01f2aaeffe8fb86be5a386f25d73b358b8
lucaswelper <lucaswelper@gmail.com>
2011-07-13 07:37:09 +0000

    added /www/js/jquery.tools.min.js to the include section on admissions_application.php
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1864 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/admissions_application.php
+++ local/minisite_templates/modules/admissions_application.php
@@ -170,6 +170,7 @@ class AdmissionsApplicationModule extends DefaultMinisiteModule {
             $head_items->add_stylesheet('/reason/jquery-ui-1.8.12.custom/css/redmond/jquery-ui-1.8.12.custom.css');
             $head_items->add_stylesheet('/reason/css/giftform.css');
             $head_items->add_javascript('/reason/js/admissions_application.2.js');
+            $head_items->add_javascript('/reason/js/jquery.tools.min.js');
             
         }
         // Insert refresh headers when in kiosk mode
