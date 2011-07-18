94b3198e829ac4365f32c62fb2e3b1efeb79eb4c
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-18 23:03:36 +0000

    add required to fields
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1957 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/royal_visit.php
+++ local/minisite_templates/modules/form/views/thor/royal_visit.php
@@ -18,22 +18,19 @@ class RoyalVisitThorForm extends LutherDefaultThorForm {
         
         $guest_first_name = $this->get_element_name_from_label('Guest First Name');
         $guest_last_name = $this->get_element_name_from_label('Guest Last Name');
-        $guest_address = $this->get_element_name_from_label('Guest Address');
-        $guest_city = $this->get_element_name_from_label('Guest City');
-        $guest_state = $this->get_element_name_from_label('Guest State');
-        $guest_zip_code = $this->get_element_name_from_label('Guest Zip code');
-        $guest_phone = $this->get_element_name_from_label('Guest Phone');
         $guest_email = $this->get_element_name_from_label('Guest Email');
         
-        if ($this->get_value_from_label('Do you want an extra guest ticket?') == 'Yes' ) {
+        $seating_describe = $this->get_element_name_from_label('Please describe');
+        
+        if ($this->get_value_from_label('Special seating?') == 'Yes' ) {
+
+            $this->add_required($seating_describe);
+        }
+        
+        if ($this->get_value_from_label('Do you want an extra guest ticket for $20?') == 'Yes' ) {
 
             $this->add_required($guest_first_name);
             $this->add_required($guest_last_name);
-            $this->add_required($guest_address);
-            $this->add_required($guest_city);
-            $this->add_required($guest_state);
-            $this->add_required($guest_zip_code);
-            $this->add_required($guest_phone);
             $this->add_required($guest_email);
         }
     }
