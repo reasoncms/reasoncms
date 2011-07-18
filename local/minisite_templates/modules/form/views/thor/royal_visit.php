7d001ae8203a6cae1ed65a8f822b037521159ad1
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-18 22:53:14 +0000

    move 1961 reception row above luncheon
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1956 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/royal_visit.php
+++ local/minisite_templates/modules/form/views/thor/royal_visit.php
@@ -6,9 +6,37 @@ class RoyalVisitThorForm extends LutherDefaultThorForm {
 
     function on_every_time() {
         parent::on_every_time();
+        
+        echo '<script type="text/javascript" src="/reason/js/royalvisit.js"></script>';
 
-        $herp_element = $this->get_element_name_from_label('Herp');
-        $this->change_element_type($herp_element, 'colorpicker');
+        
+        
     }
+    
+    function pre_error_check_actions() {
+        parent::pre_error_check_actions();
+        
+        $guest_first_name = $this->get_element_name_from_label('Guest First Name');
+        $guest_last_name = $this->get_element_name_from_label('Guest Last Name');
+        $guest_address = $this->get_element_name_from_label('Guest Address');
+        $guest_city = $this->get_element_name_from_label('Guest City');
+        $guest_state = $this->get_element_name_from_label('Guest State');
+        $guest_zip_code = $this->get_element_name_from_label('Guest Zip code');
+        $guest_phone = $this->get_element_name_from_label('Guest Phone');
+        $guest_email = $this->get_element_name_from_label('Guest Email');
+        
+        if ($this->get_value_from_label('Do you want an extra guest ticket?') == 'Yes' ) {
+
+            $this->add_required($guest_first_name);
+            $this->add_required($guest_last_name);
+            $this->add_required($guest_address);
+            $this->add_required($guest_city);
+            $this->add_required($guest_state);
+            $this->add_required($guest_zip_code);
+            $this->add_required($guest_phone);
+            $this->add_required($guest_email);
+        }
+    }
+    
 }
 ?>
