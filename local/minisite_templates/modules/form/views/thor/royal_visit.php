98db3a1082d1a0e95c3fdd18458c7d95e5078a39
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-07-20 18:34:17 +0000

    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1968 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/form/views/thor/royal_visit.php
+++ local/minisite_templates/modules/form/views/thor/royal_visit.php
@@ -9,14 +9,14 @@ class RoyalVisitThorForm extends LutherDefaultThorForm {
         
         echo '<script type="text/javascript" src="/reason/js/royalvisit.js"></script>';
         
-        $special_seating = $this->get_element_name_from_label('Special seating?');
+        $special_seating = $this->get_element_name_from_label('Accessibility Issues?');
         $extra_guest_ticket = $this->get_element_name_from_label('Do you want an extra guest ticket for $20?');
         
         $this->change_element_type($special_seating, 'radio_inline_no_sort');
         
         $url = get_current_url();
-        if ($url = 'https://reasondev.luther.edu/150/events/royalvisit/stafflottery/'){
-            $this->change_element_type($extra_guest_ticket, 'radio_inline_no_sort',array('comments' => 'Additional ticket is $20, unless a faculty/staff person'));
+        if ($url == 'https://reasondev.luther.edu/150/events/royalvisit/stafflottery/'){
+            $this->change_element_type($extra_guest_ticket, 'radio_inline_no_sort',array('comments' => 'Additional ticket is $20, unless that person is a faculty/staff person'));
         }else{
            $this->change_element_type($extra_guest_ticket, 'radio_inline_no_sort'); 
         }
@@ -31,7 +31,7 @@ class RoyalVisitThorForm extends LutherDefaultThorForm {
         
         $seating_describe = $this->get_element_name_from_label('Please describe');
         
-        if ($this->get_value_from_label('Special seating?') == 'Yes' ) {
+        if ($this->get_value_from_label('Accessibility Issues?') == 'Yes' ) {
 
             $this->add_required($seating_describe);
         }
