cd7b059ad47f5090f57fd6390d9391321dd5d117
Matt Ryan <mryan@carleton.edu>
2011-07-28 16:39:28 +0000

    new upgrader framework pieces

---
+++ reason_4.0/lib/core/classes/upgrade/upgrade_assistant.php
@@ -0,0 +1,44 @@
+<?php
+include_once('reason_header.php');
+reason_include_once('function_libraries/file_finders.php');
+
+class reasonUpgradeAssistant
+{
+	function get_upgraders($upgrade_string, $name = NULL)
+	{
+		$dir_path = 'scripts/upgrade/'.$upgrade_string.'/';
+		$files = reason_get_merged_fileset($dir_path);
+		$upgraders = array();
+		if(!empty($files))
+		{
+			foreach($files as $file)
+			{
+				reason_include_once($dir_path.$file);
+				$name = basename($file, '.php');
+				if(!empty($GLOBALS['_reason_upgraders'][$upgrade_string][$name]))
+				{
+					$classname = $GLOBALS['_reason_upgraders'][$upgrade_string][$name];
+					if(class_exists($classname))
+					{
+						$obj = new $classname;
+						if($obj instanceof reasonUpgraderInterface)
+							$upgraders[$name] = new $classname;
+						else
+							trigger_error('Upgraders must implement the reasonUpgraderInterface; '.$classname.' appears not to.');
+					}
+					else
+						trigger_error('Unable to instantiate upgrader class '.$classname.' -- it does not appear to exist');
+				}
+				else
+				{
+					trigger_error('The upgrader file '.$file.' does not appear to have registered itself properly. At the top of the file there should be'.
+							' a declaration like this: $GLOBALS[\'_reason_upgraders\'][\''.$upgrade_string.'\'][\''.$name.'\'] = \'NameOfUpgraderClass\'');
+				}
+			}
+		}
+		ksort($upgraders);
+		return $upgraders;
+	}
+}
+
+?>
---
+++ reason_4.0/lib/core/classes/upgrade/upgrader_interface.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package reason
+ * @subpackage classes
+ */
+
+/**
+ * Interface for upgrader classes
+ */
+interface reasonUpgraderInterface
+{
+	/**
+ 	 * Set and get the Reason ID of the current user
+	 * @param integer $user_id
+	 * @return integer $user_id
+	 */
+	function user_id($user_id = NULL);
+	/**
+	 * Get the title of the upgrader
+	 * @return string
+	 */
+	public function title();
+	/**
+	 * Get a description of what this upgrade script will do
+	 * @return string HTML description
+	 */
+	public function description();
+	/**
+         * Do a test run of the upgrader
+         * @return string HTML report
+         */
+	public function test();
+	/**
+	 * Run the upgrader
+	 * @return string HTML report
+	 */
+	public function run();
+}
+
+?>
