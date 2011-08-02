a17380ae87db13592fe6c7798a7e71d92c24a7b1
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-08-02 19:19:40 +0000

    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1990 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/study_skills_assessment.php
+++ local/minisite_templates/modules/study_skills_assessment.php
@@ -1,15 +1,22 @@
 <?php
 
-class StudySkillsAssessment extends FormStep
-{
+//reason_include_once( 'minisite_templates/modules/default.php' );
+
+//include_once(DISCO_INC . 'disco.php');
+
+//$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'StudySkillsAssessment';
+
+class StudySkillsAssessment extends Disco {
+    
 	var $_log_errors = true;
 	var $error;
 	
 	var $elements = array(
 		'sss_header' => array(
 			'type' => 'comment',
-			'text' => '<h3>Gift Amount</h3>',
-		));
+			'text' => '<h3>TESTT1</h3>',
+		)
+            );
 
 
 }
