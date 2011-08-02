388867454a67e3540e743e1dd8d2a9c6a0eeaccf
leo <leo@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-08-02 19:02:29 +0000

    added study skills assessment module
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@1989 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/study_skills_assessment.php
@@ -0,0 +1,16 @@
+<?php
+
+class StudySkillsAssessment extends FormStep
+{
+	var $_log_errors = true;
+	var $error;
+	
+	var $elements = array(
+		'sss_header' => array(
+			'type' => 'comment',
+			'text' => '<h3>Gift Amount</h3>',
+		));
+
+
+}
+?>
