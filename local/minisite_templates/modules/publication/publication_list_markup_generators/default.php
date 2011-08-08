fd51cbd99a0e7cfa366ca583fd9870c6fa328d93
brifishjones <jonebr01@gmail.com>
2011-08-08 23:56:31 +0000

    more publication not showing news fixes
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2020 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/publication/publication_list_markup_generators/default.php
+++ local/minisite_templates/modules/publication/publication_list_markup_generators/default.php
@@ -135,10 +135,9 @@ class PublicationListMarkupGenerator extends PublicationMarkupGenerator
 			if($do_section_headings)
 				$markup_string .= '</ul>'."\n"; 
 		}
-		
 		// put Read more... at end of teaser
 		//$markup_string = preg_replace("|\<\/p\>\s?\<\/div\>\s?.*?\s?.*?\s?\<li class\=\"permalink\"\>\s?(\<a href\=\".*?)Permalink\<\/a\>\<\/li\>\s?<\/ul\>\s?|", "&nbsp;&nbsp;\\1 Read more...</a></p></div>", $markup_string);
-		$markup_string = preg_replace("|(\<\/p\>)\s?\<\/div\>\s?.*?\s?.*?\s?\<li class\=\"permalink\"\>\s?(\<a href\=\".*?)Permalink\<\/a\>\<\/li\>\s?(\<li class\=\"comments\"\>\s?\<a href\=\".*?View comments \(\d+\).*?\<\/a\>\<\/li\>\s?)?<\/ul\>\s?|", "&nbsp;&nbsp;\\2Read more...</a></p></div>", $markup_string);
+		$markup_string = preg_replace("|(\<\/p\>)?\s?\<\/div\>\s?\<ul class\=\"links\"\>\s?.*?\s?\<li class\=\"permalink\"\>\s?(\<a href\=\".*?)Permalink\<\/a\>\<\/li\>\s?(\<li class\=\"comments\"\>\s?\<a href\=\".*?View comments \(\d+\).*?\<\/a\>\<\/li\>\s?)?<\/ul\>\s?|", "&nbsp;&nbsp;\\2Read more...</a></p></div>", $markup_string);
 		// remove permalink on pages with no posts
 		$markup_string = preg_replace("|\<ul class\=\"links\"\>\s?\<li class\=\"permalink\"\>\s?\<a href\=\".*?Permalink\<\/a\>\<\/li\>\s?\<\/ul\>\s?|", "", $markup_string);
 		// remove permalink on news posts with no description
