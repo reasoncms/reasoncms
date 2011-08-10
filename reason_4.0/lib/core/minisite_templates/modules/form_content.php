c16355ce25f54ca2c7fe8b627c374d3da4944b0f
Nathan White <nwhite@carleton.edu>
2011-08-10 18:16:35 +0000

    updated to support parameters to control content hiding after submission, in admin view, or on edit

--- reason_4.0/lib/core/minisite_templates/modules/form_content.php
+++ reason_4.0/lib/core/minisite_templates/modules/form_content.php
@@ -12,7 +12,11 @@
 	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'FormContentModule';
 
 	/**
-	 * A minisite module that only displays the current page's content if the page is not responding to a form submission
+	 * A minisite module that hides the page content in various situations.
+	 *
+	 * - Hide after submission - Hides the page content immediately after a submission. DEFAULT ON
+	 * - Hide in admin view - Hides the page content when form admin view is active. DEFAULT ON
+	 * - Hide on edit - Hides the page content when editing (whenever a form_id > 0 is present). DEFAULT OFF
 	 *
 	 * When used in conjunction with a form module, this hides the page's content on form submission
 	 * so that the form's thank you message is not buried onder the page content
@@ -22,14 +26,41 @@
 	 */
 	class FormContentModule extends EditableContentModule
 	{
+		var $cleanup_rules = array('submission_key' => 'turn_into_string',
+									    'form_id' => 'turn_into_int',
+									    'form_admin_view' => array('function' => 'check_against_array', 'extra_args' => array('true')));
+									    
+		var $acceptable_params = array('hide_after_submission' => true,
+								            'hide_in_admin_view' => true,
+								            'hide_on_edit' => false);
+								            
 		function run()
 		{
+			if (!$this->hide_after_submission() && !$this->hide_in_admin_view() && !$this->hide_on_edit())
+			{
+				parent::run();
+			}
 			// Don't display the content if the form has already been
 			// submitted, because we'll already be displaying a
 			// thank-you message in main_post, and it would look silly
 			// to have the content there too
-			if ( !array_key_exists('submission_key', $_REQUEST) )
-				parent::run();
+			//if ( !array_key_exists('submission_key', $_REQUEST) )
+			//	parent::run();
+		}
+		
+		function hide_after_submission()
+		{
+			return ($this->params['hide_after_submission'] && (isset($this->request['submission_key']) && !empty($this->request['submission_key'])));
+		}
+		
+		function hide_in_admin_view()
+		{
+			return ($this->params['hide_in_admin_view'] && (isset($this->request['form_admin_view'])));
+		}
+		
+		function hide_on_edit()
+		{
+			return ($this->params['hide_on_edit'] && (isset($this->request['form_id']) && ($this->request['form_id'] > 0)));
 		}
 	}
 ?>
