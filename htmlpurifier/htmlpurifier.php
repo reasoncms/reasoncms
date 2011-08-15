7e79d7c0bda65817ae675bf4efa349cdf14a292c
Nathan White <nwhite@carleton.edu>
2011-08-15 20:45:47 +0000

    removed old versions, updated to 4.3.0

--- htmlpurifier/htmlpurifier.php
+++ htmlpurifier/htmlpurifier.php
@@ -1,10 +1,10 @@
 <?php
-	include_once('paths.php');
-	include_once(CARL_UTIL_INC . 'basic/misc.php');
+include_once('paths.php');
+include_once(CARL_UTIL_INC . 'basic/misc.php');
 
-	/**
-	 * Loads the HTML_Purifier class appropriate for the version of PHP
-	 */
-	if (carl_is_php5()) require_once( HTML_PURIFIER_INC . 'htmlpurifier-4.0.0-standalone/HTMLPurifier.standalone.php' );
-	else require_once( HTML_PURIFIER_INC . 'htmlpurifier-2.1.5-standalone/HTMLPurifier.standalone.php' );
+/**
+ * Loads the HTML_Purifier class appropriate for the version of PHP
+ */
+
+require_once( HTML_PURIFIER_INC . 'htmlpurifier-4.3.0-standalone/HTMLPurifier.standalone.php' );
 ?>
