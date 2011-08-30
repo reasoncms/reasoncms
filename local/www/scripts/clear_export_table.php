2be6fae7183f5d2dc2b7e43acb9aa5393dffe3a5
slylth <slylth@gmail.com>
2011-08-30 21:41:47 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2082 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/www/scripts/clear_export_table.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Stub for the clearing info from the /var/reason_admissions_app_exports/application_exports
+ *
+ * No additional code should be placed in this file
+ *
+ * @package reason_package_local
+ * @subpackage scripts
+ */
+include_once('reason_header.php');
+reason_include_once('/minisite_templates/modules/admissions_application/clear_export_table.php');
+
+?>
