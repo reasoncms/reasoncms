a68af43e153348f9c62358b7e4ca7150761c2da5
slylth <slylth@gmail.com>
2011-09-01 01:04:46 +0000

    calling script that creates the files when page loads modified the table (not showing filetype)
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2086 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/admissions_application_export.php
+++ local/minisite_templates/modules/admissions_application_export.php
@@ -1,13 +1,21 @@
 <?php
-include('/var/reason_admissions_app_exports/export_admissions_apps.php');
-connectDB(REASON_DB);
+
 reason_include_once('minisite_templates/modules/default.php');
 
 $GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'admissionsApplicationExportModule';
 
 class admissionsApplicationExportModule extends DefaultMinisiteModule {
 
+    function  init($args = array()) {
+        parent::init($args);
+
+        // run the script that will create the export file for download
+        include('admissions_application/admissions_application_export.php');
+        connectDB(REASON_DB);
+    }
+
     function run() {
+        
         $base_path = '/var/reason_admissions_app_exports/application_exports/';
         if ($application_directory = opendir($base_path)) {
             while (false !== ($entryName = readdir($application_directory))) {
@@ -24,15 +32,16 @@ class admissionsApplicationExportModule extends DefaultMinisiteModule {
             sort($dirArray);
 
             // echo 'em
-            echo('<TABLE border="0" cellpadding="0" cellspacing="0" class=tablesorter>'. "\n");
-            echo("<thead><TR><TH>Filename</TH><th>Filetype</th><th>Filesize</th></TR></thead>\n");
+            echo '<TABLE border="0" cellpadding="0" cellspacing="0" class=tablesorter>'. "\n";
+            echo "<thead>" .
+                "<TR>" .
+                    "<TH>Filename</TH>" .
+                    "<th>Filesize</th>" .
+                "</TR></thead>\n";
             // loop through the array of files and echo them all
             for ($index = 0; $index < $indexCount; $index++) {
                 if (substr("$dirArray[$index]", 0, 1) != ".") { // don't list hidden files
                     echo("<TR><TD><a href=/reason/scripts/admissions_retrieve_app_export_file.php?file_name=" . $dirArray[$index] . ">" . $dirArray[$index] . "</a></td>");
-                    echo("<td>");
-                    echo(filetype($base_path . $dirArray[$index]));
-                    echo("</td>");
                     echo("<td>");
                     echo(format_bytes_as_human_readable(filesize($base_path . $dirArray[$index])));
 
@@ -41,6 +50,8 @@ class admissionsApplicationExportModule extends DefaultMinisiteModule {
                 }
             }
             echo("</TABLE>\n");
+            echo '<p></p>';
+            echo "\n";
         }
     }
 
