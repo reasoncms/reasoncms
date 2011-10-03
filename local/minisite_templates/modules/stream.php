3d5ca0bdc9e347ecf3cef12df0b0204c2d976714
slylth <slylth@gmail.com>
2011-10-03 23:40:12 +0000

    added date/time check to only show the stream during chapel times added link to iTunes U archives
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2131 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/modules/stream.php
+++ local/minisite_templates/modules/stream.php
@@ -1,38 +1,44 @@
 <?php
-	reason_include_once( 'minisite_templates/modules/default.php' );
 
-	$GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'streamModule';
+reason_include_once('minisite_templates/modules/default.php');
 
-	class StreamModule extends DefaultMinisiteModule
-	{
-		function init( $args = array() )
-		{
+$GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'streamModule';
 
-		}
-		function has_content()
-		{
-			return true;
-		}
-		function run()
-		{
-                        echo '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"';
-                        echo 'width="699" height="598"';
-                        echo 'codebase="http://www.apple.com/qtactivex/qtplugin.cab">';
-                        echo '<param name="SRC" value = "/reason/images/270672.jpg" />';
-                        echo '<param name="QTSRC" value = "/reason/images/270672.jpg" />';
-                        echo '<param name="HREF" value = "rtsp://video-3.luther.edu/worship.sdp" />';
-                        echo '<param name="AUTOPLAY" value = "true" />';
-                        echo '<param name="CONTROLLER" value = "false" />';
-                        echo '<param name="TYPE" value = "video/quicktime" />';
-                        echo '<param name="TARGET" value = "myself" />';
-                        echo '<embed src = "/reason/images/352571.png" qtsrc = "/reason/images/352571.png" href =';
+class streamModule extends DefaultMinisiteModule {
+
+    function run() {
+        $day_of_week = date('l');
+        $hour = idate('H');
+        $minutes = idate('i');
+        
+        if (($day_of_week == 'Monday' || $day_of_week == 'Wednesday' || $day_of_week == 'Friday')
+                && ($hour == 10) && ($minutes > 25) && ($minutes < 55)) {
+            echo '<object classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"';
+            echo 'width="699" height="598"';
+            echo 'codebase="http://www.apple.com/qtactivex/qtplugin.cab">';
+            echo '<param name="SRC" value = "/reason/images/270672.jpg" />';
+            echo '<param name="QTSRC" value = "/reason/images/270672.jpg" />';
+            echo '<param name="HREF" value = "rtsp://video-3.luther.edu/worship.sdp" />';
+            echo '<param name="AUTOPLAY" value = "true" />';
+            echo '<param name="CONTROLLER" value = "false" />';
+            echo '<param name="TYPE" value = "video/quicktime" />';
+            echo '<param name="TARGET" value = "myself" />';
+            echo '<embed src = "/reason/images/352571.png" qtsrc = "/reason/images/352571.png" href =';
 //                        echo '<embed href =';
-                        echo '"rtsp://video-3.luther.edu/worship.sdp" target ="myself" controller =';
-                        echo '"false" width = "699" height = "598" loop = "False" autoplay = "true"';
-                        echo 'plugin = "quicktimeplugin" type ="video/quicktime" cache = "false"';
-                        echo 'pluginspage= "http://www.apple.com/quicktime/download/" >';
-                        echo '</embed>';
-                        echo '</object>';
-		}
-	}
+            echo '"rtsp://video-3.luther.edu/worship.sdp" target ="myself" controller =';
+            echo '"false" width = "699" height = "598" loop = "False" autoplay = "true"';
+            echo 'plugin = "quicktimeplugin" type ="video/quicktime" cache = "false"';
+            echo 'pluginspage= "http://www.apple.com/quicktime/download/" >';
+            echo '</embed>';
+            echo '</object>';
+        } else {
+            echo '<p>The stream is available during Chapel 10:30 - 10:50 a.m. cst on Mondays, Wednesdays and Fridays.
+                Please come back at those times. Thank you.</p>';
+            echo '<p></p>';
+        }
+        echo '<p><a href="http://itunes.apple.com/WebObjects/MZStore.woa/wa/viewPodcast?id=467424979" target="_blank">Chapel Archives</a> on iTunes U.</p>';
+    }
+
+}
+
 ?>
