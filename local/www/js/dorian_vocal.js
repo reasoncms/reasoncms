f3a2c8807a0e33de7dd2df48b81aa14a921859ba
slylth <slylth@gmail.com>
2011-10-07 20:01:09 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2134 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/www/js/dorian_vocal.js
@@ -0,0 +1,19 @@
+/* 
+ * 
+ * Dorian Vocal Festival registration js
+ * https://reasondev.luther.edu/music/dorian/vocal/nominate
+ * shows or hides info based on answers
+ * 
+ */
+
+$(document).ready(function() {
+   
+   
+});
+
+function toggle_cc_eligibilty(){
+    if ($("#checkbox_desired_participation_2:checked").val() == '$20 - 1 treat'){
+        alert('Yo');
+    }
+    
+}
