5a45ac933bc820f79ca0fd2485811c27c7dcf289
burkaa01 <burkaa01@722e0bee-fe6a-0410-96d5-b98bc7338b40>
2011-10-19 23:38:54 +0000

    A toggle_all helper function to toggle extra options when a user is logged out.
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2150 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/directory_logout.js
+++ local/www/js/directory_logout.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
         show_all();
     }
 });
-
 function show_all(){
     $("#searchforRow").show();
     $("#departRow").show();
@@ -15,13 +14,13 @@ function show_all(){
     $("#displayasRow").show();
     $("#hidecommentRow").show();
     //table data shown only when logged in
-    $("#phonenumberRow").show();
-    $("#roomRow").show();
-    $("#studentcommentRow").show();
-    $("#majorRow").show();
-    $("#yearRow").show();
-    $("#facultycommentRow").show();
-//$("#facultycommentRow").animate({"height": "toggle"}, { duration: 1000 });
+    //$("#phonenumberRow").show();
+    //$("#roomRow").show();
+    //$("#studentcommentRow").show();
+    //$("#majorRow").show();
+    //$("#yearRow").show();
+    //$("#facultycommentRow").show();
+    //$("#facultycommentRow").animate({"height": "toggle"}, { duration: 1000 });
 }
 function hide_all()
 {
@@ -31,14 +30,28 @@ function hide_all()
     $("#displayasRow").hide();
     $("#hidecommentRow").hide();
     //table data hiden only when logged in
-    $("#phonenumberRow").hide();
-    $("#roomRow").hide();
-    $("#studentcommentRow").hide();
-    $("#majorRow").hide();
-    $("#yearRow").hide();
-    $("#facultycommentRow").hide();
+    //$("#phonenumberRow").hide();
+    //$("#roomRow").hide();
+    //$("#studentcommentRow").hide();
+    //$("#majorRow").hide();
+    //$("#yearRow").hide();
+    //$("#facultycommentRow").hide();
 }
-
+function toggle_all()
+{
+    $("#searchforRow").toggle();
+    $("#departRow").toggle();
+    $("#titleRow").toggle();
+    $("#displayasRow").toggle();
+    $("#hidecommentRow").toggle();
+    //table data hiden only when logged in
+    //$("#phonenumberRow").toggle();
+    //$("#roomRow").toggle();
+    //$("#studentcommentRow").toggle();
+    //$("#majorRow").toggle();
+    //$("#yearRow").toggle();
+    //$("#facultycommentRow").toggle();
+}
 function hide_field(element)
 {
     element = "tr#"+element;
@@ -58,7 +71,6 @@ function animate_field(element)
         duration: 0
     });
 }
-
 function input_error_check(html_id) {
 
     if ($(html_id).val() != "")
