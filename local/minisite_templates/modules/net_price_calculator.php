eb628138f808df3851951e4f15e6c303cd40436a
slylth <slylth@gmail.com>
2011-10-21 20:27:52 +0000

    initial add
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2153 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/minisite_templates/modules/net_price_calculator.php
@@ -0,0 +1,17 @@
+<?php
+
+reason_include_once('minisite_templates/modules/default.php');
+
+$GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'netPriceCalculatorModule';
+
+class netPriceCalculatorModule extends DefaultMinisiteModule {
+
+    function run() {
+
+        echo '<a style="display:none" id="npclink" href="https://npc.collegeboard.org/student/app/luther?iframe=true" target="npcframe"></a>';
+        echo '<iframe style="display:none" src = "https://npc.collegeboard.org/student/app/luther?iframe=true" id="npcframe" name="npcframe" width="1000px" height="700px" scrolling="auto"></iframe>';
+        echo '<script type="text/javascript" src="https://npc.collegeboard.org/student/static/js/iframe_display.js"></script>';
+    }
+
+}
+?>
