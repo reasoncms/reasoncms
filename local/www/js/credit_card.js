694b5be4ebe1a08e03d351c2f8d4ce1674c7f610
slylth <slylth@gmail.com>
2011-10-26 01:00:06 +0000

    hides credit card payment fields if a '$' is not present in the payment amount options
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2162 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/credit_card.js
+++ local/www/js/credit_card.js
@@ -1,43 +1,61 @@
+/**
+ *
+ * Hides credit card fields for forms that offer a no payment options
+ *
+ * @author Steve Smith
+ * @author Ben Wilbur
+ * 
+ * @todo add features for all credit card payments and add to /usr/local/webapps/reason/reason_package_local/local/minisite_templates/modules/form/views/thor/credit_card_payment.php
+ * @todo feature - if multipage disco, autofill credit card and billing fields with previous information (checkbox "Use same address....")
+ */
 
 
 $(document).ready(function() {
-    $(".words:contains('Payment Amount')").sibling().each(function(){
-        alert(this.html());
-    });
+
+    if ($("input[value='No Payment Option']")) {
+        hide_credit_card_info();
+    }
     
-    $(".words:contains('Payment Amount')").change(function()
-    {
-        // If "Yes" is checked
-        if ($("#radio_transfer_0").is(":checked"))
-        {
-            //show "transfer college" element
-            $("#transfercollegeRow").show();
+    $(".words:contains('Payment Amount')").next().children().children().children().children().change(function(){        
+        var value = $(this+":checked").val();
+        if (value.charAt(0) == '$'){
+            show_credit_card_info();
+        } else {
+            hide_credit_card_info();
         }
-        else
-        {
-            //otherwise, hide it
-            $("#transfercollegeRow").hide();
-        }
-    })
-           
+
+    });           
 });
 
-function toggle_credit_card_info(){
+function hide_credit_card_info(){
     
-    if ($("input[value='No Payment Option']")) {
-        alert('yo');
-        $("#paymentnoteRow").css('display', 'none');
-        $("#creditcardtypeRow").css('display', 'none');
-        $("#creditcardnumberRow").css('display', 'none');
-        $("#creditcardexpirationmonthRow").css('display', 'none');
-        $("#creditcardexpirationyearRow").css('display', 'none');
-        $("#creditcardnameRow").css('display', 'none');
-        $("#billingstreetaddressRow").css('display', 'none');
-        $("#billingcityRow").css('display', 'none');
-        $("#billingstateprovinceRow").css('display', 'none');
-        $("#billingzipRow").css('display', 'none');
-        $("#billingcountryRow").css('display', 'none');
-    }
+    $("#paymentnoteRow").css('display', 'none');
+    $("#creditcardtypeRow").css('display', 'none');
+    $("#creditcardnumberRow").css('display', 'none');
+    $("#creditcardexpirationmonthRow").css('display', 'none');
+    $("#creditcardexpirationyearRow").css('display', 'none');
+    $("#creditcardnameRow").css('display', 'none');
+    $("#billingstreetaddressRow").css('display', 'none');
+    $("#billingcityRow").css('display', 'none');
+    $("#billingstateprovinceRow").css('display', 'none');
+    $("#billingzipRow").css('display', 'none');
+    $("#billingcountryRow").css('display', 'none');
+    
+}
+
+function show_credit_card_info(){
+    
+    $("#paymentnoteRow").show();
+    $("#creditcardtypeRow").show();
+    $("#creditcardnumberRow").show();
+    $("#creditcardexpirationmonthRow").show();
+    $("#creditcardexpirationyearRow").show();
+    $("#creditcardnameRow").show();
+    $("#billingstreetaddressRow").show();
+    $("#billingcityRow").show();
+    $("#billingstateprovinceRow").show();
+    $("#billingzipRow").show();
+    $("#billingcountryRow").show();
     
 }
 
