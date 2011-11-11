94018298e13c7e25f3ba2a39a8e6cde130210661
slylth <slylth@gmail.com>
2011-11-11 23:12:54 +0000

    added show/hide functionality for various fields
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2191 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/www/js/dorian_band.js
+++ local/www/js/dorian_band.js
@@ -1,15 +1,51 @@
 $(document).ready(function() {
-	alert('hihi');
-    toggle_cc_eligibilty(0);
-    $("input#id_banquet_0").change(function(){
-        toggle_cc_eligibilty(500);
-        });
+
+    // Director Form //
+    toggle_banquet_attendees('N', 0);
+    $("input[name$='banquet']").click(function(){
+        toggle_banquet_attendees($(this).val(), 500);
+    });
+        
+    // Student Form //
+    toggle_allstate_details('N', 0);
+    $("input[name$='all_state']").click(function(){
+        toggle_allstate_details($(this).val(), 500);
+    });
+    
+    toggle_honor_band_details('N', 0);
+    $("input[name$='honor_band']").click(function(){
+        toggle_honor_band_details($(this).val(), 500);
+    });
 });
 
-function toggle_cc_eligibilty(time){
-    if ($('input#id_banquet_0').is(':checked')){
-        $('#input#id_banquet_attendees').show(time);
+function toggle_banquet_attendees(value, time){
+    if (value == 'Y'){
+        $('#banquet_attendees').show(time);
     } else {
-        $('# input#id_banquet_attendees').hide(time);
+        $('#banquet_attendees').hide(time);
+    }
+}
+
+function toggle_allstate_details(value, time){
+    if (value == 'Y'){
+        $('#allStateYears').show(time);
+        $('#allStatePart').show(time);
+        $('#allStateChair').show(time);
+    } else {
+        $('#allStateYears').hide(time);
+        $('#allStatePart').hide(time);
+        $('#allStateChair').hide(time);
+    }
+}
+
+function toggle_honor_band_details(value, time){
+    if (value == 'Y'){
+        $('#honorBandYears').show(time);
+        $('#honorBandPart').show(time);
+        $('#honorBandChair').show(time);
+    } else {
+        $('#honorBandYears').hide(time);
+        $('#honorBandPart').hide(time);
+        $('#honorBandChair').hide(time);
     }
 }
