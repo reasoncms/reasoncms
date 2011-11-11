af91fab3d47b2890412a219fd4859691441b414f
slylth <slylth@gmail.com>
2011-11-11 18:48:06 +0000

    initial adds. moving from /var/www/reason.luther.edu/htdoc/javascripts to reason www folder
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2184 722e0bee-fe6a-0410-96d5-b98bc7338b40

---
+++ local/www/js/eaglecam.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(document).bind("contextmenu",function(e){
+        return false;
+    });
+    
+   setTimeout("location.href = 'http://www.luther.edu/eaglecam/timeout';",900000);
+});
+
