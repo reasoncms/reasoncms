5fd8773ba704064e24caea728363921335dfcd96
Matt Ryan <mryan@carleton.edu>
2011-12-09 18:06:13 +0000

    shortened max execution time from 45 seconds to 30 seconds, as FF was still timing out sometimes

--- reason_4.0/lib/core/scripts/upgrade/4.0_to_4.1/thumbnail_standardize.php
+++ reason_4.0/lib/core/scripts/upgrade/4.0_to_4.1/thumbnail_standardize.php
@@ -73,11 +73,11 @@ class ReasonUpgrader_41_ThumbnailStandardize implements reasonUpgraderInterface
 			$php_max_time = ini_get('max_execution_time');
 			if($php_max_time == 0)
 			{
-				$time_limit = 45;
+				$time_limit = 30;
 			}
 			else
 			{
-				$time_limit = min($php_max_time/2, 45);
+				$time_limit = min($php_max_time/2, 30);
 			}
 			$es = new entity_selector();
 			$es->add_type(id_of('image'));
