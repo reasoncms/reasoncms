60a9f8f2ef71fddc9494456ea6b21ff735481273
Nathan White <nwhite@carleton.edu>
2012-01-30 21:27:53 +0000

    added phpdocumentor stuff

--- www/disco/plugins/input_limiter/get_char_count.php
+++ www/disco/plugins/input_limiter/get_char_count.php
@@ -1,19 +1,27 @@
 <?php
 /**
- *
- * @author Nick Jones
  * Simple character counting script that responds to AJAX POST
- * Used to maintain consistency in character counting between counting characters in real time in
- * the browser and with counting characters in Reason/php/database
+ *
+ * This is used to maintain consistency in character counting between counting characters in 
+ * real time in the browser and with counting characters in Reason/php/database.
+ *
+ * @package disco
+ * @subpackage plugins
+ * @author Nick Jones
  */
-    include_once( 'paths.php');
-    include_once( CARL_UTIL_INC . 'basic/misc.php' );
-    if(isset($_REQUEST['text']))
-    {
-	    echo carl_util_count_html_text_characters($_REQUEST['text']);
-	}
-	else
-	{
-		echo 0;
-	}
+
+/**
+ * Include dependencies.
+ */
+include_once( 'paths.php');
+include_once( CARL_UTIL_INC . 'basic/misc.php' );
+
+if(isset($_REQUEST['text']))
+{
+	echo carl_util_count_html_text_characters($_REQUEST['text']);
+}
+else
+{
+	echo 0;
+}
 ?>
