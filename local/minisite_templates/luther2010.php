830b5de75149b0367331740b8f81c3b208e14066
brifishjones <jonebr01@gmail.com>
2012-02-02 12:24:36 +0000

    fix broken addthis links
    git-svn-id: http://svn.luther.edu/svn/reasonLocalRepository@2278 722e0bee-fe6a-0410-96d5-b98bc7338b40

--- local/minisite_templates/luther2010.php
+++ local/minisite_templates/luther2010.php
@@ -492,14 +492,14 @@ class LutherTemplate2010 extends MinisiteTemplate
 		}
 		echo '<!-- AddThis Button BEGIN -->'."\n";
 		echo '<div class="addthis_toolbox addthis_default_style">'."\n";
-		echo '<a href="www.addthis.com/bookmark.php?v=250&amp;pub=lutheraddthis" class="addthis_button_compact"></a>'."\n";
+		echo '<a href="//www.addthis.com/bookmark.php?v=250&amp;pub=lutheraddthis" class="addthis_button_compact"></a>'."\n";
 		echo '<span class="addthis_separator">|</span>'."\n";
 		echo '<a class="addthis_button_facebook"></a>'."\n";
 		echo '<a class="addthis_button_twitter"></a>'."\n";
 		echo '<a class="addthis_button_email"></a>'."\n";
 		echo '<a class="addthis_button_print"></a>'."\n";
 		echo '</div>'."\n";
-		echo '<script type="text/javascript" src="s7.addthis.com/js/250/addthis_widget.js#pub=lutheraddthis">'."\n";
+		echo '<script type="text/javascript" src="//s7.addthis.com/js/250/addthis_widget.js#pub=lutheraddthis">'."\n";
 		echo 'var addthis_share = {'."\n";
 		echo 'url : ' . get_current_url() . ','."\n";
 		echo '}'."\n";
