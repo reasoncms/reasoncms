deea00a1c5f82e1d9dfc6885acf0e22221f64763
Benjamin Wilbur <wilburb@wilburweb.com>
2012-05-18 15:38:06 -0500

    Testing some git procedures.

--- googlecaltoreason/index.php
+++ googlecaltoreason/index.php
@@ -1,7 +1,7 @@
 <?php
 /* 
  * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor!
  */
 $startDate = date('Y-m-d');
 $endDate = date('Y-m-d', strtotime('+1 week'));
