d944f4c37578e6afd20e21630459d1ddab605476
Benjamin Wilbur <wilburb@wilburweb.com>
2012-05-18 15:56:44 -0500

    more git testing

--- googlecaltoreason/zendcal.php
+++ googlecaltoreason/zendcal.php
@@ -4,7 +4,6 @@
  *
  * LICENSE
  *
- *
  * This source file is subject to the new BSD license that is bundled
  * with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
