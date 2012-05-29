fa988beb74fc3be9c0ce25ca8a94cbf865cc632a
Nathan White <natepixel@gmail.com>
2012-05-29 21:22:36 +0000

    first commit settings for akismet

---
+++ settings/akismet_settings.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file setups the following:
+ *
+ * - akismet API key
+ *
+ * If you provide an API key, Reason's publications module will consult akismet to try to determine 
+ * whether comments submitted by anonymous individuals are spam and should be rejected.
+ *
+ * As of May 29, 2012, keys are free for personal blogs, and enterprise licenses cost $50 per month.
+ *
+ * http://akismet.com/
+ *
+ * @todo option to check authenticated comments?
+ */
+
+/**
+ * Path to our akismet php library.
+ */
+define('AKISMET_INC', INCLUDE_PATH.'akismet/');
+
+/**
+ * Akismet API Key.
+ */
+domain_define('AKISMET_API_KEY', '' );
+?>
