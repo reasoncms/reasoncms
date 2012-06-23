7f6572e9ab3d38fe760a01a399443516c6b593a1
Matt Ryan <mryan@carleton.edu>
2012-06-23 16:49:10 +0000

    stripped out show_all functionality, as it is now supported in the parent class

--- reason_4.0/lib/core/minisite_templates/modules/policy_related.php
+++ reason_4.0/lib/core/minisite_templates/modules/policy_related.php
@@ -18,50 +18,12 @@ $GLOBALS[ '_module_class_names' ][ basename( __FILE__, '.php' ) ] = 'RelatedPoli
  */
 class RelatedPolicyModule extends PolicyModule
 {
-	function get_cleanup_rules()
-	{
-		$cr = parent::get_cleanup_rules();
-		$cr['show_all'] = array( 'function' => 'check_against_array', 
-								 'extra_args' => array( 'true', 'false' ) );
-								 
-		$cr['policy_id'] = array( 'function' => 'turn_into_int' );
-		return $cr;
-	}
-	
 	protected function _get_es()
 	{
 		$es = parent::_get_es();
 		$es->add_right_relationship( $this->page_id, relationship_id_of('page_to_policy') );
 		return $es;
 	}
-	
-	// Temporarily disabled until we can figure about a better way to support show_all
-	/* function run() // {{{
-	{
-		if( !empty($this->request[ 'show_all' ]) )
-		{
-			// this->values contains the policies associated with this page.
-			// lopo through those looking for root nodes to list
-			foreach( $this->values AS $node )
-				if( $node->id() == $node->get_value( 'parent_id' ) )
-				{
-					// HUUUUUUUUUUUUUUUUGE HACK
-					// this really isn't that bad.  the reason it works is
-					// because of the flawed design of the listers.  the policy
-					// navigation lister depends on the policy_id IN the request
-					// array to determine which root to use.  So what I'm doing
-					// is just running the do_display method several times while
-					// changing the internal storage of the policy_id
-					$this->request['policy_id'] = $node->id();
-					$this->pages->do_display();
-				}
-
-		}
-		else
-		{
-			parent::run();
-		}
-	} // }}} */
 }
 	
 ?>
