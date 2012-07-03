538f85659efa12dde6b61992f697a9e569c216d8
Nathan White <natepixel@gmail.com>
2012-07-03 21:45:12 +0000

    enabled inline editing for page title

--- reason_4.0/lib/core/minisite_templates/modules/page_title.php
+++ reason_4.0/lib/core/minisite_templates/modules/page_title.php
@@ -16,13 +16,115 @@
 	 */
 	class PageTitleModule extends DefaultMinisiteModule
 	{
+		var $_init_title;
+	
+		function init( $args = array() )
+		{
+			$this->_init_title = $this->parent->title;
+			// register the module for inline editing
+			$inline_edit =& get_reason_inline_editing($this->page_id);
+			$inline_edit->register_module($this, $this->user_can_inline_edit());
+		}
+		
+		/**
+		 * Determines whether or not the user can inline edit. Only admin users may 
+		 * perform inline editing for the page title.
+		 *
+		 * @return boolean;
+		 */
+		function user_can_inline_edit()
+		{
+			if (!isset($this->_user_can_inline_edit))
+			{
+				$this->_user_can_inline_edit = (reason_check_authentication() && reason_check_access_to_site($this->site_id));
+			}
+			return $this->_user_can_inline_edit;
+		}
+	
 		function has_content()
 		{
 			return !empty( $this->parent->title );
 		}
+		
 		function run()
 		{
-  			echo '<h2 class="pageTitle"><span>'.$this->parent->title.'</span></h2>';
+			echo '<div class="pageTitle">'."\n";
+			$inline_edit =& get_reason_inline_editing($this->page_id);
+			if ($inline_edit->available_for_module($this) && ($this->_init_title == $this->parent->title) )
+			{
+				$this->run_editable();
+			}
+			else
+			{
+				echo $this->get_formatted_page_title();
+			}
+			echo '</div>'."\n";
 		}
+		
+		function run_editable()
+		{
+			// Show a small disco inline editing form if it is activated, display the title
+			// and an activation button if not.
+			$inline_edit =& get_reason_inline_editing($this->page_id);
+			$active = $inline_edit->active_for_module($this);
+			$class = ($active) ? 'editable editing' : 'editable';
+			echo '<div class="'.$class.'">'."\n";
+			if ($inline_edit->active_for_module($this))
+			{												
+				$form = new Disco();
+				$form->strip_tags_from_user_input = true;
+				$form->allowable_HTML_tags = '';
+				$form->add_element('page_title', 'text');
+				$form->set_value('page_title', $this->parent->title);
+				$form->add_required('page_title');
+				$form->add_callback(array(&$this, 'process_editable'),'process');
+				$form->add_callback(array(&$this, 'where_to_editable'), 'where_to');
+				$form->run();
+			}
+			else
+			{
+				$url = carl_make_link($inline_edit->get_activation_params($this));
+				
+				$link = '<p><a href="'.$url.'" class="editThis">Edit Page Title</a></p>'."\n";
+				$pre = '<div class="editRegion">'."\n";
+				$post = '</div>'."\n";
+				$output = $pre . $this->get_formatted_page_title() . $link . $post;
+				
+				echo $output;
+			}
+			echo '</div>'."\n";
+		}
+		
+		/**
+		 * After we save the title change we also need to destroy the navigation cache for the site.
+		 */
+		function process_editable(&$disco)
+		{
+			$page = new entity($this->page_id);
+			$values['name'] = trim(strip_tags($disco->get_value('page_title')));
+			if ($page->get_value('name') != $values['name'])
+			{
+				$user_id = get_user_id(reason_check_authentication());
+				reason_update_entity($this->page_id, $user_id, $values, true);
+			
+				// clear nav cache
+				reason_include_once('classes/object_cache.php');
+				$cache = new ReasonObjectCache($this->site_id . '_navigation_cache');
+				$cache->clear();
+			}
+		}
+		
+		function where_to_editable(&$disco)
+		{
+			$inline_edit =& get_reason_inline_editing($this->page_id);
+			$url = carl_make_redirect($inline_edit->get_deactivation_params($this));
+			return $url;
+		}
+	
+		function get_formatted_page_title()
+		{
+			return '<h2 class="pageTitle"><span>'.$this->parent->title.'</span></h2>';
+		}
+		
 	}
 ?>
