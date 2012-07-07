c20d461595035a9b4df399d6cabda391cb29f5f2
Matt Ryan <mryan@carleton.edu>
2012-07-07 17:47:02 +0000

    Added support for locks

--- reason_4.0/lib/core/classes/admin/modules/sort_posts.php
+++ reason_4.0/lib/core/classes/admin/modules/sort_posts.php
@@ -54,6 +54,15 @@
 			$es->set_order('dated.datetime DESC');
 			$this->posts = $es->run_one();
 			
+			$user = new entity($this->admin_page->user_id);
+			foreach($this->posts as $id => $post)
+			{
+				if(!$post->user_can_edit_field('datetime',$user))
+				{
+					$this->locked_posts[$id] = $post;
+				}
+			}
+			
 		}
 		
 		/**
@@ -63,6 +72,7 @@
 		{
 			$d = new sortPostsDisco();
 			$d->set_posts($this->posts);
+			$d->set_locked_posts($this->locked_posts);
 			$d->set_user_id($this->admin_page->user_id);
 			echo '<div class="sortPostsModule">'."\n";
 			$d->run();
@@ -74,6 +84,7 @@
 	class sortPostsDisco extends disco
 	{
 		var $posts = array();
+		var $locked_posts = array();
 		var $user_id;
 		var $changes_made = false;
 		
@@ -87,6 +98,18 @@
 				$this->set_display_name($id, $post->get_value('name'));
 			}
 		}
+		function set_locked_posts($posts)
+		{
+			$this->locked_posts = $posts;
+			foreach($this->locked_posts as $id=>$post)
+			{
+				if($this->is_element($id))
+				{
+					$this->change_element_type($id, 'solidtext');
+					$this->set_comments($id, '<img 	class="lockIndicator" src="'.REASON_HTTP_BASE_PATH.'ui_images/lock_12px.png" alt="locked" width="12" height="12" />', 'before' );
+				}
+			}
+		}
 		function set_user_id($user_id)
 		{
 			$this->user_id = $user_id;
@@ -95,7 +118,7 @@
 		{
 			foreach($this->posts as $id=>$post)
 			{
-				if($this->get_value($id) && $this->get_value($id) != $post->get_value('datetime'))
+				if($this->get_value($id) && $this->get_value($id) != $post->get_value('datetime') && !isset($this->locked_posts[$id]) )
 				{
 					reason_update_entity( $id, $this->user_id, array('datetime'=>$this->get_value($id)));
 					$this->changes_made = true;
