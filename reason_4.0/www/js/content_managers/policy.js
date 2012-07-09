20806f1219e9327ffa8403c76e297ed39b60affe
Matt Ryan <mryan@carleton.edu>
2012-07-09 19:57:24 +0000

    javascript for the policy content manager

---
+++ reason_4.0/www/js/content_managers/policy.js
@@ -0,0 +1,13 @@
+function do_policy_element_hiding() {
+	selector = '#descriptionRow, #keywordsRow, #lastreviseddateRow, #lastrevieweddateRow, #approvalsRow, #limitaccessRow, #audiencesRow';
+	if( $('option:selected', '#parent_idElement').index() > 1)
+		$(selector).hide();
+	else
+		$(selector).show();
+}
+$(document).ready(function(){
+	do_policy_element_hiding();
+	$('#parent_idElement').change(function(){
+		do_policy_element_hiding();
+	});
+});
