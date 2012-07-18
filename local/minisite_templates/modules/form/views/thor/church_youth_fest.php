5233278b00b56ba4a38e1d1587ba9aef179bfcc7
Hannah Ranum <ranuha01@luther.edu>
2012-07-18 11:17:23 -0500

    trying to move parent call around

--- local/minisite_templates/modules/form/views/thor/church_youth_fest.php
+++ local/minisite_templates/modules/form/views/thor/church_youth_fest.php
@@ -19,13 +19,13 @@ class ChurchYouthFestPaymentForm extends CreditCardThorForm {
     // style up the form and add comments et al
     function on_every_time() {
 
-      parent::on_every_time();
-       $this->change_element_type($this->get_element_name_from_label('State'), 'state');
-       //$this->change_element_type($this->get_element_name_from_label('Payment Amount Placeholder'), 'solidtext');
-       $this->set_element_properties($this->get_element_name_from_label('Zipcode'), array('size'=>5));
-       $this->set_element_properties($this->get_element_name_from_label('# of Youth @ $25.00'), array('size'=>3));
-       $this->set_element_properties($this->get_element_name_from_label('# of Sponsors @ $20.00'), array('size'=>3));
-       $this->set_element_properties($this->get_element_name_from_label('# for Ropes Course @ $5.00'), array('size'=>3));
+      // parent :: on_every_time(); 
+      $this->change_element_type($this->get_element_name_from_label('State'), 'state');
+      //$this->change_element_type($this->get_element_name_from_label('Payment Amount Placeholder'), 'solidtext');
+      $this->set_element_properties($this->get_element_name_from_label('Zipcode'), array('size'=>5));
+      $this->set_element_properties($this->get_element_name_from_label('# of Youth @ $25.00'), array('size'=>3));
+      $this->set_element_properties($this->get_element_name_from_label('# of Sponsors @ $20.00'), array('size'=>3));
+      $this->set_element_properties($this->get_element_name_from_label('# for Ropes Course @ $5.00'), array('size'=>3));
     }
 
     function get_amount(){
