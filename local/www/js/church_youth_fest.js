c17a94fb0e4b7e19cdea90d42206b55ae7e2dc37
Steve Smith <slylth@gmail.com>
2012-07-20 14:34:21 -0500

    removed redundant code (clean up)

--- local/www/js/church_youth_fest.js
+++ local/www/js/church_youth_fest.js
@@ -1,10 +1,5 @@
 $(document).ready(function() {
 
-  var dolamt = 0;
-  var youth = 0;
-  var adults = 0;
-  var ropes = 0;
-
   var studentNumber = $('#id_3fb4o1j748Element'); //# of Youth @ $25.00
   var sponsorNumber = $('#id_15354l4d84Element'); //# of Sponsors @ $20.00
   var ropesNumber = $('#id_1HJ33534J4Element'); //# for Ropes Course @ $5.00
@@ -13,25 +8,20 @@ $(document).ready(function() {
   setTotal();
 
   if ($(studentNumber).val().length > 0  || $(sponsorNumber).val().length > 0 || $(ropesNumber).val().length > 0){
-    dolamt = add();
+    setTotal();
   }
-        //# of Youth @ $25.00
-      $('#id_3fb4o1j748Element').blur(function() {
-          dolamt = add();
-          setTotal();
-        });
-      //# of Sponsors @ $20.00
-      $('#id_15354l4d84Element').blur(function() {
-        var sponsorvalue = $(this).val();
-        dolamt = add();
-        setTotal();
-      });
-      //# for Ropes Course @ $5.00
-      $('#id_1HJ33534J4Element').blur(function() {
-        var ropesvalue = $(this).val();
-        dolamt = add();
-        setTotal();
-      });
+    //# of Youth @ $25.00
+  $('#id_3fb4o1j748Element').blur(function() {
+      setTotal();
+    });
+  //# of Sponsors @ $20.00
+  $('#id_15354l4d84Element').blur(function() {
+    setTotal();
+  });
+  //# for Ropes Course @ $5.00
+  $('#id_1HJ33534J4Element').blur(function() {
+    setTotal();
+  });
 
   function getStudentAmount(number){
     return number * 25;
