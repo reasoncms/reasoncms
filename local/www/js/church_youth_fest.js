e736ff24cb79f3d47956cff3453820241d9c1d9e
Steve Smith <slylth@gmail.com>
2012-07-20 10:25:12 -0500

    fixed javascript adding and cleaned up code

--- local/www/js/church_youth_fest.js
+++ local/www/js/church_youth_fest.js
@@ -1,75 +1,55 @@
 $(document).ready(function() {
-  
+
   var dolamt = 0;
   var youth = 0;
   var adults = 0;
   var ropes = 0;
 
-  var sub_total = 0;
-  var total = 0;
-
-  var studentNumber = $('#id_3fb4o1j748Element');
-  // var studentNumber = parseInt($('#id_3fb4o1j748Element').val()) || 0;
-  // var sponsorNumber = parseInt($('#id_15354l4d84Element').val()) || 0;
-  var sponsorNumber = $('#id_15354l4d84Element');
-  // var ropesNumber = parseInt($('#id_1HJ33534J4Element').val()) || 0;
-  var ropesNumber = $('#id_1HJ33534J4Element');
-
+  var studentNumber = $('#id_3fb4o1j748Element'); //# of Youth @ $25.00
+  var sponsorNumber = $('#id_15354l4d84Element'); //# of Sponsors @ $20.00
+  var ropesNumber = $('#id_1HJ33534J4Element'); //# for Ropes Course @ $5.00
 
   $('#payment_amountElement').attr('readonly','readonly');
   setTotal();
 
-  if ($(studentNumber).val().length > 0  || $(sponsorNumber).val().length > 0 || $(ropesNumber).val().length > 0 ){
-    alert('i');
-    dolamt = getStudentAmount(parseInt(studentNumber.val())) + getSponsorAmount(sponsorNumber) + getRopesAmount(ropesNumber);
+  if ($(studentNumber).val().length > 0  || $(sponsorNumber).val().length > 0 || $(ropesNumber).val().length > 0){
+    dolamt = add();
   }
-
-
- 
-        
-        $('#id_3fb4o1j748Element').blur(function() {
-          var studentvalue = parseInt($(this).val()) || 0;
-          alert(studentNumber.val());
-          // youth = studentvalue * 25;
+        //# of Youth @ $25.00
+      $('#id_3fb4o1j748Element').blur(function() {
           dolamt = add();
           setTotal();
         });
-
+      //# of Sponsors @ $20.00
       $('#id_15354l4d84Element').blur(function() {
-         
-         var sponsorvalue = $(this).val();
-         // adults = sponsorvalue * 20;
-        dolamt = getStudentAmount(studentNumber) + getSponsorAmount(sponsorNumber) + getRopesAmount(ropesNumber); 
+        var sponsorvalue = $(this).val();
+        dolamt = add();
         setTotal();
       });
-
+      //# for Ropes Course @ $5.00
       $('#id_1HJ33534J4Element').blur(function() {
-         var ropesvalue = $(this).val();
-        // ropes = ropesvalue * 5;
-        dolamt = getStudentAmount(studentNumber) + getSponsorAmount(sponsorNumber) + getRopesAmount(ropesNumber);
-        setTotal();  
+        var ropesvalue = $(this).val();
+        dolamt = add();
+        setTotal();
       });
 
   function getStudentAmount(number){
     return number * 25;
-  } 
-
+  }
   function getSponsorAmount(number){
     return number * 20;
-  } 
+  }
 
   function getRopesAmount(number){
     return number * 5;
-  }  
+  }
 
   function add(){
-    return getStudentAmount(parseInt(studentNumber.val())) + getSponsorAmount(parseInt(sponsorNumber.val())) + getRopesAmount(parseInt(ropesNumber.val()));
+    return getStudentAmount(studentNumber.val()) + getSponsorAmount(sponsorNumber.val()) + getRopesAmount(ropesNumber.val());
   }
 
   function setTotal(){
-    $('#payment_amountElement').val(dolamt);
+    $('#payment_amountElement').val(add());
     $('#payment_amountElement').effect( 'highlight');
   }
 });
-
-  
