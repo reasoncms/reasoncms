4d88e6ee8ee4b96617a17789db249b692043e858
Matt Ryan <mryan@carleton.edu>
2012-08-02 15:40:35 +0000

    Added ability to filter users by audience

--- reason_4.0/lib/core/classes/admin/modules/active_users.php
+++ reason_4.0/lib/core/classes/admin/modules/active_users.php
@@ -8,6 +8,7 @@
   * Include the default module and other needed utilities
   */
 	reason_include_once('classes/admin/modules/default.php');
+	include_once( CARL_UTIL_INC . 'dir_service/directory.php' );
 	include_once(DISCO_INC.'disco.php');
 	
 	/**
@@ -31,16 +32,39 @@
 				return;
 			}
 			
+			// get audiences in REason
+			$es = new entity_selector();
+			$es->add_type(id_of('audience_type'));
+			$audiences = $es->run_one();
+			
+			$options = array();
+			
+			foreach ($audiences as $aud)
+			{
+				$options[$aud->get_value('directory_service_value')] = $aud->get_value('name');
+			}
+			
 			$d = new disco();
-			$d->add_element('active_since','textdatetime');
+			$d->add_element('active_since', 'textdatetime');
+			$d->add_element('affiliations', 'checkboxgroup', array('options' => $options));
+			$d->set_display_name('affiliations', 'Audiences');
+			$d->add_comments('affiliations', form_comment('Leaving these checkboxes blank won\'t filter the results.'));
+			
 			$d->set_actions(array('run'=>'Run'));
 			$d->run();
 			
 			if($d->get_value('active_since'))
-			{
+			{	
 				$user_ids = $this->_get_active_user_ids($d->get_value('active_since'));
 				echo count($user_ids).' Reason users modified at least one item since '.prettify_mysql_datetime($d->get_value('active_since')).'<br />';
-				$users = $this->_get_active_users_from_ids($user_ids);
+								
+				if ($d->get_value('affiliations'))
+					$affiliations = array_values($d->get_value('affiliations'));	
+				else
+					$affiliations = array();
+				
+				
+				$users = $this->_get_active_users_from_ids($user_ids, $affiliations);
 				echo '<br />'.count($users).' of the above users currently have access to at least one site<br />';
 
 				if(!empty($users))
@@ -83,17 +107,55 @@
 			return $ids;
 		}
 		
-		function _get_active_users_from_ids($ids)
+		function _get_active_users_from_ids($ids, $affiliations)
 		{
 			@array_walk($ids,'addslashes');
 			
 			$es = new entity_selector();
 			$es->add_type(id_of('user'));
+			$es->enable_multivalue_results();
 			$es->add_relation('`entity`.`id` IN ("'.implode('","',$ids).'")');
 			$es->add_right_relationship_field('site_to_user','entity','id','site_membership_id'); // This is here so we only grab users who have access to a site
 			$users = $es->run_one();
+			
+			if (!empty($affiliations))
+			{
+				// First, retrieve all users from LDAP that are affiliated with the given audiences.
+				$dir = new directory_service();
+				$dir->search_by_attribute('ds_affiliation', $affiliations, array('ds_username'));
+				
+				$dir_results = $dir->get_records();				
+
+				// Merge the two arrays
+				$users = $this->_merge_users($users, array_keys($dir_results));
+			}
+			
 			return $users;
 		}
+		
+		function _merge_users($reason_users, $ldap_users)
+		{
+			$merged = array();
+			foreach ($reason_users as $user)
+			{
+				if (in_array($user->get_value('name'), $ldap_users))
+				{
+					$merged[] = $user;
+				}
+			}
+			
+			function cmp($a, $b)
+			{
+				if ($a->get_value('name') == $b->get_value('name')) {
+					return 0;
+				}
+				return ($a->get_value('name') < $b->get_value('name')) ? -1 : 1;
+			}
+			
+			usort($merged, 'cmp');
+			return $merged;
+		}
+		
 		function _get_email_addresses_from_users($users)
 		{
 			$usernames = array();
@@ -112,6 +174,9 @@
 					$emails[$rec['ds_username'][0]] = $rec['ds_email'][0];
 				}
 			}
+			
+			asort($emails);
+			
 			return $emails;
 		}
 	} // }}}
