1ced9446aa1d418bf523b1c9b2842cc659c80606
Matt Ryan <mryan@carleton.edu>
2012-08-20 13:59:29 +0000

    Added nice picker for theme customizers

--- reason_4.0/lib/core/content_managers/theme.php
+++ reason_4.0/lib/core/content_managers/theme.php
@@ -13,11 +13,25 @@
 	 */
 	class ThemeManager extends ContentManager
 	{
-		function alter_data() // {{{
+		function alter_data()
 		{
 			$this->add_relationship_element('template', id_of('minisite_template'), 
 			relationship_id_of('theme_to_minisite_template'),'right','select');
-		} // }}}
+			$this->change_element_type( 'theme_customizer','select',array('options'=>$this->get_theme_customizers() ) );
+		}
+		function get_theme_customizers( )
+		{
+			$array = array();
+			foreach(reason_get_merged_fileset('theme_customizers') as $k=>$v)
+			{
+				if($v != 'interface.php')
+				{
+					$name = basename($v, '.php');
+					$array[$name] = $name;
+				}
+			}
+			return prettify_array($array);
+		}
 		
 	}
 ?>
