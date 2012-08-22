b285fa06474fb02d9c00fc16010d4a96be7f4c91
Brian Jones <jonebr01@gmail.com>
2012-08-22 10:12:52 -0500

    removed duplicate breadcrumbs for issue based publications

--- reason_4.0/lib/local/minisite_templates/modules/luther_breadcrumbs.php
+++ reason_4.0/lib/local/minisite_templates/modules/luther_breadcrumbs.php
@@ -26,11 +26,23 @@
 			$b = $this->parent->_get_breadcrumb_markup($this->parent->_get_breadcrumbs(), $this->parent->site_info->get_value('base_breadcrumbs'), '&nbsp;&#187;&nbsp;');
 			
 			$url = get_current_url();
-			if (preg_match("/story_id=\d+$/", $url) // publication inserts link to story as well as the story itself so remove the link
-				|| preg_match("/[&?]event_id=\d+/", $url)) // event does too	
+			
+			$is_story = preg_match("/[&?]story_id=\d+/", $url);
+			$is_event = preg_match("/[&?]event_id=\d+/", $url);
+			$is_issue = preg_match("/[&?]issue_id=\d+/", $url);
+			
+			if ($is_story // publication inserts link to story as well as the story itself so remove the link
+				|| $is_event) // event does too	
 			{
 				$ba = explode('&nbsp;&#187;&nbsp;', $b);
-				array_splice($ba, -2, 1);
+				if ($is_issue)
+				{
+					array_splice($ba, -4, 3);
+				}
+				else
+				{
+					array_splice($ba, -2, 1);
+				}
 				$b = implode('&nbsp;&#187;&nbsp;', $ba);
 			}
 			$b = preg_replace("|(^.*?)\s\((w?o?m?en)\)$|", "\\1", $b);
