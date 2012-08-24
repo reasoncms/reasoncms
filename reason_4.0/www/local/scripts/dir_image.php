a0fa79f129546a41929691b001fee04f7dc5f418
Steve Smith <slylth@gmail.com>
2012-08-24 17:42:25 -0500

    changed path for new server

--- reason_4.0/www/local/scripts/dir_image.php
+++ reason_4.0/www/local/scripts/dir_image.php
@@ -2,5 +2,5 @@
     $image = $_GET['image'];
     header('Content-Type: image/jpg');
 //    header('Content-length:'.filesize($image));
-    readfile('/var/person_photos/'.$image.'.jpg');
+    readfile('/var/reason/person_photos/'.$image.'.jpg');
 ?>
