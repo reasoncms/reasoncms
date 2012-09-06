d80fd83348cbb09da1ff75a383b01d53253de30d
Matt Ryan <mryan@carleton.edu>
2012-09-06 15:49:33 +0000

    Added access checks & login requirement if page is behind login; updated to support kaltura video; switched to default to all media attached to page; added ability to specify number of items

--- reason_4.0/lib/core/feeds/media_files.php
+++ reason_4.0/lib/core/feeds/media_files.php
@@ -10,15 +10,119 @@
 include_once( 'reason_header.php' );
 reason_include_once( 'feeds/default.php' );
 reason_include_once( 'function_libraries/url_utils.php' );
+reason_include_once( 'function_libraries/user_functions.php' );
+reason_include_once( 'content_listers/tree.php3' );
+reason_include_once( 'minisite_templates/nav_classes/default.php' );
+reason_include_once( 'classes/page_access.php' );
+reason_include_once( 'classes/group_helper.php' );
 $GLOBALS[ '_feed_class_names' ][ basename( __FILE__, '.php' ) ] = 'mediaFileFeed';
 
 class mediaFileFeed extends defaultFeed
 {
 	var $feed_class = 'mediaFileRSS';
 	var $default_num_works = 15;
+	var $page_limited_default_num_works = -1;
 	var $_page;
 	var $_page_check_state = 'unchecked';
 	//var $page_types = array('av');
+	
+	
+	function run($send_header = true)
+	{
+		$this->get_site_id();
+		if($page = $this->_get_page())
+		{ 
+			if($groups = $this->get_page_restriction_groups($page,$this->site))
+			{
+				foreach($groups as $group)
+				{
+					$gh = new group_helper();
+					$gh->set_group_by_entity($group);
+					if($gh->requires_login())
+					{
+						$username = reason_require_http_authentication();
+						if(!$gh->is_username_member_of_group($username))
+						{
+							$this->_send_unauthorized_output($send_header);
+							die();
+						}
+					}
+				}
+			}
+		}
+		else
+		{
+			$pages = $this->get_sitewide_media_pages($this->site);
+			if(!empty($pages))
+			{
+				$restricted_pages = array();
+				$page_group_helpers = array();
+				foreach($pages as $page)
+				{
+					if($groups = $this->get_page_restriction_groups($page,$this->site))
+					{
+						foreach($groups as $group)
+						{
+							$gh = new group_helper();
+							$gh->set_group_by_entity($group);
+							if($gh->requires_login())
+							{
+								$restricted_pages[$page->id()] = $page;
+								if(!isset($page_group_helpers[$page->id()]))
+									$page_group_helpers[$page->id()] = array();
+								$page_group_helpers[$page->id()][] = $gh;
+							}
+						}
+					}
+				}
+				if(count($restricted_pages) >= count($pages))
+				{
+					$username = reason_require_http_authentication();
+					$access_ok = false;
+					foreach($restricted_pages as $page)
+					{
+						$is_member = true;
+						foreach($page_group_helpers[$page->id()] as $gh)
+						{
+							if(!$gh->is_username_member_of_group($username))
+							{
+								$is_member = false;
+								break;
+							}
+						}
+						if($is_member)
+						{
+							$access_ok = true;
+							break;
+						}
+					}
+					if(!$access_ok)
+					{
+						$this->_send_unauthorized_output($send_header);
+						die();
+					}
+				}
+			}
+			else
+			{
+				$this->_send_unauthorized_output($send_header);
+				die();
+			}
+		}
+		parent::run($send_header);
+	}
+	function _send_unauthorized_output($send_header)
+	{
+		if($send_header)
+			header('Content-type: text/xml');
+		echo '<?xml version="1.0" encoding="UTF-8"?>'."\n";
+		echo '<rss version="2.0">'."\n";
+		echo '<channel>'."\n";
+		echo '<title>Unauthorized</title>'."\n";
+		echo '</channel>'."\n";
+		echo '</rss>'."\n";
+		die();
+	}
 	function _get_page()
 	{
 		// note -- limiting to page only works for site-specific feeds to reduce spelunking
@@ -41,6 +145,44 @@ class mediaFileFeed extends defaultFeed
 			}
 		}
 		return $this->_page;
+	}
+	function get_page_restriction_groups($page,$site)
+	{
+		$rpa = new reasonPageAccess();
+		$page_tree = $this->get_page_tree($site);
+		$rpa->set_page_tree($page_tree);
+		return $rpa->get_groups($page->id());
+	}
+	function get_page_tree($site)
+	{
+		$pages = new MinisiteNavigation();
+		$pages->site_info = $site;
+		$pages->init( $this->site_id, id_of('minisite_page') );
+		return $pages;
+	}
+	function get_sitewide_media_pages($site)
+	{
+		$pts = page_types_that_use_module(array('av','av_with_filters'));
+		$ptq = array();
+		foreach($pts as $pt)
+		{
+			if(isset($GLOBALS['_reason_page_types'][$pt]))
+			{
+				foreach($GLOBALS['_reason_page_types'][$pt] as $loc=>$mod)
+				{
+					if(is_array($mod) && isset($mod['limit_to_current_page']) && false == $mod['limit_to_current_page'])
+						$ptq[] = addslashes($pt);
+				}
+			}
+		}
+		if(!empty($ptq))
+		{
+			$es = new entity_selector($this->site->id());
+			$es->add_type(id_of('minisite_page'));
+			$es->add_relation('custom_page IN("'.implode('","',$ptq).'")');
+			return $es->run_one();
+		}
+		return NULL;
 	}
 	function get_feed_title()
 	{
@@ -95,7 +237,10 @@ class mediaFileFeed extends defaultFeed
 		}
 		
 		$this->feed->set_item_field_map('title','id');
-		$this->feed->set_item_field_map('pubDate','work_publication_datetime');
+		if(empty($this->request['rel_sort']))
+			$this->feed->set_item_field_map('pubDate','work_publication_datetime');
+		else
+			$this->feed->set_item_field_map('pubDate','');
 		$this->feed->set_item_field_map('enclosure','id');
 		$this->feed->set_item_field_map('description','work_description');
 		$this->feed->set_item_field_handler( 'description', 'strip_tags', false );
@@ -104,7 +249,7 @@ class mediaFileFeed extends defaultFeed
 		// We're going to have to either improve the reason_rss class's handling of elements to do enclosures, or we're going to have to migrate to a general-purpose xml tool for it.
 		// Enclosure example:
 		// <enclosure url="http://www.scripting.com/mp3s/weatherReportSuite.mp3" length="12216320" type="audio/mpeg" />
-		$this->feed->set_item_field_validator( 'enclosure', 'validate_media_format_for_rss_enclosure' );
+		//$this->feed->set_item_field_validator( 'enclosure', 'validate_media_format_for_rss_enclosure' );
 		$this->feed->es->add_relation('url.url != ""');
 		$this->feed->es->add_relation('av.media_is_progressively_downloadable != "false"');
 		$this->feed->es->set_order('av.av_part_number ASC');
@@ -112,7 +257,7 @@ class mediaFileFeed extends defaultFeed
 		$ok_formats = array_keys(reason_get_valid_formats_for_podcasting());
 		$ok_formats = array_map('addslashes',$ok_formats);
 		// this is a hack because .mp4s should be included even if the media_format field is "invalid".
-		$this->feed->es->add_relation('((av.media_format IN ("'.implode('","',$ok_formats).'")) OR (url.url LIKE "%.mp4"))');
+		$this->feed->es->add_relation('((av.media_format IN ("'.implode('","',$ok_formats).'")) OR (url.url LIKE "%.mp4") OR (mime_type IN("video/mp4","audio/mpeg")) )');
 		$this->feed->es->set_site(NULL);
 		$this->feed->es->set_num(-1);
 
@@ -120,9 +265,17 @@ class mediaFileFeed extends defaultFeed
 		{
 			$this->feed->set_num_works($this->request['num_works']);
 		}
+		elseif(!empty($p))
+		{
+			$this->feed->set_num_works($this->page_limited_default_num_works);
+		}
 		else
 		{
 			$this->feed->set_num_works($this->default_num_works);
+		}
+		if(!empty($this->request['rel_sort']))
+		{
+			$this->feed->set_rel_sort(true);
 		}
 		if($this->_page_check_state == 'fail')
 		{
@@ -134,6 +287,7 @@ class mediaFileFeed extends defaultFeed
 class mediaFileRSS extends ReasonRSS
 {
 	var $num_works = 15;
+	var $rel_sort = false;
 	var $_page_id;
 	var $_nullify_items = false;
 	function set_num_works( $num )
@@ -143,6 +297,10 @@ class mediaFileRSS extends ReasonRSS
 	function set_page_id( $id )
 	{
 		$this->_page_id = $id;
+	}
+	function set_rel_sort($val)
+	{
+		$this->rel_sort = $val;
 	}
 	function nullify_items()
 	{
@@ -180,28 +338,38 @@ class mediaFileRSS extends ReasonRSS
 		$works_es->add_type( id_of('av') );
 		$works_es->set_num( $this->num_works );
 		$works_es->add_relation('show_hide.show_hide = "show"');
+		$works_es->add_relation('( integration_library = "" OR integration_library IS NULL OR ( integration_library = "kaltura" AND transcoding_status = "ready") )');
 		$works_es->set_order('media_work.media_publication_datetime DESC');
 		if(!empty($this->_page_id))
 		{
 			$works_es->add_right_relationship($this->_page_id, relationship_id_of('minisite_page_to_av'));
+			if($this->rel_sort)
+			{
+				$works_es->add_rel_sort_field($this->_page_id, relationship_id_of('minisite_page_to_av'));
+				$works_es->set_order('rel_sort_order ASC');
+			}
 		}
 		
 		$media_works = $works_es->run_one();
+		
 		
 		foreach($media_works as $work)
 		{
 			$es = carl_clone($this->es);
 			$es->add_right_relationship($work->id(),relationship_id_of('av_to_av_file'));
-			
+			if($work->get_value('integration_library') == 'kaltura')
+			{
+				$es->set_num(1);
+				$es->set_order('av.height DESC');
+			}
 			$media_files = $es->run_one();
-			/* echo 'Files found: '.count($media_files)."\n";
-			echo 'Query: '.$es->get_one_query()."\n"; */
 			foreach($media_files as $media_file)
 			{
 				$media_file->set_value('work_publication_datetime',$work->get_value('media_publication_datetime'));
 				$media_file->set_value('work_name',$work->get_value('name'));
 				$media_file->set_value('work_description',$work->get_value('description'));
 				$media_file->set_value('author',$work->get_value('author'));
+				$media_file->set_value('integration_library',$work->get_value('integration_library'));
 				$this->items[$media_file->id()] = $media_file;
 			}
 		}
@@ -220,19 +388,39 @@ class mediaFileRSS extends ReasonRSS
 				$size = 5242880;
 			}
 			$additional_attrs = $this->get_additional_enclosure_arributes( $item );
-			return '<'.$attr.' url="'.$item->get_value('url').'" length="'.$size.'" '.implode(' ',$additional_attrs).' />'."\n";
+			$url = $this->get_media_file_url( $item );
+			return '<'.$attr.' url="'.$url.'" length="'.$size.'" '.implode(' ',$additional_attrs).' />'."\n";
 		}
 		else
 		{
 			return '';
 		}
 	}
+	function get_media_file_url( $item )
+	{
+		if($item->get_value('integration_library') == 'kaltura')
+		{
+			switch($item->get_value('mime_type'))
+			{
+				case 'video/mp4':
+					return $item->get_value('url').'/a.mp4?novar=0';
+				case 'audio/mpeg':
+					return $item->get_value('url').'/a.mp3?novar=0';
+			}
+			
+		}
+		return $item->get_value('url');
+	}
 	// This is meant to be overloaded so that the rss feed can get the appropriate attributes
 	function get_additional_enclosure_arributes( $item )
 	{
 		$return = array();
 		$valid_formats = reason_get_valid_formats_for_podcasting();
-		if(array_key_exists($item->get_value('media_format'), $valid_formats))
+		if($item->get_value('mime_type'))
+		{
+			$return[] = 'type="'.$item->get_value('mime_type').'"';
+		}
+		elseif(array_key_exists($item->get_value('media_format'), $valid_formats))
 		{
 			$return[] = 'type="'.$valid_formats[$item->get_value('media_format')].'"';
 		}
