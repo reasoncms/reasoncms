255fc214f1b97d6b0abaa96635f7611ee2591e3b
Matt Ryan <mryan@carleton.edu>
2012-09-06 15:25:00 +0000

    Switched to html5 doctype

--- reason_4.0/lib/core/errors/404.php
+++ reason_4.0/lib/core/errors/404.php
@@ -13,7 +13,7 @@
   */
 	include_once( 'reason_header.php' );
    	reason_include( 'scripts/urls/404action.php' );
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+?><!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
