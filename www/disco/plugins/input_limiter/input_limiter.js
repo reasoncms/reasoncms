1386101a876a8964bcc64f0cc62b569bf8b4d017
Matt Ryan <mryan@carleton.edu>
2012-09-07 20:44:25 +0000

    Added support for Loki and TinyMCE

--- www/disco/plugins/input_limiter/input_limiter.js
+++ www/disco/plugins/input_limiter/input_limiter.js
@@ -2,11 +2,13 @@
  * input_limiter.js 
  * Manages the limiting of characters a user can input for a given element in a Disco form
  * 
- * @author Nick Jones
+ * @author Nick Jones, Noah Carnahan
  * @requires jQuery
  */
 $(document).ready(function()
 {
+
+
 
     /* 
         - Triggers an AJAX call to count the characters (for consistency on the server-side) 
@@ -21,11 +23,11 @@ $(document).ready(function()
         counter_src = script_src.replace('input_limiter.js', 'get_char_count.php');
     });
     
-    function count_and_update(text_element)
+    function count_and_update(text_element, cur_text)
     {
         var limit_note = text_element.siblings('div.inputLimitNote');
         var over_limit_note = text_element.siblings('div.overLimitNote');
-        var current_text = text_element.val();
+        var current_text = cur_text;
         var char_limit = parseInt(limit_note.children('span.charLimit').html());
         var chars_remaining_element = limit_note.children('span.charsRemaining');
         
@@ -36,7 +38,7 @@ $(document).ready(function()
             $.get(counter_src, 
                 {text: current_text},
                 function(returned_count)
-                {
+                {                
                     chars_remaining = char_limit - returned_count;
                     chars_remaining_element.html(chars_remaining);
                     var over_limit = -chars_remaining;
@@ -69,19 +71,92 @@ $(document).ready(function()
             over_limit_note.hide();
         }
     }
-    $('div.inputLimitNote').siblings('textarea, :text').bind('keyup', function()
-    {
-        // if an AJAX call was in queue to be made, replace it with a new one
-        if(typeof(t_out) != 'undefined')
-        {
-            clearTimeout(t_out);
-        }
-        var copy_of_this = $(this);
-        t_out = setTimeout(function(){ count_and_update(copy_of_this) }, 700);
-    });
-    
-    // trigger character count upon the first rendering of page
+        
+    //bind chatacter count upon keyup events in text areas
+	$('div.inputLimitNote').siblings('textarea, :text').bind('keyup', function()
+	{
+		// if an AJAX call was in queue to be made, replace it with a new one
+		if(typeof(t_out) != 'undefined')
+		{
+			clearTimeout(t_out);
+		}
+		var copy_of_this = $(this);
+		t_out = setTimeout(function(){ count_and_update(copy_of_this, copy_of_this.val()) }, 700);
+	});
+	// trigger character count upon the first rendering of page
     $('div.inputLimitNote').siblings('textarea, :text').each(function(){
-        count_and_update($(this));
+        count_and_update($(this), $(this).val());
     });
+		
+	function bind_loki_handler()
+	{
+		//alert('binding loki handlers');
+		$('div.inputLimitNote').siblings('.loki').find('iframe').each(function(){
+			//alert('in the each');
+			var iframeNode = this;
+			function handler(){
+				var text_element = $(iframeNode).parents('.loki');
+				var cur_text = $(this).html();
+				if(typeof(t_out) != 'undefined')
+				{
+					clearTimeout(t_out);
+				}
+				t_out = setTimeout(function(){ count_and_update(text_element, cur_text) }, 700);
+			}
+			$(this).contents().find("body").unbind('keyup', handler);
+			$(this).contents().find("body").keyup(handler);
+		});
+	}
+	
+	//bind character count upon keyup events in loki frames
+	$(window).load(function(){
+	
+		// bind it to the current iframe
+		bind_loki_handler();
+		//setTimeout(bind_loki_handler, 300);
+		
+		//bind it to any future iframes
+		$('iframe').load(function(){
+			//alert('from the load');
+			bind_loki_handler();
+		});
+		
+		//bind it to current and future frames in ie. (the above methods dont work for ie8)
+		$(document).on('focus click', 'iframe', function(){
+			//alert('yeah!?');
+			bind_loki_handler();
+		});
+		
+		//bind it to any future textareas in loki divs (for html source view)
+		$(document).on('keyup', '.loki textarea', function(){
+			var text_element = $(this).parents('.loki');
+			var cur_text = $(this).val();
+			if(typeof(t_out) != 'undefined')
+			{
+				clearTimeout(t_out);
+			}
+			t_out = setTimeout(function(){ count_and_update(text_element, cur_text) }, 700);
+		});
+	});
+
+	//bind count_and_update to the keyup event of appropriate tinyMCE editors
+	if (typeof tinymce !== 'undefined') {
+		tinymce.onAddEditor.add(function(mgr,ed) {
+			ed.onInit.add(function (ed) {
+				if ($(ed.getContainer()).siblings('div.inputLimitNote').size() > 0) {
+					ed.onKeyUp.add(function(ed, e) {
+						var elementToUpdate = $(ed.getContainer());
+						var content = ed.getContent();
+						if(typeof(t_out) != 'undefined')
+						{
+							clearTimeout(t_out);
+						}
+						t_out = setTimeout(function(){ count_and_update(elementToUpdate, ed.getContent()) }, 700);
+					});
+				}
+			});
+		});
+	}
 });
+
+
