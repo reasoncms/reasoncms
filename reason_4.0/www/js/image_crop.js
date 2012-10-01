9998617e459a7de9849f7bc0f0534bd7062d12c0
Mark Heiman <mheiman@carleton.edu>
2012-10-01 17:24:20 +0000

    fixed updateCoords() to target the correct set of hidden elements

--- reason_4.0/www/js/image_crop.js
+++ reason_4.0/www/js/image_crop.js
@@ -17,24 +17,25 @@ $(document).ready(function()
 
 		var true_w;
 		var true_h;
+		var container = $(this).parent();
 		
 		setTimeout(function() {
 			// Figure out the size of the image. If the original proportions are set on the
 			// form, use those, otherwise, use the size of the displayed image.
-			if ($('input[name="_reason_upload_orig_h"]').val()) {
-				true_h = $('input[name="_reason_upload_orig_h"]').val();
+			if ($('input[name="_reason_upload_orig_h"]').val(), container) {
+				true_h = $('input[name="_reason_upload_orig_h"]', container).val();
 			} else {
-				true_h = $('img.representation').height();
+				true_h = $('img.representation', container).height();
 			}
-			if ($('input[name="_reason_upload_orig_w"]').val()) {
-				true_w = $('input[name="_reason_upload_orig_w"]').val();
+			if ($('input[name="_reason_upload_orig_w"]', container).val()) {
+				true_w = $('input[name="_reason_upload_orig_w"]', container).val();
 			} else {
-				true_w = $('img.representation').width();
+				true_w = $('img.representation', container).width();
 			}
 
-			var ratio = parseFloat($('input[name="_reason_upload_crop_ratio"]').val());
+			var ratio = parseFloat($('input[name="_reason_upload_crop_ratio"]', container).val());
 			
-			$('img.representation').Jcrop({
+			$('img.representation', container).Jcrop({
 				aspectRatio: ratio,
 				onSelect: updateCoords,
 				bgOpacity: .4,
@@ -44,7 +45,7 @@ $(document).ready(function()
 			});
 
 			// Preselect a cropped region if called for.
-			if ($('input[name="_reason_upload_crop_preselect"]').val()) {
+			if ($('input[name="_reason_upload_crop_preselect"]', container).val()) {
 				
 				// If we have a non-zero ratio, fit the crop shape to the image shape.
 				if (ratio) {
@@ -60,13 +61,19 @@ $(document).ready(function()
 			}
 		}, 1200);
 	});
-	
+
+	/* Called when the crop region is changed. Updates the hidden crop parameter elements
+	   so that we can pass the cropping details back to the server. */
 	function updateCoords(c)
 	{
-		$('input[name="_reason_upload_crop_x"]').val(c.x);
-		$('input[name="_reason_upload_crop_y"]').val(c.y);
-		$('input[name="_reason_upload_crop_w"]').val(c.w);
-		$('input[name="_reason_upload_crop_h"]').val(c.h);
+		// Find the containing element so we can update the correct set of
+		// crop elements
+		var container = this.ui.holder.parent().parent();
+		
+		$('input[name="_reason_upload_crop_x"]', container).val(c.x);
+		$('input[name="_reason_upload_crop_y"]', container).val(c.y);
+		$('input[name="_reason_upload_crop_w"]', container).val(c.w);
+		$('input[name="_reason_upload_crop_h"]', container).val(c.h);
 	};
 
 	function checkCoords()
