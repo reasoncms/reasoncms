5a97d406c6cc819e368b5571bb66318ea1905800
Steve Smith <slylth@gmail.com>
2012-11-20 08:53:12 -0600

    hack to not load jquery.init.js if the page is a slate_form

--- reason_4.0/lib/local/minisite_templates/modules/luther_footer.php
+++ reason_4.0/lib/local/minisite_templates/modules/luther_footer.php
@@ -51,8 +51,9 @@
 				//echo '<script src="/javascripts/jquery.metadata.js" type="text/javascript"></script>'."\n";
 				//echo '<script src="/javascripts/tablesorter.min.js" type="text/javascript"></script>'."\n";
 				//echo '<script src="/javascripts/jquery.hoverIntent.min.js" type="text/javascript"></script>'."\n";
-				//echo '<script src="/javascripts/cluetip/jquery.cluetip.js" type="text/javascript"></script>'."\n";
-				echo '<script src="/javascripts/jquery.init.js" type="text/javascript"></script>'."\n";
+				//echo '<script src="/javascripts/cluetip/jquery.cluetip.js" type="text/javascript"></script>'."\n"; 
+                if ($this->cur_page->get_value( 'custom_page' ) != 'slate_form')
+				    echo '<script src="/javascripts/jquery.init.js" type="text/javascript"></script>'."\n";
 				//echo '<script src="/reason/jquery.watermark-3.1.3/jquery.watermark.min.js" type="text/javascript"></script>'."\n";
 				//echo '<script type="text/javascript" src="/reason/js/jquery.tools.min.js"></script> '."\n";
 				//echo '<script type="text/javascript" src="/reason/js/jquery.maskedinput-1.3.min.js"></script>'."\n";
