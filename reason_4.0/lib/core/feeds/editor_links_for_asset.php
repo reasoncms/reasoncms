4c078b29b979403b071bc6c60caa89a92370426a
Nathan White <natepixel@gmail.com>
2013-01-28 23:30:43 +0000

    changed to get owner_id in initial query for a speed bump by avoiding call to get_owner

--- reason_4.0/lib/core/feeds/editor_links_for_asset.php
+++ reason_4.0/lib/core/feeds/editor_links_for_asset.php
@@ -11,6 +11,9 @@ include_once( 'reason_header.php' );
 reason_include_once('feeds/default.php');
 $GLOBALS[ '_feed_class_names' ][ basename( __FILE__, '.php' ) ] = 'editorAssetsFeed';
 
+/**
+ * @todo lets try grabbing owner in the initial entity selector instead of calling it a bunch of times ... get_owner is slow.
+ */
 class editorAssetsFeed extends defaultFeed
 {
 	var $feed_class = 'editorAssetRSS';
@@ -27,6 +30,7 @@ class editorAssetsFeed extends defaultFeed
 		$this->feed->set_item_field_handler('link', 'make_link', true);
 		$this->feed->es->set_num( 50000 );
 		$this->feed->es->set_order( 'entity.name ASC' );
+		$this->feed->es->add_right_relationship_field('site_owns_asset', 'entity', 'id', 'owner_id');
 	}
 	function do_default_field_mapping()
 	{
@@ -38,9 +42,9 @@ class editorAssetRSS extends ReasonRSS
 {
 	function make_link($id)
 	{
-		$asset = new entity($id);
-		$owner_site = $asset->get_owner();
-		
+		$asset = $this->items[$id];
+		$owner_id = $asset->get_value('owner_id');
+		$owner_site = new entity($owner_id);
 		return 'http://' . REASON_HOST . $owner_site->get_value('base_url') .MINISITE_ASSETS_DIRECTORY_NAME.'/'.$asset->get_value( 'file_name' );
 	}
 }
