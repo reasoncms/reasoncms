77b4be389ad7735ac63f57bfdacaa33e24d47bd7
Matt Ryan <mryan@carleton.edu>
2013-02-28 18:11:33 +0000

    Updated to use an HTML5 doctype

--- reason_4.0/lib/core/classes/assets/asset_access.php
+++ reason_4.0/lib/core/classes/assets/asset_access.php
@@ -107,7 +107,7 @@ class ReasonAssetAccess
 			}
 			else
 			{
-				echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'."\n";
+				echo '<!DOCTYPE html>'."\n";
 				echo '<html xmlns="http://www.w3.org/1999/xhtml">'."\n";
 				echo '<head><title>File Not Found (HTTP 404)</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head>'."\n";
 				echo '<body>'."\n";
@@ -127,7 +127,7 @@ class ReasonAssetAccess
 			}
 			else
 			{
-				echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">'."\n";
+				echo '<!DOCTYPE html>'."\n";
 				echo '<html xmlns="http://www.w3.org/1999/xhtml">'."\n";
 				echo '<head><title>Access Forbidden (HTTP 403)</title><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /></head>'."\n";
 				echo '<body>'."\n";
