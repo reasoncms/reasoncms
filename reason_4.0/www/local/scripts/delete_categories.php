9d08f08e10deb18dbc5f5e703dccc1c1995379af
Steve Smith <slylth@gmail.com>
2013-03-13 15:06:20 -0500

    adding script to delete categories from the old home site

---
+++ reason_4.0/www/local/scripts/delete_categories.php
@@ -0,0 +1,10 @@
+<?php
+    /**
+     * Stub for the Delete Categories fixer script
+     * No additional code should be placed in this file
+     * @package reason
+     * @subpackage scripts
+     */
+    include_once( 'reason_header.php' );
+    reason_include_once( 'scripts/delete_categories.php' );
+?>
