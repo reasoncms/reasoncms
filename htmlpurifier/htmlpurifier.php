31770f828f06764f356fa1b1eeedb768c8fa3175
Nathan White <natepixel@gmail.com>
2013-04-04 15:14:19 +0000

    updated to 4.5.0

--- htmlpurifier/htmlpurifier.php
+++ htmlpurifier/htmlpurifier.php
@@ -6,5 +6,5 @@ include_once(CARL_UTIL_INC . 'basic/misc.php');
  * Loads the HTML_Purifier class appropriate for the version of PHP
  */
 if (!defined("HTMLPURIFIER_CACHE")) define("HTMLPURIFIER_CACHE", '/tmp/');
-require_once( HTML_PURIFIER_INC . 'htmlpurifier-4.4.0-standalone/HTMLPurifier.standalone.php' );
+require_once( HTML_PURIFIER_INC . 'htmlpurifier-4.5.0-standalone/HTMLPurifier.standalone.php' );
 ?>
