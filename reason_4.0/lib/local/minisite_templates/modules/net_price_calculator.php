960eacf8122094665ef541db41b512b91edb1f30
Steve Smith <slylth@gmail.com>
2013-04-11 08:22:40 -0500

    taking new npc live

--- reason_4.0/lib/local/minisite_templates/modules/net_price_calculator.php
+++ reason_4.0/lib/local/minisite_templates/modules/net_price_calculator.php
@@ -7,21 +7,26 @@ $GLOBALS['_module_class_names'][basename(__FILE__, '.php')] = 'netPriceCalculato
 class netPriceCalculatorModule extends DefaultMinisiteModule {
 
     function run() {
-        echo '<script type="text/javascript">
-				var
-				NPC_CLIENT_DOMAIN		= "luther",
-				NPC_CLIENT_HEIGHT		= 1400,
-				NPC_CLIENT_WIDTH		= 700,
-				NPC_CONTAINER_PROTOCOL	= "https:",
-				
-				/*Do NOT edit the following code */
-				NPC_EMBEDDED_PROTOCOL	= "https:",
-				NPC_IGNITION_LOCATION	= "birch";
-				(function() {var
-				d=document,s=d.createElement("script");s.type="text/javascript";
-				s.src=NPC_EMBEDDED_PROTOCOL+"//"+NPC_IGNITION_LOCATION+".aidcalculator.com/ignition/key.js";
-				d.getElementsByTagName("head")[0].appendChild(s);})();</script><noscript>Please enable Javascript to use this tool.</noscript>
-				<div id="npc_container"></div>';
+        echo '<div id="npc_container"></div>
+            <script type="text/javascript">
+
+            var
+            /* Update these three variables immediately */
+            NPC_CLIENT_DOMAIN = "luther",
+            NPC_CLIENT_HEIGHT = 1400,
+            NPC_CLIENT_WIDTH  = 700, /* minimum: 740 */
+
+
+
+            /* Do NOT edit the following code */
+            NPC_CONTAINER_PROTOCOL  = "https:",
+            NPC_EMBEDDED_PROTOCOL   = "https:",
+            NPC_IGNITION_VERSION    = "1";
+
+            (function(){var d=document,s=d.createElement("script");s.type="text/javascript";s.src=NPC_EMBEDDED_PROTOCOL+"//"+NPC_CLIENT_DOMAIN+".aidcalculator.com/scripts/embed/ignition_v"+NPC_IGNITION_VERSION+".js";d.getElementsByTagName("head")[0].appendChild(s);})();
+
+            </script>
+            <noscript><p>Please enable Javascript to use this tool.</p></noscript>';
     }
 
 }
