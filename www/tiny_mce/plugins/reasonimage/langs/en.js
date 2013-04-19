99f76e93d9284a2df85373efdf5e96b70fe8c417
Andrew Bacon <abacon@gmail.com>
2013-04-18 19:20:45 -0500

    Upgraded tinymce to 4.0b1, set it to use minified version by default.

--- www/tiny_mce/plugins/reasonimage/langs/en.js
+++
@@ -1,3 +0,0 @@
-tinyMCE.addI18n('en.reasonimage',{
-	desc : 'This is just a template button'
-});
--- www/tiny_mce/plugins/reasonimage/langs/en_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('en.reasonimage_dlg',{
-	title : 'This is just a example title',
-	existing_tab : 'Existing Image',
-	new_tab : 'New Image'
-});
