3e81cbefd1fd7dfd89758cb1f790c87c85ec3a16
Brian Jones <jonebr01@gmail.com>
2013-05-30 15:46:49 -0500

    music page type using feature

--- reason_4.0/lib/local/minisite_templates/page_types_local.php
+++ reason_4.0/lib/local/minisite_templates/page_types_local.php
@@ -608,7 +608,13 @@ $GLOBALS['_reason_page_types_local'] = array(
 		'post_sidebar' => 'luther_flickr_slideshow',
 	),
 	'luther2010_music' => array(
-		'main_head' => 'luther_carousel',
+		'main_head' => array(
+			'module' => 'feature/feature',
+			'shuffle' => false,
+			'autoplay_timer' => 12,
+			'width'=>716,
+			'height'=>288
+		),
 		'main_head_5' => '',
 		'main_2' => '',
 		'main_3' => '',
