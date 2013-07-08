2f8f912365c9505b0350dfea4b6b40b4d113b665
Nathan White <nwhite@carleton.edu>
2013-07-08 10:57:47 -0500

    added ability to specify a default model, view, or controller in the MVC module

--- reason_4.0/lib/core/minisite_templates/modules/social_account/social_account.php
+++ reason_4.0/lib/core/minisite_templates/modules/social_account/social_account.php
@@ -12,20 +12,23 @@ reason_include_once( 'minisite_templates/modules/mvc.php' );
 $GLOBALS[ '_module_class_names' ][ module_basename( __FILE__, '.php' ) ] = 'ReasonSocialAccountModule';
 
 /**
- * An MVC based module for presenting social accounts.
+ * An MVC based module for presenting social accounts. Currently it does just two things.
  *
- * You may pass other models and views via the page type.
+ * - Sets a default model and view to display profile links.
+ * - Passes the site_id as a configuration parameter to the model.
+ *
+ * @todo theme customizer integration to pick social icons via theme options.
  *
  * @author Nathan White
  */
 class ReasonSocialAccountModule extends ReasonMVCModule
 {
+	var $model = 'minisite_templates/modules/social_account/models/profile_links.php';
+	var $view = 'minisite_templates/modules/social_account/views/profile_links.php';
+	
 	// set everything up and get the model data.
 	function init( $args = array() )
 	{
-		// if we use the module without configuration we'll use the profile_link model and view
-		if (empty($this->params['model'])) $this->params['model'] = 'minisite_templates/modules/social_account/models/profile_links.php';
-		if (empty($this->params['view'])) $this->params['view'] = 'minisite_templates/modules/social_account/views/profile_links.php';
 		$controller = $this->get_controller();
 		$model = $controller->model();
 		$model->config('site_id', $this->site_id);
