4d1894aa76260f4d3a37d0e3915aff0eac1b39b0
Matt Ryan <mryan@carleton.edu>
2013-08-23 16:12:01 +0000

    Added support for transparent processing of less-based stylesheets

--- reason_4.0/lib/core/classes/head_items.php
+++ reason_4.0/lib/core/classes/head_items.php
@@ -43,9 +43,49 @@ class HeadItems
 	 */
 	var $_to_remove = array();
 	
+	/**
+	 * What elements does the class recognize?
+	 * @var array
+	 */
 	var $allowable_elements = array('base','link','meta','script','style','title');
+	
+	/**
+	 * What elements are self-closing?
+	 * @var array
+	 */
 	var $elements_that_may_have_content = array('script','style','title');
+	
+	/**
+	 * What elements are never self-closing?
+	 * @var array
+	 */
 	var	$elements_that_may_not_self_close = array('script','title');
+	
+	/**
+	 * Less variables that should be added to every less stylesheet
+	 *
+	 * Note that values must be in the proper format for lessphp; see here:
+	 * http://leafo.net/lessphp/docs/#setting_variables_from_php
+	 *
+	 * @var array key->value pairs
+	 */
+	protected $default_less_variables = array();
+	
+	/**
+	 * Less functions that should be added to every less stylesheet
+	 * @var array PHP callable functions
+	 */
+	protected $default_less_functions = array();
+	
+	/**
+	 * Should old css files created by less be deleted?
+	 *
+	 * Change this to false if you are not able to redirect users to the most recent one where needed,
+	 * since page caching can cause Reason to serve up out-of-date URLs.
+	 *
+	 * @var boolean
+	 */
+	protected $delete_old_less_css = true;
 	
 	function HeadItems()
 	{
@@ -88,15 +128,156 @@ class HeadItems
 	 * @param string $url
 	 * @param string $media optional media attribute
 	 * @param boolean $add_to_top
+	 * @param array $wrapper 'before' and 'after' keys with values to prepend and append
 	 */
 	function add_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>'') )
 	{
+		if(substr($url, 0, 1) == '/' && substr($url, -5) == '.less')
+		{
+			return $this->add_less_stylesheet( $url, $media, $add_to_top, $wrapper, $this->default_less_variables, $this->default_less_functions );
+		}
 		$attrs = array('rel'=>'stylesheet','type'=>'text/css','href'=>$url);
 		if(!empty($media))
 		{
 			$attrs['media'] = $media;
 		}
 		$this->add_head_item('link', $attrs, '', $add_to_top, $wrapper);
+	}
+	
+	function add_default_less_variable($key,$value)
+	{
+		$this->default_less_variables[$key] = $value;
+	}
+	function remove_default_less_variable($key)
+	{
+		if(isset($this->default_less_variables[$key]))
+			unset($this->default_less_variables[$key]);
+	}
+	function set_default_less_variables($array)
+	{
+		$this->default_less_variables = $array;
+	}
+	function add_default_less_function($less_function_name, $php_function)
+	{
+		$this->default_less_functions[$less_function_name] = $php_function;
+	}
+	function remove_default_less_function($less_function_name)
+	{
+		if(isset($this->default_less_functions[$less_function_name]))
+			unset($this->default_less_functions[$less_function_name]);
+	}
+	function set_default_less_functionss($array)
+	{
+		$this->default_less_functions = $array;
+	}
+	
+	/**
+	 * Add a less-based stylesheet
+	 * @param string $url
+	 * @param string $media optional media attribute
+	 * @param boolean $add_to_top
+	 * @param array $wrapper
+	 * @param array $less_variables
+	 * @param array $less_functions
+	 * @return boolean success
+	 */
+	function add_less_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>''), $less_variables = array(), $less_functions = array() )
+	{
+		if(substr($url, 0, 1) != '/')
+		{
+			trigger_error('Less stylesheets must be specified relative to server root (i.e. starting with "/"). Path given ('.$url.') does not conform to this specification. Stylesheet not added.');
+			return false;
+		}
+		
+		if(!include_once(LESSPHP_INC.'lessc.inc.php'))
+		{
+			trigger_error('Unable to process .less file -- LESSPHP not configured correctly on your server. Check the LESSPHP_INC setting in settings/package_settings.php.');
+			return false;
+		}
+		
+		$input_path = WEB_PATH.substr($url, 1);
+		if(!file_exists($input_path))
+		{
+			trigger_error('Less stylesheet not found at "'.$input_path.'". Stylesheet not added.');
+			return false;
+		}
+		$hash = md5($input_path.serialize($less_variables).serialize(array_keys($less_functions)));
+		$first2 = substr($hash, 0, 2);
+		$output_filename = $hash.'_'.filemtime($input_path).'.css';
+		
+		$output_url = WEB_TEMP.'less_compiled/'.$first2.'/'.$output_filename;
+		
+		$base_output_directory = WEB_PATH.substr(WEB_TEMP, 1).'less_compiled/';
+		if(!file_exists($base_output_directory))
+			mkdir($base_output_directory);
+		$output_directory = $base_output_directory.$first2.'/';
+		if(!file_exists($output_directory))
+			mkdir($output_directory);
+		$output_path = $output_directory.$output_filename;
+		$cache_path = $output_directory.$hash.'.cache';
+		
+		$less = new lessc;
+		$less->setFormatter('compressed');
+		$less->setVariables($less_variables);
+		foreach($less_functions as $less_function_name => $php_function)
+		{
+			$less->registerFunction($less_function_name, $php_function);
+		}
+		try
+		{
+			if($this->less_cached_compile($input_path, $output_path, $less, $cache_path))
+			{
+				$compiled = true; // something has changed
+				if ($this->delete_old_less_css && ( $handle = opendir($output_directory) ) )
+				{
+					while (false !== ($entry = readdir($handle)))
+					{
+        				if($entry != $output_filename && strpos($entry, $hash.'_') === 0 && substr($entry, -4) == '.css')
+        				{
+        					unlink($output_directory.$entry);
+        				}
+    				}
+				}
+				else
+				{
+					trigger_error('Unable to delete old less css files');
+				}
+			}
+			else
+			{
+				$compiled = false; // nothing has changed
+			}
+		}
+		catch (Exception $ex)
+		{
+			trigger_error( 'lessphp unable to compile less at "'.$input_path.'". Stylesheet not added. Message: '.$ex->getMessage() );
+			return;
+		}
+		
+		return $this->add_stylesheet( $output_url, $media, $add_to_top, $wrapper );
+		
+		return true;
+	}
+	/**
+	 * Compile less using a cache for included files
+	 * @return boolean true if newly compiled, false if cached version unchanged
+	 */
+	function less_cached_compile($input_path, $output_path, $less, $cache_path)
+	{
+		if (file_exists($cache_path))
+			$cache = unserialize(file_get_contents($cache_path));
+		else
+			$cache = $input_path;
+
+		$new_cache = $less->cachedCompile($cache);
+
+		if (!is_array($cache) || $new_cache["updated"] > $cache["updated"])
+		{
+			file_put_contents($cache_path, serialize($new_cache));
+			file_put_contents($output_path, $new_cache['compiled']);
+			return true;
+		}
+		return false;
 	}
 
 	/**
