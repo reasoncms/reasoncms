45bb8b438e29c9247f6867edf17e566318792e33
Nathan White <nwhite@carleton.edu>
2013-08-26 13:26:42 -0500

    moved update_google_map to 4.4_to_4.5 upgrader folder - needs to be rewritten to be a valid upgrader

--- reason_4.0/www/scripts/upgrade/update_google_map.php
+++
@@ -1,9 +0,0 @@
-<?php
-        /**
-         * Stub for the upgrade google map
-         * @package reason
-         * @subpackage scripts
-         */
-        include_once( 'reason_header.php' );
-        reason_include_once( 'scripts/upgrade/update_google_map.php' );
-?>
