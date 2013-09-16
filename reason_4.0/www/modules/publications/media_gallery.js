5df7dcc5f4b100a834a542c79b53098ef44928c6
Matt Ryan <mryan@carleton.edu>
2013-09-16 23:53:28 +0000

    Script for media gallery interactivity in publication/blog posts

---
+++ reason_4.0/www/modules/publications/media_gallery.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+	var galleryItems = $('div.publication div.fullPost .media .mediaGallery>li');
+	var widest = 100;
+	galleryItems.find('img.placard').show();
+	galleryItems.find('img.placard').each(function(){
+		width = $(this).width();
+		if(width > widest)
+			widest = width;
+	});
+	var playingMediaBlock = $('<div class="playingMedia"></div>');
+	$('div.publication div.fullPost .media .mediaGallery').addClass('galleryActive').before('<a id="playingMediaBlockAnchor"></a>').before(playingMediaBlock);
+	var focus_media = function(listItem){
+		galleryItems.removeClass('showing');
+		listItem.addClass('showing');
+		playingMediaBlock.empty();
+		listItem.find('.mediaDisplay').contents().clone().prependTo(playingMediaBlock);
+	}
+	galleryItems.find('.mediaDisplay').hide();
+	var link = $('<a href="#" title="View media" class="pickerLink"></a>');
+	link.css('width', widest + 'px');
+	link.click(function(e){
+		e.preventDefault();
+		listItem = $(this).parents('div.publication div.fullPost .media .mediaGallery>li');
+		if(listItem.hasClass('showing'))
+			return;
+		focus_media(listItem);
+		$('#playingMediaBlockAnchor').focus();
+	});
+	galleryItems.find('.titleBlock').wrapInner(link);
+	var tallest = 125;
+	$(window).load(function(){
+		galleryItems.find('a.pickerLink').each(function(){
+			height = $(this).height();
+			if(height > tallest)
+				tallest = height;
+		});
+		galleryItems.find('a.pickerLink').css('height', tallest + 'px');
+	});
+	focus_media( galleryItems.first() );
+});
