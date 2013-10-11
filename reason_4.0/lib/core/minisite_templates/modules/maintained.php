b4ec9ce8334369f341e4344875e63f5a9e8d3e05
Matt Ryan <mryan@carleton.edu>
2013-10-11 16:45:06 +0000

    Added support for social media links using [[socialmedialinks]]

--- reason_4.0/lib/core/minisite_templates/modules/maintained.php
+++ reason_4.0/lib/core/minisite_templates/modules/maintained.php
@@ -24,6 +24,8 @@
 	 *
 	 * @todo Rework to remove reference to the template
 	 * @todo Figure out how to reliably report on last modified times if this module is not run last
+	 * @todo Use standard Reason caching instead of writing to the site entity
+	 * @todo Allow setting of social media view
 	 */
 	class MaintainedModule extends DefaultMinisiteModule
 	{
@@ -35,7 +37,7 @@
 			$format = $this->_get_format();
 			if(!empty($format))
 			{
-				$replacements = array('[[sitename]]'=>'_get_site_name_html','[[maintainer]]'=>'_get_maintainer_html','[[lastmodified]]'=>'_get_last_modified_date_html');
+				$replacements = array('[[sitename]]'=>'_get_site_name_html','[[maintainer]]'=>'_get_maintainer_html','[[lastmodified]]'=>'_get_last_modified_date_html','[[socialmedialinks]]'=>'_get_social_media_links_html',);
 				foreach($replacements as $search=>$function)
 				{
 					if(strpos($format,$search) === false)
@@ -102,61 +104,14 @@
 			$maintainer = $this->parent->site_info->get_value('primary_maintainer');
 			if( !empty($maintainer) )
 			{
-				// Check to see if it's before or after 7 am, and set the last colleague->ldap sync time appropriately.
 				
-				if(carl_date('G') < 7) // it's before 7am
+				$maintainer_info = $this->_get_maintainer_info($maintainer);
+				if (!empty($maintainer_info['full_name']))
 				{
-					$ldap_last_sync_time = strtotime('7 am yesterday');
-				}
-				else // it's after 7 am
-				{
-					$ldap_last_sync_time = strtotime('7 am today');
-				}
-				
-				/*	Either of the following conditions will fire the ldap->reason sync:
-					1: the cached info predates the last colleague->ldap sync (presumed to be daily by 7 am.)
-					2: the primary maintainer has been changed since the last ldap->reason sync. */
-					
-				if($this->parent->site_info->get_value('cache_last_updated') <= date('Y-m-d', $ldap_last_sync_time) 
-					|| $this->parent->site_info->get_value('username_cache') != $this->parent->site_info->get_value('primary_maintainer') )
-				{					
-					$dir = new directory_service();
-					if ($dir->search_by_attribute('ds_username', $maintainer, array('ds_email','ds_fullname')))
-					{
-						$email = $dir->get_first_value('ds_email');
-						$full_name = $dir->get_first_value('ds_fullname');
-						// lets fall back to the maintainer username if a valid full name is not found for the user
-						$full_name = (!carl_empty_html($full_name)) ? $full_name : trim(strip_tags($maintainer));
-						$values = array('email_cache'=>$email, 'name_cache'=>$full_name, 'cache_last_updated'=>date('Y-m-d H:i:s'), 'username_cache'=>$maintainer);
-						$update_vals = array('ldap_cache'=>$values);
-						
-						reason_include_once( 'function_libraries/admin_actions.php' );
-						
-						/* I know this is nonstandard, but it's the only way right now 
-						to update the entity without creating an archive and changing 
-						the last_updated field on all the sites every day... */
-						
-						$sqler = new SQLER;
-						foreach( $update_vals AS $table => $fields )
-						{
-							$sqler->update_one( $table, $fields, $this->parent->site_info->id() );
-						}
-					
-					}
-				}
-				//If info cached on site is new, don't do ldap stuff-just grab off of site info
-				else
-				{
-					$email = $this->parent->site_info->get_value('email_cache');
-					$full_name = $this->parent->site_info->get_value('name_cache');
-				}
-				
-				if (!empty($full_name))
-				{
-					if(!empty($email))
-						$html = '<a href="mailto:'.htmlspecialchars($email,ENT_QUOTES,'UTF-8').'">'.htmlspecialchars($full_name,ENT_QUOTES,'UTF-8').'</a>';
+					if(!empty($maintainer_info['email']))
+						$html = '<a href="mailto:'.htmlspecialchars($maintainer_info['email'],ENT_QUOTES,'UTF-8').'">'.htmlspecialchars($maintainer_info['full_name'],ENT_QUOTES,'UTF-8').'</a>';
 					else
-						$html = $full_name;
+						$html = $maintainer_info['full_name'];
 				}
 				else
 				{
@@ -165,6 +120,58 @@
 				}
 			}
 			return $html;
+		}
+		function _get_maintainer_info($maintainer)
+		{
+			// Check to see if it's before or after 7 am, and set the last colleague->ldap sync time appropriately.
+				
+			if(carl_date('G') < 7) // it's before 7am
+			{
+				$ldap_last_sync_time = strtotime('7 am yesterday');
+			}
+			else // it's after 7 am
+			{
+				$ldap_last_sync_time = strtotime('7 am today');
+			}
+				
+			/*	Either of the following conditions will fire the ldap->reason sync:
+				1: the cached info predates the last colleague->ldap sync (presumed to be daily by 7 am.)
+				2: the primary maintainer has been changed since the last ldap->reason sync. */
+					
+			if($this->parent->site_info->get_value('cache_last_updated') <= date('Y-m-d', $ldap_last_sync_time) 
+				|| $this->parent->site_info->get_value('username_cache') != $this->parent->site_info->get_value('primary_maintainer') )
+			{					
+				$dir = new directory_service();
+				if ($dir->search_by_attribute('ds_username', $maintainer, array('ds_email','ds_fullname')))
+				{
+					$email = $dir->get_first_value('ds_email');
+					$full_name = $dir->get_first_value('ds_fullname');
+					// lets fall back to the maintainer username if a valid full name is not found for the user
+					$full_name = (!carl_empty_html($full_name)) ? $full_name : trim(strip_tags($maintainer));
+					$values = array('email_cache'=>$email, 'name_cache'=>$full_name, 'cache_last_updated'=>date('Y-m-d H:i:s'), 'username_cache'=>$maintainer);
+					$update_vals = array('ldap_cache'=>$values);
+					
+					reason_include_once( 'function_libraries/admin_actions.php' );
+					
+					/* I know this is nonstandard, but it's the only way right now 
+					to update the entity without creating an archive and changing 
+					the last_updated field on all the sites every day... */
+					
+					$sqler = new SQLER;
+					foreach( $update_vals AS $table => $fields )
+					{
+						$sqler->update_one( $table, $fields, $this->parent->site_info->id() );
+					}
+					
+				}
+			}
+			//If info cached on site is new, don't do ldap stuff-just grab off of site info
+			else
+			{
+				$email = $this->parent->site_info->get_value('email_cache');
+				$full_name = $this->parent->site_info->get_value('name_cache');
+			}
+			return array('email'=>$email,'full_name'=>$full_name);
 		}
 		function _get_last_modified_date_html()
 		{
@@ -182,6 +189,22 @@
 			}
 			
 			return prettify_mysql_timestamp( $date, $this->last_mod_date_format );
+		}
+		function _get_social_media_links_html()
+		{
+			reason_include_once('minisite_templates/modules/social_account/models/profile_links.php');
+			reason_include_once('minisite_templates/modules/social_account/views/profile_links.php');
+			
+			/** instantiate and setup the model **/
+			$model = new ReasonSocialProfileLinksModel;
+			$model->config('site_id', $this->site_id);
+
+			/** instantiate the view **/
+			$view = new ReasonSocialProfileLinksView;
+
+			/** setup and run the controller **/
+			$controller = new ReasonMVCController($model, $view);
+			return $controller->run();
 		}
 		function get_documentation()
 		{
