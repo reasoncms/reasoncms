b985dc543bc13c3f934668b37983ceefc9d2c5d2
Matt Ryan <mryan@carleton.edu>
2013-11-01 20:52:17 +0000

    First commit of the function_bundle class
    git-svn-id: file:///usr/local/webapps/svn/wsg/TRUNK_OPEN@39453 6e4f3189-35f9-0310-b3ef-d16b6aaee2c9

---
+++ reason_4.0/lib/core/classes/function_bundle.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @package reason
+ * @subpackage classes
+ */
+/**
+ * A tool for wrapping up a set of functions into a single package
+ *
+ * Use:
+ *
+ * $bundle = new functionBundle();
+ *
+ * $blundle->set_function('do_something','phpinfo');
+ * $bundle->set_function('method', array($object, 'method_name'));
+ * $bundle->set_function('static_method', array('class_name', 'static_method_name'));
+ * 
+ * $bundle->do_something(); // calls phpinfo();
+ * $bundle->method('argument'); // calls $object->method_name('argument');
+ * $bundle->static_method('argument1', 'argument2'); // calls classname::static_method_name('argument1', 'argument2');
+ */
+class functionBundle
+{
+	/**
+	 * An array of functions that have been set
+	 *
+	 * Keys: function names
+	 *
+	 * Values: php callbacks
+	 *
+	 * @var array
+	 */
+	protected $functions = array();
+	
+	/**
+	 * Add a function to the object
+	 *
+	 * @param string $name The function name
+	 * @param callback $function The function to be called
+	 * @return success
+	 */
+	function set_function($name, $function)
+	{
+		$this->functions[$name] = $function;
+		return true;
+	}
+	
+	/**
+	 * Magic method to call set functions
+	 *
+	 * @param string $name The function name
+	 * @param array $arguments
+	 * @return mixed function return value or NULL if function not found
+	 */
+	function __call( $name, $arguments )
+	{
+		if($function = $this->get_function($name))
+			return call_user_func_array($function, $arguments);
+		return null;
+	}
+	
+	/**
+	 * Get the callback function with a given name
+	 *
+	 * @param string $name The function name
+	 * @return mixed the callback or NULL if function not found
+	 */
+	function get_function($name)
+	{
+		if(isset($this->functions[$name]))
+			return $this->functions[$name];
+		return null;
+	}
+	
+	/**
+	 * Does this bundle have a function with a given name?
+	 *
+	 * @param string $name The function name
+	 * @return boolean
+	 */
+	function has_function($name)
+	{
+		return isset($this->functions[$name]);
+	}
+	
+	/**
+	 * Get the full array of functions set on the class
+	 *
+	 * @return array
+	 */
+	function get_functions()
+	{
+		return $this->functions;
+	}
+}
