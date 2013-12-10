ef5f0684104065447193f8e72100d39c084bc720
Steve Smith <slylth@gmail.com>
2013-12-10 16:27:44 -0600

    more fleshed out canonicalized class

--- reason_4.0/lib/core/classes/canonicalizer.php
+++ reason_4.0/lib/core/classes/canonicalizer.php
@@ -4,6 +4,7 @@ class reasonCanonicalizer
 {
 	protected $modules = array();
 	protected $num_canonical_mods = 0;
+	protected $data = array();
 	public function register($module)
 	{
 		$this->modules[] = $module;
@@ -11,35 +12,64 @@ class reasonCanonicalizer
 	public function get_canonical_url()
 	{
 		$full_request_keys = array_keys($_GET);
-		$data = array();
 		foreach($this->modules as $key=>$module)
 		{
-			$data[$key]['non_canonical'] = $module->get_noncanonical_request_keys();
-			$data[$key]['all'] = array_keys($module->get_cleanup_rules());
-			$data[$key]['canonical'] = array_diff($non_canonical, $all);
-			if (!empty($data[$key]['canonical']))
+			$this->data[$key]['non_canonical'] = $module->get_noncanonical_request_keys();
+			$this->data[$key]['all'] = array_keys($module->get_cleanup_rules());
+			$this->data[$key]['canonical'] = array_diff($non_canonical, $all);
+			if (!empty($this->data[$key]['canonical']))
 			{
-				$num_canonical_mods ++;
+				$num_canonical_mods++;
+				$canonical_module = $module;
 			}
 
 		}
 		// logic goes here
 		if ($num_canonical_mods == 1) //follow left side of pdf
 		{
-
+			return $canonical_module->get_canonical_url();
+			// return '/foo/bar/';
 		} 
 		else //follow right side
 		{
-			$this->get_non_conanical_query_string_params();
+			$non_cans = $this->has_non_canonical_url_params(); // Any non-canonical QSPs?
+			if (!empty($non_cans))
+			{
+				//Add rel=canonical
+				//Strip non-canonical QSPs from URL
+				// Stringify all canonical QSPs
+				$params_string = implode('&', $this->get_canonical_url_params());
+			}
+			return $params_string;
 		}
-
-		// return $modules[$key]->get_canonical_url();
-
-		return '/foo/bar/';
 	}
 
-	private function get_non_conanical_query_string_params()
+	private function has_non_canonical_url_params()
 	{
-		
+		// $params = array();
+		foreach ($this->data as $module => $canon_type) {
+			if ($canon_type == 'non_canonical' && (!empty($canon_type['non_canonical']))){
+				// foreach ($canon_type as $url_param) {
+						return true;
+					// array_push($params, $url_param);
+				// }
+			}
+		}
+		// return $params;
 	}
+
+	private function get_canonical_url_params()
+	{
+		$params = array();
+		foreach ($this->data as $module => $canon_type) {
+			if ($canon_type == 'canonical' && (!empty($canon_type['non_canonical']))){
+				foreach ($canon_type as $url_param) {
+					array_push($params, $url_param);
+				}
+			}
+		}
+		return $params;
+	}
+
+
 }
