4348bac5cd821a6e2fe0bf9ef26aaa953a14347c
Steve Smith <slylth@gmail.com>
2014-01-17 11:00:27 -0600

    Adds authentication to the info.php page

--- reason_4.0/www/local/scripts/info.php
+++ reason_4.0/www/local/scripts/info.php
@@ -1,5 +1,13 @@
 <?php
-echo 'test';
-phpinfo();
-echo $REASON_FLASH_VIDEO_PLAYER_URI;
+include_once('reason_header.php');
+reason_include_once('function_libraries/relationship_finder.php');
+reason_include_once('function_libraries/admin_actions.php');
+reason_include_once('function_libraries/user_functions.php');
+
+if (!reason_user_has_privs( id_of( reason_check_authentication() ), 'db_maintenance' ) )
+{
+    die('<html><head><title>Reason: PHP info</title></head><body><h1>Sorry.</h1><p>You do not have permission to view this page.</p><p>Only Reason users who have database maintenance privileges may do that.</p></body></html>');
+} else {
+    phpinfo();
+}
 ?>
