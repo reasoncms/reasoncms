a04be5cfb8fa212ccfca33e5b39d9e33a957352b
Steve Smith <slylth@gmail.com>
2014-06-25 21:41:28 -0500

    Adds scripts for the responsive upgrade. Everything can be deleted after the upgrade.

---
+++ reason_4.0/www/local/scripts/responsive/sidebar_blurbs_pagetype_to_default.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Stub for the Page Type Info script
+     *
+     * No additional code should be placed in this file
+     *
+     * @package reason
+     * @subpackage scripts
+     */
+    include_once( 'reason_header.php' );
+    reason_include_once( 'scripts/responsive/sidebar_blurbs_pagetype_to_default.php' );
+?>
