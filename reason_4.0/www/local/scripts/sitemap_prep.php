119afe07e8552a6d00e24bb8f3d0f556c6bd220d
Steve Smith <slylth@gmail.com>
2014-07-08 14:59:51 -0500

    Adds script to show pages that are hidden from navigation.

---
+++ reason_4.0/www/local/scripts/sitemap_prep.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Stub for the sitemap prep script
+     *
+     * No additional code should be placed in this file
+     *
+     * @package reason
+     * @subpackage scripts
+     */
+    include_once( 'reason_header.php' );
+    reason_include_once( 'scripts/sitemap_prep.php' );
+?>
