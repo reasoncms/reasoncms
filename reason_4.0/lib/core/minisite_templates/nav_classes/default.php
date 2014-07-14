fe280def4b5f6c324be7338ef1f9c88e41b9bbc0
Steve Smith <slylth@gmail.com>
2014-07-14 12:41:30 -0500

    checks for virtual domains (e.g. some.site.domain.edu) and ignores ip address based domains

--- reason_4.0/lib/core/minisite_templates/nav_classes/default.php
+++ reason_4.0/lib/core/minisite_templates/nav_classes/default.php
@@ -129,9 +129,23 @@
 			if(isset($this->values[$id]) && $url)
 			{
 				$class .= ' jump';
+				// check to see if link point of the domain
+				// add jump-internal or jump-external class
 				if (( filter_var($url, FILTER_VALIDATE_URL, FILTER_FLAG_HOST_REQUIRED) !== FALSE ))
 				{
-					if ( preg_match( '#'.(REASON_HOST).'#', $url ))
+					// check for virtual host redirects (e.g. sports.domain.edu)
+					// also ignore ip addressed hosts
+					if ( substr_count(REASON_HOST, '.') > 1
+						|| !preg_match('\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b', REASON_HOST) )
+					{
+						//lop off virtual host parts
+						$host_array = explode('.', REASON_HOST);
+						$c = count($host_array);
+						$host = $host_array[$c - 2] .'.'. $host_array[$c - 1];
+					} else {
+						$host = REASON_HOST;
+					}
+					if ( preg_match( '/'.$host.'/', $url ) )
 					{
 						$class .= ' jump-internal';
 					} else {
