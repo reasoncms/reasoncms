f0f2114f0736fa964a6fbd6a1a9680bd67e110b9
Steve Smith <slylth@gmail.com>
2014-10-03 14:26:10 -0500

    handles the problem of a NULL in an upload element

--- reason_4.0/lib/local/minisite_templates/modules/form/views/thor/art_scholarship.php
+++ reason_4.0/lib/local/minisite_templates/modules/form/views/thor/art_scholarship.php
@@ -17,6 +17,8 @@ class ArtScholarshipForm extends DefaultThorForm
 {
     var $statement_dest;
     var $portfolio_dest;
+    var $portfolio;
+    var $teacher_statement;
 
     function on_every_time()
     {
@@ -26,14 +28,33 @@ class ArtScholarshipForm extends DefaultThorForm
             'acceptable_types' => array('application/pdf'),
             'allow_upload_on_edit' => true);
 
-        $portfolio = $this->get_element_name_from_label('Portfolio (pdf)');
-        $this->change_element_type($portfolio, 'ReasonUpload', $params);
+        $this->portfolio = $this->get_element_name_from_label('Portfolio (pdf)');
+        $this->change_element_type($this->portfolio, 'ReasonUpload', $params);
 
-        $teacher_statement = $this->get_element_name_from_label('Letter of Recommendation (pdf)');
-        $this->change_element_type($teacher_statement, 'ReasonUpload', $params);
+        $this->teacher_statement = $this->get_element_name_from_label('Letter of Recommendation (pdf)');
+        $this->change_element_type($this->teacher_statement, 'ReasonUpload', $params);
 
         $personal_statement = $this->get_element_name_from_label('Please insert applicant\'s personal statement here: (student only)');
         $this->change_element_type($personal_statement, 'tiny_mce');
+    }
+
+    function run_error_checks()
+    {
+        if (!$this->get_value($this->portfolio))
+        {
+            $this->change_element_type($this->portfolio, 'text');
+            $this->set_value($this->portfolio, 'None provided');
+        } else {
+            $this->set_value($this->portfolio , $this->portfolio_dest);
+        }
+        if (!$this->get_value($this->teacher_statement))
+        {
+            $this->change_element_type($this->teacher_statement, 'text');
+            $this->set_value($this->teacher_statement, 'None provided');
+        } else {
+            $this->set_value($this->teacher_statement, $this->statement_dest);
+        }
+        // parent::run_error_checks();
     }
 
     function process() // {{{
@@ -85,8 +106,10 @@ class ArtScholarshipForm extends DefaultThorForm
                     if (end($documents) === $document)
                     {
                         $this->statement_dest = $dir . $first . $last . 'Statement.pdf';
+                        touch($this->statement_dest);
                     } else {
                         $this->portfolio_dest = $dir . $first . $last . 'Portfolio.pdf';
+                        touch($this->portfolio_dest);
                     }
 
                     //move the file - if windows and the destination exists, unlink it first.
@@ -95,17 +118,19 @@ class ArtScholarshipForm extends DefaultThorForm
                         unlink($asset_dest);
                     }
                     rename ($document->tmp_full_path, $this->statement_dest );
-                    chown($statement_dest, REASON_SITE_DIRECTORY_OWNER);
-                    chgrp($statement_dest, REASON_SITE_DIRECTORY_OWNER);
-                    chmod($statement_dest, 0644);
+                    chown($this->statement_dest, REASON_SITE_DIRECTORY_OWNER);
+                    chgrp($this->statement_dest, REASON_SITE_DIRECTORY_OWNER);
+                    chmod($this->statement_dest, 0644);
                     rename ($document->tmp_full_path, $this->portfolio_dest );
-                    chown($portfolio_dest, REASON_SITE_DIRECTORY_OWNER);
-                    chgrp($portfolio_dest, REASON_SITE_DIRECTORY_OWNER);
-                    chmod($portfolio_dest, 0644);
+                    chown($this->portfolio_dest, REASON_SITE_DIRECTORY_OWNER);
+                    chgrp($this->portfolio_dest, REASON_SITE_DIRECTORY_OWNER);
+                    chmod($this->portfolio_dest, 0644);
                 }
 
                 // make sure to ignore the 'asset' field
-                $this->form->_process_ignore[] = 'upload_file';
+                // $this->form->_process_ignore[] = 'upload_file';
+                $this->_process_ignore[] = $this->portfolio;
+                $this->_process_ignore[] = $this->teacher_statement;
             }
 
             $this->email_form_data();
@@ -165,7 +190,7 @@ class ArtScholarshipForm extends DefaultThorForm
         if(!$mail->Send()) {
             echo "There was a problem sending your email.\n";
             echo "Please contact <a href='mailto:{$recipient}?subject=Art%20Scholarship%20Submission%20Error' target=_blank>{$recipient}</a> to confirm that we've received your materials.\n";
-            die();
+            // die();
         } else {
             echo "Message sent!";
         }
