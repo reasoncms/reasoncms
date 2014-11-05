2bcb61d7a09cc8b38f572a6eab9116ae5b55a5e5
Gage Dykema <gdykema@gmail.com>
2014-11-05 10:55:48 -0600

    added HeyNorse and NorseSports tagboard pagetypes

--- reason_4.0/lib/local/minisite_templates/page_types_local.php
+++ reason_4.0/lib/local/minisite_templates/page_types_local.php
@@ -641,10 +641,11 @@ $GLOBALS['_reason_page_types_local'] = array(
 			'num_to_display' => 0,
 		)
 	),
-	'tagboard_full' =>array(
+	'tagboard_full_HeyNorse' =>array(
 		'pre_main_head' => '',
 		'main' => array(
 			'module' => 'tagboard',
+			'name_id'=>'HeyNorse/184201',
 		),
 		'call_to_action_blurb' => '',
 		'pre_sidebar' => 'b',
@@ -655,6 +656,22 @@ $GLOBALS['_reason_page_types_local'] = array(
 		'navigation' => '',
 		'sub_nav' => '',
 		'contact_blurb' => '',		
+	),
+	'tagboard_full_NorseSports' =>array(
+		'pre_main_head' => '',
+		'main' => array(
+				'module' => 'tagboard',
+				'name_id' => 'NorseSports/185650',
+		),
+		'call_to_action_blurb' => '',
+		'pre_sidebar' => 'b',
+		'pre_sidebar_2' => '',
+		'sidebar' => '',
+		'sidebar_2' =>'',
+		'post_sidebar' => '',
+		'navigation' => '',
+		'sub_nav' => '',
+		'contact_blurb' => '',
 	),
 	'tagboard_nav' =>array(
 		'pre_main_head' => '',
