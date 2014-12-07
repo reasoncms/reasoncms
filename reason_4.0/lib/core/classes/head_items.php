631b58d9958936f2fe6621d9ed3043eba1d727f3
Zimzat <zimzat@zimzat.com>
2014-12-06 20:09:50 -0600

    Implement SCSSPHP into the HeadItems class and convert it and LESSPHP to use the new checkedCachedCompile methods in each.

--- reason_4.0/lib/core/classes/head_items.php
+++ reason_4.0/lib/core/classes/head_items.php
@@ -90,6 +90,28 @@ class HeadItems
 	 */
 	protected $delete_old_less_css = true;
 	
+	/**
+	 *
+	 * @var array key->value pairs
+	 */
+	protected $default_scss_variables = array();
+
+	/**
+	 * Scss functions that should be added to every less stylesheet
+	 * @var array PHP callable functions
+	 */
+	protected $default_scss_functions = array();
+
+	/**
+	 * Should old css files created by less be deleted?
+	 *
+	 * Change this to false if you are not able to redirect users to the most recent one where needed,
+	 * since page caching can cause Reason to serve up out-of-date URLs.
+	 *
+	 * @var boolean
+	 */
+	protected $delete_old_scss_css = true;
+
 	function HeadItems()
 	{
 	}
@@ -135,9 +157,13 @@ class HeadItems
 	 */
 	function add_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>'') )
 	{
-		if(substr($url, 0, 1) == '/' && substr($url, -5) == '.less')
+		if($url[0] === '/' && substr($url, -5) === '.less')
 		{
-			return $this->add_less_stylesheet( $url, $media, $add_to_top, $wrapper, $this->default_less_variables, $this->default_less_functions );
+			return $this->add_less_stylesheet( $url, $media, $add_to_top, $wrapper );
+		}
+		if ($url[0] === '/' && substr($url, -5) === '.scss')
+		{
+			return $this->add_scss_stylesheet( $url, $media, $add_to_top, $wrapper );
 		}
 		$attrs = array('rel'=>'stylesheet','type'=>'text/css','href'=>$url);
 		if(!empty($media))
@@ -174,113 +200,177 @@ class HeadItems
 		$this->default_less_functions = $array;
 	}
 	
+	function add_default_scss_variable($key,$value)
+	{
+		$this->default_scss_variables[$key] = $value;
+	}
+	function remove_default_scss_variable($key)
+	{
+		if(isset($this->default_scss_variables[$key]))
+			unset($this->default_scss_variables[$key]);
+	}
+	function set_default_scss_variables($array)
+	{
+		$this->default_scss_variables = $array;
+	}
+	function add_default_scss_function($scss_function_name, $php_function)
+	{
+		$this->default_scss_functions[$scss_function_name] = $php_function;
+	}
+	function remove_default_scss_function($scss_function_name)
+	{
+		if(isset($this->default_scss_functions[$scss_function_name]))
+			unset($this->default_scss_functions[$scss_function_name]);
+	}
+	function set_default_scss_functions($array)
+	{
+		$this->default_scss_functions = $array;
+	}
+
 	/**
 	 * Add a less-based stylesheet
 	 * @param string $url
 	 * @param string $media optional media attribute
 	 * @param boolean $add_to_top
 	 * @param array $wrapper
-	 * @param array $less_variables
-	 * @param array $less_functions
-	 * @return boolean success
 	 */
-	function add_less_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>''), $less_variables = array(), $less_functions = array() )
+	function add_less_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>'') )
 	{
-		if(substr($url, 0, 1) != '/')
+		$parser = $this->get_less_parser($this->default_less_variables, $this->default_less_functions);
+		$output_url = $this->create_parsed_stylesheet($parser, $url, $this->default_less_variables, $this->default_less_functions, $this->delete_old_less_css);
+		if (!$output_url) return;
+		$this->add_stylesheet($output_url, $media, $add_to_top, $wrapper);
+	}
+
+	/**
+	 * Add a scss-based stylesheet
+	 * @param string $url
+	 * @param string $media optional media attribute
+	 * @param boolean $add_to_top
+	 * @param array $wrapper
+	 */
+	function add_scss_stylesheet( $url, $media = '', $add_to_top = false, $wrapper = array('before'=>'','after'=>'') )
+	{
+		$parser = $this->get_scss_parser($this->default_scss_variables, $this->default_scss_functions);
+		$output_url = $this->create_parsed_stylesheet($parser, $url, $this->default_scss_variables, $this->default_scss_functions, $this->delete_old_scss_css);
+		if (!$output_url) return;
+		$this->add_stylesheet($output_url, $media, $add_to_top, $wrapper);
+	}
+
+	/**
+	 *
+	 * @param type $parser
+	 * @param type $url
+	 * @param type $variables
+	 * @param type $functions
+	 * @param type $delete_cached
+	 * @return string|boolean
+	 */
+	function create_parsed_stylesheet( $parser, $url, $variables, $functions, $delete_cached = true )
+	{
+		if (!$parser)
 		{
-			trigger_error('Less stylesheets must be specified relative to server root (i.e. starting with "/"). Path given ('.$url.') does not conform to this specification. Stylesheet not added.');
 			return false;
 		}
-		
+
+		if($url[0] !== '/')
+		{
+			trigger_error('Parsed stylesheets must be specified relative to server root (i.e. starting with "/"). Path given ('.$url.') does not conform to this specification. Stylesheet not added.');
+			return false;
+		}
+
+		$input_path = WEB_PATH.substr($url, 1);
+		if(!file_exists($input_path))
+		{
+			trigger_error('Stylesheet not found at "'.$input_path.'". Stylesheet not added.');
+			return false;
+		}
+
+		$hash = md5($input_path.serialize($variables).serialize(array_keys($functions)));
+		$first2 = substr($hash, 0, 2);
+
+		$output_filename = $hash.'_'.filemtime($input_path).'.css';
+		$output_url = WEB_TEMP.'compiled/'.$first2.'/'.$output_filename;
+		$output_directory = WEB_PATH.substr(WEB_TEMP, 1).'compiled/' . $first2 .'/';
+		$output_path = $output_directory.$output_filename;
+		if (!file_exists($output_directory))
+		{
+			mkdir($output_directory, 0777, true);
+		}
+
+		// Track our modified time so we know to delete older files.
+		$mtime = $delete_cached && file_exists($output_path) ? filemtime($output_path) : null;
+
+		try
+		{
+			$parser->checkedCachedCompile($input_path, $output_path);
+		}
+		catch (Exception $ex)
+		{
+			trigger_error( 'Unable to compile file at "'.$input_path.'". Stylesheet not added. Message: '.$ex->getMessage() );
+			return;
+		}
+
+		if ($delete_cached && $mtime !== filemtime($output_path))
+		{
+			foreach (glob($output_directory . $hash . '_*.css*') as $file) {
+				if (strpos($file, $output_filename) === false) {
+					unlink($file);
+				}
+			}
+		}
+
+		return $output_url;
+	}
+
+	/**
+	 *
+	 * @param type $variables
+	 * @param type $functions
+	 * @return \Leafo\ScssPhp\Server|boolean
+	 */
+	function get_scss_parser($variables, $functions)
+	{
+		if(!include_once(SCSSPHP_INC.'scss.inc.php'))
+		{
+			trigger_error('Unable to process .scss file -- SCSSPHP not configured correctly on your server. Check the SCSSPHP_INC setting in settings/package_settings.php.');
+			return false;
+		}
+
+		$scss = new Leafo\ScssPhp\Compiler();
+		$scss->setFormatter('Leafo\ScssPhp\Formatter\Compressed');
+		$scss->setVariables($variables);
+		foreach ($functions as $name => $func)
+		{
+			$scss->registerFunction($name, $func);
+		}
+
+		return new \Leafo\ScssPhp\Server('.', '.', $scss);
+	}
+
+	/**
+	 *
+	 * @param array $variables
+	 * @param array $functions
+	 * @return \lessc|boolean
+	 */
+	function get_less_parser($variables, $functions)
+	{
 		if(!include_once(LESSPHP_INC.'lessc.inc.php'))
 		{
 			trigger_error('Unable to process .less file -- LESSPHP not configured correctly on your server. Check the LESSPHP_INC setting in settings/package_settings.php.');
 			return false;
 		}
-		
-		$input_path = WEB_PATH.substr($url, 1);
-		if(!file_exists($input_path))
-		{
-			trigger_error('Less stylesheet not found at "'.$input_path.'". Stylesheet not added.');
-			return false;
-		}
-		$hash = md5($input_path.serialize($less_variables).serialize(array_keys($less_functions)));
-		$first2 = substr($hash, 0, 2);
-		$output_filename = $hash.'_'.filemtime($input_path).'.css';
-		
-		$output_url = WEB_TEMP.'less_compiled/'.$first2.'/'.$output_filename;
-		
-		$base_output_directory = WEB_PATH.substr(WEB_TEMP, 1).'less_compiled/';
-		if(!file_exists($base_output_directory))
-			mkdir($base_output_directory);
-		$output_directory = $base_output_directory.$first2.'/';
-		if(!file_exists($output_directory))
-			mkdir($output_directory);
-		$output_path = $output_directory.$output_filename;
-		$cache_path = $output_directory.$hash.'.cache';
-		
-		$less = new lessc;
+
+		$less = new lessc();
 		$less->setFormatter('compressed');
-		$less->setVariables($less_variables);
-		foreach($less_functions as $less_function_name => $php_function)
+		$less->setVariables($variables);
+		foreach ($functions as $name => $func)
 		{
-			$less->registerFunction($less_function_name, $php_function);
+			$less->registerFunction($name, $func);
 		}
-		try
-		{
-			if($this->less_cached_compile($input_path, $output_path, $less, $cache_path))
-			{
-				$compiled = true; // something has changed
-				if ($this->delete_old_less_css && ( $handle = opendir($output_directory) ) )
-				{
-					while (false !== ($entry = readdir($handle)))
-					{
-        				if($entry != $output_filename && strpos($entry, $hash.'_') === 0 && substr($entry, -4) == '.css')
-        				{
-        					unlink($output_directory.$entry);
-        				}
-    				}
-				}
-				else
-				{
-					trigger_error('Unable to delete old less css files');
-				}
-			}
-			else
-			{
-				$compiled = false; // nothing has changed
-			}
-		}
-		catch (Exception $ex)
-		{
-			trigger_error( 'lessphp unable to compile less at "'.$input_path.'". Stylesheet not added. Message: '.$ex->getMessage() );
-			return;
-		}
-		
-		return $this->add_stylesheet( $output_url, $media, $add_to_top, $wrapper );
-		
-		return true;
-	}
-	/**
-	 * Compile less using a cache for included files
-	 * @return boolean true if newly compiled, false if cached version unchanged
-	 */
-	function less_cached_compile($input_path, $output_path, $less, $cache_path)
-	{
-		if (file_exists($cache_path))
-			$cache = unserialize(file_get_contents($cache_path));
-		else
-			$cache = $input_path;
 
-		$new_cache = $less->cachedCompile($cache);
-
-		if (!is_array($cache) || $new_cache["updated"] > $cache["updated"])
-		{
-			file_put_contents($cache_path, serialize($new_cache));
-			file_put_contents($output_path, $new_cache['compiled']);
-			return true;
-		}
-		return false;
+		return $less;
 	}
 
 	/**
