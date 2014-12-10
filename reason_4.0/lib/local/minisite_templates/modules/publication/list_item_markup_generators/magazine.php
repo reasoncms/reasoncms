879e36a384eb3327c2bc2f088e128f6705dfb1c7
Nathan Dirks <nathandirks@gmail.com>
2014-12-10 15:16:12 -0600

    Initial commit of magazine markup generators.

---
+++ reason_4.0/lib/local/minisite_templates/modules/publication/list_item_markup_generators/magazine.php
@@ -0,0 +1,203 @@
+<?php
+/**
+ *  @package reason
+ *  @subpackage minisite_modules
+ */
+ 
+ /**
+  * Include the base class
+  */
+reason_include_once( 'minisite_templates/modules/publication/markup_generator.php' );
+
+/**
+*  A basic post listing page that displays the posts' categories after the publication date and images at custom sizes.
+*
+*/
+
+class MagazinePublicationListItemMarkupGenerator extends PublicationMarkupGenerator
+{
+	//variables needed to be passed from the publication module
+	var $variables_needed = array( 	'use_dates_in_list', 
+									'date_format', 
+									'item',
+									'item_comment_count', 
+									'link_to_full_item', 
+									'permalink',
+									'teaser_image',
+									'item_categories',
+									'publication',
+									'current_issue',
+								  	'commenting_status',
+									);
+	
+	var $show_section_name = false;
+
+	function PublicationListItemMarkupGenerator ()
+	{
+	}
+	
+	function run ()
+	{	
+		
+		$this->markup_string .= $this->get_teaser_image_markup();
+		//$this->markup_string .= $this->get_item_category_markup();
+		//$this->markup_string .= $this->get_title_markup();
+		//$this->markup_string .= $this->get_description_markup();
+		$this->markup_string .= $this->get_content_block_markup();
+		//$this->markup_string .= $this->get_date_markup();
+		
+		//$this->markup_string .= $this->get_section_markup();
+		//$this->markup_string .= $this->get_links_markup();
+	}
+	
+	function get_pre_markup()
+	{
+		return $this->get_teaser_image_markup();
+	}
+	
+	function get_item_category_markup()
+	{
+		$link_markup = array();
+		$markup_string = '<ul class="post-cats">';
+		foreach ($this->passed_vars['item_categories'] as $cat)
+		{
+			$link_markup[] = '<li class="post-cat"><a href="' . carl_make_link( array(), '', '', false, false) . $cat->get_value('category_url') . '">' . $cat->get_value('name') . '</a></li>';
+		}
+		$markup_string .= implode("\n ", $link_markup);
+		$markup_string .= '</ul>';
+		return $markup_string;
+	}
+
+	// FULL SIZE TEASER IMG MARKUP
+	function get_teaser_image_markup() // {{{
+	{
+		$markup_string = '';
+		$image = current($this->passed_vars['teaser_image']);
+		$link_to_full_item = isset($this->passed_vars['link_to_full_item']) ? $this->passed_vars['link_to_full_item'] : '';
+		if (!empty($image))
+		{
+			$markup_string .= '<figure class="primaryImage">';
+			if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+				$markup_string .=  '<a href="' .$link_to_full_item. '">';
+				
+			$markup_string .= '<img src="'.WEB_PHOTOSTOCK.reason_get_image_filename( $image->id() ).'" alt="'.str_replace('"', "'", $image->get_value( 'description' )).'"/>';
+
+			if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+				$markup_string .=  '</a>';
+			
+			$markup_string .= '</figure>';
+		} 
+		else {
+			$markup_string .= '<figure class="primaryImage">';
+			if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+				$markup_string .=  '<a href="' .$link_to_full_item. '">';
+			//$markup_string .= '<img src="'.WEB_PHOTOSTOCK.reason_get_image_filename( $image->id() ).'" width="'.$image->get_value( 'width' ).'" height="'.$image->get_value( 'height' ).'" alt="'.str_replace('"', "'", $image->get_value( 'description' )).'"/>';
+			
+			$markup_string .= '<img src="http://192.168.56.102/reason/images/562005.jpg" />';
+
+			if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+			$markup_string .=  '</a>';
+			$markup_string .= '</figure>';
+		}
+		return $markup_string;
+	}
+
+	// CUSTOM TEASER IMG SIZE MARKUP
+	// function get_teaser_image_markup()
+	// {
+	// 	$markup_string = '';
+	// 	$image = $this->passed_vars['teaser_image'];
+	// 	if (!empty($image))
+	// 	{
+	// 		$markup_string .= '<figure class="teaserImage">';
+
+	// 		if(is_array($image))
+	// 			$image = reset($image);
+			
+	// 		$rsi = new reasonSizedImage();
+	// 		$rsi->set_id($image->id());
+	// 		$rsi->set_width(400);
+	// 		$rsi->set_height(250);
+	// 		$rsi->set_crop_style('fill');
+
+	// 		ob_start();	
+	// 		show_image( $rsi,true,false,false );
+	// 		$markup_string .= ob_get_contents();
+	// 		ob_end_clean();
+	// 		//$markup_string .= '<img src="/reason/sized_images/540620/b25879bda30d8a9542e03ab9670e730e.gif?cb=1397165736">';
+	// 		$markup_string .= '</figure>';
+	// 	}
+	// 	return $markup_string;
+	// }
+
+	// ORIGINAL TEASER IMG MARKUP	
+	// function get_teaser_image_markup() // {{{
+	// {
+	// 	$markup_string = '';
+	// 	$image = $this->passed_vars['teaser_image'];
+	// 	if (!empty($image))
+	// 	{
+	// 		$markup_string .= '<div class="teaserImage">';
+	// 		ob_start();	
+	// 		show_image( reset($image), true,false,false );
+	// 		$markup_string .= ob_get_contents();
+	// 		ob_end_clean();
+	// 		$markup_string .= '</div>';
+	// 	} 
+	// 	return $markup_string;
+	// }
+
+	function get_content_block_markup() {
+		$link_to_full_item = isset($this->passed_vars['link_to_full_item']) ? $this->passed_vars['link_to_full_item'] : '';
+		
+		$markup_string .=  '<div class="content-block">';
+		$markup_string .= $this->get_item_category_markup();
+
+		if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+			$markup_string .=  '<a href="' .$link_to_full_item. '">';
+		
+		$markup_string .= $this->get_title_markup();
+		$markup_string .= $this->get_description_markup();
+		
+		if(isset($link_to_full_item) &&  !empty($link_to_full_item))
+			$markup_string .=  '</a>';
+		$markup_string .=  '</div>';
+		return $markup_string;
+	}
+	
+	function get_title_markup()
+	{
+		$markup_string = '';
+		$item = $this->passed_vars['item'];
+		$markup_string .=  '<h4 class="title">';
+		$markup_string .= $item->get_value('release_title');
+		$markup_string .=  '</h4>'."\n";
+		return $markup_string;
+	}
+	
+	function get_description_markup()
+	{
+		$item = $this->passed_vars['item'];
+		if($item->get_value('description'))
+			return '<div class="desc">'.$item->get_value('description').'</div>'."\n";
+	}
+	
+	// function get_title_markup()
+	// {
+	// 	$markup_string = '';
+	// 	$item = $this->passed_vars['item'];
+	// 	$markup_string .=  '<h4 class="title">';
+	// 	$markup_string .= $item->get_value('release_title');
+	// 	$markup_string .=  '</h4>'."\n";
+	// 	return $markup_string;
+	// }
+	
+	// function get_description_markup()
+	// {
+	// 	$item = $this->passed_vars['item'];
+	// 	if($item->get_value('description'))
+	// 		return '<div class="desc">'.$item->get_value('description').'</div>'."\n";
+	// }
+
+}
+?>
---
+++ reason_4.0/lib/local/minisite_templates/modules/publication/publication_list_markup_generators/magazine.php
@@ -0,0 +1,555 @@
+<?php
+/**
+ *  @package reason
+ *  @subpackage minisite_modules
+ */
+
+/**
+ * Include the parent class
+ */
+
+reason_include_once( 'minisite_templates/modules/publication/publication_list_markup_generators/default.php' );
+	
+/*
+*  This extends our custom default publication list markup generator for the Magazine theme.
+*/
+
+class MagazinePublicationListMarkupGenerator extends PublicationListMarkupGenerator
+{
+
+	function get_variables_needed()
+	{
+		$this->variables_needed[] = 'filter_interface_markup';
+		$this->variables_needed[] = 'search_interface_markup';
+		return parent::get_variables_needed();
+	}
+
+	function run()
+	{
+		//$this->markup_string .= $this->this_is_a_filtered_mofo();
+		$this->markup_string .= $this->category_filter_title_markup();
+		//$this->markup_strong .= $this->get_issue_title_markup();
+		//$this->markup_strong .= $this->get_issue_selector_markup();
+		//$this->markup_strong .= $this->get_issue_blurb_module_markup();
+		$this->markup_string .= $this->get_featured_items_markup();
+		$this->markup_string .= $this->get_list_markup();
+		$this->markup_string .= $this->get_post_list_markup();
+		$this->markup_string .= $this->get_search_and_filter_interface_markup();
+	}
+
+	//overloaded from generic3
+	function show_style_string()
+	{
+		$class_string = ($this->related_mode) ? 'relatedPub' : 'publication';
+		if(!empty( $this->current_item_id ) )
+			$class_string .= ' fullPostDisplay';
+		echo '<div id="'.$this->style_string.'" class="'.$class_string.'">'."\n";
+	}
+
+	function get_issue_title_markup()
+	{
+		//if this is an issued publication, show what issue we're looking at
+		if(!empty($this->passed_vars['current_issue']))
+			$this->markup_string .= $this->get_current_issue_markup($this->passed_vars['current_issue']);
+	}
+
+	function get_issue_selector_markup()
+	{
+		//if there are other issues, display a "jump to other issues" dropdown
+		if(!empty($this->passed_vars['issues_by_date']))
+			$this->markup_string .= $this->get_issue_links_markup();
+	}
+
+	function get_issue_blurb_module_markup()
+	{
+		if(!empty($this->passed_vars['issue_blurbs']))
+			$this->markup_string .= $this->get_issue_blurbs_markup();
+	}
+
+	function category_filter_title_markup()
+	{
+		//if we're listing filtered items ....
+		if(!empty($this->passed_vars['current_filters']))
+		{
+			$this->markup_string .= $this->get_filter_message_markup();
+		}
+	}
+
+	function get_pre_list_markup()
+	{
+		if(!empty($this->passed_vars['search_string']))
+			$this->markup_string .= $this->get_search_header_markup();
+			
+		//if we're just listing items from one section ....
+		if(!empty($this->passed_vars['current_section']))
+		{
+			//show what section we're looking at
+			$this->markup_string .= $this->get_current_section_markup($this->passed_vars['current_section']);
+						
+			//if we're just looking at the items in this section from one issue, provide a link to see items in this section from all issues
+			if(!empty($this->passed_vars['view_all_items_in_section_link']) && !empty($this->passed_vars['current_issue']) )
+				$this->markup_string .= $this->get_all_items_in_section_link_markup();
+		}
+	
+	}
+
+	function get_search_and_filter_interface_markup()
+	{
+		$markup = '';
+		if(!empty($this->passed_vars['search_interface_markup']) || !empty($this->passed_vars['filter_interface_markup']))
+		{
+			$markup .= '<div class="searchAndFilterInterface">'."\n";
+			// if(!empty($this->passed_vars['search_interface_markup']))
+			// {
+			// 	$markup .= '<div class="searchInterface">'."\n";
+			// 	$markup .= $this->passed_vars['search_interface_markup'];
+			// 	$markup .= '</div>'."\n";
+			// }
+			if(!empty($this->passed_vars['filter_interface_markup']))
+			{
+				$markup .= '<div class="filterInterface">'."\n";
+				$markup .= $this->passed_vars['filter_interface_markup'];
+				$markup .= '</div>'."\n";
+			}
+			$markup .= '</div>'."\n";
+		}
+		return $markup;
+	}	
+	
+	// function get_post_list_markup()
+	// {
+	// 	$markup_string = '';
+		
+	// 	if(!empty($this->passed_vars['current_section']))
+	// 	{
+	// 		$markup_string .= '<div class="postList">'."\n";
+	// 		//provide links to other sections in the publication
+	// 		if(!empty($this->passed_vars['links_to_sections']))
+	// 			$this->markup_string .= $this->get_section_links_markup();
+	// 		$markup_string .= '<div class="back">'."\n";
+	// 		$main_list_name = $this->passed_vars['publication']->get_value('name');
+	// 	 	if(!empty($this->passed_vars['current_issue']))
+	// 			$main_list_name .= ': '.$this->passed_vars['current_issue']->get_value('name');
+	// 		$markup_string .= '<a href="'.$this->passed_vars['back_link'].'">Return to '.$main_list_name.'</a>'."\n";
+	// 	 	$markup_string .= '</div>'."\n"; //close back
+	// 	 	$markup_string .= '</div>'."\n"; // close postList
+	// 	}
+	// 	return $markup_string;
+	// }
+
+/////
+//  List item methods
+/////
+
+
+	
+	function get_list_markup()
+	{
+		$markup_string = '';
+
+		if(!empty($this->passed_vars['list_item_markup_strings']))
+		{
+			$do_section_headings = false;
+			if(!empty($this->passed_vars['sections']) && $this->passed_vars['group_by_section'])
+			{
+				$do_section_headings = true;
+				$markup_string .= '<div class="sections">'."\n";
+				$markup_string .= '<div class="grid-sizer"></div>'."\n"; // This is necessary for Isotope.js
+			}
+			
+			$ordered_sections = $this->get_section_ids_in_order();		
+			foreach($ordered_sections as $section_id)
+			{
+				$list = $this->passed_vars['items_by_section'][$section_id];
+				
+				// Change section name into a css-id-friendly string
+				$section_entity = $this->passed_vars['sections'][$section_id];
+				$name = $section_entity->get_value('name');
+				//Lower case everything
+				$name = strtolower($name);
+				//Make alphanumeric (removes all other characters)
+				$name = preg_replace("/[^a-z0-9_\s-]/", "", $name);
+				//Clean up multiple dashes or whitespaces
+				$name = preg_replace("/[\s-]+/", " ", $name);
+				//Convert whitespaces and underscore to dash
+				$name = preg_replace("/[\s_]/", "-", $name);
+
+				if($do_section_headings && array_key_exists($section_id, $this->passed_vars['sections']))
+				{	
+					$markup_string .= '<div class="section" id="'.$name.'">'."\n";
+					//$markup_string .= $this->get_section_heading_markup($section_id);
+					$markup_string .= $this->get_list_markup_for_these_items(array_keys($list));
+					//$markup_string .= $this->get_section_footer_markup($section_id);
+					$markup_string .='</div>'."\n";
+				}
+				elseif(array_key_exists($section_id, $this->passed_vars['items_by_section']))
+				{
+					$markup_string .= $this->get_list_markup_for_these_items(array_keys($list))."\n";
+				}
+			}
+			if($do_section_headings)
+				$markup_string .= '</div>'."\n"; 
+		}
+		return $markup_string;
+	}
+
+
+	
+	
+	/**
+	* Given a array of item ids, returns the markup for those items in the form of an unordered list.
+	* Helper function to {@link get_list_markup}.
+	* @param array $item_ids Array of ids of news item entities.
+	* @return string Markup for the given items.
+	*/	
+	function get_list_markup_for_these_items ($item_ids)
+	{
+
+		$markup_string = '';
+		if(!empty($this->passed_vars['list_item_markup_strings']) && !empty($item_ids))
+		{
+			/* this might seem somewhat backward but it's a reasonably efficient way 
+			to ensure that the ul in only output if there is in fact at least one list item to show */
+			$list_body = '';
+			foreach($item_ids as $item_id)
+			{
+				if(!empty($this->passed_vars['list_item_markup_strings'][$item_id]) && !array_key_exists($item_id, $this->passed_vars['featured_item_markup_strings']))
+					$list_body .= '<article class="post"><div class="inner">'.$this->passed_vars['list_item_markup_strings'][$item_id].'</div></article>'."\n";
+			}
+			if(!empty($list_body))
+			{
+				//$markup_string .= '<div class="posts">'."\n";
+				$markup_string .= $list_body;
+				//$markup_string .= '</div>'."\n";
+			}
+		}
+		return $markup_string;
+	}
+	
+	// function get_search_header_markup()
+	// {
+	// 	if(!empty($this->passed_vars['search_string']))
+	// 	{
+	// 		return '<h3 class="searchTitle">Results for <span class="searchPhrase">"'.$this->passed_vars['search_string'].'"</span></h3>'."\n";
+	// 	}
+	// 	return '';
+	// }
+	
+	function get_issue_blurbs_markup()
+	{
+		if(!empty($this->passed_vars['issue_blurbs']))
+		{
+			$ret = '<div class="issueBlurbs">';
+			foreach($this->passed_vars['issue_blurbs'] as $blurb)
+			{
+				$ret.= '<div class="issueBlurb">'.demote_headings($blurb->get_value('content'), 1).'</div>'."\n";
+			}
+			$ret .= '</div>'."\n";
+			return $ret;
+		}
+		return '';
+	}
+	
+//////
+// Featured item methods
+//////
+	
+	function get_featured_items_markup()
+	{
+		$markup_string = '';
+		$featured_items = $this->get_featured_items_to_show();
+
+		if(!empty($featured_items))
+		{
+			$feature_header_string = '';
+			
+			if(count($featured_items) > 1)
+			{
+				if (!empty($feature_header_string)) $feature_header_string .= 's';
+			}
+			
+			$markup_string = '<div id="featuredItems">'."\n";
+			if (!empty($feature_header_string)) $markup_string .= '<h3> '.$feature_header_string.' </h3>'."\n";
+			
+			$markup_string .= '<div class="posts">'."\n";
+			foreach($this->passed_vars['featured_item_markup_strings'] as $list_item_string)
+			{
+				$markup_string .= '<article class="post">'.$list_item_string.'</article>'."\n";
+			}
+			$markup_string .= '</div>'."\n";
+			$markup_string .= '</div>'."\n";
+		}
+		
+		return $markup_string;
+	}
+	
+	
+	/**
+	* Determines which news items should be displayed as featured items, if any.  
+	* @return array Array of markup strings for featured items that should be shown, format $id => $markup_string.
+	*/	
+	// function get_featured_items_to_show()
+	// {
+	// 	if(empty($this->passed_vars['current_section']))
+	// 		return $this->passed_vars['featured_item_markup_strings'];
+	// 	else
+	// 	{
+	// 		$featured_items_in_this_section = array();
+
+	// 		foreach($this->passed_vars['featured_item_markup_strings'] as $id => $markup)
+	// 		{
+	// 			if(!empty($this->passed_vars['items_by_section'][$this->passed_vars['current_section']->id()]))
+	// 				$section_array = $this->passed_vars['items_by_section'][$this->passed_vars['current_section']->id()];
+	// 			elseif(!empty($this->passed_vars['no_section_key']) && isset($this->passed_vars['items_by_section'][$this->passed_vars['no_section_key']]) )
+	// 				$section_array = $this->passed_vars['items_by_section'][$this->passed_vars['no_section_key']];
+	// 			else
+	// 				$section_array = array();
+
+	// 			if(array_key_exists($id, $section_array))
+	// 			{
+	// 				$featured_items_in_this_section[$id] = $markup;
+	// 			}	
+	// 		}
+	// 		return $featured_items_in_this_section;
+	// 	}
+	// }
+
+	
+/////
+//  Issue methods
+/////
+	// function get_current_issue_markup($issue)
+	// {
+	// 	$markup_string = '';
+	// 	$markup_string .= '<div class="issueName"><h3>'.$this->_get_issue_label($issue).'</h3></div>'."\n";
+	// 	return $markup_string;
+	// }
+	
+	function get_issue_links_markup()
+	{
+		$issues_by_date = $this->passed_vars['issues_by_date'];
+		//krsort($issues_by_date);
+		$links_to_issues = $this->passed_vars['links_to_issues'];
+
+		$markup_string = '';
+		
+		$cur_issue_id = '';
+		if(!empty($this->passed_vars['current_issue']))
+		{
+			$cur_issue_id = $this->passed_vars['current_issue']->id();
+		}
+		
+		if(count($issues_by_date) > 1 )
+		{
+			$markup_string .= '<div class="issueMenu">'."\n";
+			$markup_string .= '<form action="'.htmlspecialchars(get_current_url(),ENT_QUOTES,'UTF-8').'">'."\n";
+			$markup_string .= '<label for="pubIssueMenuElement" class="issueLabel">Issue:</label>'."\n";
+
+			$markup_string .= '<script type="text/javascript">'."\n";
+			$markup_string .= '/* <![CDATA[ */'."\n";
+			$markup_string .= '
+			if (jQuery)
+			{
+				$(document).ready(function(){
+					$(".issueMenu input[type=\'submit\']").hide();
+					$(".issueMenu select[name=\'issue_id\']").change(function(){
+						$(this).parent("form").submit();
+					});
+				});
+			}';
+			$markup_string .= '/* ]]> */'."\n";
+			$markup_string .= '</script>';
+			
+			$markup_string .= '<select name="issue_id" id="pubIssueMenuElement">'."\n";
+			if (!$cur_issue_id)
+			{
+				$markup_string .= '<option value="'.$cur_issue_id.'" selected="selected">Select Issue</option>'."\n";
+			}
+			foreach($issues_by_date as $id => $issue)
+			{
+				$selected = ($cur_issue_id == $id) ? ' selected="selected"' : '';
+				$markup_string .= '<option value="'.$id.'"'.$selected.'>'.strip_tags($this->_get_issue_label($issue)).'</option>'."\n";
+			}
+			$markup_string .= '</select>'."\n";
+			$markup_string .= ($this->passed_vars['text_only'] == 1) ? '<input type="hidden" name="textonly" value="1">' : '';
+			$markup_string .= '<input type="submit" value="Go" />'."\n";
+			$markup_string .= '</form>'."\n";
+			$link = carl_make_link(array('issue_id' => 0));
+			//$markup_string .= '<div class="allIssuesLink"><a href="'.$link.'">List all issues</a></div>';
+			$markup_string .= '</div>'."\n";
+		}
+		return $markup_string;
+	}
+	
+	// function _get_issue_label($issue)
+	// {
+	// 	$name = $issue->get_value('name');
+	// 	if(!empty($this->passed_vars['links_to_issues'][$issue->id()]) )
+	// 	{
+	// 		$name = '<a href="'.$this->passed_vars['links_to_issues'][$issue->id()].'">'.$name.'</a>';
+	// 	}
+	// 	if($issue->get_value('show_hide') == 'hide')
+	// 			$name = '[Unpublished] '.$name;
+	// 	$date = prettify_mysql_datetime( $issue->get_value( 'datetime' ), $this->passed_vars['date_format'] );
+	// 	return $name.' <span class="date">('.$date.')</span>';
+	// }
+	
+	
+/////
+//  Section methods
+/////
+	// function get_current_section_markup($section)
+	// {
+	// 	$markup_string = '';
+	// 	$name = $section->get_value('name');
+	// 	$markup_string .= '<div class="curSection"><h3>'.$name.'</h3></div>'."\n";
+	// 	return $markup_string;
+	// }
+	
+	// function get_section_links_markup()
+	// {	
+	// 	$markup_string = '';
+	// 	if(!empty($this->passed_vars['sections']))
+	// 	{
+	// 		$markup_string .= '<div class="sectionMenu">'."\n";
+	// 		$markup_string .= '<h4 class="sectionMenuHead">Sections</h4>'."\n";
+	// 		$markup_string .= '<ul class="sections">'."\n";
+	// 		foreach($this->passed_vars['sections'] as $section_id => $section)
+	// 		{
+	// 			if(!empty($this->passed_vars['current_section']) && $this->passed_vars['current_section']->id() == $section_id)
+	// 			{
+	// 				$markup_string .= '<li class="current"><strong>'.$section->get_value('name').'</strong></li>';
+	// 			}
+	// 			else
+	// 			{
+	// 				$markup_string .=  '<li><a href="'.$this->passed_vars['links_to_sections'][$section_id].'">'.$section->get_value('name').'</a></li>';
+	// 			}
+	// 		}
+	// 		$markup_string .= '</ul>'."\n";	
+	// 		$markup_string .= '</div>'."\n";
+	// 	}
+	// 	return $markup_string;
+	// }
+	
+	// function get_all_items_in_section_link_markup()
+	// {
+	// 	$url = $this->passed_vars['view_all_items_in_section_link'];
+	// 	$link_text = 'View items in this section from all issues';	
+	// 	return '<div class="allIssues"><a href="'.$url.'">'.$link_text.'.</a></div>'."\n";
+	// }
+	
+
+	// function get_section_ids_in_order()
+	// {
+	// 	$section_ids = array_keys($this->passed_vars['items_by_section']);		
+	// 	$ordered_sections = array(); 	 //this will be an array keyed by whatever we're sorting by, which maps to the section ids
+
+	// 	$keys_that_are_not_section_ids = array();
+	// 	foreach($section_ids as $section_id)
+	// 	{
+	// 		if(array_key_exists($section_id, $this->passed_vars['sections']))
+	// 		{
+	// 			$section = $this->passed_vars['sections'][$section_id];
+	// 			$ordered_sections[$section->get_value('name')] = $section_id;
+	// 		}
+	// 		else
+	// 			$keys_that_are_not_section_ids[] = $section_id;
+	// 	}
+		
+	// 	//ksort($ordered_sections);
+	// 	$ordered_sections = array_merge($ordered_sections, $keys_that_are_not_section_ids);
+		
+	// 	return $ordered_sections; 
+	// }
+	
+	
+	// function get_section_heading_markup($section_id)
+	// {
+	// 	$markup_string = '';
+	// 	if(!empty($this->passed_vars['items_by_section'][$section_id]))
+	// 	{
+	// 		$markup_string .= '<div class="sectionInfo">';
+	// 		$section_entity = $this->passed_vars['sections'][$section_id];
+	// 		$url = $this->passed_vars['links_to_sections'][$section_id];
+			
+	// 		if(!empty($url))
+	// 			$markup_string .= '<h2><a href="'.$url.'">'.$section_entity->get_value('name').'</a></h2>'."\n";
+	// 		else
+	// 			$markup_string .= '<h2>'.$section_entity->get_value('name').'</h2>'."\n";
+			
+	// 		$description = $section_entity->get_value('description');
+	// 		if(!empty($description))
+	// 		{
+	// 			$markup_string .= '<p class="sectionDesc">'.$description.'</p>'."\n";
+	// 		}
+	// 		$markup_string .= '</div>';
+	// 	}
+	// 	return $markup_string;		
+	// }
+	
+	// function get_section_footer_markup($section_id)
+	// {
+	// 	$markup_string = '';
+	// 	if(empty($this->passed_vars['current_section']) && !empty($this->passed_vars['items_by_section'][$section_id]))
+	// 	{
+	// 		$url = $this->passed_vars['links_to_sections'][$section_id];
+	// 		if(!empty($url))
+	// 		{
+	// 			$section_name = $this->passed_vars['sections'][$section_id]->get_value('name');
+	// 			$markup_string .= '<div class="sectionFoot"><span class="viewEntireSection">View all items in <a href="'.$url.'">'.$section_name.'</a>.</span></div>'."\n";
+	// 		}
+	// 	}
+	// 	return $markup_string;		
+	// }
+	
+	function get_filter_message_markup()
+	{
+		$markup_string = '';
+		if($msg = $this->_get_filter_message())
+		{
+			$markup_string .= '<div class="filterMessage">'."\n";
+			$markup_string .= '<h3>'.$msg.' <span class="clear"><a href="./">(Back to all posts)</a></span></h3>'."\n";
+			$markup_string .= '</div>'."\n";
+		}
+		return $markup_string;	
+	}
+	function _get_filter_message()
+	{
+		$msg = '';
+		if(!empty($this->passed_vars['current_filters']))
+		{
+			$parts = array();
+			foreach($this->passed_vars['current_filters'] as $filterkey => $entities)
+			{
+				foreach($entities as $e)
+				{
+					$parts[] = $e->get_value('name');
+				}
+			}
+			$glue = ' ';
+			$num_parts = count($parts);
+			if($num_parts > 1)
+			{
+				$last_part = 'and '.array_pop($parts);
+				$parts[] = $last_part;
+				if($num_parts > 2)
+				{
+					$glue = ', ';
+				}
+			}
+			$msg = 'Category: <strong>'.implode($glue, $parts).'</strong>';
+		}
+		return $msg;
+	}
+
+	// function this_is_a_filtered_mofo()
+	// {	
+	// 	$msg = '';
+	// 	if(!empty($this->passed_vars['current_filters']))
+	// 	{
+	// 		$msg = '<h1>This is filtered, yo!</h1>';
+	// 	}
+	// 	return $msg;
+	// }
+}
+?>
