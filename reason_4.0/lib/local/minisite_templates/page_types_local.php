080a7eb98ff734b60f06c4aa0e6cc4aed4ac747c
Nathan Dirks <nathandirks@gmail.com>
2015-02-06 17:51:01 -0600

    Move files and rearrange structure.

---
+++ reason_4.0/lib/local/minisite_templates/page_types_local.php
@@ -0,0 +1,49 @@
+<?php
+
+$GLOBALS['_reason_page_types_local'] = array(
+	'cloak_home' => array(
+		'pre_bluebar' => '',
+		'main_head' => 'page_title',
+		'post_banner' => array(
+			'module' => 'feature/feature',
+			'shuffle' => false,
+			'autoplay_timer' => 3,
+			'width' => 1680,
+			'height' => 651,
+		),
+		'main' => 'events_mini',
+		// 'main_post' => array(
+		// 	'module' => 'publication',
+		// 	'related_mode' => 'true',
+		// 	// 'markup_generator_info' =>
+		// 	// array('list_item' => array(
+		// 	// 				'classname' => 'MinimalListItemMarkupGenerator', 
+		// 	// 				'filename' => 'minisite_templates/modules/publication/list_item_markup_generators/minimal.php',
+		// 	// 				)
+		// 	// 	 ),
+		// 	'max_num_items' => 4,
+		// ),
+		'main_post_2' => array(
+			'module' => 'blurb',
+			'num_to_display' => 4,
+		),
+		'main_post_3' => '',
+		'edit_link' => 'login_link',
+		'pre_banner' => 'announcements',
+		'banner_xtra' => 'search',
+		//'post_banner' => 'navigation_top',
+		'pre_sidebar' => array(
+			'module' => 'blurb',
+			'num_to_display' => 1,
+		),
+		'sidebar' => 'content',
+		'post_sidebar' => '',
+		'navigation' => '',
+		'footer' => 'maintained',
+		'sub_nav' => '',
+		'sub_nav_2' => '',
+		'sub_nav_3' => '',
+		'post_foot' => '',
+	),
+);
+?>
