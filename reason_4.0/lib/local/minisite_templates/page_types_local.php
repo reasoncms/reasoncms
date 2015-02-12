9c9b50ad71fc31936f099864eb82ef44925792af
Nathan Dirks <nathandirks@gmail.com>
2015-02-12 10:02:10 -0600

    Updated page types local with new publication markup generators.

--- reason_4.0/lib/local/minisite_templates/page_types_local.php
+++ reason_4.0/lib/local/minisite_templates/page_types_local.php
@@ -10,23 +10,31 @@ $GLOBALS['_reason_page_types_local'] = array(
 			'autoplay_timer' => 3,
 			'width' => 1680,
 			'height' => 651,
+
 		),
 		'main' => array(
-			'module' => 'events_mini',
-			'ideal_count' => 5,
+			'module' => 'publication',
+				'show_featured_items' => true,
+				'related_mode' => 'true',
+				'markup_generator_info' => array(
+					'list' => array(
+						'classname' => 'CloakRelatedListMarkupGenerator', 
+						'filename' => 'minisite_templates/modules/publication/publication_list_markup_generators/cloak_related_list.php',
+					),
+					'list_item' => array(
+						'classname' => 'CloakMinimalWithImageListItemMarkupGenerator',
+						'filename' => 'minisite_templates/modules/publication/list_item_markup_generators/cloak_minimal_with_image.php',
+					),
+					'featured_item' => array(
+						'classname' => 'CloakWithImageListItemMarkupGenerator', 
+						'filename' => 'minisite_templates/modules/publication/list_item_markup_generators/cloak_with_image.php',
+					),
+				),
+			'max_num_items' => 4,
 		),
 		'main_post' => array(
-			'module' => 'publication',
-			'related_mode' => 'true',
-			'related_title' => 'Muush',
-			'show_featured_items' => true,
-			// 'related_markup_generator_info' =>
-			// 	array('list_item' => array(
-			// 		'classname' => 'ListItemNoImageMarkupGenerator', 
-			// 		'filename' => 'minisite_templates/modules/publication/list_item_markup_generators/no_image.php',
-			// 		),
-			//  	),
-			'max_num_items' => 4,
+			'module' => 'events_mini',
+			'ideal_count' => 5,
 		),
 		'main_post_2' => '',
 		'pre_sidebar' => array(
