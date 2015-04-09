abbbccca469eb8aaafb221f102d2dfac8680ccd2
Gage Dykema <gdykema@gmail.com>
2015-04-09 13:58:10 -0500

    finished credit card type prediction

--- reason_4.0/www/local/luther_2014/javascripts/creditcard.js
+++ reason_4.0/www/local/luther_2014/javascripts/creditcard.js
@@ -22,8 +22,8 @@ function getCreditCardType(accountNumber)
   }
 
   //then check for Discover
-  //have to get rid of possible - for Discover or just take it into account in regex
-  else if(/^6011/.test(accountNumber) || /^64[4-9]/.test(accountNumber) || /^65/.test(accountNumber) || /^3[47]/.test(accountNumber))
+  else if(/^6011-?0/.test(accountNumber) || /^6011-?[2-4]/.test(accountNumber) || /^6011-?74/.test(accountNumber) || /^6011-?7[7-9]/.test(accountNumber) || /^6011-?8[6-9]/.test(accountNumber) || /^6011-?9/.test(accountNumber) ||
+    /^64[4-9]/.test(accountNumber) || /^65/.test(accountNumber) )
   {
     result = "Discover";
   }
@@ -31,28 +31,71 @@ function getCreditCardType(accountNumber)
   return result;
 }
 
-function showCreditCardType(accountNumber)
+function showCreditCardType()
 {
-  var type = getCreditCardType(accountNumber);
+  var value   = event.target.value;
+  if(value === undefined){
+    value = $("#credit_card_numberElement")[0].value;
+  }
+  var type = getCreditCardType(value);
   switch (type)
   {
     case "Mastercard":
       //show MasterCard icon
+      $("#mastercardIcon").addClass("selectedCCType");
+      hideRest("mastercard");
+      selectButton("MasterCard");
       break;
 
     case "Visa":
       //show Visa icon
+      $("#visaIcon").addClass("selectedCCType");
+      hideRest("visa");
+      selectButton("Visa");
       break;
 
     case "American Express":
       //show American Express icon
+      $("#amexIcon").addClass("selectedCCType")
+      hideRest("amex");
+      selectButton("American Express");
       break;
 
     case "Discover":
       //show Discover icon
+      $("#discoverIcon").addClass("selectedCCType");
+      hideRest("discover");
+      selectButton("Discover");
       break;
 
     default:
-      //Error
+      //Error or none selected
+      deselectAll();
+      selectButton("none");
   }
 }
+
+function selectButton(ccType){
+  $("input[name='credit_card_type'][value='"+ccType+"']")[0].checked=true;
+}
+
+function deselectAll(){
+  $(".selectedCCType").removeClass("selectedCCType");
+  $(".nonSelectedCCType").removeClass("nonSelectedCCType");
+}
+
+function hideRest(selected){
+  var cardTypes = ["mastercard","visa","discover","amex"];
+  for (i = 0; i < cardTypes.length; i++) {
+    if(cardTypes[i] != selected){
+      $("#"+cardTypes[i]+"Icon").addClass("nonSelectedCCType");
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  showCreditCardType();
+  var textbox = document.getElementById("credit_card_numberElement");
+  textbox.addEventListener("keyup", showCreditCardType, false);
+  textbox.addEventListener("blur", showCreditCardType, false);
+}, false);
