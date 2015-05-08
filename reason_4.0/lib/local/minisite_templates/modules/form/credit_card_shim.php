26c20c2def7563da117890e6e00576c3936ff2a8
Steve Smith <slylth@gmail.com>
2015-05-08 09:44:39 -0500

    moves name element to top

--- reason_4.0/lib/local/minisite_templates/modules/form/credit_card_shim.php
+++ reason_4.0/lib/local/minisite_templates/modules/form/credit_card_shim.php
@@ -16,6 +16,7 @@ class creditCardShim
 	{
 		$disco->add_element('payment_amount', 'hidden');
 		$disco->add_element('payment_note', 'comment', array('text' => '<h3>Payment Method</h3>'));
+		$disco->add_element('credit_card_name', 'text', array('display_name' => 'Name as it appears on card', 'size' => 35));
 		$disco->add_element('credit_card_number', 'text', array('size' => 35));
 		$disco->add_element('credit_card_type', 'radio_no_sort', array('options' => array('Visa'=>'Visa','MasterCard'=>'MasterCard','American Express'=>'American Express','Discover'=>'Discover','none'=>'none')));
 		$disco->add_element('credit_card_type_icon', 'comment', array('text' => "<i class='fa fa-cc-visa formCCType' id='visaIcon'></i><i class='fa fa-cc-mastercard formCCType' id='mastercardIcon'></i><i class='fa fa-cc-amex formCCType' id='amexIcon'></i><i class='fa fa-cc-discover formCCType' id='discoverIcon'></i>"));
@@ -23,12 +24,11 @@ class creditCardShim
 		$disco->add_element('credit_card_expiration_year', 'text');
 		$disco->add_element('credit_card_security_code', 'text', array('size' => 4, 'display_name' => 'Security Code'));
 		$disco->set_comments('credit_card_security_code', form_comment('
-			<p class="formComment"><a data-reveal-id="cvv2Iframe">What\'s this?</a></p>
+			<p><a data-reveal-id="cvv2Iframe">What\'s this?</a></p>
 			<div class="reveal-modal medium" id="cvv2Iframe" data-reveal="">
                 <iframe height="300px" width="100%" src="https://www.cvvnumber.com/cvv.html"></iframe>
                 <a class="close-reveal-modal">×</a>
             </div>'));
-		$disco->add_element('credit_card_name', 'text', array('display_name' => 'Name as it appears on card', 'size' => 35));
 		// Currently the billing address is not needed since a product is not being delivered.
 		//$disco->add_element('billing_address', 'radio_no_sort', array('display_name' => 'Billing Address', 'default' => 'entered', 'options' => array('entered' => 'Use address provided on previous page', 'new' => 'Use a different address')));
 		//$disco->add_element('billing_street_address', 'textarea', array('display_name' => 'Street Address', 'rows' => 3, 'cols' => 35));
@@ -53,7 +53,6 @@ class creditCardShim
 		$disco->change_element_type('credit_card_expiration_year', 'numrange', array('start' => $year, 'end' => $year + 15, 'display_name' => 'Expiration Year'));
 		$disco->add_element_group('inline', 'expiration_group', array('credit_card_expiration_month', 'credit_card_expiration_year'), array('use_element_labels' => false, 'display_name' => 'Expiration mm/yyyy'));
 		$disco->move_element('expiration_group', 'before', 'credit_card_security_code');
-		
 	}
 
 }
