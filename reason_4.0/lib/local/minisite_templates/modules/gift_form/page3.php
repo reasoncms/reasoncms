1de5cacd985a1d30cb7b045a9342c2e39b2a7235
Steve Smith <slylth@gmail.com>
2015-09-09 09:48:54 -0500

    Fixes comment section not saving to db or emailing

--- reason_4.0/lib/local/minisite_templates/modules/gift_form/page3.php
+++ reason_4.0/lib/local/minisite_templates/modules/gift_form/page3.php
@@ -348,7 +348,7 @@ class GiftPageThreeForm extends FormStep {
                 $txt .= '</li>' . "\n";
             }
             if ($this->controller->get('other_designation_details')) {
-                $txt .= '<li>Comments/Other Designation: ' . strip_tags($this->controller->get('other_designation_details')) . '</li>' . "\n";
+                $txt .= '<li>Comments/Special Instructions: ' . strip_tags($this->controller->get('comments_special_instructions')) . '</li>' . "\n";
             }
             $txt .= '</ul>' . "\n"; // Designated specifics end
             $txt .= '</li>' . "\n"; //Designated giving end
