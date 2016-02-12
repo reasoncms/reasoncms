03af0e594a32a235b5c1519e5e86745eadd51e8b
Mark Heiman <mheiman@carleton.edu>
2016-02-12 16:23:17 -0600

    Attempting to reduce memory footprint for get_sections() when section count is high.

--- reason_4.0/lib/core/function_libraries/course_functions.php
+++ reason_4.0/lib/core/function_libraries/course_functions.php
@@ -134,7 +134,7 @@ class CourseTemplateType extends Entity
 				foreach ( $sections as $key => $section)
 				{
 					$this->sections[$section->id()] = new $GLOBALS['course_section_class']($section->id());
-					$this->sections[$section->id()]->get_values();
+					$this->sections[$section->id()]->get_value('academic_session');
 				}
 				
 				// Loading all the data for all the sections is potentially a big memory hog, so to start
