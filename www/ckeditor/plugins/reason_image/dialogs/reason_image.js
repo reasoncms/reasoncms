ac1552fb4b453f55ac540773af27945c44e9e075
Brian Jones <jonebr01@gmail.com>
2016-05-10 09:09:43 -0500

    ckeditor figcaption work, generic hostname, code cleanup

--- www/ckeditor/plugins/reason_image/dialogs/reason_image.js
+++ www/ckeditor/plugins/reason_image/dialogs/reason_image.js
@@ -14,12 +14,12 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 	function writeImagesHtml() {
 		var keys;
 		var document = dialog.getElement().getDocument();
-		var element = document.getById('testdiv');
+		var element = document.getById('image-list');
 		var images = [];
 		
 		keys = filteredImgKeys.length == 0 ? imgKeys : filteredImgKeys
 		for (var i = 0; i < keys.length; i++) {
-			images.push('<figure class="cke_chrome" style="float: left; width: 125px; height: 200px; padding: 2px; margin: 3px;"><img src=\"' + dataObjects[keys[i]].link.replace(/\.(jpe?g|png|gif)$/, '_tn.$1') + '\"><figcaption class="ui-dialog" style="width:100px; word-wrap:normal;">' + dataObjects[keys[i]].name + '</figcaption></figure>');
+			images.push('<figure class="cke_chrome" style="float: left; width: 125px; height: 154px; overflow: hidden; padding: 2px; margin: 3px;"><img src=\"' + dataObjects[keys[i]].link.replace(/\.(jpe?g|png|gif)$/, '_tn.$1') + '\"><figcaption class="ui-dialog" style="width:125px; white-space: normal;">' + dataObjects[keys[i]].name + '</figcaption></figure>');
 		}
 
         if (element)
@@ -31,7 +31,7 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 
 		// Basic properties of the dialog window: title, minimum size.
 		title: 'Insert image',
-		minWidth: 400,
+		minWidth: 425,
 		minHeight: 400,
 
 		// Dialog window content definition.
@@ -63,16 +63,16 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 					    style: 'display: inline-block',
 					},
 					{
-						// Horizontal window where images and captions are displayed
-					    type: 'hbox',
-					    widths: [ '100%' ],
+						// Window where images and captions are displayed
+					    type: 'vbox',
+					    id: 'vbox_img',
 					    children: [
 					               {
 					            	   type: 'html',
 					            	   id: 'html_img',
-					            	   html: '<div id="testdiv" style="overflow-y: scroll; height:200px;"></div>'
+					            	   html: '<div id="image-list" style="overflow-y: scroll; height:254px;"></div>'
 					               }
-					               ]
+					               ],
 					},
 				]
 			},
@@ -104,28 +104,26 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 			}
 		],
 		
+		// Called when the dialog is first created
 		onLoad: function() {
 			dialog = this;
-	        //var exists = urlExists(url);
-			var jsonImg = $.getJSON('http://192.168.56.101/reason/displayers/generate_json.php?site_id=240622&type=image');
-
-			$.getJSON("http://192.168.56.101/reason/displayers/generate_json.php?site_id=240622&type=image", function( data ) {
+			
+			// getJSON is called asynchronously, and onShow: gets called before getJSON returns
+			$.getJSON("//" + window.location.host + "/reason/displayers/generate_json.php?site_id=240622&type=image", function( data ) {
 				  // "count" and "items" are json "data" object keys
 				  $.each(data.items, function(key, value) {
-					  console.log(key, value);
-					  console.log(value.link);
-					  console.log(value.name);
+					  //console.log(key, value);
+					  //console.log(value.link);
+					  //console.log(value.name);
 					  dataObjects.push(value);
 					  imgKeys.push(key);
 				  });
 				  writeImagesHtml();
 			});
-
-			console.log(dialog.setValueOf('tab-web', 'id', "Yo web"));
 		
-// TODO: fix the #cke_39_textInput hack using registerEvents()
+			// TODO: fix the #cke_39_textInput hack using registerEvents() instead
 			$("#cke_39_textInput").keyup(function() {
-				console.log(dataObjects);
+				//console.log(dataObjects);
 				var filter = dialog.getValueOf('tab-existing', 'filter').toLowerCase();
 				filteredImgKeys = [];
 				for (var i = 0; i < dataObjects.length; i++) {
@@ -133,7 +131,7 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 						filteredImgKeys.push(i);
 					}
 				}
-				console.log(filteredImgKeys);
+				//console.log(filteredImgKeys);
 				writeImagesHtml();
 			});
 			
@@ -146,35 +144,36 @@ CKEDITOR.dialog.add( 'reasonImageDialog', function( editor ) {
 					$(this).addClass('cke_menubutton_on');
 				}
 			});
+		},
+		
+		// Called every time the dialog is opened
+		onShow: function() {
 			
-			$(document).ready(function(){
-				$('.cke_reset_all *').css('white-space: wrap;');
-			});
-			
+			if (imgKeys.length > 0) {
+				$('figure').removeClass('cke_menubutton_on');
+				filteredImgKeys = []
+				writeImagesHtml();
+			}
 		},
 
-		// This method is invoked once a user clicks the OK button, confirming the dialog.
+		// Method is invoked once a user clicks the OK button, confirming the dialog.
 		onOk: function() {
 
-			// The context of this function is the dialog object itself.
-			// http://docs.ckeditor.com/#!/api/CKEDITOR.dialog
 			var dialog = this;
-
+			
 			// Create a new img url link
 			var reason_image = editor.document.createElement('img');
 			
-			reason_image.setAttribute('src', $('figure.cke_menubutton_on > img').attr('src'));
-			reason_image.setAttribute('alt', $('figure.cke_menubutton_on > figcaption').html());
-			reason_image.setAttribute('style', 'float: ' + dialog.getValueOf('tab-existing', 'alignment'));
-
-			// Set element attribute and text by getting the defined field values.
-			//reason_image.setAttribute( 'title', dialog.getValueOf( 'tab-existing', 'title' ) );
-			//reason_image.setText( dialog.getValueOf( 'tab-existing', 'abbr' ) );
-
-			// Now get yet another field value from the Advanced Settings tab.
-			//var id = dialog.getValueOf( 'tab-web', 'id' );
-			//if ( id )
-			//	reason_image.setAttribute( 'id', id );
+			if (dialog.definition.dialog._.currentTabId == 'tab-existing') {
+				reason_image.setAttribute('src', $('figure.cke_menubutton_on > img').attr('src'));
+				reason_image.setAttribute('alt', $('figure.cke_menubutton_on > figcaption').html());
+				reason_image.setAttribute('style', 'float: ' + dialog.getValueOf('tab-existing', 'alignment'));
+			}
+			else if (dialog.getValueOf('tab-web', 'location') != '') {
+				reason_image.setAttribute('src', dialog.getValueOf('tab-web', 'location'));
+				reason_image.setAttribute('alt', dialog.getValueOf('tab-web', 'description'));
+				reason_image.setAttribute('style', 'float: ' + dialog.getValueOf('tab-web', 'alignment'));				
+			}
 
 			// Insert the element into the editor at the caret position.
 			editor.insertElement(reason_image);
