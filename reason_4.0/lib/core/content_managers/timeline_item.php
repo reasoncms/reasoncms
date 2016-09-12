6f865830817018c6fa0f75384308e14b3c2aba5c
Matt Ryan <mryan@carleton.edu>
2016-09-12 15:18:08 -0500

    Added relationship element to make it easier to place timeline items on timelines

--- reason_4.0/lib/core/content_managers/timeline_item.php
+++ reason_4.0/lib/core/content_managers/timeline_item.php
@@ -15,6 +15,20 @@ $GLOBALS[ '_content_manager_class_names' ][ basename( __FILE__) ] = 'TimelineIte
  */
 class TimelineItemManager extends ContentManager
 {
+	function alter_data()
+	{
+		$this->add_relationship_element('timelines', id_of('timeline_type'), 
+				relationship_id_of('timeline_to_timeline_item'),'left','checkbox',true);
+		
+		if($this->_is_first_time() && !$this->has_errors() && $this->is_new_entity() && !$this->get_value('name') )
+		{
+			$timelines = $this->get_element_property('timelines', 'options');
+			$selected_timelines = $this->get_value('timelines');
+			if(empty($selected_timelines) && count($timelines) == 1)
+			{
+				reset($timelines);
+				$this->set_value('timelines',array(key($timelines)));
+			}
+		}
+	}
 }
-
-?>
