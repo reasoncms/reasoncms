2031414ba2560521f13aebc08b3e315e9fedd3e6
Matt Lauer <mlauer@carleton.edu>
2016-12-27 11:04:11 -0600

    restore css needed for mobile

--- reason_4.0/lib/core/scripts/media/media_iframe.php
+++ reason_4.0/lib/core/scripts/media/media_iframe.php
@@ -256,6 +256,18 @@ audio { width: 100%;
                 right: 0; 
                 top: 0; 
                 bottom: 0; } 
+video, 
+img { width: 100%; 
+      height: auto; 
+      max-height: 100%; }
+video, 
+canvas { top: 0; 
+         left: 0; 
+         width: 100%; 
+         max-width: 100%; 
+         height: auto; }
+video { height: 100%; 
+        position: absolute; }
 body { margin: 0; 
        height: 100%; 
        width: 100%; }
