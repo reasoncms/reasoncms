926fc4b186a473a677a33f7c03f55db3ad9b7ab0
Matt Lauer <thefork@gmail.com>
2016-12-27 14:39:25 -0600

    update phpmailer via composer, fixes CVE-2016-10033
    Package operations: 0 installs, 1 update, 0 removals
      - Updating phpmailer/phpmailer (v5.2.15 => v5.2.19) Loading from cache

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit4e92c9263ecee3b5b10564bcd73926d6
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit4e92c9263ecee3b5b10564bcd73926d6', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
