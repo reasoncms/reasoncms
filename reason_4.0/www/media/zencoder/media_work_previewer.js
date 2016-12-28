ee0a9715fca1bbcb45f2a8df92af45d3954734c7
Matt Ryan <mryan@carleton.edu>
2016-12-28 15:53:38 -0600

    Handled zencoder media file links more nicely

--- reason_4.0/www/media/zencoder/media_work_previewer.js
+++ reason_4.0/www/media/zencoder/media_work_previewer.js
@@ -5,24 +5,29 @@
  * @author Marcus Huderle
  * @requires jQuery
  */
-
+ 
 $(document).ready(function() 
 {
-	var embed_row = $("<div class=\"embed_toggler\"></div>");
-	var show_embed = $("<a href=\"\">Show Embedding Code</a>");
+	var small_field = $("#small_embedding_code_preview_field");
+	var medium_field = $("#medium_embedding_code_preview_field");
+	var large_field = $("#large_embedding_code_preview_field");
+	
+	var embed_row = $('<div class="embed_toggler"></div>');
+	var show_embed = $('<a href="#">Embedding Code</a>');
 	
 	embed_row.append(show_embed);
-	$("#small_embedding_code_preview_field").before(embed_row);
+	
+	small_field.before(embed_row);
 		
 	hide_elements();
 	
 	function hide_elements()
 	{
-		if ($("#medium_embedding_code_preview_field").html() != null)
+		if (medium_field.html() != null)
 		{
-			$("#small_embedding_code_preview_field").hide();
-			$("#medium_embedding_code_preview_field").hide();
-			$("#large_embedding_code_preview_field").hide();
+			small_field.hide();
+			medium_field.hide();
+			large_field.hide();
 		}
 		else
 		{
@@ -33,9 +38,26 @@ $(document).ready(function()
 	show_embed.click(function()
 	{
 		embed_row.hide();
-		$("#small_embedding_code_preview_field").show();
-		$("#medium_embedding_code_preview_field").show();
-		$("#large_embedding_code_preview_field").show();
+		small_field.show();
+		medium_field.show();
+		large_field.show();
+		return false;
+	});
+	
+	// links are defined as anything between the large field and the id
+	var links = large_field.nextUntil('#id_preview_field');
+	
+	var links_row = $('<div class="links_toggler"></div>');
+	var show_links = $('<a href="#">Raw Media Files</a>"');
+	links_row.append(show_links);
+	links.first().before(links_row);
+	
+	links.hide();
+	
+	show_links.click(function()
+	{
+		links_row.hide();
+		links.show();
 		return false;
 	});
 	
