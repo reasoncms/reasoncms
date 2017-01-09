3dc3d79c2f0d10714f9ae66436c24468aaa02369
Tom Brice <tbrice@carleton.edu>
2017-01-09 15:22:46 -0600

    adds "allowedContent" sp the inserted `omg` tag does not get filtered
    See more about the Advanced Content Filter in CKEditor
    (http://docs.ckeditor.com/#!/guide/plugin_sdk_integration_with_acf)

--- www/ckeditor/plugins/reason_image/plugin.js
+++ www/ckeditor/plugins/reason_image/plugin.js
@@ -2,7 +2,9 @@ CKEDITOR.plugins.add( 'reason_image', {
     icons: 'reason_image',
     init: function( editor ) {
     	// Define editor command that opens the reason image dialog window
-    	editor.addCommand('reason_image', new CKEDITOR.dialogCommand('reasonImageDialog'));
+    	editor.addCommand('reason_image', new CKEDITOR.dialogCommand('reasonImageDialog',{
+            allowedContent: 'img[alt,src,style]'
+        }));
         editor.ui.addButton( 'reason_image', {
             label: 'Insert Image',
             command: 'reason_image',
@@ -10,5 +12,5 @@ CKEDITOR.plugins.add( 'reason_image', {
         });
         // Register dialog file -- this.path is the plugin folder path
         CKEDITOR.dialog.add('reasonImageDialog', this.path + 'dialogs/reason_image.js');
-    }
+    },
 });
