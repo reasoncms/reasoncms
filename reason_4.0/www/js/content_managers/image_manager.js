f44237a4eb56a217dfb1d8ca63a43aeb1873292a
Matt Ryan <mryan@carleton.edu>
2017-01-12 11:47:43 -0600

    Updated the image content manager and the image import to better support tableless forms

--- reason_4.0/www/js/content_managers/image_manager.js
+++ reason_4.0/www/js/content_managers/image_manager.js
@@ -9,32 +9,32 @@
  */
 $(document).ready(function()
 {
-	
+	console.log('up');
 	/* 
 	 *   Hides thumbnail row, sets thumbnail to be default, displays option to upload full sized image and ignore minimum size check for the image.
 	 *   Should be triggered by the uploading of a new image 
 	*/
 	function display_new_image_upload_options()
 	{
-	    $("#defaultthumbnailRow").removeClass('nonVisibleRow');
+	    $("#defaultthumbnailItem").removeClass('nonVisibleRow');
 		$("#checkbox_default_thumbnail").attr('checked', 'checked');
-		$("#donotresizeRow").removeClass('nonVisibleRow');
-		$("#ignoreminimgsizecheckRow").removeClass('nonVisibleRow');
-		$('#thumbnailRow').addClass('nonVisibleRow');
+		$("#donotresizeItem").removeClass('nonVisibleRow');
+		$("#ignoreminimgsizecheckItem").removeClass('nonVisibleRow');
+		$('#thumbnailItem').addClass('nonVisibleRow');
 	}
 	/* 
 	 *   Default is to hide these two rows 
 	 */
-	$("#donotresizeRow").addClass('nonVisibleRow');
-	$("#ignoreminimgsizecheckRow").addClass('nonVisibleRow');	
+	$("#donotresizeItem").addClass('nonVisibleRow');
+	$("#ignoreminimgsizecheckItem").addClass('nonVisibleRow');	
 	
 	// Case where new image is being uploaded
-	if($("#imageRow img.representation").attr('src') == '')
+	if($("#imageItem img.representation").attr('src') == '')
 	{
-		$("#thumbnailRow").addClass('nonVisibleRow');
-		$("#donotresizeRow").addClass('nonVisibleRow');
-		$("#ignoreminimgsizecheckRow").addClass('nonVisibleRow');
-		$('#defaultthumbnailRow').addClass('nonVisibleRow');
+		$("#thumbnailItem").addClass('nonVisibleRow');
+		$("#donotresizeItem").addClass('nonVisibleRow');
+		$("#ignoreminimgsizecheckItem").addClass('nonVisibleRow');
+		$('#defaultthumbnailItem').addClass('nonVisibleRow');
 		// should be checked to start with
 		$("#checkbox_default_thumbnail").attr('checked', 'checked');
 	}
@@ -61,12 +61,12 @@ $(document).ready(function()
 	 *	hide the do not resize etc.)
 	*/
 	
-	$('#imageRow .file_upload a.reset').click(function(){
+	$('#imageItem .file_upload a.reset').click(function(){
 		$("#checkbox_default_thumbnail").attr('checked', false);
-		$("#defaultthumbnailRow").removeClass('nonVisibleRow');
-		$('#thumbnailRow').removeClass('nonVisibleRow');
-		$("#donotresizeRow").addClass('nonVisibleRow');
-		$("#ignoreminimgsizecheckRow").addClass('nonVisibleRow');
+		$("#defaultthumbnailItem").removeClass('nonVisibleRow');
+		$('#thumbnailItem').removeClass('nonVisibleRow');
+		$("#donotresizeItem").addClass('nonVisibleRow');
+		$("#ignoreminimgsizecheckItem").addClass('nonVisibleRow');
 	});
 	
 	/*
@@ -74,25 +74,28 @@ $(document).ready(function()
 	 *	when it is not checked, show the actual thumbnail and option to give a new one
 	*/
 	$('#checkbox_default_thumbnail').bind('change', function(){
-		var checked = $('#checkbox_default_thumbnail').attr('checked');
-		if(checked || checked == 'checked')
-			$('#thumbnailRow').addClass('nonVisibleRow');
-		else
-			$('#thumbnailRow').removeClass('nonVisibleRow');
+		var checked = this.checked;
+		console.log('checked val: ' + checked);
+		if(checked){
+			$('#thumbnailItem').addClass('nonVisibleRow');
+		}
+		else{
+			$('#thumbnailItem').removeClass('nonVisibleRow');
+		}
 	});
 	
-	$("#imageRow .file_upload").bind('uploadSuccess', function() {
+	$("#imageItem .file_upload").bind('uploadSuccess', function() {
         // Automatically say "yes" to regenerating the thumbnail when we pick
         // a new file.
         $('input[name=default_thumbnail]').attr('checked', 'checked');
         
     });
     
-    $("#assetRow .file_upload").bind('uploadSuccess', function() {
+    $("#assetItem .file_upload").bind('uploadSuccess', function() {
         // Hide the filename changing row when we pick a new file; it won't
         // have any effect if changed when received along with a new file.
-        $('#filenameRow').fadeOut('normal', function() {
-            $("#assetRow a.upload").repositionUploadButton();
+        $('#filenameItem').fadeOut('normal', function() {
+            $("#assetItem a.upload").repositionUploadButton();
         });
     });
     
