fd5184ef93990382d30d1b791a45ca360a55788f
David Huyck <dhuyck@carleton.edu>
2019-05-03 09:54:03 -0500

    Edit to better-accommodate WP Migration Assistant tools

--- reason_4.0/lib/core/classes/admin/modules/copy_borrowing.php
+++ reason_4.0/lib/core/classes/admin/modules/copy_borrowing.php
@@ -3,13 +3,13 @@
  * @package reason
  * @subpackage admin
  */
- 
+
  /**
   * Include the default module and other needed utilities
   */
 	reason_include_once('classes/admin/modules/default.php');
 	include_once( DISCO_INC . 'disco.php');
-	
+
 	/**
 	 * An administrative module that allows a use to select a site to borrow a given item into
 	 */
@@ -74,41 +74,46 @@
 				echo 'This tool can be run only in the context of the master admin';
 				return;
 			}
-		
+
 			$site_options = $this->entities_to_options($this->get_all_sites());
 			$type_options = $this->entities_to_options($this->get_all_types());
-			
+
+
 			$d = new Disco();
 			$d->set_box_class( 'StackedBox' );
-			$d->add_element('site_to_copy_from', 'select', array('options' => $site_options));
-			
-			$d->add_element('site_to_copy_to', 'select', array('options' => $site_options));
-			
-			$d->add_element('type_to_copy', 'select', array('options' => $type_options));
+
+			$copy_from    = ( $_REQUEST['site_to_copy_from'] ) ? intval( $_REQUEST['site_to_copy_from'] ) : null;
+			$copy_to      = ( $_REQUEST['site_to_copy_to'] ) ? intval( $_REQUEST['site_to_copy_to'] ) : null;
+			$type_to_copy = ( $_REQUEST['type_to_copy'] ) ? intval( $_REQUEST['type_to_copy'] ) : null;
+
+			$d->add_element( 'site_to_copy_from', 'select', array( 'options' => $site_options, 'default' => $copy_from ) );
+			$d->add_element( 'site_to_copy_to', 'select', array( 'options' => $site_options, 'default' => $copy_to ) );
+			$d->add_element( 'type_to_copy', 'select', array( 'options' => $type_options, 'default' => $type_to_copy ) );
+
 			$d->add_required('site_to_copy_from');
 			$d->add_required('site_to_copy_to');
 			$d->add_required('type_to_copy');
-			
+
 			$d->set_actions(array('copy'=>'Copy Borrowing Relationships'));
 			$d->add_callback(array($this,'process_borrow_form'), 'process');
 			$d->run();
-			
+
 		}
-		
+
 		function process_borrow_form($d)
 		{
 			$allowable_relationship_id = get_borrows_relationship_id($d->get_value('type_to_copy'));
-			
+
 			$es = new entity_selector($d->get_value('site_to_copy_from'));
 			$es->add_type($d->get_value('type_to_copy'));
 			$es->set_sharing( 'borrows' );
 			$es->limit_tables();
 			$es->limit_fields();
 			$entity_ids = $es->get_ids();
-			
+
 			$successes = array();
 			$failures = array();
-			
+
 			foreach($entity_ids as $entity_id)
 			{
 				if( create_relationship( $d->get_value('site_to_copy_to'), $entity_id, $allowable_relationship_id) )
@@ -120,7 +125,7 @@
 					$failures = $entity_id;
 				}
 			}
-			
+
 			echo '<h3>Successfully copied over ' . count($successes) .' borrow relationships</h3>';
 			echo '<h3>Failed to copy over ' . count($failures) . ' borrow relationships</h3>';
 		}
