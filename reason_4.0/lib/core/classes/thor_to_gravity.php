7bc9a8c0f38adf4990eafb7dd22fe5cc1b5e9230
Matt Ryan <mryan@carleton.edu>
2019-06-14 13:54:50 -0500

    Added better messaging, support for some prefills, pretty printing, more field types

--- reason_4.0/lib/core/classes/thor_to_gravity.php
+++ reason_4.0/lib/core/classes/thor_to_gravity.php
@@ -8,11 +8,20 @@ require_once( INCLUDE_PATH . 'xml/xmlparser.php' );
 
 class reasonFormToGravityJson
 {
-	protected $messages = array();
+	protected $messages = array(
+		'Not yet implemented: notification emails',
+		'Not yet implemented: confirmation message',
+		'Not yet implemented: access restrictions',
+		'Not yet implemented: scheduling',
+		'Not yet implemented: entry limiting',
+		'Not yet implemented: prepopulation from ldap',
+		'Not yet implemented: payment forms or other custom thor forms',
+	);
 	function get_json($form)
 	{
 		$json_data = $this->get_initial_form_data();
 		$json_data['title'] = $form->get_value('name');
+		
 		if ($form->get_value('thor_content'))
 		{
 			$xml_object = new XMLParser($form->get_value('thor_content'));
@@ -26,7 +35,11 @@ class reasonFormToGravityJson
 		return json_encode(array(
 			0 => $json_data,
 			'version' => '2.4.5.10',
-		));
+		), JSON_PRETTY_PRINT);
+	}
+	function add_message($message)
+	{
+		$this->messages[] = $message;
 	}
 	function get_messages()
 	{
@@ -40,22 +53,6 @@ class reasonFormToGravityJson
 	{
 		return (!empty($xml_object->document->tagAttrs['submit'])) ? $xml_object->document->tagAttrs['submit'] : 'Submit';
 	}
-	protected function get_field_type_map()
-	{
-		return array(
-			'input' => 'text',
-			'date' => '',
-			'time' => '',
-			'textarea' => '',
-			'radiogroup' => '',
-			'checkboxgroup' => '',
-			'optiongroup' => 'select',
-			'hidden' => '',
-			'comment' => '',
-			'upload' => '',
-			'event_tickets' => '',
-		);
-	}
 	protected function get_field_data_from_parsed_xml($xml_object, $form)
 	{
 		$json_data = array();
@@ -65,13 +62,12 @@ class reasonFormToGravityJson
 			$method = 'get_json_data_for_' . $element->tagName;
 			if(method_exists($this, $method))
 			{
-				$json_data[] = $this->$method($element, $form);
+				$json_data[] = $this->$method($element, $form, ($index + 1) );
 				$index++;
 			}
 			else
 			{
-				// @todo include info about the not-included field
-				$this->messages[] = 'Field not included in data: ' . $this->get_label_for_element($element) . ' (' . $element->tagName . ')';
+				$this->add_message( 'Not able to include "' . $this->get_label_for_element($element) . '". Code has not yet been written to support the export of ' . $element->tagName . ' fields.' );
 			}
 		}
 		return $json_data;
@@ -178,9 +174,10 @@ class reasonFormToGravityJson
 	{
 		return (!empty($element->tagAttrs['label'])) ? $element->tagAttrs['label'] : '';
 	}
-	protected function get_json_data_for_input($element, $form)
+	protected function get_json_data_for_input($element, $form, $gravity_id)
 	{
 		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
 		$data['type'] = 'text'; 
 		$data['label'] = $this->get_label_for_element($element);
 		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
@@ -193,6 +190,232 @@ class reasonFormToGravityJson
 			if($element->tagAttrs['size'] > 40) $data['size'] = 'large'; // @todo: check on potential values in Gravity Forms
 			elseif($element->tagAttrs['size'] < 20) $data['size'] = 'small'; // @todo: check on potential values in Gravity Forms
 		}
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function get_json_data_for_textarea($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'textarea'; 
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		$data['maxLength'] = (!empty($element->tagAttrs['maxlength'])) ? $element->tagAttrs['maxlength'] : '';
+		$data['defaultValue'] = (!empty($element->tagAttrs['value'])) ? $element->tagAttrs['value'] : '';
+
+		$data['size'] = 'medium';	
+		if(!empty($element->tagAttrs['rows']) || !empty($element->tagAttrs['cols']))
+		{
+			$this->add_message('Did not set rows and columns on "' . $data['label'] . '". This functionality does not exist in Gravity Forms.');
+		}
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function get_json_data_for_radiogroup($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'radio';
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		$data['defaultValue'] = (!empty($element->tagAttrs['value'])) ? $element->tagAttrs['value'] : '';
+		$choices = array();
+		foreach($element->tagChildren as $child)
+		{
+			$value = (!empty($child->tagAttrs['value'])) ? $child->tagAttrs['value'] : '';
+			$choices[] = array(
+				'text' => $value,
+				'value' => $value,
+				'isSelected' => (!empty($child->tagAttrs['selected'])) ? true : false,
+				'price' => '',
+			);
+		}
+		$data['choices'] = $choices;
+		$data = $this->modify_values_for_prefill($data, $element, $form, $gravity_id);
+		return $data;
+	}
+	protected function get_json_data_for_optiongroup($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'select';
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		$data['defaultValue'] = '';
+		$choices = array();
+		foreach($element->tagChildren as $child)
+		{
+			$value = (!empty($child->tagAttrs['value'])) ? $child->tagAttrs['value'] : '';
+			$choices[] = array(
+				'text' => $value,
+				'value' => $value,
+				'isSelected' => (!empty($child->tagAttrs['selected'])) ? true : false,
+				'price' => '',
+			);
+		}
+		$data['choices'] = $choices;
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function get_json_data_for_checkboxgroup($element, $form, $gravity_id) {
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'checkbox';
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		$data['defaultValue'] = '';
+		$choices = array();
+		$inputs = array();
+		$index = 1;
+		foreach($element->tagChildren as $child)
+		{
+			$value = (!empty($child->tagAttrs['value'])) ? $child->tagAttrs['value'] : '';
+			$choices[] = array(
+				'text' => $value,
+				'value' => $value,
+				'isSelected' => (!empty($child->tagAttrs['selected'])) ? true : false,
+				'price' => '',
+			);
+			$inputs[] = array(
+				'id' => $gravity_id . '.' . $index,
+				'label' => $value,
+				'name' => '',
+			);
+			$index++;
+		}
+		$data['choices'] = $choices;
+		$data['inputs'] = $inputs;
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function standardize_upload_restrictions($stuff)
+	{
+		$rv = Array();
+		$explodedStuff = explode(",",$stuff);
+		foreach ($explodedStuff as $stuffChunk) {
+			$rv[] = strtolower(trim($stuffChunk));
+		}
+		return $rv;
+	}
+	protected function get_json_data_for_upload($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'fileupload'; 
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		if (!empty($element->tagAttrs['restrict_extensions'])) {
+			$data['allowedExtensions'] = $this->standardize_upload_restrictions($element->tagAttrs['restrict_extensions']);
+		}
+
+		if (!empty($element->tagAttrs['restrict_types'])) {
+			$this->add_message('Unable to restrict mime types on "' . $data['label'] . '". This restriction is not supported in Gravity forms.');
+		}
+		if (!empty($element->tagAttrs['restrict_maxsize'])) {
+			// @todo figure out the format of this. "1MB" seems to resolve to "1" but need to figure out more
+			$this->add_message('Unable to restrict maximum size on "' . $data['label'] . '". Export not yet supported.');
+		}
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function get_json_data_for_hidden($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'hidden'; 
+		$data['label'] = $this->get_label_for_element($element);
+		$data['isRequired'] = (!empty($element->tagAttrs['required'])) ? true : false;
+		$data['defaultValue'] = (!empty($element->tagAttrs['value'])) ? $element->tagAttrs['value'] : '';
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function get_json_data_for_comment($element, $form, $gravity_id)
+	{
+		$data = $this->get_initial_field_data();
+		$data['id'] = $gravity_id;
+		$data['type'] = 'html'; 
+		$data['label'] = $this->get_label_for_element($element);
+		$data['content'] = $element->tagData;
+		$data = $this->modify_values_for_prefill($data, $element, $form);
+		return $data;
+	}
+	protected function does_form_prefill($form)
+	{
+		switch($form->get_value('magic_string_autofill'))
+		{
+			case 'editable':
+			case 'not_editable':
+				return $form->get_value('magic_string_autofill');
+			default:
+				return false;
+		}
+	}
+	protected function modify_values_for_prefill($data, $element, $form)
+	{
+		if($this->does_form_prefill($form))
+		{
+			$label = $this->get_label_for_element($element);
+			$match_label = strtolower($label);
+			$match_label = trim($match_label);
+			$match_label = trim($match_label, ':');
+			$match_label = trim($match_label);
+			$match_label = str_replace(' ', '_', $match_label);
+			$function = 'modify_json_data_for_prefill_' . $match_label;
+			if(method_exists($this, $function))
+			{
+				$data = $this->$function($data, $element, $form);
+			}
+		}
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_name($data, $element, $form)
+	{
+		$data['allowsPrepopulate'] = true;
+		$data['inputName'] = 'fullName';
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_full_name($data, $element, $form)
+	{
+		$data['allowsPrepopulate'] = true;
+		$data['inputName'] = 'fullName';
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_first_name($data, $element, $form)
+	{
+		$data['allowsPrepopulate'] = true;
+		$data['inputName'] = 'firstName';
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_last_name($data, $element, $form)
+	{
+		$data['allowsPrepopulate'] = true;
+		$data['inputName'] = 'lastName';
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_department($data, $element, $form)
+	{
+		$data['allowsPrepopulate'] = true;
+		$data['inputName'] = 'department';
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_email($data, $element, $form)
+	{
+		$this->add_message('Unable to set prefill for "' . $this->get_label_for_element($element) . '". Code not yet written.');
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_home_phone($data, $element, $form)
+	{
+		$this->add_message('Unable to set prefill for "' . $this->get_label_for_element($element) . '". Code not yet written.');
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_work_phone($data, $element, $form)
+	{
+		$this->add_message('Unable to set prefill for "' . $this->get_label_for_element($element) . '". Code not yet written.');
+		return $data;
+	}
+	protected function modify_json_data_for_prefill_your_title($data, $element, $form)
+	{
+		$this->add_message('Unable to set prefill for "' . $this->get_label_for_element($element) . '". This prefill is not currently supported in Gravity Forms.');
 		return $data;
 	}
 }
